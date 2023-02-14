5258fcfe2330f1eabb0fafac9e4b60c209b2da2a
Peggy <peggy.zhang@quartech.com>
2023-02-14 14:07:08 -0800

    Peggy/dyna and automapper (#10)
    * step 1
    
    * add autogenerated cs file

--- backend/SPD.DynamicsProxy/DynamicsContext.cs
+++ backend/SPD.DynamicsProxy/DynamicsContext.cs
@@ -1,31 +1,9 @@
-﻿using Microsoft.OData.Client;
-using SPD.DynamicsProxy.Entities;
-
-namespace SPD.DynamicsProxy
+﻿namespace SPD.DynamicsProxy
 {
-    public class DynamicsContext : DataServiceContext
+    public class DynamicsContext : Microsoft.Dynamics.CRM.System
     {
-        public DataServiceQuery<OrgRegistration> OrgRegistrations { get; set; }
-        public DataServiceQuery<Contact> Contacts { get; set; }
         public DynamicsContext(Uri serviceRoot) : base(serviceRoot)
         {
-            OrgRegistrations = base.CreateQuery<OrgRegistration>("spd_orgregistrations");
-            Contacts = base.CreateQuery<Contact>("contacts");
         }
-
-        public async Task AddAsync<T> (T entity, CancellationToken cancellationToken) where T : DynamicsEntity
-        {
-            string entitySetName = "spd_orgregistrations";
-            OriginalNameAttribute originalNameAttr =(OriginalNameAttribute)typeof(T)
-                .GetCustomAttributes(typeof(OriginalNameAttribute), true)
-                .FirstOrDefault();
-            if (originalNameAttr != null) 
-            {
-                entitySetName = $"{originalNameAttr.OriginalName}s";
-            }
-            base.AddObject(entitySetName, entity);
-            await base.SaveChangesAsync(cancellationToken);
-
-        }        
-    }   
+    }
 }
---
+++ backend/SPD.Services/Mappings/OrgRegistrationMapping.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Dynamics.CRM;
+using SPD.Common.ViewModels.Organization;
+
+namespace SPD.Services.Mappings
+
+{
+    public class OrgRegistrationMapping : Profile
+    {
+        public OrgRegistrationMapping()
+        {
+            CreateMap<OrgRegistrationCreateRequest, Spd_orgregistration>()
+                .ForMember(d => d.Spd_city, opt => opt.MapFrom(s => s.MailingCity))
+                .ForMember(d => d.Spd_country, opt => opt.MapFrom(s => s.MailingCountry))
+                .ForMember(d => d.Spd_postalcode, opt => opt.MapFrom(s => s.MailingPostalCode))
+                .ForMember(d => d.Spd_province, opt => opt.MapFrom(s => s.MailingProvince))
+                .ForMember(d => d.Spd_street1, opt => opt.MapFrom(s => s.MailingAddressLine1))
+                .ForMember(d => d.Spd_street2, opt => opt.MapFrom(s => s.MailingAddressLine2))
+                .ForMember(d => d.Spd_organizationname, opt => opt.MapFrom(s => s.OrganizationName))
+                .ForMember(d => d.Spd_orgregistrationid, opt => opt.MapFrom(s => Guid.NewGuid()))
+            ;
+        }
+    }
+}
