98a6b86ff81dd454bc9a0d453af09bea095a15b2
Carol <carol.carpenter@quartech.com>
2023-02-14 16:18:07 -0800

    Add Dashbooard Module (may or may not be needed)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
 	selector: 'app-root',
 	template: `
 		<body class="d-flex flex-column h-100">
 			<ngx-spinner name="loaderSpinner" type="square-jelly-box" [fullScreen]="true"></ngx-spinner>
-			<app-header title="SPD"></app-header>
+			<app-header [title]="title"></app-header>
 
 			<div class="container">
 				<router-outlet></router-outlet>
@@ -20,4 +22,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'SPD';
+
+	constructor(private router: Router) {}
+
+	ngOnInit() {
+		this.router.events
+			.pipe(
+				filter((event) => event instanceof NavigationEnd),
+				map(() => {
+					let route: ActivatedRoute = this.router.routerState.root;
+					let routeTitle = '';
+					while (route.firstChild) {
+						route = route.firstChild;
+					}
+					if (route.snapshot.data['title']) {
+						routeTitle = route.snapshot.data['title'];
+					}
+					return routeTitle;
+				})
+			)
+			.subscribe((title: string) => {
+				this.title = title ? title : 'SPD';
+			});
+	}
 }
