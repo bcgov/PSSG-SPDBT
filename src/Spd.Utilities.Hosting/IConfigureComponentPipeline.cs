e35664643f99dac8a9c27190dd5bbd89a56b70f1
peggy <peggy.zhang@quartech.com>
2023-02-18 08:37:01 -0800

    draft2

---
+++ src/Spd.Utilities.Hosting/IConfigureComponentPipeline.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+
+namespace Spd.Utilities.Hosting
+{
+    public class PipelineServices
+    {
+        public IApplicationBuilder Application { get; set; } = null!;
+        public IConfiguration Configuration { get; set; } = null!;
+        public IHostEnvironment Environment { get; set; } = null!;
+    }
+
+    public interface IConfigureComponentPipeline
+    {
+        void ConfigurePipeline(PipelineServices services);
+    }
+}
