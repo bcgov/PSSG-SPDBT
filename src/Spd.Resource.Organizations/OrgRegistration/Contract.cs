8fcf4267582cdf689908a5c0ea1981f26468e032
peggy <peggy.zhang@quartech.com>
2023-02-18 08:37:01 -0800

    draft2

---
+++ src/Spd.Resource.Organizations/OrgRegistration/Contract.cs
@@ -0,0 +1,16 @@
+﻿using Spd.Manager.Membership.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Resource.Organizations.OrgRegistration
+{
+    public interface IOrgRegistrationRepository
+    {
+        Task<bool> CreateOrgRegistrationAsync(OrgRegistrationCreateRequest createRequest, CancellationToken cancellationToken);
+        Task<List<OrgRegistrationResponse>> GetAllOrgRegistrations();
+    }
+
+}
---
+++ src/Spd.Resource.Organizations/OrgRegistration/OrgRegistrationRepository.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.Dynamics.CRM;
+using Spd.Manager.Membership.ViewModels;
+using SPD.DynamicsProxy;
+
+namespace Spd.Resource.Organizations.OrgRegistration
+{
+    public class OrgRegistrationRepository : IOrgRegistrationRepository
+    {
+        public readonly DynamicsContext _dynaContext;
+        private readonly IMapper _mapper;
+        public OrgRegistrationRepository(IDynamicsContextFactory ctx, IMapper mapper)
+        {
+            _dynaContext = ctx.Create();
+            _mapper = mapper;
+        }
+
+        public async Task<bool> CreateOrgRegistrationAsync(OrgRegistrationCreateRequest createRequest, CancellationToken cancellationToken)
+        {
+            //need to investigate how to validate the request.
+            var existed = _dynaContext.Spd_orgregistrations.Where(s => s.Spd_organizationname == createRequest.OrganizationName).ToList();
+            if (existed.Count == 0)
+            {
+                Spd_orgregistration orgregistration = _mapper.Map<Spd_orgregistration>(createRequest);
+                _dynaContext.AddToSpd_orgregistrations(orgregistration);
+                await _dynaContext.SaveChangesAsync(cancellationToken);
+            }
+            else
+            {
+                throw new Exception("the organization has been registered.");
+            }
+            return true;
+        }
+
+        public async Task<List<OrgRegistrationResponse>> GetAllOrgRegistrations()
+        {
+            var orgs = await _dynaContext.Spd_orgregistrations.GetAllPagesAsync();
+            //todo: add mapping here
+            List<OrgRegistrationResponse> result = new List<OrgRegistrationResponse>();
+            return result;
+        }
+    }
+}
