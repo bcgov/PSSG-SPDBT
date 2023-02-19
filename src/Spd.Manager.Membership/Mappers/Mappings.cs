1352a8477197e9a8ddb5041df4d0a1aac4acdf68
peggy <peggy.zhang@quartech.com>
2023-02-19 15:36:02 -0800

    more

---
+++ src/Spd.Manager.Membership/Mappers/Mappings.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Spd.Manager.Membership.ViewModels;
+
+namespace Spd.Resource.Organizations
+{
+    public class Mappings : Profile
+    {
+        public Mappings()
+        {
+            CreateMap<OrgRegistrationCreateRequest, CreateRegistrationCmd>();
+        }
+    }
+}
---
+++ src/Spd.Manager.Membership/OrgRegistrationManager.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Spd.Manager.Membership.ViewModels;
+using Spd.Resource.Organizations;
+
+namespace Spd.Manager.Membership
+{
+    public class OrgRegistrationManager
+        : IRequestHandler<CreateOrgRegistrationCommand, Unit>,
+        IOrgRegistrationManager
+    {
+        private readonly IOrganizationRepository _organizationRepository;
+        private readonly IMapper _mapper;
+        public OrgRegistrationManager(IOrganizationRepository organiztionRepository, IMapper mapper)
+        {
+            _organizationRepository = organiztionRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(CreateOrgRegistrationCommand request, CancellationToken cancellationToken)
+        {
+            var createOrgRegistration = _mapper.Map<CreateRegistrationCmd>(request.CreateOrgRegistrationRequest);
+            await _organizationRepository.RegisterAsync(createOrgRegistration, cancellationToken);
+
+            return default;
+        }
+    }
+}
