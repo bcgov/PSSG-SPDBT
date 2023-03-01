fa53b5a15b47f643eaa97e3caa2f0f64e972a0a9
Carol <carol.carpenter@quartech.com>
2023-02-28 17:08:58 -0800

    Add filtering overlay component

--- src/Spd.Presentation.Screening/ClientApp/src/app/modules/dashboard/dashboard.module.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/modules/dashboard/dashboard.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { DashboardHeaderComponent } from './components/dashboard-header.component';
 import { DashboardHomeComponent } from './components/dashboard-home.component';
+import { EditUserModalComponent } from './components/edit-user-modal.component';
 import { GenericUploadsComponent } from './components/generic-uploads.component';
+import { PaymentFilterComponent } from './components/payment-filter.component';
 import { PaymentsComponent } from './components/payments.component';
 import { UsersComponent } from './components/users.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
-import { DashboardHeaderComponent } from './components/dashboard-header.component';
-import { EditUserModalComponent } from './components/edit-user-modal.component';
 
 @NgModule({
 	declarations: [
@@ -16,8 +17,9 @@ import { EditUserModalComponent } from './components/edit-user-modal.component';
 		DashboardHomeComponent,
 		PaymentsComponent,
 		UsersComponent,
-  DashboardHeaderComponent,
-  EditUserModalComponent,
+		DashboardHeaderComponent,
+		EditUserModalComponent,
+		PaymentFilterComponent,
 	],
 	imports: [SharedModule, DashboardRoutingModule],
 })
