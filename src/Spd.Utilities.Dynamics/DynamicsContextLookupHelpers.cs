b3e18bbeafabb76c515fe84c54ecd5f05ed9bb87
peggy <peggy.zhang@quartech.com>
2023-03-06 16:14:07 -0800

    use hard coded guid

--- src/Spd.Utilities.Dynamics/DynamicsContextLookupHelpers.cs
+++ src/Spd.Utilities.Dynamics/DynamicsContextLookupHelpers.cs
@@ -4,11 +4,34 @@ namespace Spd.Utilities.Dynamics
 {
     public static class DynamicsContextLookupHelpers
     {
-        public static spd_organizationtype? LookupOrganizationType(this DynamicsContext context, int orgCategory, string name)
+        private static Dictionary<string, Guid> OrganizationTypeGuidDictionary = new Dictionary<string, Guid>()
         {
-            if (string.IsNullOrWhiteSpace(name)) return null;
+            {"Volunteer-NonProfit", Guid.Parse("c2cc6851-56b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-Childcare", Guid.Parse("1b4dbc58-56b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-Healthcare", Guid.Parse("b4dc3164-56b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-Education", Guid.Parse("5c7ff643-59b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-ProvFunded", Guid.Parse("93f5c34f-59b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-CrownCorp", Guid.Parse("1ec4a858-59b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-ProvGovt", Guid.Parse("ce908e5f-59b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-Registrant", Guid.Parse("e93ee56f-59b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-Municipality", Guid.Parse("7a7a0b99-59b8-ed11-b83e-00505683fbf4")},
+            {"Volunteer-PostSec", Guid.Parse("30b68faf-59b8-ed11-b83e-00505683fbf4")},
+            {"Employee-Childcare", Guid.Parse("d2ebf78b-5ab8-ed11-b83e-00505683fbf4")},
+            {"Employee-Healthcare", Guid.Parse("4ba37d93-5ab8-ed11-b83e-00505683fbf4")},
+            {"Employee-Education", Guid.Parse("e6ee8aa9-5ab8-ed11-b83e-00505683fbf4")},
+            {"Employee-Funding", Guid.Parse("054ea3b0-5ab8-ed11-b83e-00505683fbf4")},
+            {"Employee-CrownCorp", Guid.Parse("b48933b8-5ab8-ed11-b83e-00505683fbf4")},
+            {"Employee-ProvGovt", Guid.Parse("b30335c1-5ab8-ed11-b83e-00505683fbf4")},
+            {"Employee-Registrant", Guid.Parse("489d1f0a-5bb8-ed11-b83e-00505683fbf4")},
+            {"Employee-GovnBody", Guid.Parse("fcaa5d11-5bb8-ed11-b83e-00505683fbf4")},
+            {"Employee-Appointed", Guid.Parse("61b9301b-5bb8-ed11-b83e-00505683fbf4")},
+        };
+        public static spd_organizationtype? LookupOrganizationType(this DynamicsContext context, string key)
+        {
+            var keyExisted = OrganizationTypeGuidDictionary.TryGetValue(key, out Guid guid);
+            if (!keyExisted) return null;
             return context.spd_organizationtypes
-                .Where(s => s.spd_organizationcategory == orgCategory && s.spd_name == name)
+                .Where(s => s.spd_organizationtypeid == guid)
                 .FirstOrDefault();
         }
     }
