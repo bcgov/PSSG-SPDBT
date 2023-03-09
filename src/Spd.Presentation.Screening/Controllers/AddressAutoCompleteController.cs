72890daa8816328b992546a57162364cbddbbbdb
Peggy <peggy.zhang@quartech.com>
2023-03-09 13:51:00 -0800

    Backend-Canada postal Address auto-complete (#39)
    * temp
    
    * find
    
    * retrieve
    
    * add country
    
    * add logging
    
    * change back to enable nullable
    
    * cleanup
    
    * respond to review
    
    * respond to review

---
+++ src/Spd.Presentation.Screening/Controllers/AddressAutoCompleteController.cs
@@ -0,0 +1,51 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Spd.Manager.Admin;
+using System.ComponentModel.DataAnnotations;
+
+namespace Spd.Presentation.Screening.Controllers
+{
+    [ApiController]
+    public class AddressAutoCompleteController : ControllerBase
+    {
+        private readonly ILogger<AddressAutoCompleteController> _logger;
+        private readonly IMediator _mediator;
+
+        public AddressAutoCompleteController(ILogger<AddressAutoCompleteController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Find addresses matching the search term.
+        /// </summary>
+        /// <param name="searchTerm">required</param>
+        /// <param name="country">optional: The ISO 2 or 3 character code for the country to search in. If not specified, default would be CAN.</param>
+        /// <returns></returns>
+        /// Exp: GET http://localhost:5114/api/metadata/address?search=1
+        /// Exp: GET http://localhost:5114/api/metadata/address?search=1&country=USA
+        [Route("api/metadata/address")]
+        [HttpGet]
+        public async Task<IEnumerable<AddressFindResponse>> Find([FromQuery][Required] string search, string? country)
+        {
+            if (string.IsNullOrWhiteSpace(country))
+                return await _mediator.Send(new FindAddressQuery(search));
+            else
+                return await _mediator.Send(new FindAddressQuery(search, country));
+        }
+
+        /// <summary>
+        /// To retrieve the address details with Id for the item from Find method.
+        /// </summary>
+        /// <param name="id">the id from find items, like CAN|1520704</param>
+        /// <returns></returns>
+        /// Exp: GET http://localhost:5114/api/metadata/address/1520704
+        [Route("api/metadata/address/{id}")]
+        [HttpGet]
+        public async Task<IEnumerable<AddressRetrieveResponse>> Retrieve([FromRoute] string id)
+        {
+            return await _mediator.Send(new RetrieveAddressByIdQuery(id));
+        }
+    }
+}
---
+++ src/Spd.Utilities.Address/AddressAutocompleteClient.cs
@@ -0,0 +1,135 @@
+using Flurl;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Net.Http.Json;
+
+namespace Spd.Utilities.Address;
+
+internal class AddressAutocompleteClient : IAddressAutocompleteClient
+{
+    private readonly string _apiKey;
+    private readonly int _maxSuggestions;
+    private readonly HttpClient _client;
+    private ILogger _logger { get; }
+
+    public AddressAutocompleteClient(
+        HttpClient httpClient,
+        ILogger<AddressAutocompleteClient> logger,
+        IOptions<AddressAutoCompleteClientConfigurations> config)
+    {
+        _apiKey = config.Value.ApiKey;
+        _maxSuggestions = config.Value.MaxSuggestions;
+        _client = httpClient;
+        _logger = logger;
+    }
+
+    public async Task<IEnumerable<AddressQueryResponse>> Query(AddressQuery query, CancellationToken cancellationToken)
+    {
+
+        if (query is AddressSearchQuery searchQuery)
+        {
+            return await Find(searchQuery.SearchTerm, searchQuery.Country, cancellationToken);
+        }
+        
+        if(query is AddressRetrieveQuery retrieveQuery)
+        { 
+            return await Retrieve(retrieveQuery.Id, cancellationToken);
+        }
+
+        return null;
+    }
+
+    ///// Returns addresses matching the search term.
+    ///// https://www.canadapost-postescanada.ca/ac/support/api/addresscomplete-interactive-find/
+    private async Task<IEnumerable<AddressAutocompleteFindResponse>> Find(string searchTerm, string country, CancellationToken cancellationToken)
+    {
+        try
+        {
+            string url = "Find/v2.10/json3ex.ws";
+            var queryValues = new
+            {
+                Key = _apiKey,
+                SearchTerm = searchTerm,
+                MaxSuggestions = _maxSuggestions,
+                Country = country
+            };
+            using var request = new HttpRequestMessage(HttpMethod.Get, url.SetQueryParams(queryValues, NullValueHandling.Remove));
+            using var response = await _client.SendAsync(request, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseMessage = response.Content != null
+                    ? await response.Content.ReadAsStringAsync(cancellationToken)
+                    : "";
+
+                _logger.LogWarning($"Did not receive a successful status code. {response.StatusCode}, {responseMessage}");
+                return Enumerable.Empty<AddressAutocompleteFindResponse>();
+            }
+
+            if (response.Content == null)
+            {
+                _logger.LogInformation("Response content was null");
+                return Enumerable.Empty<AddressAutocompleteFindResponse>();
+            }
+
+            var deserializationResult = await response.Content.ReadFromJsonAsync<AddressAutocompleteApiResponse<AddressAutocompleteFindResponse>>(cancellationToken: cancellationToken);
+            return deserializationResult?.Items ?? Enumerable.Empty<AddressAutocompleteFindResponse>();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            throw;
+        }
+    }
+
+    ///// Returns the full address details based on the Id.
+    ///// https://www.canadapost-postescanada.ca/ac/support/api/addresscomplete-interactive-retrieve/
+    private async Task<IEnumerable<AddressAutocompleteRetrieveResponse>> Retrieve(string id, CancellationToken cancellationToken)
+    {
+        try
+        {
+            string url = "Retrieve/v2.11/json3ex.ws";
+            var queryValues = new
+            {
+                Key = _apiKey,
+                Id = id
+            };
+            using var request = new HttpRequestMessage(HttpMethod.Get, url.SetQueryParams(queryValues, NullValueHandling.Remove));
+
+            using var response = await _client.SendAsync(request, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                var responseMessage = response.Content != null
+                    ? await response.Content.ReadAsStringAsync(cancellationToken)
+                    : "";
+
+                _logger.LogWarning($"Did not receive a successful status code. {response.StatusCode}, {responseMessage}");
+                return Enumerable.Empty<AddressAutocompleteRetrieveResponse>();
+            }
+
+            if (response.Content == null)
+            {
+                _logger.LogInformation("Response content was null");
+                return Enumerable.Empty<AddressAutocompleteRetrieveResponse>();
+            }
+
+            var deserializationResult = await response.Content.ReadFromJsonAsync<AddressAutocompleteApiResponse<AddressAutocompleteRetrieveResponse>>(cancellationToken: cancellationToken);
+            return deserializationResult?.Items ?? Enumerable.Empty<AddressAutocompleteRetrieveResponse>();
+        }
+        catch (Exception exception)
+        {
+            _logger.LogError(exception.Message);
+            return Enumerable.Empty<AddressAutocompleteRetrieveResponse>(); 
+        }
+    }
+
+    /// <summary>
+    /// The response wrapper for the web service that contains result items.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class AddressAutocompleteApiResponse<T>
+    {
+        public IEnumerable<T>? Items { get; set; }
+    }
+}
