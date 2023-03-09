42d414bf3c8de477c9ab0889b5a3de119ae5e424
Peggy <peggy.zhang@quartech.com>
2023-03-09 13:51:00 -0800

    Backend-Canada postal Address auto-complete (#39)
    * temp
    
    * find
    
    * retrieve
    
    * add country
    
    * add logging
    
    * change back to enable nullable
    
    * cleanup
    
    * respond to review
    
    * respond to review

---
+++ src/Spd.Manager.Admin/AdminManager.cs
@@ -0,0 +1,34 @@
+﻿using AutoMapper;
+using MediatR;
+using Spd.Utilities.Address;
+
+namespace Spd.Manager.Admin
+{
+    internal class AdminManager :
+        IRequestHandler<FindAddressQuery, IEnumerable<AddressFindResponse>>,
+        IRequestHandler<RetrieveAddressByIdQuery, IEnumerable<AddressRetrieveResponse>>,
+        IAdminManager
+    {
+        private readonly IAddressAutocompleteClient _addressClient;
+        private readonly IMapper _mapper;
+        public AdminManager(IAddressAutocompleteClient addressClient, IMapper mapper)
+        {
+            _addressClient = addressClient;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<AddressFindResponse>> Handle(FindAddressQuery query, CancellationToken cancellationToken)
+        {
+            IEnumerable<AddressAutocompleteFindResponse> result =
+                (IEnumerable<AddressAutocompleteFindResponse>)await this._addressClient.Query(new AddressSearchQuery { SearchTerm = query.SearchTerm, Country = query.Country }, cancellationToken);
+            return _mapper.Map<IEnumerable<AddressFindResponse>>(result);
+        }
+
+        public async Task<IEnumerable<AddressRetrieveResponse>> Handle(RetrieveAddressByIdQuery query, CancellationToken cancellationToken)
+        {
+            IEnumerable<AddressAutocompleteRetrieveResponse> result =
+                (IEnumerable<AddressAutocompleteRetrieveResponse>)await this._addressClient.Query(new AddressRetrieveQuery { Id = query.Id }, cancellationToken);
+            return _mapper.Map<IEnumerable<AddressRetrieveResponse>>(result);
+        }
+    }
+}
---
+++ src/Spd.Utilities.Address/AddressAutoCompleteClientConfigurations.cs
@@ -0,0 +1,9 @@
+﻿namespace Spd.Utilities.Address
+{
+    internal class AddressAutoCompleteClientConfigurations
+    {
+        public string ApiKey { get; set; } = string.Empty;
+        public string Url { get; set; } = string.Empty;
+        public int MaxSuggestions { get; set; } = 7;
+    }
+}
