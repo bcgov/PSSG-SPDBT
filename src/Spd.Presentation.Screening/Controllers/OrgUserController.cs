6d26e791b4d7512e6abe389e75841b82ec27d2e9
Peggy <peggy.zhang@quartech.com>
2023-03-14 07:54:10 -0700

    add user (#44)

---
+++ src/Spd.Presentation.Screening/Controllers/OrgUserController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Spd.Manager.Membership.OrgUser;
+using System.ComponentModel.DataAnnotations;
+
+namespace Spd.Presentation.Screening.Controllers
+{
+    [ApiController]
+    public class OrgUserController : ControllerBase
+    {
+        private readonly ILogger<OrgRegistrationController> _logger;
+        private readonly IMediator _mediator;
+
+        public OrgUserController(ILogger<OrgRegistrationController> logger, IMediator mediator)
+        {
+            _logger = logger;
+            _mediator = mediator;
+        }
+
+        [Route("api/org-user")]
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody][Required] OrgUserCreateRequest orgUserCreateRequest)
+        {
+            orgUserCreateRequest.OrganizationId = Guid.Parse("4165bdfe-7cb4-ed11-b83e-00505683fbf4");
+            await _mediator.Send(new CreateOrgUserCommand(orgUserCreateRequest));
+            return Ok();
+        }
+    }
+}
