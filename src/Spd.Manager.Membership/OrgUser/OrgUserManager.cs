57e5cd2f69b4ba5e510cd86f6c0a59ed777c2f57
peggy <peggy.zhang@quartech.com>
2023-03-13 18:12:31 -0700

    add user

---
+++ src/Spd.Manager.Membership/OrgUser/OrgUserManager.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using MediatR;
+using Spd.Resource.Organizations;
+
+namespace Spd.Manager.Membership.OrgUser
+{
+    public class OrgUserManager
+        : IRequestHandler<CreateOrgUserCommand, Unit>,
+        IOrgUserManager
+    {
+        private readonly IOrganizationRepository _organizationRepository;
+        private readonly IMapper _mapper;
+        public OrgUserManager(IOrganizationRepository organizationRepository, IMapper mapper)
+        {
+            _organizationRepository = organizationRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<Unit> Handle(CreateOrgUserCommand request, CancellationToken cancellationToken)
+        {
+            var createOrgUser = _mapper.Map<CreateUserCmd>(request.CreateOrgUserRequest);
+            await _organizationRepository.AddUserAsync(createOrgUser, cancellationToken);
+
+            return default;
+        }
+    }
+}
