e1c0147918877689a10ad222594e1e6325ceca34
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2023-03-22 10:03:33 -0700

    Carol/dashboard UI (#58)
    * dashboard UI / duplicate check
    
    * ui updates

--- src/Spd.Presentation.Screening/ClientApp/src/app/api/services/org-registration.service.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/api/services/org-registration.service.ts
@@ -10,6 +10,7 @@ import { Observable } from 'rxjs';
 import { map, filter } from 'rxjs/operators';
 
 import { ActionResult } from '../models/action-result';
+import { CheckDuplicateResponse } from '../models/check-duplicate-response';
 import { OrgRegistrationCreateRequest } from '../models/org-registration-create-request';
 
 @Injectable({
@@ -71,6 +72,57 @@ export class OrgRegistrationService extends BaseService {
 
     return this.apiOrgRegistrationsPost$Response(params).pipe(
       map((r: StrictHttpResponse<ActionResult>) => r.body as ActionResult)
+    );
+  }
+
+  /**
+   * Path part for operation apiOrgRegistrationsDetectDuplicatePost
+   */
+  static readonly ApiOrgRegistrationsDetectDuplicatePostPath = '/api/org-registrations/detect-duplicate';
+
+  /**
+   * This method provides access to the full `HttpResponse`, allowing access to response headers.
+   * To access only the response body, use `apiOrgRegistrationsDetectDuplicatePost()` instead.
+   *
+   * This method sends `application/*+json` and handles request body of type `application/*+json`.
+   */
+  apiOrgRegistrationsDetectDuplicatePost$Response(params: {
+    context?: HttpContext
+    body: OrgRegistrationCreateRequest
+  }
+): Observable<StrictHttpResponse<CheckDuplicateResponse>> {
+
+    const rb = new RequestBuilder(this.rootUrl, OrgRegistrationService.ApiOrgRegistrationsDetectDuplicatePostPath, 'post');
+    if (params) {
+      rb.body(params.body, 'application/*+json');
+    }
+
+    return this.http.request(rb.build({
+      responseType: 'json',
+      accept: 'application/json',
+      context: params?.context
+    })).pipe(
+      filter((r: any) => r instanceof HttpResponse),
+      map((r: HttpResponse<any>) => {
+        return r as StrictHttpResponse<CheckDuplicateResponse>;
+      })
+    );
+  }
+
+  /**
+   * This method provides access to only to the response body.
+   * To access the full response (for headers, for example), `apiOrgRegistrationsDetectDuplicatePost$Response()` instead.
+   *
+   * This method sends `application/*+json` and handles request body of type `application/*+json`.
+   */
+  apiOrgRegistrationsDetectDuplicatePost(params: {
+    context?: HttpContext
+    body: OrgRegistrationCreateRequest
+  }
+): Observable<CheckDuplicateResponse> {
+
+    return this.apiOrgRegistrationsDetectDuplicatePost$Response(params).pipe(
+      map((r: StrictHttpResponse<CheckDuplicateResponse>) => r.body as CheckDuplicateResponse)
     );
   }
 
