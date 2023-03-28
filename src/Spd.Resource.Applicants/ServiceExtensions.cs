f7836a3455e3e2f40393560065b8cd86b8e4e594
Peggy <peggy.zhang@quartech.com>
2023-03-28 14:27:52 -0700

    create screening invitation. (#69)
    * add screening invite
    
    * add bone for invitation
    
    * change screening to application, IList to IEnumerable

---
+++ src/Spd.Resource.Applicants/ServiceExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spd.Utilities.Hosting;
+
+namespace Spd.Resource.Applicants
+{
+    public class ServiceExtension : IConfigureComponentServices
+    {
+        public void ConfigureServices(ConfigurationServices configurationServices)
+        {
+            configurationServices.Services.AddTransient<IApplicationRepository, ApplicationRepository>();
+        }
+    }
+}
