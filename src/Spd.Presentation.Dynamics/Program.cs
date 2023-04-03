2ec991e6015d12f8b300feb6274259915001da3a
Yossi T <48697347+ytqsl@users.noreply.github.com>
2023-04-03 10:03:29 -0700

    added integration tests for Dynamics API (#73)

--- src/Spd.Presentation.Dynamics/Program.cs
+++ src/Spd.Presentation.Dynamics/Program.cs
@@ -1,27 +1,16 @@
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Spd.Utilities.FileStorage;
 using Spd.Utilities.Hosting;
-using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.Configure<IISServerOptions>(options =>
-{
-    options.MaxRequestBodySize = int.MaxValue;
-});
 builder.Services.Configure<KestrelServerOptions>(options =>
 {
     options.Limits.MaxRequestBodySize = int.MaxValue; // if don't set default value is: 30 MB
 });
 
-//builder.Services.Configure<FormOptions>(options =>
-//{
-//    options.ValueLengthLimit = int.MaxValue;
-//    options.MultipartBodyLengthLimit = int.MaxValue; // if don't set default value is: 128 MB
-//    options.MultipartHeadersLengthLimit = int.MaxValue;
-//});
-
-// Add services to the container.
 builder.Services.ConfigureCors(builder.Configuration);
 var assemblyName = $"{typeof(Program).Assembly.GetName().Name}";
 builder.Services.ConfigureSwagger(assemblyName);
@@ -34,17 +23,32 @@ builder.Services.AddControllers()
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddFileStorageProxy(builder.Configuration);
 
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, options =>
+{
+    builder.Configuration.GetSection("authentication:jwt").Bind(options);
+    options.Validate();
+});
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+    {
+        policy
+            .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+            .RequireAuthenticatedUser();
+    });
+    options.DefaultPolicy = options.GetPolicy(JwtBearerDefaults.AuthenticationScheme)!;
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
