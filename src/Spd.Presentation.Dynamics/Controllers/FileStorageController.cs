e68a7e8ef6f2943ac06a381e8d4d29c264a59601
Yossi T <48697347+ytqsl@users.noreply.github.com>
2023-04-03 10:03:29 -0700

    added integration tests for Dynamics API (#73)

--- src/Spd.Presentation.Dynamics/Controllers/FileStorageController.cs
+++ src/Spd.Presentation.Dynamics/Controllers/FileStorageController.cs
@@ -1,73 +1,75 @@
 using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Spd.Presentation.Dynamics.Models;
 using Spd.Utilities.FileStorage;
 using Spd.Utilities.Shared;
 using File = Spd.Utilities.FileStorage.File;
 
-namespace Spd.Presentation.Dynamics.Controllers
+namespace Spd.Presentation.Dynamics.Controllers;
+
+/// <summary>
+/// For upload and download file
+/// </summary>
+[Authorize]
+public class FileStorageController : SpdControllerBase
 {
-    /// <summary>
-    /// For upload and download file
-    /// </summary>
-    public class FileStorageController : SpdControllerBase
+    private readonly IFileStorageService _storageService;
+    private readonly IMapper _mapper;
+
+    public FileStorageController(IFileStorageService storageService, IMapper mapper) : base()
     {
-        private readonly IFileStorageService _storageService;
-        private readonly IMapper _mapper;
-        public FileStorageController(IFileStorageService storageService, IMapper mapper) : base()
-        {
-            _storageService = storageService;
-            _mapper = mapper;
-        }
+        _storageService = storageService;
+        _mapper = mapper;
+    }
 
-        /// <summary>
-        /// Upload file
-        /// The maximum file size would be 30M.
-        /// </summary>
-        /// <param name="request"></param>
-        /// <returns></returns>
-        [HttpPost]
-        [Route("api/files")]
-        public async Task<UploadFileResponse> UploadFile([FromForm]UploadFileRequest request)
-        {
-            using var ms = new MemoryStream();
-            await request.File.CopyToAsync(ms);
+    /// <summary>
+    /// Upload file
+    /// The maximum file size would be 30M.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [Route("api/files")]
+    public async Task<UploadFileResponse> UploadFile([FromForm] UploadFileRequest request)
+    {
+        using var ms = new MemoryStream();
+        await request.File.CopyToAsync(ms);
 
-            File spdFile = _mapper.Map<File>(request);
-            spdFile.Content = ms.ToArray();
+        File spdFile = _mapper.Map<File>(request);
+        spdFile.Content = ms.ToArray();
 
-            string id = await _storageService.HandleCommand(new UploadFileCommand { File = spdFile }, CancellationToken.None);
+        string id = await _storageService.HandleCommand(new UploadFileCommand { File = spdFile }, CancellationToken.None);
 
-            return new UploadFileResponse { Id = id };
-        }
+        return new UploadFileResponse { Id = id };
+    }
 
-        //[HttpPost]
-        //[Route("api/files")]
-        //public async Task<UploadFileResponse> UploadFile([FromBody] UploadFileRequestJson request)
-        //{
-        //    File file = _mapper.Map<File>(request);
-        //    string id = await _storageService.HandleCommand(new UploadItemCommand { File = file }, CancellationToken.None);
-        //    return new UploadFileResponse { Id = id };
-        //}
+    //[HttpPost]
+    //[Route("api/files")]
+    //public async Task<UploadFileResponse> UploadFile([FromBody] UploadFileRequestJson request)
+    //{
+    //    File file = _mapper.Map<File>(request);
+    //    string id = await _storageService.HandleCommand(new UploadItemCommand { File = file }, CancellationToken.None);
+    //    return new UploadFileResponse { Id = id };
+    //}
 
-        //[HttpGet]
-        //[Route("api/files/{fileId}")]
-        //public async Task<DownloadFileResponse> DownloadFile(string fileId)
-        //{
-        //    StorageQueryResults result = await _storageService.HandleQuery(new GetItemByKeyQuery { Key = fileId }, CancellationToken.None);
-        //    return _mapper.Map<DownloadFileResponse>(result.File);
-        //}
+    //[HttpGet]
+    //[Route("api/files/{fileId}")]
+    //public async Task<DownloadFileResponse> DownloadFile(string fileId)
+    //{
+    //    StorageQueryResults result = await _storageService.HandleQuery(new GetItemByKeyQuery { Key = fileId }, CancellationToken.None);
+    //    return _mapper.Map<DownloadFileResponse>(result.File);
+    //}
 
-        [HttpGet]
-        [Route("api/files/{fileId}")]
-        public async Task<IActionResult> DownloadFile(string fileId)
-        {
-            StorageQueryResults result = await _storageService.HandleQuery(new FileQuery { Key = fileId }, CancellationToken.None);
+    [HttpGet]
+    [Route("api/files/{fileId}")]
+    public async Task<IActionResult> DownloadFile(string fileId)
+    {
+        StorageQueryResults result = await _storageService.HandleQuery(new FileQuery { Key = fileId }, CancellationToken.None);
 
-            var content = new System.IO.MemoryStream(result.File.Content);
-            var contentType = result.File.ContentType;
-            var fileName = result.File.FileName;
-            return File(content, contentType, fileName);
-        }
+        var content = new System.IO.MemoryStream(result.File.Content);
+        var contentType = result.File.ContentType;
+        var fileName = result.File.FileName;
+        return File(content, contentType, fileName);
     }
 }
---
+++ test/Spd.Tests.Presentation.Dynamics/Integration/FileStorageScenarios.cs
@@ -0,0 +1,73 @@
+﻿using System.Net;
+using Alba;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using Spd.Presentation.Dynamics.Models;
+using Xunit.Abstractions;
+
+namespace Spd.Tests.Presentation.Dynamics.Integration;
+
+public class FileStorageScenarios : ScenarioContextBase
+{
+    public FileStorageScenarios(ITestOutputHelper output, WebAppFixture fixture) : base(output, fixture)
+    {
+    }
+
+    [Fact]
+    public async Task Upload_File_Success()
+    {
+        var payload = new UploadFileRequest
+        {
+            Tag1 = "tag1",
+            Tag2 = "tag2",
+            Tag3 = "tag3",
+            FileName = "test.txt",
+            ContentType = "text/plain",
+            File = CreateFormFile("some text", "test.txt"),
+            EntityName = "test",
+            EntityId = Guid.NewGuid(),
+            Classification = "public"
+        };
+
+        await Host.Scenario(_ =>
+        {
+            _.Post.FormData(payload).ToUrl("/api/files/");
+            _.StatusCodeShouldBeOk();
+        });
+    }
+
+    [Fact]
+    public async Task Upload_File_NoAuth_Unauthorized()
+    {
+        var payload = new UploadFileRequest
+        {
+            Tag1 = "tag1",
+            Tag2 = "tag2",
+            Tag3 = "tag3",
+            FileName = "test.txt",
+            ContentType = "text/plain",
+            File = CreateFormFile("some text", "test.txt"),
+            EntityName = "test",
+            EntityId = Guid.NewGuid(),
+            Classification = "public"
+        };
+
+        await Host.Scenario(_ =>
+        {
+            _.RemoveRequestHeader(HeaderNames.Authorization);
+            _.Post.FormData(payload).ToUrl("/api/files/");
+            _.StatusCodeShouldBe(HttpStatusCode.Unauthorized);
+        });
+    }
+
+    private static IFormFile CreateFormFile(string content, string fileName)
+    {
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        writer.Write(content);
+        writer.Flush();
+        stream.Position = 0;
+
+        return new FormFile(stream, 0, stream.Length, fileName, fileName);
+    }
+}
