e8f3a80209aca6539fa18ba3c844127b13eca343
Carol <carol.carpenter@quartech.com>
2023-04-05 08:12:38 -0700

    minor fixes

--- src/Spd.Resource.Applicants/Mappings.cs
+++ src/Spd.Resource.Applicants/Mappings.cs
@@ -26,7 +26,6 @@ namespace Spd.Resource.Applicants
             .ForMember(d => d.spd_dateofbirth, opt => opt.MapFrom(s => s.DateOfBirth))
             .ForMember(d => d.spd_phonenumber, opt => opt.MapFrom(s => s.PhoneNumber))
             .ForMember(d => d.spd_bcdriverslicense, opt => opt.MapFrom(s => s.DriversLicense))
-            .ForMember(d => d.spd_dateofbirth, opt => opt.MapFrom(s => s.DateOfBirth))
             .ForMember(d => d.spd_birthplace, opt => opt.MapFrom(s => s.BirthPlace))
             .ForMember(d => d.spd_applicantsposition, opt => opt.MapFrom(s => s.JobTitle))
             .ForMember(d => d.spd_addressline1, opt => opt.MapFrom(s => s.AddressLine1))
@@ -36,26 +35,35 @@ namespace Spd.Resource.Applicants
             .ForMember(d => d.spd_province, opt => opt.MapFrom(s => s.Province))
             .ForMember(d => d.spd_country, opt => opt.MapFrom(s => s.Country))
             .ForMember(d => d.spd_declaration, opt => opt.MapFrom(s => s.AgreeToCompleteAndAccurate))
-            .ForMember(d => d.spd_consent, opt => opt.MapFrom(s => s.HaveVerifiedIdentity)); //todo cbc is this right?
+            .ForMember(d => d.spd_identityconfirmed, opt => opt.MapFrom(s => s.HaveVerifiedIdentity));
 
             _ = CreateMap<AliasCreateCmd, spd_alias>()
-                .ForMember(d => d.spd_aliasid, opt => opt.MapFrom(s => Guid.NewGuid()))
-                .ForMember(d => d.spd_aliastype, opt => opt.MapFrom(s => 100000002)) // todo fix
-                .ForMember(d => d.spd_firstname, opt => opt.MapFrom(s => s.GivenName))
-                .ForMember(d => d.spd_middlename1, opt => opt.MapFrom(s => s.MiddleName1))
-                .ForMember(d => d.spd_middlename2, opt => opt.MapFrom(s => s.MiddleName2))
-                .ForMember(d => d.spd_surname, opt => opt.MapFrom(s => s.Surname));
+            .ForMember(d => d.spd_aliasid, opt => opt.MapFrom(s => Guid.NewGuid()))
+            .ForMember(d => d.spd_aliastype, opt => opt.MapFrom(s => AliasTypeOptionSet.FormerName))
+            .ForMember(d => d.spd_firstname, opt => opt.MapFrom(s => s.GivenName))
+            .ForMember(d => d.spd_middlename1, opt => opt.MapFrom(s => s.MiddleName1))
+            .ForMember(d => d.spd_middlename2, opt => opt.MapFrom(s => s.MiddleName2))
+            .ForMember(d => d.spd_surname, opt => opt.MapFrom(s => s.Surname))
+            .ForMember(d => d.spd_fullname, opt => opt.MapFrom(s => s.Surname + ", " + s.GivenName + " " + s.MiddleName1 + " " + s.MiddleName2));
 
             _ = CreateMap<ApplicationCreateCmd, contact>()
             .ForMember(d => d.contactid, opt => opt.MapFrom(s => Guid.NewGuid()))
             .ForMember(d => d.firstname, opt => opt.MapFrom(s => s.GivenName))
-            .ForMember(d => d.middlename, opt => opt.MapFrom(s => s.MiddleName1))
             .ForMember(d => d.spd_middlename1, opt => opt.MapFrom(s => s.MiddleName1))
             .ForMember(d => d.spd_middlename2, opt => opt.MapFrom(s => s.MiddleName2))
             .ForMember(d => d.lastname, opt => opt.MapFrom(s => s.Surname))
             .ForMember(d => d.emailaddress1, opt => opt.MapFrom(s => s.EmailAddress))
+            .ForMember(d => d.jobtitle, opt => opt.MapFrom(s => s.JobTitle))
+            //.ForMember(d => d.birthdate, opt => opt.MapFrom(s => s.DateOfBirth)) // todo cbc Datatype issue
+            .ForMember(d => d.telephone1, opt => opt.MapFrom(s => s.PhoneNumber))
             .ForMember(d => d.spd_bcdriverslicense, opt => opt.MapFrom(s => s.DriversLicense))
-            .ForMember(d => d.spd_birthplace, opt => opt.MapFrom(s => s.BirthPlace));
+            .ForMember(d => d.spd_birthplace, opt => opt.MapFrom(s => s.BirthPlace))
+            .ForMember(d => d.address1_line1, opt => opt.MapFrom(s => s.AddressLine1))
+            .ForMember(d => d.address1_line2, opt => opt.MapFrom(s => s.AddressLine2))
+            .ForMember(d => d.address1_city, opt => opt.MapFrom(s => s.City))
+            .ForMember(d => d.address1_postalcode, opt => opt.MapFrom(s => s.PostalCode))
+            .ForMember(d => d.address1_stateorprovince, opt => opt.MapFrom(s => s.Province))
+            .ForMember(d => d.address1_country, opt => opt.MapFrom(s => s.Country));
         }
     }
 }
