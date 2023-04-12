b1f084d79c481272522beeef9cdf33c1c26beb53
peggy <peggy.zhang@quartech.com>
2023-04-12 11:12:31 -0700

    add ownerid to org registration
    fix
    
    
    fix

--- src/Spd.Utilities.Dynamics/Constants.cs
+++ src/Spd.Utilities.Dynamics/Constants.cs
@@ -6,5 +6,6 @@
         public static readonly int StateCode_Inactive = 1;
         public static readonly int StatusCode_Active = 1;
         public static readonly int StatusCode_Inactive = 2;
+        public static readonly string Client_Service_Team_Guid = "65e0b015-ee9d-ed11-b83d-00505683fbf4";
     }
 }
