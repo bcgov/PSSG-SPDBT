f3a461a35ba042f425b44829391b57dd2e2ffd15
Peggy <peggy.zhang@quartech.com>
2023-04-19 17:46:06 -0700

    initial setup for integration test (#105)
    * initial setup for integration test
    
    * fix

---
+++ src/Spd.Tests.Presentation.Screening/Integration/DynamicsTestData.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Dynamics.CRM;
+using Spd.Utilities.Dynamics;
+
+namespace Spd.Tests.Presentation.Screening
+{
+    public class DynamicsTestData
+    {
+        private readonly string testPrefix="spd-auto-test";
+        private readonly DynamicsContext _context;
+
+        public DynamicsTestData(IDynamicsContextFactory factory)
+        {
+            _context = factory.Create();
+        }
+
+        public async Task CreateOrg(Guid orgId, string orgName)
+        {
+            _context.AddToaccounts(new account
+            {
+                accountid = orgId,
+                name = $"{testPrefix}{orgName}",
+                spd_organizationlegalname = $"{testPrefix}{orgName}",
+                address1_city = "victoria",
+            });
+            await _context.SaveChangesAsync();
+        }
+
+
+    }
+}
---
+++ src/Spd.Tests.Presentation.Screening/Integration/OrgSenarios.cs
@@ -0,0 +1,74 @@
+﻿using Alba;
+using Spd.Manager.Membership.Org;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Spd.Tests.Presentation.Screening.Integration;
+
+public class OrgScenarios : ScenarioContextBase
+{
+    public OrgScenarios(ITestOutputHelper output, WebAppFixture fixture) : base(output, fixture)
+    {}
+
+
+    [Fact]
+    public async Task GetOrgFromId_NoAuth_Unauthorized()
+    {
+        Guid orgId = Guid.NewGuid();
+        await fixture.testData.CreateOrg(orgId,"org1");
+        
+        await Host.Scenario(_ =>
+        {
+            _.Get.Url($"/api/org/{orgId}");
+            //todo, once we know how to make pkce auth working, uncomment following code.
+            //_.StatusCodeShouldBe(HttpStatusCode.Unauthorized);
+            _.StatusCodeShouldBeOk();
+        });
+    }
+
+    [Fact]
+    public async Task GetOrgFromId_WithCorrectAuth_Success()
+    {
+        Guid orgId = Guid.NewGuid();
+        await fixture.testData.CreateOrg(orgId, "org1");
+
+        await Host.Scenario(_ =>
+        {
+            _.Get.Url($"/api/org/{orgId}");
+            _.ContentShouldContain(orgId.ToString());
+            _.StatusCodeShouldBeOk();
+        });
+    }
+
+    [Fact]
+    public async Task UpdateOrg_WithCorrectAuth_Success()
+    {
+        Guid orgId = Guid.NewGuid();
+        await fixture.testData.CreateOrg(orgId, "org1");
+
+        await Host.Scenario(_ =>
+        {
+            _.Put.Json<OrgUpdateRequest>(Create_OrgUpdateRequest(orgId)).ToUrl($"/api/org/{orgId}");
+            _.ContentShouldContain(orgId.ToString());
+            _.StatusCodeShouldBeOk();
+        });
+    }
+
+    private OrgUpdateRequest Create_OrgUpdateRequest(Guid orgId)
+    {
+        return new OrgUpdateRequest
+        {
+            Id = orgId,
+            AddressCity="city",
+            Email="test@test.com",
+            OrganizationName="org2",
+            OrganizationLegalName="legalorg2",
+            PayerPreference=Manager.Membership.Shared.PayerPreferenceTypeCode.Applicant,
+            PhoneNumber="111-111-1111",
+            AddressLine1="line1",
+            AddressCountry="canada",
+            AddressPostalCode="ABCEDF",
+            AddressProvince="BC"
+        };
+    }
+}
