495789e4a437a7df0db9686e44f928e26e84a254
Peggy <peggy.zhang@quartech.com>
2023-04-21 10:27:42 -0700

    integration clean up and schema update (#109)
    * update schema
    
    * adjust format
    
    * remove no-use pr-template

--- src/Spd.Tests.Presentation.Screening/Integration/scenarios/OrgRegistrationScenarios.cs
+++ src/Spd.Tests.Presentation.Screening/Integration/scenarios/OrgRegistrationScenarios.cs
@@ -1,10 +1,11 @@
-﻿using Spd.Manager.Membership.OrgRegistration;
+﻿using Alba;
+using Spd.Manager.Membership.OrgRegistration;
 using Spd.Presentation.Screening.Controllers;
 using System.Net;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Spd.Tests.Presentation.Screening.Integration;
+namespace Spd.Tests.Presentation.Screening.Integration.scenarios;
 
 public class OrgRegistrationScenarios : ScenarioContextBase
 {
@@ -17,7 +18,7 @@ public class OrgRegistrationScenarios : ScenarioContextBase
     {
         await Host.Scenario(_ =>
         {
-            _.Post.Json<OrgRegistrationCreateRequest>(Create_OrgRegistrationCreateRequest()).ToUrl($"/api/org-registrations");
+            _.Post.Json(Create_OrgRegistrationCreateRequest()).ToUrl($"/api/org-registrations");
             _.StatusCodeShouldBe(HttpStatusCode.Unauthorized);
         });
     }
@@ -27,21 +28,22 @@ public class OrgRegistrationScenarios : ScenarioContextBase
     {
         await Host.Scenario(_ =>
         {
-            _.Post.Json<OrgRegistrationCreateRequest>(Create_OrgRegistrationCreateRequest()).ToUrl($"/api/org-registrations");
+            _.Post.Json(Create_OrgRegistrationCreateRequest()).ToUrl($"/api/org-registrations");
             //todo: should be success, 
             _.StatusCodeShouldBe(HttpStatusCode.Unauthorized);
         });
     }
 
     [Fact]
-    public async Task AnonymousRegisterOrg_InvalidCaptcha_Unauthorized()
+    public async Task AnonymousRegisterOrg_ValidCaptcha_Success()
     {
+        //we use google test client key and secrets in app for integration test.
         AnonymousOrgRegistrationCreateRequest request = Create_AnonymousOrgRegistrationCreateRequest();
 
         await Host.Scenario(_ =>
         {
             _.Post.Json<OrgRegistrationCreateRequest>(request).ToUrl($"/api/anonymous-org-registrations");
-            _.StatusCodeShouldBe(HttpStatusCode.BadRequest);
+            _.StatusCodeShouldBeOk();
         });
     }
 
@@ -49,20 +51,20 @@ public class OrgRegistrationScenarios : ScenarioContextBase
         new AnonymousOrgRegistrationCreateRequest
         {
             ContactSurname = "test",
-            RegistrationTypeCode=RegistrationTypeCode.Volunteer,
-            VolunteerOrganizationTypeCode=VolunteerOrganizationTypeCode.NonProfit,
-            OrganizationName= "test",
-            ContactEmail= "test@test.com",
-            ContactGivenName= "test",
-            ContactPhoneNumber= "111-111-1111",
-            ContactJobTitle= "test",
-            MailingAddressLine1= "address1",
-            MailingCity="victoria",
-            MailingPostalCode="abcedf",
-            GenericEmail= "test@email.com",
-            GenericPhoneNumber="111-222-2222",
-            AgreeToTermsAndConditions= true, 
-            Recaptcha= "Invalid_recapthca" 
+            RegistrationTypeCode = RegistrationTypeCode.Volunteer,
+            VolunteerOrganizationTypeCode = VolunteerOrganizationTypeCode.NonProfit,
+            OrganizationName = "test",
+            ContactEmail = "test@test.com",
+            ContactGivenName = "test",
+            ContactPhoneNumber = "111-111-1111",
+            ContactJobTitle = "test",
+            MailingAddressLine1 = "address1",
+            MailingCity = "victoria",
+            MailingPostalCode = "abcedf",
+            GenericEmail = "test@email.com",
+            GenericPhoneNumber = "111-222-2222",
+            AgreeToTermsAndConditions = true,
+            Recaptcha = "recapthca_test_token"
         };
 
     private OrgRegistrationCreateRequest Create_OrgRegistrationCreateRequest() =>
