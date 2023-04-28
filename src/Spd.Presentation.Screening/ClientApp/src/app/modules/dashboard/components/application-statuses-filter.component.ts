e501e80d281440f817331395cfc6072f234b78da
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2023-04-27 17:33:01 -0700

    application statuses (#121)

--- src/Spd.Presentation.Screening/ClientApp/src/app/modules/dashboard/components/application-statuses-filter.component.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/modules/dashboard/components/application-statuses-filter.component.ts
@@ -1,11 +1,45 @@
 import { Component, Input } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { BaseFilterComponent } from 'src/app/shared/components/base-filter.component';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { UtilService } from 'src/app/core/services/util.service';
+import { BaseFilterComponent, FilterQueryList } from 'src/app/shared/components/base-filter.component';
+
+export const ApplicationStatusFiltersTypes = [
+	{ desc: 'Verify Identity', code: 'verifyIdentity' },
+	{ desc: 'In Progress', code: 'inProgress' },
+	{ desc: 'Pay Now', code: 'payNow' },
+	{ desc: 'Awaiting Third Party', code: 'awaitingThirdParty' },
+	{ desc: 'Awaiting Applicant', code: 'awaitingApplicant' },
+	{ desc: 'Under Assessment', code: 'underAssessment' },
+	{ desc: 'Incomplete', code: 'incomplete' },
+	{ desc: 'Completed - Cleared', code: 'cleared' },
+	{ desc: 'Completed - Risk Found', code: 'riskFound' },
+	{ desc: 'Closed - Judicial Review', code: 'judicialReview' },
+	{ desc: 'Closed - No Response', code: 'noResponse' },
+	{ desc: 'Closed - No Applicant Consent', code: 'noApplicantConsent' },
+	{ desc: 'Cancelled by Organization', code: 'cancelledByOrg' },
+	{ desc: 'Cancelled by Applicant', code: 'cancelledByAppl' },
+];
+
+export class ApplicationStatusFilter {
+	search: string = '';
+	statuses: string[] = [];
+	applicantName: string = '';
+	createdOn: string = '';
+	contractedCompanyName: string = '';
+}
+
+export const ApplicationStatusFilterMap: Record<keyof ApplicationStatusFilter, string> = {
+	search: 'searchBe',
+	statuses: 'statusCodeBe',
+	applicantName: 'applicantNameBe',
+	createdOn: 'createdOn',
+	contractedCompanyName: 'contractedCompanyName',
+};
 
 @Component({
 	selector: 'app-application-statuses-filter',
 	template: `
-		<div class="filter-panel">
+		<div class="filter-container mat-elevation-z8">
 			<form [formGroup]="formGroup" novalidate>
 				<mat-toolbar>
 					<span class="fw-bold">Filters</span>
@@ -15,40 +49,35 @@ import { BaseFilterComponent } from 'src/app/shared/components/base-filter.compo
 				</mat-toolbar>
 				<mat-card>
 					<mat-card-content class="mb-2 text-start">
-						<div class="row">
-							<div class="col-sm-12">
-								<strong>Statuses</strong>
-								<mat-checkbox formControlName="verifyIdentity" class="text-start"> Verify Identity </mat-checkbox>
-								<mat-checkbox formControlName="inProgress" class="text-start"> In Progress </mat-checkbox>
-								<!-- <mat-checkbox formControlName="payNow" class="text-start"> Pay Now</mat-checkbox>
-								<mat-checkbox formControlName="awaitingThirdParty" class="text-start">
-									Awaiting Third Party
-								</mat-checkbox>
-								<mat-checkbox formControlName="awaitingApplicant" class="text-start"> Awaiting Applicant</mat-checkbox>
-								<mat-checkbox formControlName="underAssessment" class="text-start"> Under Assessment</mat-checkbox>
-								<mat-checkbox formControlName="incomplete" class="text-start"> Incomplete</mat-checkbox>
-								<mat-checkbox formControlName="cleared" class="text-start"> Completed - Cleared</mat-checkbox>
-								<mat-checkbox formControlName="riskFound" class="text-start"> Completed - Risk Found</mat-checkbox>
-								<mat-checkbox formControlName="judicialReview" class="text-start">
-									Closed - Judicial Review
-								</mat-checkbox>
-								<mat-checkbox formControlName="noResponse" class="text-start"> Closed - No Response</mat-checkbox>
-								<mat-checkbox formControlName="noApplicantConsent" class="text-start">
-									Closed - No Applicant Consent
-								</mat-checkbox>
-								<mat-checkbox formControlName="cancelledByOrg" class="text-start">
-									Cancelled by Organization
-								</mat-checkbox>
-								<mat-checkbox formControlName="cancelledByAppl" class="text-start">
-									Cancelled by Applicant
-								</mat-checkbox> -->
-							</div>
-						</div>
+						<mat-form-field class="status-filter" style="min-width: 600px; min-height: 200px">
+							<mat-label>Filter by status</mat-label>
+							<mat-select formControlName="statuses" placeholder="All statuses" multiple>
+								<mat-select-trigger>
+									<mat-chip-listbox>
+										<mat-chip
+											*ngFor="let status of statuses.value"
+											[removable]="true"
+											(removed)="onItemRemoved(status)"
+											selected
+										>
+											{{ getStatusDesc(status) }}
+											<mat-icon matChipRemove>cancel</mat-icon>
+										</mat-chip>
+									</mat-chip-listbox>
+								</mat-select-trigger>
+
+								<mat-option *ngFor="let status of applicationStatusFiltersTypes" [value]="status.code">
+									{{ status.desc }}
+								</mat-option>
+							</mat-select>
+						</mat-form-field>
 					</mat-card-content>
 					<mat-divider class="my-3"></mat-divider>
 					<mat-card-actions>
-						<button mat-stroked-button class="action-button" (click)="emitFilterClear()">Clear</button>
-						<button mat-flat-button class="action-button" color="primary" (click)="emitFilterChange()">Search</button>
+						<button mat-stroked-button class="large action-button" (click)="emitFilterClear()">Clear</button>
+						<button mat-flat-button class="large action-button" color="primary" (click)="emitFilterChange()">
+							Search
+						</button>
 					</mat-card-actions>
 				</mat-card>
 			</form>
@@ -77,6 +106,10 @@ import { BaseFilterComponent } from 'src/app/shared/components/base-filter.compo
 				justify-content: space-between;
 			}
 
+			.status-column {
+				max-width: 15em;
+			}
+
 			.action-button {
 				width: unset;
 			}
@@ -84,18 +117,48 @@ import { BaseFilterComponent } from 'src/app/shared/components/base-filter.compo
 	],
 })
 export class ApplicationStatusesFilterComponent extends BaseFilterComponent {
+	applicationStatusFiltersTypes = ApplicationStatusFiltersTypes;
+
 	@Input() formGroup!: FormGroup;
 
-	constructor(private formBuilder: FormBuilder) {
+	constructor(private formBuilder: FormBuilder, private utilService: UtilService) {
 		super();
 	}
 
+	onItemRemoved(item: string) {
+		const items = [...this.statuses.value] as string[];
+		this.utilService.removeFirst(items, item);
+		this.statuses.setValue(items); // To trigger change detection
+	}
+
 	emitFilterChange() {
-		this.filterChange.emit();
+		this.filterChange.emit(this.constructFilterString(this.constructFilterList(this.formGroup.value)));
 	}
 
 	override emitFilterClear() {
 		this.formGroup.reset();
 		this.filterClear.emit();
+	}
+
+	getStatusDesc(code: string): string {
+		return ApplicationStatusFiltersTypes.find((item) => item.code == code)?.desc ?? '';
+	}
+
+	private constructFilterList(formGroupValue: ApplicationStatusFilter): FilterQueryList[] {
+		let filterList: FilterQueryList[] = [];
+
+		if (formGroupValue.statuses?.length > 0) {
+			filterList.push({
+				key: ApplicationStatusFilterMap['statuses'],
+				operator: 'caseInsensitiveStringContains',
+				value: formGroupValue.statuses.join('|'),
+			});
+		}
+
+		return filterList;
+	}
+
+	get statuses(): FormControl {
+		return this.formGroup.get('statuses') as FormControl;
 	}
 }
