f97a0c9ea14a7d6d11850ee6848c624258c91951
Yossi T <48697347+ytqsl@users.noreply.github.com>
2023-04-28 12:09:40 -0700

    SPDBT-992: added application statistics query (#123)
    * added application statistics query
    
    * fix conflicts
    
    ---------
    
    Co-authored-by: peggy <peggy.zhang@quartech.com>

---
+++ src/Spd.Tests.Presentation.Screening/Integration/Resources/Applicants/ApplicationsTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using Spd.Resource.Applicants.Application;
+using Xunit.Abstractions;
+
+namespace Spd.Tests.Presentation.Screening.Integration.Resources.Applicants;
+
+public class ApplicationsTests : ScenarioContextBase
+{
+    public ApplicationsTests(ITestOutputHelper output, WebAppFixture fixture) : base(output, fixture)
+    {
+    }
+
+    [Fact]
+    public async Task CanGetStatistics()
+    {
+        var applicationRepository = Host.Services.GetRequiredService<IApplicationRepository>();
+        var org = await fixture.testData.CreateOrg("org1");
+        var stats = await applicationRepository.QueryAsync(new ApplicationStatisticsQuery(org.accountid!.Value), CancellationToken.None);
+        stats.Statistics.ShouldNotBeEmpty();
+    }
+}
