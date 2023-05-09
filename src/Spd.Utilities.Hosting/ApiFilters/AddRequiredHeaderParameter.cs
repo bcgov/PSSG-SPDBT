fc97f50ec86103ae2c000d5bff1896a3acc8e4cd
Peggy <peggy.zhang@quartech.com>
2023-05-09 16:56:10 -0700

    swagger file generation: add for file upload (#149)
    * temp
    
    * add swagger filter
    
    * fix bug

---
+++ src/Spd.Utilities.Hosting/ApiFilters/AddRequiredHeaderParameter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Rsvp.Cms.Api.Filters;
+public class AddRequiredHeaderParameter : IOperationFilter
+{
+    private static List<string> IgnoreOrgHeaderControllerName = new List<string>{ "Configuration", "OrgRegistration", "AddressAutoComplete"};
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        
+        var descriptor = context.ApiDescription.ActionDescriptor as ControllerActionDescriptor;
+        if (descriptor != null &&
+            !IgnoreOrgHeaderControllerName.Contains(descriptor.ControllerName))
+        {
+            operation.Parameters.Add(
+            new OpenApiParameter
+            {
+                Name = "organization",
+                In = ParameterLocation.Header,
+                Description = "Add Organization Id",
+                Required = true
+            });
+        };
+    }
+}
