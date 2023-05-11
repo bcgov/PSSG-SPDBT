c5e084d9be554710cd88a0ca8d157d0fac3fad03
Peggy <peggy.zhang@quartech.com>
2023-05-11 09:03:01 -0700

    spdbt-1106 : add dataProtectionProvider (#153)
    * add dataProvider
    
    * update odata schema
    
    * fix
    
    * when link expired
    
    * update views

---
+++ src/Spd.Utilities.Hosting/ServiceExtensionDataProtection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Spd.Utilities.Hosting
+{
+    public static class ServiceExtensionDataProtection
+    {
+        public static void ConfigureDataProtection(this IServiceCollection services,
+            IConfiguration configuration, string appName)
+        {
+            var dataProtectionPath = configuration.GetValue("KEY_RING_PATH", string.Empty);
+            var dpBuilder = services.AddDataProtection()
+                    .SetApplicationName(appName);
+
+            if (!string.IsNullOrEmpty(dataProtectionPath)) dpBuilder.PersistKeysToFileSystem(new DirectoryInfo(dataProtectionPath));
+        }
+    }
+}
