a6442e8f4ddb463fd859e5b027bf70304d2253c1
Peggy <peggy.zhang@quartech.com>
2023-05-17 10:20:37 -0700

    upload bulk: file parse and duplicate check (#166)
    * upload file-controller
    
    * bulk upload file parser
    
    * change contract
    
    * add validation
    
    * temp
    
    * refine, add it
    
    * add more valiation
    
    * batch max

---
+++ src/Spd.Engine.Validation/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spd.Utilities.Hosting;
+
+namespace Spd.Engine.Validation;
+public class ServiceExtension : IConfigureComponentServices
+{
+    public void ConfigureServices(ConfigurationServices configurationServices)
+    {
+        configurationServices.Services.AddTransient<IDuplicateCheckEngine, DuplicateCheckEngine>();
+    }
+}
