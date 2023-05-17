2cbcae49734074c123a975d5e1071f3aa0826a80
peggy <peggy.zhang@quartech.com>
2023-05-17 12:51:22 -0700

    banner repo

---
+++ src/Spd.Resource.Organizations/Config/Contract.cs
@@ -0,0 +1,12 @@
+﻿namespace Spd.Resource.Organizations.Config
+{
+    public interface IConfigRepository
+    {
+        Task<ConfigResult?> Query(ConfigQuery query, CancellationToken ct);
+    }
+
+    //query
+    public record ConfigQuery;
+    public record ConfigResult(string BannerMsg);
+
+}
