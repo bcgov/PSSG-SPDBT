ea3314f94a9bdb30b3b4e00ec5a8e5029cf32687
Peggy <peggy.zhang@quartech.com>
2023-05-17 16:22:10 -0700

    banner msg, hook up with dynamics (#171)
    * fix wrong loc
    
    * banner repo
    
    * make banner work
    
    * add it

---
+++ src/Spd.Tests.Presentation.Screening/Integration/Controllers/ConfigurationScenarios.cs
@@ -0,0 +1,20 @@
+﻿using Xunit.Abstractions;
+
+namespace Spd.Tests.Presentation.Screening.Integration.Controllers;
+
+public class ConfigurationScenarios : ScenarioContextBase
+{
+    public ConfigurationScenarios(ITestOutputHelper output, WebAppFixture fixture) : base(output, fixture)
+    { }
+
+
+    [Fact]
+    public async Task GetConfiguration_WithoutHeader_Unauthorized_ShouldSuccess()
+    {
+        await Host.Scenario(_ =>
+        {
+            _.Get.Url($"/api/configuration");
+            _.StatusCodeShouldBe(200);
+        });
+    }
+}
--- src/Spd.Utilities.Shared/SpdConstants.cs
+++ src/Spd.Utilities.Shared/SpdConstants.cs
@@ -7,4 +7,6 @@ public static class SpdConstants
     public static readonly long UPLOAD_FILE_MAX_SIZE = 26214400; //25M
     public static readonly string BULK_APP_UPLOAD_COL_SEPERATOR = "\"\t\"";
     public static readonly string BULK_APP_UPLOAD_BIRTHDATE_FORMAT = "yyyy-MM-dd";
+    public static readonly string DEFAULT_BANNER_MSG = "10 business days for online applications and 20 business days for manual applications.";
+    public static readonly string BANNER_MSG_CONFIG_KEY = "ScreeningPortalProcessingTimeBanner";
 }
