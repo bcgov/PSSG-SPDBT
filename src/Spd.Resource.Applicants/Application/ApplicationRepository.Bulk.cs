d107876a099fe437c81b9778d5a203f861a801a7
Peggy <peggy.zhang@quartech.com>
2023-05-19 15:12:41 -0700

    Upload bulk batch file (#175)
    * refactor app repo
    
    * refactor
    
    * temp
    
    * work
    
    * add history
    
    * add test
    
    * cleanup
    
    * temp

--- src/Spd.Resource.Applicants/Application/ApplicationRepository.Bulk.cs
+++ src/Spd.Resource.Applicants/Application/ApplicationRepository.Bulk.cs
@@ -1,6 +1,7 @@
 using Microsoft.Dynamics.CRM;
 using Microsoft.OData.Client;
 using Spd.Utilities.Dynamics;
+using Spd.Utilities.Shared.Exceptions;
 
 namespace Spd.Resource.Applicants.Application;
 internal partial class ApplicationRepository : IApplicationRepository
@@ -41,9 +42,81 @@ internal partial class ApplicationRepository : IApplicationRepository
         return response;
     }
 
-    public async Task<Guid?> AddApplicationsInBatchAsync(IEnumerable<ApplicationCreateCmd> createApplicationCmds, CancellationToken cancellationToken)
+    public async Task<BulkAppsCreateResp> AddBulkAppsAsync(BulkAppsCreateCmd cmd, CancellationToken ct)
     {
-        return null;
+        account? org = await _context.GetOrgById(cmd.OrgId, ct);
+        spd_portaluser? user = await _context.GetUserById(cmd.UserId, ct);
+        if (org == null || user == null)
+            throw new ApiException(System.Net.HttpStatusCode.PreconditionFailed, "Invalid organization or user.");
+
+        //create applications
+        List<ApplicationCreateCmd> createApps = cmd.CreateApps.ToList();
+        List<ApplicationCreateRslt> results =new List<ApplicationCreateRslt>();
+        for(int i=0; i<createApps.Count; i++)
+        {
+            results.Add(new ApplicationCreateRslt { LineNumber = i + 1 });
+        }
+
+        int begin = 0;
+        //dynamics constraints: A maximum number of '1000' operations are allowed in a change set.
+        //for each app create request, there is max 12 operations
+        int oneChangeSetMaxApps = 1000 / 12;
+        while (begin < createApps.Count)
+        {
+            int len = createApps.Count - begin;
+            if (len > oneChangeSetMaxApps)
+                len = oneChangeSetMaxApps;
+            for (int index = begin; index < begin + len; index++)
+            {
+                var app = await CreateAppAsync(createApps[index], org, user);
+                results[index].ApplicationId = (Guid)app.spd_applicationid;
+            }
+            await _context.SaveChangesAsync(SaveChangesOptions.BatchWithSingleChangeset, ct);
+            results.ForEach(r =>
+            {
+                if (r.LineNumber >= begin && r.LineNumber < begin + oneChangeSetMaxApps)
+                {
+                    r.CreateSuccess = true;
+                }
+            });
+            begin += len;
+        }
+
+        //return result
+        int bulkSize = createApps.Count;
+        int successCount = results.Count(r => r.CreateSuccess);
+
+        BulkAppsCreateResultCd bulkResult;
+        if (successCount == bulkSize)
+        {
+            bulkResult = BulkAppsCreateResultCd.Success;
+        }
+        else if (successCount == 0)
+        {
+            bulkResult = BulkAppsCreateResultCd.Failed;
+        }
+        else
+        {
+            bulkResult = BulkAppsCreateResultCd.PartiallySuccess;
+        }
+
+        //add history
+        if(bulkResult != BulkAppsCreateResultCd.Failed)
+        {
+            spd_genericupload bulkInfo = new spd_genericupload
+            {
+                spd_genericuploadid = Guid.NewGuid(),
+                spd_filename = cmd.FileName,
+                spd_datetimeuploaded = DateTimeOffset.UtcNow,
+                spd_batchnumber = DateTime.Now.Ticks.ToString() //waiting for dynamics decision on format
+            };
+            _context.AddTospd_genericuploads(bulkInfo);
+            _context.SetLink(bulkInfo, nameof(bulkInfo.spd_UploadedBy), user);
+            _context.SetLink(bulkInfo, nameof(bulkInfo.spd_OrganizationId), org);
+            await _context.SaveChangesAsync(ct);
+        }
+
+        return new BulkAppsCreateResp { CreateAppResults = results, BulkAppsCreateCode = bulkResult };
     }
 }
 
