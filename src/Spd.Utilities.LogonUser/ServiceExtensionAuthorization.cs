da63d88ac5b7c09c47e8e35cb64d0c2faab0e691
peggy <peggy.zhang@quartech.com>
2023-06-01 21:53:01 -0700

    clean

--- src/Spd.Utilities.LogonUser/ServiceExtensionAuthorization.cs
+++ src/Spd.Utilities.LogonUser/ServiceExtensionAuthorization.cs
@@ -1,14 +1,6 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.Net.Http.Headers;
 using Spd.Utilities.LogonUser.Configurations;
-using System.Configuration;
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 
 namespace Spd.Utilities.LogonUser
 {
@@ -19,7 +11,7 @@ namespace Spd.Utilities.LogonUser
             services.AddAuthorization(options =>
             {
                 var onlyBcscPolicyBuilder = new AuthorizationPolicyBuilder(BcscAuthenticationConfiguration.AuthSchemeName);
-                options.AddPolicy("OnlyBcsc", 
+                options.AddPolicy("OnlyBcsc",
                     onlyBcscPolicyBuilder.RequireAuthenticatedUser().Build());
 
                 var onlyBceidPolicyBuilder = new AuthorizationPolicyBuilder(BCeIDAuthenticationConfiguration.AuthSchemeName);
