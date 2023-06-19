610a3832043ec365cdfbea10597146f1c1ce2650
Carol <carol.carpenter@quartech.com>
2023-06-19 13:46:51 -0700

    Fix GenderOptionSet

--- src/Spd.Presentation.Screening/ClientApp/src/app/core/constants/constants.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/core/constants/constants.ts
@@ -6,7 +6,6 @@ export const SPD_CONSTANTS = {
 		birthDateStartAt: new Date(2000, 0, 1),
 		dateFormat: 'yyyy-MM-dd',
 		monthYearFormat: 'MMM yyyy',
-		monthYearHyphenFormat: 'MMM-yyyy',
 		dateTimeFormat: 'yyyy-MM-dd HH:mm',
 	},
 	phone: {
