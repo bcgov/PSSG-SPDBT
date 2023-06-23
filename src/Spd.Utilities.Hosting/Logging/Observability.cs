2c1753a26c9677193353ff2a24ca63abcbcef8b0
Yossi T <48697347+ytqsl@users.noreply.github.com>
2023-06-23 13:06:46 -0700

    added serilog to hosts (#260)

---
+++ src/Spd.Utilities.Hosting/Logging/Observability.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Enrichers.Span;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Extensions.Logging;
+using System.Globalization;
+using System.Net;
+
+namespace Spd.Utilities.Hosting.Logging;
+
+public static class Observability
+{
+    public const string LogOutputTemplate = "[{Timestamp:HH:mm:ss} {Level:u3} {SourceContext}] {Message:lj}{NewLine}{Exception}";
+
+    /// <summary>
+    /// Create an initial logger to use during Program startup.
+    /// </summary>
+    /// <param name="configuration">The configuration to use.</param>
+    /// <returns>An instance of a logger.</returns>
+    public static Microsoft.Extensions.Logging.ILogger GetInitialLogger(IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .ReadFrom.Configuration(configuration)
+            .Enrich.FromLogContext()
+            .WriteTo.Console(outputTemplate: LogOutputTemplate, formatProvider: CultureInfo.InvariantCulture)
+            .CreateBootstrapLogger();
+
+        using var factory = new SerilogLoggerFactory(Log.Logger);
+        return factory.CreateLogger("Startup");
+    }
+
+    public static IHostBuilder UseDefaultLogging(this IHostBuilder builder, string serviceName)
+    {
+        builder.UseSerilog((ctx, services, config) => config.ConfigureDefaultLogging(ctx.Configuration, services, serviceName));
+
+        return builder;
+    }
+
+    public static IApplicationBuilder UseDefaultHttpRequestLogging(this IApplicationBuilder app)
+    {
+        app.UseSerilogRequestLogging(
+            opts =>
+            {
+                opts.IncludeQueryInRequestPath = true;
+                opts.GetLevel = ExcludeHealthChecks;
+                opts.EnrichDiagnosticContext = (diagCtx, httpCtx) =>
+                {
+                    diagCtx.Set("User", httpCtx.User.Identity?.Name ?? string.Empty);
+                    diagCtx.Set("Host", httpCtx.Request.Host.Value);
+                    diagCtx.Set("ContentLength", httpCtx.Response.ContentLength?.ToString(CultureInfo.InvariantCulture) ?? string.Empty);
+                    diagCtx.Set("Protocol", httpCtx.Request.Protocol);
+                    diagCtx.Set("Scheme", httpCtx.Request.Scheme);
+                };
+            });
+
+        return app;
+    }
+
+    private static LoggerConfiguration ConfigureDefaultLogging(this LoggerConfiguration loggerConfiguration, IConfiguration configuration, IServiceProvider services, string serviceName)
+    {
+        loggerConfiguration
+            .ReadFrom.Configuration(configuration)
+            .ReadFrom.Services(services)
+            .Enrich.WithMachineName()
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithProperty("service", serviceName)
+            .Enrich.WithEnvironmentName()
+            .Enrich.WithEnvironmentUserName()
+            .Enrich.WithCorrelationId()
+            .Enrich.WithCorrelationIdHeader()
+            .Enrich.WithClientAgent()
+            .Enrich.WithClientIp()
+            .Enrich.WithSpan(new SpanOptions() { IncludeBaggage = true, IncludeTags = true, IncludeOperationName = true, IncludeTraceFlags = true })
+            .WriteTo.Console(outputTemplate: LogOutputTemplate, formatProvider: CultureInfo.InvariantCulture)
+            ;
+
+        return loggerConfiguration;
+    }
+
+    private static LogEventLevel ExcludeHealthChecks(HttpContext ctx, double _, Exception? ex)
+    {
+        if (ex != null || ctx.Response.StatusCode >= (int)HttpStatusCode.InternalServerError)
+        {
+            return LogEventLevel.Error;
+        }
+
+        return ctx.Request.Path.StartsWithSegments("/hc", StringComparison.InvariantCultureIgnoreCase)
+            ? LogEventLevel.Verbose
+            : LogEventLevel.Information;
+    }
+}
