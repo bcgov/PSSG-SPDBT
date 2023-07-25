81facbc67928bf0a04d752610db6fff59fc8d9d3
Peggy <peggy.zhang@quartech.com>
2023-07-25 13:50:56 -0700

    Secure payment link error processing (#340)
    * for secure payment link ui
    
    * Add frontend support for payment link success/fail
    
    ---------
    
    Co-authored-by: Carol <carol.carpenter@quartech.com>

--- src/Spd.Presentation.Screening/ClientApp/src/app/modules/crrp-portal/components/crrp-payment-fail.component.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/modules/crrp-portal/components/crrp-payment-fail.component.ts
@@ -4,6 +4,7 @@ import { switchMap } from 'rxjs/operators';
 import { PaymentLinkCreateRequest, PaymentLinkResponse, PaymentMethodCode, PaymentResponse } from 'src/app/api/models';
 import { PaymentService } from 'src/app/api/services';
 import { AppRoutes } from 'src/app/app-routing.module';
+import { SPD_CONSTANTS } from 'src/app/core/constants/constants';
 import { AuthUserService } from 'src/app/core/services/auth-user.service';
 import { CrrpRoutes } from '../crrp-routing.module';
 
@@ -12,7 +13,7 @@ import { CrrpRoutes } from '../crrp-routing.module';
 	template: `
 		<app-payment-fail
 			[payment]="payment"
-			[numberOfAttempts]="numberOfAttempts"
+			[numberOfAttemptsRemaining]="numberOfAttemptsRemaining"
 			(backRoute)="onBackRoute()"
 			(payNow)="onPayNow()"
 			(downloadManualPaymentForm)="onDownloadManualPaymentForm()"
@@ -21,7 +22,7 @@ import { CrrpRoutes } from '../crrp-routing.module';
 	styles: [],
 })
 export class CrrpPaymentFailComponent implements OnInit {
-	numberOfAttempts: number = 0;
+	numberOfAttemptsRemaining: number = 0;
 	payment: PaymentResponse | null = null;
 
 	constructor(
@@ -58,7 +59,8 @@ export class CrrpPaymentFailComponent implements OnInit {
 				})
 			)
 			.subscribe((numberOfFails) => {
-				this.numberOfAttempts = numberOfFails;
+				const remaining = SPD_CONSTANTS.payment.maxNumberOfAttempts - numberOfFails;
+				this.numberOfAttemptsRemaining = remaining <= 0 ? 0 : remaining;
 			});
 	}
 
