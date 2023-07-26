5fccff14ebfd6765e2fff1a985b48986f1c38940
Peggy <peggy.zhang@quartech.com>
2023-07-26 09:48:46 -0700

    be: idir authentication and authorization (#343)
    * idir
    
    * Idir authentication

---
+++ src/Spd.Utilities.LogonUser/Configurations/IdirAuthenticationConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spd.Utilities.LogonUser.Configurations
+{
+    public class IdirAuthenticationConfiguration
+    {
+        [Required]
+        public string Authority { get; set; }
+
+        [Required]
+        public string Issuer { get; set; }
+
+        [Required]
+        public string Audiences { get; set; }
+
+        public const string Name = "IdirAuthentication";
+
+        public const string AuthSchemeName = "Idir";
+        public string ResponseType { get; set; } = "code";
+        public string Scope { get; set; } = "openid profile email offline_access";
+        public string ClientId { get; set; } = null!;
+        public string PostLogoutRedirectUri { get; set; } = null;
+
+    }
+}
