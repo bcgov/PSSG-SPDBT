ba05d14073d71dd2e866ea4895c6edc04d06d917
Carol <carol.carpenter@quartech.com>
2023-08-02 13:49:09 -0700

    SPDBT-967 - update common login page

--- src/Spd.Presentation.Screening/ClientApp/src/app/landing.component.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/landing.component.ts
@@ -9,15 +9,15 @@ import { SecurityScreeningRoutes } from './modules/security-screening-portal/sec
 	selector: 'app-landing',
 	template: `
 		<section class="step-section col-xxl-4 col-xl-4 col-lg-5 col-md-6 col-sm-12 mx-auto mt-4 p-4">
-			<h1>Criminal Record Check Portal</h1>
-			<p class="lead">Submit and manage your organization's criminal record checks</p>
+			<h1>Organization Online Access</h1>
+			<p class="mt-4 lead">Submit and manage criminal record checks for your employees or volunteers</p>
 
 			<mat-divider class="my-4"></mat-divider>
 
-			<div>Log in with:</div>
+			<div>Select your login method:</div>
 			<button mat-flat-button color="primary" class="large my-2" (click)="goToCrrp()">Business BCeID Account</button>
 			<button mat-stroked-button color="primary" class="large my-2" (click)="goToPsso()">IDIR Account</button>
-			<p class="mt-4 mb-0">Need access to the Criminal Records Review Program?</p>
+			<p class="mt-4 mb-0">Need to register an account with the Criminal Records Review Program?</p>
 			<a (click)="goToOrgRegistration()"> Register now </a>
 		</section>
 
