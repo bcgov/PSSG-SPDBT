761f3332cac90948c9481af2955057e6f5bd4d62
Peggy <peggy.zhang@quartech.com>
2023-08-16 11:10:55 -0700

    invoice payment service -create invoice (#389)
    * change paybc setting structure
    
    * token provider
    
    * payment-invoice service

---
+++ src/Spd.Utilities.Payment/TokenProviderResolver.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spd.Utilities.Payment.TokenProviders;
+using System;
+
+namespace Spd.Utilities.Payment;
+internal interface ITokenProviderResolver
+{
+    ISecurityTokenProvider GetTokenProviderByName(string name);
+}
+
+internal class TokenProviderResolver : ITokenProviderResolver
+{
+    private readonly IServiceProvider _serviceProvider;
+    public TokenProviderResolver(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    public ISecurityTokenProvider GetTokenProviderByName(string name)
+    {
+        if (name == "BasicTokenProvider")
+            return _serviceProvider.GetService<BasicSecurityTokenProvider>();
+        else
+            return _serviceProvider.GetService<BearerSecurityTokenProvider>();
+    }
+
+}
