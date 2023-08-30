42e2beab038fe474fc075c93f806e2770c412e59
Carol <carol.carpenter@quartech.com>
2023-08-30 12:09:22 -0700

    SPDBT-1500 - secure link payment fail text

--- src/Spd.Presentation.Screening/ClientApp/src/app/shared/components/payment-fail.component.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/shared/components/payment-fail.component.ts
@@ -50,9 +50,9 @@ import { AppRoutes } from 'src/app/app-routing.module';
 		<div class="row mx-4">
 			<div class="col-12 mt-4">
 				<div class="fw-normal fs-3 text-center">
-					<span *ngIf="isCancelledPaymentFlow; else paymentFailedSubHeader"
-						>Your payment attempt has been cancelled</span
-					>
+					<span *ngIf="isCancelledPaymentFlow; else paymentFailedSubHeader">
+						Your payment attempt has been cancelled
+					</span>
 					<ng-template #paymentFailedSubHeader>
 						Your payment transaction has failed for<br />
 						Case ID: {{ payment?.caseNumber }}
@@ -69,25 +69,15 @@ import { AppRoutes } from 'src/app/app-routing.module';
 			</ng-container>
 
 			<ng-template #paymentFailed>
-				<ng-container *ngIf="numberOfAttemptsRemaining == 0; else remainingAttempts">
+				<ng-container *ngIf="isPayBySecureLink; else notSecureLink">
 					<div class="offset-lg-3 col-lg-6 offset-md-2 col-md-8 col-sm-12">
 						<div class="lead fs-5 mt-4">
-							Your application has been submitted, but it won't be processed until payment is received.
-						</div>
-						<div class="lead fs-5 my-4">
-							Please download and complete the
-							<a (click)="onDownloadManualPaymentForm()">Manual Payment Form</a> then follow the instructions on the
-							form to submit payment to the Security Programs Division.
-						</div>
-					</div>
-				</ng-container>
-
-				<ng-template #remainingAttempts>
-					<div class="offset-lg-3 col-lg-6 offset-md-2 col-md-8 col-sm-12">
-						<div class="lead fs-5 mt-4">
-							Please ensure the information you entered is correct and try again, or use a different credit card. You
-							have
-							{{ numberOfAttemptsRemaining }} more attempt{{ numberOfAttemptsRemaining == 1 ? '' : 's' }}.
+							Contact the Security Programs Division to request a new secure payment link:
+							<ul>
+								<li>include your Case ID</li>
+								<li>email: <strong>spdscreening@gov.bc.ca</strong></li>
+								<li>toll-free: <strong>1-855-587-0185</strong> (press option 2)</li>
+							</ul>
 						</div>
 						<div class="lead fs-5 my-4">
 							Alternatively, you can download the
@@ -95,6 +85,36 @@ import { AppRoutes } from 'src/app/app-routing.module';
 							instructions to submit it to the Security Programs Division.
 						</div>
 					</div>
+				</ng-container>
+
+				<ng-template #notSecureLink>
+					<ng-container *ngIf="numberOfAttemptsRemaining == 0; else remainingAttempts">
+						<div class="offset-lg-3 col-lg-6 offset-md-2 col-md-8 col-sm-12">
+							<div class="lead fs-5 mt-4">
+								Your application has been submitted, but it won't be processed until payment is received.
+							</div>
+							<div class="lead fs-5 my-4">
+								Please download and complete the
+								<a (click)="onDownloadManualPaymentForm()">Manual Payment Form</a> then follow the instructions on the
+								form to submit payment to the Security Programs Division.
+							</div>
+						</div>
+					</ng-container>
+
+					<ng-template #remainingAttempts>
+						<div class="offset-lg-3 col-lg-6 offset-md-2 col-md-8 col-sm-12">
+							<div class="lead fs-5 mt-4">
+								Please ensure the information you entered is correct and try again, or use a different credit card. You
+								have
+								{{ numberOfAttemptsRemaining }} more attempt{{ numberOfAttemptsRemaining == 1 ? '' : 's' }}.
+							</div>
+							<div class="lead fs-5 my-4">
+								Alternatively, you can download the
+								<a (click)="onDownloadManualPaymentForm()">Manual Payment Form</a>. Fill it out, and follow the
+								instructions to submit it to the Security Programs Division.
+							</div>
+						</div>
+					</ng-template>
 				</ng-template>
 			</ng-template>
 		</div>
@@ -109,8 +129,8 @@ import { AppRoutes } from 'src/app/app-routing.module';
 })
 export class PaymentFailComponent implements OnInit {
 	isBackRoute: boolean = false;
-	payBySecureLink = true;
 
+	@Input() isPayBySecureLink = false;
 	@Input() isCancelledPaymentFlow = false;
 
 	private _payment: PaymentResponse | null = null;
