f9bd5a8ed68abbc90333f3704528885153b9ca0e
Peggy <peggy.zhang@quartech.com>
2023-09-12 17:29:24 -0700

    delete delegate (#437)

--- src/Spd.Manager.Cases/Application/ApplicationManager.Delegate.cs
+++ src/Spd.Manager.Cases/Application/ApplicationManager.Delegate.cs
@@ -1,3 +1,4 @@
+using MediatR;
 using Spd.Resource.Applicants.Delegates;
 using Spd.Resource.Applicants.PortalUser;
 using Spd.Utilities.Shared;
@@ -23,7 +24,7 @@ namespace Spd.Manager.Cases.Application
             //if already has an user.
             Guid? userId = null;
             PortalUserListResp userList = await _portalUserRepository.QueryAsync(
-                new PortalUserQry() { OrganizationId= SpdConstants.BC_GOV_ORG_ID, UserEmail = command.CreateRequest.EmailAddress },
+                new PortalUserQry() { OrganizationId = SpdConstants.BC_GOV_ORG_ID, UserEmail = command.CreateRequest.EmailAddress },
                 ct
                 );
             if (userList.Items.Any())
@@ -32,7 +33,7 @@ namespace Spd.Manager.Cases.Application
             }
 
             //check if existing
-            DelegateListResp delegateList = null;
+            DelegateListResp? delegateList = null;
             if (userId != null)
             {
                 delegateList = (DelegateListResp)await _delegateRepository.QueryAsync(
@@ -56,6 +57,40 @@ namespace Spd.Manager.Cases.Application
             cmd.ApplicationId = command.ApplicationId;
             var resp = await _delegateRepository.ManageAsync(cmd, ct);
             return _mapper.Map<DelegateResponse>(resp);
+        }
+
+        public async Task<Unit> Handle(DeleteDelegateCommand command, CancellationToken ct)
+        {
+            //do biz checking if the login person can delete the delegate
+            bool canDoDelete = false;
+            var delegateList = await _delegateRepository.QueryAsync(
+                new DelegateQry(command.ApplicationId), ct);
+            if (delegateList.Items.Count() <= 1)
+            {
+                throw new ApiException(System.Net.HttpStatusCode.BadRequest, "Only 1 delegate left, cannot delete the delegate.");
+            }
+
+            if (command.IsPSA)
+                canDoDelete = true;
+            else
+            {
+                var d = delegateList.Items.FirstOrDefault(d => d.PortalUserId == command.CurrentUserId);
+                if (d == null)
+                    throw new ApiException(System.Net.HttpStatusCode.BadRequest, "None PSA and not connected to the application, cannot remove delegate.");
+                if (d.Id == command.Id) //can delete his own 
+                    canDoDelete = true;
+                else if (d.PSSOUserRoleCode == PSSOUserRoleEnum.Initiator) 
+                    canDoDelete = true; //initiator can delete others.
+                else 
+                    throw new ApiException(System.Net.HttpStatusCode.BadRequest, "Non initiator cannot delete other delegate.");
+            }
+
+            if (canDoDelete)
+            {
+                DeleteDelegateCmd cmd = new DeleteDelegateCmd(command.Id);
+                await _delegateRepository.ManageAsync(cmd, ct);
+            }
+            return default;
         }
     }
 }
