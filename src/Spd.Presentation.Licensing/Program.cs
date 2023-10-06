a91ce7d30149106f225057c28431674a6d58b46c
peggy <peggy.zhang@quartech.com>
2023-10-06 14:23:02 -0700

    make dockerfile working

--- src/Spd.Presentation.Licensing/Program.cs
+++ src/Spd.Presentation.Licensing/Program.cs
@@ -17,9 +17,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapFallbackToFile("index.html");
 
 app.Run();
