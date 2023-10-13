139cd1b504e7c3c2e339c3f03ea9ff3c11b8cf3a
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2023-10-13 16:26:24 -0700

    Carol/licensing p4 (#503)
    * wip
    
    * wip
    
    * wip
    
    * wip

--- src/Spd.Presentation.Licensing/ClientApp/src/app/shared/pipes/options.pipe.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/shared/pipes/options.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe } from '@angular/core';
+import { BooleanTypeCode } from 'src/app/api/models';
 import * as CodeDescTypes from 'src/app/core/code-types/code-desc-types.models';
 import { UtilService } from 'src/app/core/services/util.service';
 
@@ -7,10 +8,13 @@ export class OptionsPipe {
 	constructor(private utilService: UtilService) {}
 
 	transform(
-		input: string | null | undefined,
+		input: string | boolean | null | undefined,
 		codeTableName: keyof typeof CodeDescTypes,
 		defaultValue: string = ''
 	): string {
+		if (typeof input == 'boolean') {
+			return input ? BooleanTypeCode.Yes : BooleanTypeCode.No;
+		}
 		return input ? this.utilService.getDescByCode(codeTableName, input) : defaultValue;
 	}
 }
