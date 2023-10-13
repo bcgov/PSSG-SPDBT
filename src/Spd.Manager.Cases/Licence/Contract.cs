182a956b9a32084077de8e2066331841ab276923
Peggy <peggy.zhang@quartech.com>
2023-10-13 12:00:42 -0700

    license bcsc (#500)
    * check
    
    * add basic
    
    * cleanup
    
    * fix typo

---
+++ src/Spd.Manager.Cases/Licence/Contract.cs
@@ -0,0 +1,13 @@
+namespace Spd.Manager.Cases.Licence
+{
+    public interface ILicenceManager
+    {
+
+    }
+
+    public class WorkerLicenceCreateRequest
+    { }
+
+    public class WorkerLicenceCreateResponse
+    { }
+}
