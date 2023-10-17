8f30b72affe7a5c31442bb67e6f571b5f693eadc
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2023-10-17 16:48:40 -0700

    Licensing frontend updates (#515)
    * wip
    
    * wip
    
    * wip
    
    * wip

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/step-components/licence-category-electronic-locking-device-installer.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/step-components/licence-category-electronic-locking-device-installer.component.ts
@@ -1,56 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { SwlCategoryTypeCode } from 'src/app/core/code-types/model-desc.models';
 import { OptionsPipe } from 'src/app/shared/pipes/options.pipe';
-import { LicenceFormStepComponent } from '../licence-application.service';
+import { LicenceApplicationService, LicenceFormStepComponent } from '../licence-application.service';
 
 @Component({
 	selector: 'app-licence-category-electronic-locking-device-installer',
 	template: `
-		<section class="step-section p-3">
-			<div class="step">
-				<div class="step-container">
-					<div class="row">
-						<div class="offset-xxl-2 col-xxl-8 offset-xl-1 col-xl-9 col-lg-12">
-							<div class="text-center">
-								<mat-chip-option [selectable]="false" class="mat-chip-green me-3">
-									Category #{{ index }}
-								</mat-chip-option>
-								<span class="title" style="position: relative; top: -5px;">{{ title }}</span>
-							</div>
-
-							<mat-divider class="mt-1 mb-2"></mat-divider>
-
-							<form [formGroup]="form" class="text-center my-4" novalidate>
-								<mat-checkbox class="w-auto" formControlName="checkbox">
-									Electronic Locking Device Installer
-								</mat-checkbox>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<form [formGroup]="form" class="text-center my-4" novalidate>
+			<mat-checkbox class="w-auto" formControlName="checkbox">
+				{{ title }}
+			</mat-checkbox>
+		</form>
 	`,
 	styles: [],
 })
 export class LicenceCategoryElectronicLockingDeviceInstallerComponent implements OnInit, LicenceFormStepComponent {
-	form!: FormGroup;
+	form = this.licenceApplicationService.categoryElectronicLockingDeviceInstallerFormGroup;
 	title = '';
 
-	@Input() option: string | null = null;
-	@Input() index: number = 0;
-
-	constructor(private formBuilder: FormBuilder, private optionsPipe: OptionsPipe) {}
+	constructor(private optionsPipe: OptionsPipe, private licenceApplicationService: LicenceApplicationService) {}
 
 	ngOnInit(): void {
-		this.form = this.formBuilder.group({
-			checkbox: new FormControl({ value: true, disabled: true }),
-		});
-
-		this.title = this.optionsPipe.transform(this.option, 'SwlCategoryTypes');
+		this.form.patchValue({ checkbox: true });
+		this.title = this.optionsPipe.transform(SwlCategoryTypeCode.ElectronicLockingDeviceInstaller, 'SwlCategoryTypes');
 	}
 
 	isFormValid(): boolean {
-		return true;
+		return this.form.valid;
 	}
 }
