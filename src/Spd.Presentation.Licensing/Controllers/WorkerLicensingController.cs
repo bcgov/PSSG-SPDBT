6dce1fc011d25f792da305f12540d37511853b23
Carol <carol.carpenter@quartech.com>
2023-10-24 10:14:30 -0700

    fix

--- src/Spd.Presentation.Licensing/Controllers/WorkerLicensingController.cs
+++ src/Spd.Presentation.Licensing/Controllers/WorkerLicensingController.cs
@@ -1,10 +1,12 @@
 
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Spd.Manager.Cases.Licence;
 using Spd.Utilities.LogonUser;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Principal;
+
 
 namespace Spd.Presentation.Licensing.Controllers
 {
