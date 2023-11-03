0085673ccc5042d849b95a0381597e3036620e6c
Peggy <peggy.zhang@quartech.com>
2023-11-03 12:09:16 -0700

    swl-partial save doc expirydate (#547)
    * fix dob
    
    * remove wrong code
    
    * create contact and alias
    
    * contract
    
    * fix
    
    * read files
    
    * make authenticated worker licence working
    
    * add doc expiry date

--- src/Spd.Manager.Cases/Licence/LicenceManager.Document.cs
+++ src/Spd.Manager.Cases/Licence/LicenceManager.Document.cs
@@ -1,4 +1,4 @@
-﻿using Spd.Resource.Applicants.Application;
+using Spd.Resource.Applicants.Application;
 using Spd.Resource.Applicants.Document;
 using Spd.Resource.Applicants.LicenceApplication;
 using Spd.Utilities.TempFileStorage;
@@ -42,6 +42,140 @@ internal partial class LicenceManager
         return _mapper.Map<IEnumerable<LicenceAppDocumentResponse>>(docResps);
     }
 
+    private async Task UpdateDocumentsExpiryDate(WorkerLicenceAppUpsertRequest request, CancellationToken ct)
+    {
+        if (request.BornInCanadaDocument != null)
+        {
+            foreach (LicenceAppDocumentResponse doc in request.BornInCanadaDocument.DocumentResponses)
+            {
+                await _documentRepository.ManageAsync(new UpdateDocumentExpiryDateCmd(doc.DocumentUrlId, request.BornInCanadaDocument.ExpiryDate), ct);
+            }
+        }
+        if (request.AdditionalGovIdDocument != null)
+        {
+            foreach (LicenceAppDocumentResponse doc in request.AdditionalGovIdDocument.DocumentResponses)
+            {
+                await _documentRepository.ManageAsync(new UpdateDocumentExpiryDateCmd(doc.DocumentUrlId, request.AdditionalGovIdDocument.ExpiryDate), ct);
+            }
+        }
+    }
+
+    private async Task RemoveDeletedDocumentsAsync(WorkerLicenceAppUpsertRequest request, CancellationToken ct)
+    {
+        if (request.LicenceAppId == null) { return; }
+        var existingDocs = await _documentRepository.QueryAsync(new DocumentQry(request.LicenceAppId), ct);
+        List<Guid> allValidDocGuids = new List<Guid>();
+        if (request.AdditionalGovIdDocument?.DocumentResponses.Count > 0)
+        {
+            allValidDocGuids.AddRange(request.AdditionalGovIdDocument.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+        }
+        if (request.BornInCanadaDocument?.DocumentResponses.Count > 0)
+        {
+            allValidDocGuids.AddRange(request.BornInCanadaDocument.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+        }
+        if (request.FingerPrintProofDocument?.DocumentResponses.Count > 0)
+        {
+            allValidDocGuids.AddRange(request.FingerPrintProofDocument.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+        }
+        if (request.IdPhotoDocument?.DocumentResponses.Count > 0)
+        {
+            allValidDocGuids.AddRange(request.IdPhotoDocument.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+        }
+        if (request.MentalHealthDocument?.DocumentResponses.Count > 0)
+        {
+            allValidDocGuids.AddRange(request.MentalHealthDocument.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+        }
+        if (request.PoliceOfficerDocument?.DocumentResponses.Count > 0)
+        {
+            allValidDocGuids.AddRange(request.PoliceOfficerDocument.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+        }
+        foreach (WorkerLicenceAppCategoryData category in request.CategoryData)
+        {
+            foreach (Document d in category.Documents)
+            {
+                allValidDocGuids.AddRange(d.DocumentResponses.Select(d => d.DocumentUrlId).ToArray());
+            }
+        }
+        var shouldDeleteDocs = existingDocs.Items.Where(i => !allValidDocGuids.Contains(i.DocumentUrlId)).ToList();
+        foreach (DocumentResp doc in shouldDeleteDocs)
+        {
+            await _documentRepository.ManageAsync(new RemoveDocumentCmd(doc.DocumentUrlId), ct);
+        }
+    }
+
+    private async Task GetDocumentsAsync(Guid LicenceAppId, WorkerLicenceResponse result, CancellationToken ct)
+    {
+        var existingDocs = await _documentRepository.QueryAsync(new DocumentQry(LicenceAppId), ct);
+        var letterOfNoConflicts = existingDocs.Items.Where(d => d.DocumentType == DocumentTypeEnum.LetterOfNoConflict).ToList();
+        if (letterOfNoConflicts.Any())
+        {
+            result.PoliceOfficerDocument = new PoliceOfficerDocument
+            {
+                LicenceDocumentTypeCode = LicenceDocumentTypeCode.PoliceBackgroundLetterOfNoConflict,
+                DocumentResponses = _mapper.Map<List<LicenceAppDocumentResponse>>(letterOfNoConflicts)
+            };
+        }
+        var mentalHealths = existingDocs.Items.Where(d => d.DocumentType == DocumentTypeEnum.MentalHealthConditionForm).ToList();
+        if (mentalHealths.Any())
+        {
+            result.MentalHealthDocument = new MentalHealthDocument
+            {
+                LicenceDocumentTypeCode = LicenceDocumentTypeCode.MentalHealthCondition,
+                DocumentResponses = _mapper.Map<List<LicenceAppDocumentResponse>>(mentalHealths)
+            };
+        }
+        var fingerprints = existingDocs.Items.Where(d => d.DocumentType == DocumentTypeEnum.ConfirmationOfFingerprints).ToList();
+        if (fingerprints.Any())
+        {
+            result.FingerPrintProofDocument = new FingerprintProofDocument
+            {
+                LicenceDocumentTypeCode = LicenceDocumentTypeCode.ProofOfFingerprint,
+                DocumentResponses = _mapper.Map<List<LicenceAppDocumentResponse>>(fingerprints)
+            };
+        }
+
+        var bornInCanadas = existingDocs.Items.Where(d =>
+            d.DocumentType == DocumentTypeEnum.BirthCertificate ||
+            d.DocumentType == DocumentTypeEnum.Passport ||
+            d.DocumentType == DocumentTypeEnum.CanadianNativeStatusCard).ToList();
+        //todo: add more documentType condition here when we get the DocumentTypeEnum sorted out.
+        if (bornInCanadas.Any())
+        {
+            result.BornInCanadaDocument = new BornInCanadaDocument
+            {
+                LicenceDocumentTypeCode = GetlicenceDocumentTypeCode((DocumentTypeEnum)bornInCanadas[0].DocumentType),
+                DocumentResponses = _mapper.Map<List<LicenceAppDocumentResponse>>(bornInCanadas),
+                ExpiryDate = bornInCanadas[0].ExpiryDate
+            };
+        }
+
+        var govIdDocs = existingDocs.Items.Where(d =>
+            d.DocumentType == DocumentTypeEnum.DriverLicense ||
+            d.DocumentType == DocumentTypeEnum.CanadianFirearmsLicense ||
+            d.DocumentType == DocumentTypeEnum.BCServicesCard ||
+            d.DocumentType == DocumentTypeEnum.CanadianNativeStatusCard ||
+            d.DocumentType == DocumentTypeEnum.GovtIssuedPhotoID).ToList();
+        if (govIdDocs.Any())
+        {
+            result.AdditionalGovIdDocument = new AdditionalGovIdDocument
+            {
+                LicenceDocumentTypeCode = GetlicenceDocumentTypeCode((DocumentTypeEnum)govIdDocs.First().DocumentType),
+                DocumentResponses = _mapper.Map<List<LicenceAppDocumentResponse>>(govIdDocs),
+                ExpiryDate = govIdDocs[0].ExpiryDate
+            };
+        }
+
+        var idPhoto = existingDocs.Items.Where(d => d.DocumentType == DocumentTypeEnum.Photograph).ToList();
+        if (idPhoto.Any())
+        {
+            result.IdPhotoDocument = new IdPhotoDocument
+            {
+                LicenceDocumentTypeCode = LicenceDocumentTypeCode.PhotoOfYourself,
+                DocumentResponses = _mapper.Map<List<LicenceAppDocumentResponse>>(idPhoto)
+            };
+        }
+    }
+
     private DocumentTypeEnum GetDocumentTypeEnum(LicenceDocumentTypeCode licenceDocumentTypeCode)
     {
         var keyExisted = LicenceDocumentDictionary.TryGetValue(licenceDocumentTypeCode, out DocumentTypeEnum docTypeEnum);
@@ -68,7 +202,7 @@ internal partial class LicenceManager
         {LicenceDocumentTypeCode.MentalHealthCondition, DocumentTypeEnum.MentalHealthConditionForm},
         {LicenceDocumentTypeCode.PermanentResidentCard, DocumentTypeEnum.PermanentResidenceCard},
         {LicenceDocumentTypeCode.PoliceBackgroundLetterOfNoConflict, DocumentTypeEnum.LetterOfNoConflict},
-        {LicenceDocumentTypeCode.ProofOfFingerprint, DocumentTypeEnum.FingerprintsPkg},//todo: find which to map
+        {LicenceDocumentTypeCode.ProofOfFingerprint, DocumentTypeEnum.ConfirmationOfFingerprints},
         //{LicenceDocumentTypeCode.RecordOfLandingDocument, DocumentTypeEnum.}, //todo: find which to map
         //{LicenceDocumentTypeCode.ConfirmationOfPermanentResidenceDocument, DocumentTypeEnum.c}, //todo: find which to map
         //{LicenceDocumentTypeCode.WorkPermit, DocumentTypeEnum.LegalWorkStatus}, //todo: find which to map
