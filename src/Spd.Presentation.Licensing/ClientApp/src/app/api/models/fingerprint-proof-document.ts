3056e2b900700306a7fa38bf124f993a81959ad8
Carol <carol.carpenter@quartech.com>
2023-11-10 07:07:50 -0800

    regen

--- src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/fingerprint-proof-document.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/fingerprint-proof-document.ts
@@ -4,5 +4,6 @@ import { LicenceAppDocumentResponse } from './licence-app-document-response';
 import { LicenceDocumentTypeCode } from './licence-document-type-code';
 export interface FingerprintProofDocument {
   documentResponses?: null | Array<LicenceAppDocumentResponse>;
+  expiryDate?: null | string;
   licenceDocumentTypeCode?: LicenceDocumentTypeCode;
 }
--- src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/id-photo-document.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/id-photo-document.ts
@@ -4,5 +4,6 @@ import { LicenceAppDocumentResponse } from './licence-app-document-response';
 import { LicenceDocumentTypeCode } from './licence-document-type-code';
 export interface IdPhotoDocument {
   documentResponses?: null | Array<LicenceAppDocumentResponse>;
+  expiryDate?: null | string;
   licenceDocumentTypeCode?: LicenceDocumentTypeCode;
 }
