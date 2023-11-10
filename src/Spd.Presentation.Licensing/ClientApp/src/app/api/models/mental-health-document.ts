26e82cdb14365da434b1a041da001a8ea4a4b2d3
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2023-11-10 08:48:16 -0800

    Licensing updates (#560)
    * regen
    
    * file upload fix
    
    * fix bugs

--- src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/mental-health-document.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/mental-health-document.ts
@@ -4,5 +4,6 @@ import { LicenceAppDocumentResponse } from './licence-app-document-response';
 import { LicenceDocumentTypeCode } from './licence-document-type-code';
 export interface MentalHealthDocument {
   documentResponses?: null | Array<LicenceAppDocumentResponse>;
+  expiryDate?: null | string;
   licenceDocumentTypeCode?: LicenceDocumentTypeCode;
 }
