36d2c62c3c3ec698db0c9c41a09b3a63e6733437
Peggy <peggy.zhang@quartech.com>
2023-11-29 07:03:36 -0800

    fix applicant portal (#601)

--- src/Spd.Resource.Applicants/Application/ApplicationRepository.cs
+++ src/Spd.Resource.Applicants/Application/ApplicationRepository.cs
@@ -1,16 +1,21 @@
 using AutoMapper;
+using Microsoft.Extensions.Logging;
 using Spd.Utilities.Dynamics;
+using Spd.Utilities.Shared.ResourceContracts;
 
 namespace Spd.Resource.Applicants.Application;
 internal partial class ApplicationRepository : IApplicationRepository
 {
     private readonly DynamicsContext _context;
     private readonly IMapper _mapper;
+    private readonly ILogger<IApplicationRepository> _logger;
+    private readonly List<ServiceTypeEnum> ScreeningServiceTypes = new List<ServiceTypeEnum>() { ServiceTypeEnum.PSSO, ServiceTypeEnum.CRRP_EMPLOYEE, ServiceTypeEnum.CRRP_VOLUNTEER };
 
-    public ApplicationRepository(IDynamicsContextFactory ctx, IMapper mapper)
+    public ApplicationRepository(IDynamicsContextFactory ctx, IMapper mapper, ILogger<IApplicationRepository> logger)
     {
         _context = ctx.CreateChangeOverwrite();
         _mapper = mapper;
+        _logger = logger;
     }
 }
 
