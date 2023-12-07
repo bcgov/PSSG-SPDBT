2dedf81b29b41180410b47ee59a78a07f6dab751
Peggy <peggy.zhang@quartech.com>
2023-12-06 17:01:35 -0800

    swl anonymous create - multiple files upload with multipart streaming (#615)
    * upload files
    
    * make streaming upload multiple files working
    
    * cleanup

---
+++ src/Spd.Presentation.Licensing/Services/MultipartRequestHelper.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Net.Http.Headers;
+using System.Text;
+
+public static class MultipartRequestHelper
+{
+    // Content-Type: multipart/form-data boundary="----WebKitFormBoundarymx2fSWqWSd0OxQqq"
+    // The spec at https://tools.ietf.org/html/rfc2046#section-5.1 states that 70 characters is a reasonable limit.
+    public static string GetBoundary(MediaTypeHeaderValue contentType, int lengthLimit)
+    {
+        var boundary = HeaderUtilities.RemoveQuotes(contentType.Boundary).Value;
+
+        if (string.IsNullOrWhiteSpace(boundary))
+        {
+            throw new InvalidDataException("Missing content-type boundary.");
+        }
+
+        if (boundary.Length > lengthLimit)
+        {
+            throw new InvalidDataException($"Multipart boundary length limit {lengthLimit} exceeded.");
+        }
+
+        return boundary;
+    }
+
+    public static bool IsMultipartContentType(string contentType)
+    {
+        return !string.IsNullOrEmpty(contentType)
+            && contentType.Contains("multipart/", StringComparison.OrdinalIgnoreCase);
+    }
+
+    public static bool HasFormDataContentDisposition(ContentDispositionHeaderValue contentDisposition)
+    {
+        // Content-Disposition: form-data name="key"
+        return contentDisposition != null
+            && contentDisposition.DispositionType.Equals("form-data")
+            && string.IsNullOrEmpty(contentDisposition.FileName.Value)
+            && string.IsNullOrEmpty(contentDisposition.FileNameStar.Value);
+    }
+
+    public static bool HasFileContentDisposition(ContentDispositionHeaderValue contentDisposition)
+    {
+        // Content-Disposition: form-data; name="myfile1" filename="Misc 002.jpg"
+        return contentDisposition != null
+            && contentDisposition.DispositionType.Equals("form-data")
+            && (!string.IsNullOrEmpty(contentDisposition.FileName.Value)
+                || !string.IsNullOrEmpty(contentDisposition.FileNameStar.Value));
+    }
+
+    public static Encoding GetEncoding(MultipartSection section)
+    {
+        var hasMediaTypeHeader = MediaTypeHeaderValue.TryParse(section.ContentType, out MediaTypeHeaderValue mediaType);
+
+        if (!hasMediaTypeHeader)
+        {
+            return Encoding.UTF8;
+        }
+        return mediaType.Encoding;
+    }
+}
---
+++ src/Spd.Presentation.Licensing/Services/UploadFileInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Spd.Presentation.Licensing.Services;
+
+public class UploadFileInfo
+{
+    public string FileKey { get; set; }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+    public string FileExtension { get; set; }
+}
