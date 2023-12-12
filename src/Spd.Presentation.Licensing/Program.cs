9eaec8275b681ddcab0ab08d3143d12ae56cfdef
peggy <peggy.zhang@quartech.com>
2023-12-12 15:26:41 -0800

    add recaptcha ext

--- src/Spd.Presentation.Licensing/Program.cs
+++ src/Spd.Presentation.Licensing/Program.cs
@@ -8,6 +8,7 @@ using Spd.Utilities.Dynamics;
 using Spd.Utilities.FileStorage;
 using Spd.Utilities.Hosting;
 using Spd.Utilities.LogonUser;
+using Spd.Utilities.Recaptcha;
 using Spd.Utilities.TempFileStorage;
 using System.Reflection;
 using System.Security.Principal;
@@ -53,6 +54,7 @@ builder.Services.AddTempFileStorageService();
 builder.Services.AddTransient<IMultipartRequestService, MultipartRequestService>();
 builder.Services.AddFileStorageProxy(builder.Configuration);
 builder.Services
+  .AddGoogleRecaptcha(builder.Configuration)
   .AddBCeIDService(builder.Configuration)
   // .AddPaymentService(builder.Configuration)
   .AddDynamicsProxy(builder.Configuration)
