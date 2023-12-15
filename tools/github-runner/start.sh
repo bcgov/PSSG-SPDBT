7b0f73114b4464970cc8ac3e694261261a03591b
Yossi T <48697347+ytqsl@users.noreply.github.com>
2023-12-15 09:25:50 -0800

    SPDBT-1941: added github helper build and workflow (#632)

---
+++ tools/github-runner/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ORGANIZATION=$ORGANIZATION
+ACCESS_TOKEN=$ACCESS_TOKEN
+AGENT_NAME=$NAME
+
+cd /home/docker/actions-runner
+
+./config.sh --url https://github.com/${ORGANIZATION} --token ${ACCESS_TOKEN} --name ${AGENT_NAME} --unattended
+
+cleanup() {
+    echo "Removing runner..."
+    ./config.sh remove --token ${ACCESS_TOKEN}
+}
+
+trap 'cleanup; exit 130' INT
+trap 'cleanup; exit 143' TERM
+
+./run.sh & wait $!
