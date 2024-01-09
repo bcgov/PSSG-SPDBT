0747826b526240959d3f5e69ce9b9bd0d5593a90
Peggy <peggy.zhang@quartech.com>
2024-01-08 17:47:03 -0800

    unauth alias get and set (#666)
    # Description
    
    This PR includes the following proposed change(s):
    
    - for spdbt-1977(add alias type)

--- src/Spd.Resource.Applicants/LicenceApplication/Mappings.cs
+++ src/Spd.Resource.Applicants/LicenceApplication/Mappings.cs
@@ -139,6 +139,7 @@ internal class Mappings : Profile
           .ForMember(d => d.spd_surname, opt => opt.MapFrom(s => s.Surname))
           .ForMember(d => d.spd_middlename1, opt => opt.MapFrom(s => s.MiddleName1))
           .ForMember(d => d.spd_middlename2, opt => opt.MapFrom(s => s.MiddleName2))
+          .ForMember(d => d.spd_source, opt => opt.MapFrom(s => AliasSourceTypeOptionSet.UserEntered))
           .ReverseMap();
     }
 
