67e2ab833f8dd9497d2fffc5b4f82924b739726c
Peggy <peggy.zhang@quartech.com>
2024-01-09 16:40:07 -0800

    access code and payment fix (#671)
    # Description
    
    This PR includes the following proposed change(s):
    
    - access code and payment fix

--- src/Spd.Presentation.Licensing/Controllers/PaymentController.cs
+++ src/Spd.Presentation.Licensing/Controllers/PaymentController.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Spd.Manager.Common.ManagerContract;
 using Spd.Manager.Common.Payment;
