be4be1b74df1746eaef5e7ab7b8a6b6d3c0034d0
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-01-10 16:33:18 -0800

    SPDBT-101: BCMail+ POC (#654)
    # SPDBT-101
    
    This PR includes the following proposed change(s):
    
    - printing via BCMail+ POC code
    - better build management
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: Peggy <peggy.zhang@quartech.com>
    Co-authored-by: carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

---
+++ src/Spd.Utilities.Printing/BCMailPlus/BcMailPlusApi.cs
@@ -0,0 +1,56 @@
+﻿using System.Net.Mime;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Options;
+
+namespace Spd.Utilities.Printing.BCMailPlus;
+
+internal sealed class BcMailPlusApi : IBcMailPlusApi
+{
+    private static readonly HttpClient httpClient = new HttpClient();
+    private readonly string baseUri;
+
+    public BcMailPlusApi(IOptions<BCMailPlusSettings> options)
+    {
+        var settings = options.Value;
+        this.baseUri = $"{settings.ServerUrl}/auth={settings.User};{settings.Secret}/JSON";
+    }
+
+    public async Task<JobStatus> CreateJob(string jobClass, JsonDocument payload, CancellationToken ct)
+    {
+        using var content = new StringContent(payload.RootElement.ToString(), Encoding.UTF8, MediaTypeNames.Application.Json);
+        var uri = new Uri($"{this.baseUri}/create:{jobClass}");
+        var response = await httpClient.PostAsync(uri, content, ct);
+        response.EnsureSuccessStatusCode();
+
+        var str = await response.Content.ReadAsStringAsync(ct);
+        var jobStatusResponse = await JsonSerializer.DeserializeAsync<JobStatus>(await response.Content.ReadAsStreamAsync(ct), cancellationToken: ct);
+
+        return jobStatusResponse!;
+    }
+
+    public async Task<byte[]?> GetAsset(string jobId, string asset, CancellationToken ct)
+    {
+        using var content = new StringContent(JsonSerializer.Serialize(new { job = jobId, asset }), Encoding.UTF8, MediaTypeNames.Application.Json);
+
+        var uri = new Uri($"{this.baseUri}/asset");
+        var response = await httpClient.PostAsync(uri, content, ct);
+        response.EnsureSuccessStatusCode();
+
+        var assetResponse = await response.Content.ReadAsByteArrayAsync(ct);
+
+        return assetResponse;
+    }
+
+    public async Task<IEnumerable<JobStatus>> GetJobStatus(IEnumerable<string> jobs, CancellationToken ct)
+    {
+        using var content = new StringContent(JsonSerializer.Serialize(new { jobs }), Encoding.UTF8, MediaTypeNames.Application.Json);
+        var uri = new Uri($"{this.baseUri}/status");
+        var response = await httpClient.PostAsync(uri, content, ct);
+        response.EnsureSuccessStatusCode();
+
+        var jobStatusResponse = await JsonSerializer.DeserializeAsync<IEnumerable<JobStatus>>(await response.Content.ReadAsStreamAsync(ct), cancellationToken: ct);
+
+        return jobStatusResponse!;
+    }
+}
---
+++ src/Spd.Utilities.Printing/Contract.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+namespace Spd.Utilities.Printing;
+
+public interface IPrinter
+{
+    Task<PrintResponse> PrintAsync(PrintRequest request, CancellationToken ct);
+}
+
+public abstract record PrintRequest();
+
+public abstract record PrintResponse();
+
+public record BcMailPlusPrintRequest(string JobClass, IEnumerable<JsonDocument> Items) : PrintRequest;
+
+public record BcMailPlusPrintResponse(IEnumerable<byte[]> Items) : PrintResponse;
