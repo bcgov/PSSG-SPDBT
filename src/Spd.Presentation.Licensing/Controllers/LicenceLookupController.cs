22fb38e377cd68dca25d8e83d95585525571abb6
Peggy <peggy.zhang@quartech.com>
2024-01-17 14:34:31 -0800

    add google recaptcha for licence lookup in be (#686)
    # Description
    
    This PR includes the following proposed change(s):
    
    - add google recaptcha for licence lookup in be

--- src/Spd.Presentation.Licensing/Controllers/LicenceLookupController.cs
+++ src/Spd.Presentation.Licensing/Controllers/LicenceLookupController.cs
@@ -1,9 +1,12 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Spd.Manager.Licence;
+using Spd.Utilities.Recaptcha;
 using Spd.Utilities.Shared;
+using Spd.Utilities.Shared.Exceptions;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Principal;
+using System.Net;
 
 namespace Spd.Presentation.Licensing.Controllers
 {
@@ -11,20 +14,17 @@ namespace Spd.Presentation.Licensing.Controllers
     public class LicenceLookupController : SpdControllerBase
     {
         private readonly ILogger<LicenceLookupController> _logger;
-        private readonly IPrincipal _currentUser;
         private readonly IMediator _mediator;
-        private readonly IConfiguration _configuration;
+        private readonly IRecaptchaVerificationService _recaptchaVerificationService;
 
         public LicenceLookupController(
             ILogger<LicenceLookupController> logger,
-            IPrincipal currentUser,
             IMediator mediator,
-            IConfiguration configuration)
+            IRecaptchaVerificationService recaptchaVerificationService)
         {
             _logger = logger;
-            _currentUser = currentUser;
             _mediator = mediator;
-            _configuration = configuration;
+            _recaptchaVerificationService = recaptchaVerificationService;
         }
 
         /// <summary>
@@ -36,8 +36,31 @@ namespace Spd.Presentation.Licensing.Controllers
         /// <returns></returns>
         [Route("api/licence-lookup/{licenceNumber}")]
         [HttpGet]
+        [Authorize(Policy = "OnlyBcsc")]
         public async Task<LicenceLookupResponse> GetLicenceLookup([FromRoute][Required] string licenceNumber, [FromQuery] string accessCode = null)
         {
+            return await _mediator.Send(new LicenceLookupQuery(licenceNumber, accessCode));
+        }
+
+        /// <summary>
+        /// Get licence by licence number with google recaptcha for anonymous
+        /// Example: http://localhost:5114/api/licence-lookup/TEST-02?accessCode=TEST
+        /// </summary>
+        /// <param name="licenceNumber"></param>
+        /// <param name="accessCode"></param>
+        /// <returns></returns>
+        [Route("api/licence-lookup/anonymous/{licenceNumber}")]
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<LicenceLookupResponse> GetLicenceLookupAnonymously([FromRoute][Required] string licenceNumber, CancellationToken ct, [FromBody] GoogleRecaptcha recaptcha, [FromQuery] string accessCode = null)
+        {
+            _logger.LogInformation("do Google recaptcha verification");
+            var isValid = await _recaptchaVerificationService.VerifyAsync(recaptcha.RecaptchaCode, ct);
+            if (!isValid)
+            {
+                throw new ApiException(HttpStatusCode.BadRequest, "Invalid recaptcha value");
+            }
+
             return await _mediator.Send(new LicenceLookupQuery(licenceNumber, accessCode));
         }
     }
