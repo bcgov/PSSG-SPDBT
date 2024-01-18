a5e5f7e699ae327f77e992e24e6f84ad99a1184e
Carol <carol.carpenter@quartech.com>
2024-01-18 09:11:10 -0800

    remove unused licence portal

--- src/Spd.Presentation.Licensing/ClientApp/src/app/landing.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/landing.component.ts
@@ -1,15 +1,10 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { LicenceApplicationRoutes } from '@app/modules/licence-application/licence-application-routing.module';
-import { LicenceRoutes } from './modules/licence-portal/licence-routing.module';
 
 @Component({
 	selector: 'app-landing',
 	template: `
-		<!-- <section class="step-section col-xxl-4 col-xl-4 col-lg-5 col-md-6 col-sm-12 mx-auto my-4 p-4">
-			<button mat-stroked-button color="primary" class="large my-2" (click)="goToLicence()">Licence Portal</button>
-		</section> -->
-
 		<section class="step-section m-4 col-xxl-4 col-xl-4 col-lg-5 col-md-6 col-sm-12 mx-auto">
 			<button mat-stroked-button color="primary" class="large my-2" (click)="goToLicenceApplication()">
 				Licence Application
@@ -20,10 +15,6 @@ import { LicenceRoutes } from './modules/licence-portal/licence-routing.module';
 })
 export class LandingComponent {
 	constructor(private router: Router) {}
-
-	goToLicence(): void {
-		this.router.navigateByUrl(LicenceRoutes.path(LicenceRoutes.IN_PROGRESS_APPLICATIONS));
-	}
 
 	goToLicenceApplication(): void {
 		this.router.navigateByUrl(LicenceApplicationRoutes.path(LicenceApplicationRoutes.LOGIN_SELECTION));
