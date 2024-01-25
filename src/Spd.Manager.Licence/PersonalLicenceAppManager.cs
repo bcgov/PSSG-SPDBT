af9a561d09f6155ad127fb05ac3db22d53a3b722
peggy <peggy.zhang@quartech.com>
2024-01-25 00:51:52 -0800

    temp

--- src/Spd.Manager.Licence/PersonalLicenceAppManager.cs
+++ src/Spd.Manager.Licence/PersonalLicenceAppManager.cs
@@ -200,6 +200,7 @@ internal partial class PersonalLicenceAppManager :
                         DocumentType2 = docType2,
                         SubmittedByApplicantId = appResponse.ContactId,
                         ExpiryDate = expiredDate,
+                        ToTransientBucket = true
                     }, ct);
                 }
             }
