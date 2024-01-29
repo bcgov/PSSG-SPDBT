3daa71477bcbfade66327757a296bb090a382920
Peggy <peggy.zhang@quartech.com>
2024-01-29 09:04:47 -0800

    Transient storage: s3 delete (#706)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spdbt-1930(s3 file deletion);

--- src/Spd.Utilities.FileStorage/FileStorageService.cs
+++ src/Spd.Utilities.FileStorage/FileStorageService.cs
@@ -37,6 +37,10 @@ namespace Spd.Utilities.FileStorage
             };
         }
 
+        public async Task<string> HandleDeleteCommand(StorageDeleteCommand cmd, CancellationToken cancellationToken)
+        {
+            return await DeleteStorageItem(cmd, cancellationToken);
+        }
         private async Task<string> UploadStorageItem(UploadFileCommand cmd, CancellationToken cancellationToken)
         {
             File file = cmd.File;
@@ -62,6 +66,24 @@ namespace Spd.Utilities.FileStorage
             var response = await _amazonS3Client.PutObjectAsync(request, cancellationToken);
             response.EnsureSuccess();
 
+            return cmd.Key;
+        }
+
+        private async Task<string> DeleteStorageItem(StorageDeleteCommand cmd, CancellationToken cancellationToken)
+        {
+            var folder = cmd.Folder == null ? "" : $"{cmd.Folder}/";
+            var key = $"{folder}{cmd.Key}";
+
+            var request = new DeleteObjectRequest
+            {
+                Key = key,
+                BucketName = _config.Value.Bucket,
+            };
+
+            var response = await _amazonS3Client.DeleteObjectAsync(request, cancellationToken);
+
+            //If the delete action is successful, the service sends back an HTTP 204 response.
+            response.EnsureNoContent();
             return cmd.Key;
         }
 
@@ -246,6 +268,12 @@ namespace Spd.Utilities.FileStorage
         {
             if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
                 throw new InvalidOperationException($"Operation failed with status {response.HttpStatusCode}");
+        }
+
+        public static void EnsureNoContent(this AmazonWebServiceResponse response)
+        {
+            if (response.HttpStatusCode != System.Net.HttpStatusCode.NoContent)
+                throw new InvalidOperationException($"Delete Operation failed with status {response.HttpStatusCode}");
         }
     }
 }
