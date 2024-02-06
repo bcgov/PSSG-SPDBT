92b5a804d2ff538681fca01abd82ef96ed250b09
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-02-06 15:29:38 -0800

    Crrp invitation and first login (#743)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Fix bugs related to processing a crrp invitation and handling the
    first login of a user (showing the terms page)
    
    ---------
    
    Co-authored-by: peggy <peggy.zhang@quartech.com>

---
+++ src/Spd.Presentation.Screening/ClientApp/src/app/modules/crrp-portal/crrp-first-time-terms-and-conds.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AppRoutes } from 'src/app/app-routing.module';
+import { AuthProcessService } from 'src/app/core/services/auth-process.service';
+import { AuthUserBceidService } from 'src/app/core/services/auth-user-bceid.service';
+import { CrrpRoutes } from './crrp-routing.module';
+
+@Component({
+	selector: 'app-crrp-first-time-terms-and-conds',
+	template: `
+		<div class="container">
+			<section class="step-section my-4">
+				<div class="row m-4">
+					<div class="col-lg-8 mx-auto">
+						<h2>Terms and Conditions</h2>
+
+						<p class="mb-4">Read, download, and accept the Terms of Use to continue.</p>
+
+						<app-common-terms-and-conds (isSuccess)="onIsSuccess()"></app-common-terms-and-conds>
+					</div>
+				</div>
+			</section>
+		</div>
+	`,
+	styles: [],
+})
+export class CrrpFirstTimeTermsAndCondsComponent implements OnInit {
+	constructor(
+		private router: Router,
+		private authProcessService: AuthProcessService,
+		private authUserService: AuthUserBceidService
+	) {}
+
+	ngOnInit(): void {
+		const isLoggedIn = this.authProcessService.getCurrentWaitUntilAuthenticatedValue();
+		if (!isLoggedIn) {
+			this.router.navigate([AppRoutes.LANDING]);
+		}
+	}
+
+	onIsSuccess(): void {
+		const nextRoute = CrrpRoutes.path(CrrpRoutes.HOME);
+		const defaultOrgId = this.authUserService.bceidUserOrgProfile?.id;
+		this.router.navigate([nextRoute], { queryParams: { orgId: defaultOrgId } });
+	}
+}
