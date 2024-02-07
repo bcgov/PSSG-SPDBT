b2e1f41cd219cbdc60a4732f8980dbfa8aa8fee9
Peggy <peggy.zhang@quartech.com>
2024-02-07 07:09:24 -0800

    return cost when user do new, renew, update and replace (#744)
    # Description
    
    This PR includes the following proposed change(s):
    
    -return cost when user do new, renew, update and replace - spdbt-2186

--- src/Spd.Presentation.Licensing/Controllers/SecurityWorkerLicensingController.cs
+++ src/Spd.Presentation.Licensing/Controllers/SecurityWorkerLicensingController.cs
@@ -71,7 +71,7 @@ namespace Spd.Presentation.Licensing.Controllers
         [Route("api/worker-licence-applications")]
         [Authorize(Policy = "OnlyBcsc")]
         [HttpPost]
-        public async Task<WorkerLicenceAppUpsertResponse> SaveSecurityWorkerLicenceApplication([FromBody][Required] WorkerLicenceAppUpsertRequest licenceCreateRequest)
+        public async Task<WorkerLicenceCommandResponse> SaveSecurityWorkerLicenceApplication([FromBody][Required] WorkerLicenceAppUpsertRequest licenceCreateRequest)
         {
             _logger.LogInformation("Get WorkerLicenceAppUpsertRequest");
             var info = _currentUser.GetBcscUserIdentityInfo();
@@ -135,7 +135,7 @@ namespace Spd.Presentation.Licensing.Controllers
         [Route("api/worker-licence-applications/submit")]
         [Authorize(Policy = "OnlyBcsc")]
         [HttpPost]
-        public async Task<WorkerLicenceAppUpsertResponse> SubmitSecurityWorkerLicenceApplication([FromBody][Required] WorkerLicenceAppSubmitRequest licenceSubmitRequest, CancellationToken ct)
+        public async Task<WorkerLicenceCommandResponse> SubmitSecurityWorkerLicenceApplication([FromBody][Required] WorkerLicenceAppSubmitRequest licenceSubmitRequest, CancellationToken ct)
         {
             var validateResult = await _wslSubmitValidator.ValidateAsync(licenceSubmitRequest, ct);
             if (!validateResult.IsValid)
@@ -178,7 +178,7 @@ namespace Spd.Presentation.Licensing.Controllers
         [DisableFormValueModelBinding]
         [Consumes("multipart/form-data")]
         [RequestSizeLimit(long.MaxValue)]
-        public async Task<WorkerLicenceAppUpsertResponse> SubmitSecurityWorkerLicenceApplicationAnonymous(CancellationToken ct)
+        public async Task<WorkerLicenceCommandResponse> SubmitSecurityWorkerLicenceApplicationAnonymous(CancellationToken ct)
         {
             ICollection<UploadFileInfo> uploadedFileInfoList = null;
 
@@ -297,7 +297,7 @@ namespace Spd.Presentation.Licensing.Controllers
         /// <returns></returns>
         [Route("api/worker-licence-applications/anonymous/{keyCode}/submit")]
         [HttpPost]
-        public async Task<WorkerLicenceAppUpsertResponse> SubmitSecurityWorkerLicenceApplicationJsonAnonymous(WorkerLicenceAppAnonymousSubmitRequestJson jsonRequest, Guid keyCode, CancellationToken ct)
+        public async Task<WorkerLicenceCommandResponse> SubmitSecurityWorkerLicenceApplicationJsonAnonymous(WorkerLicenceAppAnonymousSubmitRequestJson jsonRequest, Guid keyCode, CancellationToken ct)
         {
             //validate keyCode
             if (await _cache.Get<LicenceAppDocumentsCache?>(keyCode.ToString()) == null)
