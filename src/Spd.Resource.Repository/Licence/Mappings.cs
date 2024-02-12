0c84be4a19e7e7777430ba7f23d229ddc572a7e8
Peggy <peggy.zhang@quartech.com>
2024-02-12 15:19:34 -0800

    anonymous cookie for swl (#766)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spdbt-2131(swl: unauth, use cookie session to ensure the security)
    - add two document types
    - add licence photo download

--- src/Spd.Resource.Repository/Licence/Mappings.cs
+++ src/Spd.Resource.Repository/Licence/Mappings.cs
@@ -11,6 +11,7 @@ namespace Spd.Resource.Repository.Licence
             _ = CreateMap<spd_licence, LicenceResp>()
              .ForMember(d => d.LicenceId, opt => opt.MapFrom(s => s.spd_licenceid))
              .ForMember(d => d.LicenceAppId, opt => opt.MapFrom(s => s.spd_CaseId._spd_applicationid_value))
+             .ForMember(d => d.LicenceHolderId, opt => opt.MapFrom(s => s.spd_LicenceHolder_contact.contactid))
              .ForMember(d => d.LicenceNumber, opt => opt.MapFrom(s => s.spd_licencenumber))
              .ForMember(d => d.ExpiryDate, opt => opt.MapFrom(s => SharedMappingFuncs.GetDateOnlyFromDateTimeOffset(s.spd_expirydate)))
              .ForMember(d => d.WorkerLicenceTypeCode, opt => opt.MapFrom(s => SharedMappingFuncs.GetServiceType(s._spd_licencetype_value)))
