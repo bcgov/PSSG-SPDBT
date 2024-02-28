c76cf1dd8517cb473732d6911c293799d0ccc6c5
Carol <carol.carpenter@quartech.com>
2024-02-28 11:38:59 -0800

    SPDBT-2237 Update "Statutory Declaration" to "Self Disclosure"

--- src/Spd.Presentation.Screening/ClientApp/src/app/api/models/payment-response.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/api/models/payment-response.ts
@@ -4,6 +4,7 @@ import { ApplicationTypeCode } from './application-type-code';
 import { LicenceTermCode } from './licence-term-code';
 import { PaymentStatusCode } from './payment-status-code';
 import { PaymentTypeCode } from './payment-type-code';
+import { ServiceTypeCode } from './service-type-code';
 export interface PaymentResponse {
   applicationId?: string;
   applicationTypeCode?: ApplicationTypeCode;
@@ -14,6 +15,7 @@ export interface PaymentResponse {
   paymentId?: string;
   paymentStatus?: PaymentStatusCode;
   paymentType?: PaymentTypeCode;
+  serviceTypeCode?: ServiceTypeCode;
   transAmount?: number;
   transDateTime?: string;
   transOrderId?: null | string;
