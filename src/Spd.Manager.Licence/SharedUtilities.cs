51c7b6cd360f13cbea107e361e41d4cc8475a82c
Ruben <ruben.araya@quartech.com>
2024-02-28 08:30:43 -0800

    Move body armour and armoured vehicle get methods to shared utility

---
+++ src/Spd.Manager.Licence/SharedUtilities.cs
@@ -0,0 +1,40 @@
+﻿using Spd.Resource.Repository.LicenceApplication;
+
+namespace Spd.Manager.Licence;
+public static class SharedUtilities
+{
+    public static List<BodyArmourPermitReasonCode> GetBodyArmourPermitReasonCodes(WorkerLicenceTypeEnum workerLicenceType, List<PermitPurposeEnum>? permitPurposes)
+    {
+        List<BodyArmourPermitReasonCode> bodyArmourPermitReasonCodes = new();
+
+        if (workerLicenceType != WorkerLicenceTypeEnum.BodyArmourPermit || permitPurposes == null) return bodyArmourPermitReasonCodes;
+
+        foreach (PermitPurposeEnum permitPurpose in permitPurposes)
+        {
+            BodyArmourPermitReasonCode bodyArmourPermitReasonCode;
+
+            if (Enum.TryParse(permitPurpose.ToString(), out bodyArmourPermitReasonCode))
+                bodyArmourPermitReasonCodes.Add(bodyArmourPermitReasonCode);
+        }
+
+        return bodyArmourPermitReasonCodes;
+    }
+
+    public static List<ArmouredVehiclePermitReasonCode> GetArmouredVehiclePermitReasonCodes(WorkerLicenceTypeEnum workerLicenceType, List<PermitPurposeEnum>? permitPurposes)
+    {
+        List<ArmouredVehiclePermitReasonCode> armouredVehiclePermitReasonCodes = new();
+
+        if (workerLicenceType != WorkerLicenceTypeEnum.ArmouredVehiclePermit || permitPurposes == null) return armouredVehiclePermitReasonCodes;
+
+        foreach (PermitPurposeEnum permitPurpose in permitPurposes)
+        {
+            ArmouredVehiclePermitReasonCode armouredVehiclePermitReasonCode;
+
+            if (Enum.TryParse(permitPurpose.ToString(), out armouredVehiclePermitReasonCode))
+                armouredVehiclePermitReasonCodes.Add(armouredVehiclePermitReasonCode);
+        }
+
+        return armouredVehiclePermitReasonCodes;
+    }
+
+}
