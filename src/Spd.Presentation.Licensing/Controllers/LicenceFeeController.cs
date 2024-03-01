04502cb9a8fba52ee4f7450e35111e7c50b337e9
Ruben <ruben.araya@quartech.com>
2024-03-01 09:48:11 -0800

    Revert "Remove "/licence-fee" endpoint"
    This reverts commit 05064bb284b480cf2872ac13944b47912a8eb5d6.

---
+++ src/Spd.Presentation.Licensing/Controllers/LicenceFeeController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Spd.Manager.Licence;
+using Spd.Utilities.Shared;
+
+namespace Spd.Presentation.Licensing.Controllers
+{
+    [ApiController]
+    public class LicenceFeeController : SpdControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public LicenceFeeController(
+            IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Get licence fee
+        /// Sample: api/licence-fee?workerLicenceTypeCode=SecurityWorkerLicence
+        /// </summary>
+        /// <param name="workerLicenceTypeCode"></param>
+        /// <returns></returns>
+        [Route("api/licence-fee")]
+        [HttpGet]
+        public async Task<LicenceFeeListResponse> GetLicenceFee([FromQuery] WorkerLicenceTypeCode? workerLicenceTypeCode = null)
+        {
+            return await _mediator.Send(new GetLicenceFeeListQuery(workerLicenceTypeCode));
+        }
+    }
+}
