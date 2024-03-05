87f5aa48b4e08cb8b55424fea4aaad2cef436670
Peggy <peggy.zhang@quartech.com>
2024-03-05 07:03:16 -0800

    bcsc diam integration (#827)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spdbt-2250(Integration with DIAM for BC service card.)

--- src/Spd.Utilities.LogonUser/ServiceExtensionAuthentication.cs
+++ src/Spd.Utilities.LogonUser/ServiceExtensionAuthentication.cs
@@ -144,7 +144,7 @@ namespace Spd.Utilities.LogonUser
 
                         //request userinfo claims through the backchannel
                         var response = await ctx.Options.Backchannel.GetUserInfoAsync(userInfoRequest, CancellationToken.None);
-                        if (response.IsError && response.HttpStatusCode == HttpStatusCode.OK)
+                        if (!response.IsError && response.HttpStatusCode == HttpStatusCode.OK)
                         {
                             //handle encrypted userinfo response...
                             if (response.HttpResponse.Content?.Headers?.ContentType?.MediaType == "application/jwt")
@@ -155,6 +155,15 @@ namespace Spd.Utilities.LogonUser
                                     handler.ValidateToken(response.Raw, validationParameters, out var token);
                                     var jwe = token as JwtSecurityToken;
                                     MapClaimsToPrincipalClaims(ctx.Principal, jwe.Claims);
+                                }
+                            }
+                            else if (response.HttpResponse.Content?.Headers?.ContentType?.MediaType == "application/json")
+                            {
+                                var claims = JsonSerializer.Deserialize<Dictionary<string, object>>(response.Raw);
+                                foreach (var claim in claims)
+                                {
+                                    string str = claim.Value.ToString();
+                                    ctx.Principal.AddUpdateClaim(claim.Key, str);
                                 }
                             }
                             else
@@ -192,7 +201,7 @@ namespace Spd.Utilities.LogonUser
                         {
                             JwtSecurityToken jwtToken = jwtHandler.ReadJwtToken(token);
                             if (jwtToken.Issuer.Equals(bceidConfig.Authority) ||
-                                jwtToken.Audiences.Any(a => bceidConfig.Audiences.Contains(a)))
+                                jwtToken.Audiences.Any(a => bceidConfig.Audiences.Equals(a)))
                             {
                                 //idir and bceid have the same authoritiy and audience.
                                 var identityProviderClaim = jwtToken.Claims.FirstOrDefault(c => c.Type == "identity_provider");
@@ -203,7 +212,7 @@ namespace Spd.Utilities.LogonUser
                                 return BCeIDAuthenticationConfiguration.AuthSchemeName;
                             }
                             else if (jwtToken.Issuer.Equals(bcscConfig.Issuer) ||
-                                jwtToken.Audiences.Any(a => bcscConfig.Audiences.Contains(a)))
+                                jwtToken.Audiences.Any(a => bcscConfig.Audiences.Equals(a)))
                             {
                                 return BcscAuthenticationConfiguration.AuthSchemeName;
                             }
