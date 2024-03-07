17dc4b2bbecd29b0a3c57239f1f9a27e18d58bb4
Peggy <peggy.zhang@quartech.com>
2024-03-06 16:27:35 -0800

    diam licensing bcsc (#830)
    diam licensing bcsc
    
    ---------
    
    Co-authored-by: Carol <carol.carpenter@quartech.com>

--- src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/configuration-response.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/api/models/configuration-response.ts
@@ -1,12 +1,11 @@
 /* tslint:disable */
 /* eslint-disable */
-import { BcscConfiguration } from './bcsc-configuration';
 import { LicenceFeeResponse } from './licence-fee-response';
 import { OidcConfiguration } from './oidc-configuration';
 import { RecaptchaConfiguration } from './recaptcha-configuration';
 import { WorkerCategoryTypeCode } from './worker-category-type-code';
 export interface ConfigurationResponse {
-  bcscConfiguration?: BcscConfiguration;
+  bcscConfiguration?: OidcConfiguration;
   invalidWorkerLicenceCategoryMatrixConfiguration?: null | {
 [key: string]: Array<WorkerCategoryTypeCode>;
 };
