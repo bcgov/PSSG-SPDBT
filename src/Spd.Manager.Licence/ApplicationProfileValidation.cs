258dc4deec62ad4ff84714b492bf14de96bbfdee
Ruben <ruben.araya@quartech.com>
2024-03-08 12:18:06 -0800

    Adjust validation of gender code

--- src/Spd.Manager.Licence/ApplicationProfileValidation.cs
+++ src/Spd.Manager.Licence/ApplicationProfileValidation.cs
@@ -1,5 +1,4 @@
 ﻿using FluentValidation;
-using Spd.Manager.Shared;
 
 namespace Spd.Manager.Licence;
 public class ApplicantUpdateRequestValidator : AbstractValidator<ApplicantUpdateRequest>
@@ -9,7 +8,7 @@ public class ApplicantUpdateRequestValidator : AbstractValidator<ApplicantUpdate
         RuleFor(r => r.GivenName).NotEmpty();
         RuleFor(r => r.Surname).NotEmpty();
         RuleFor(r => r.DateOfBirth).NotEmpty();
-        RuleFor(r => r.GenderCode).IsInEnum();
+        RuleFor(r => r.GenderCode).NotEmpty().IsInEnum();
         RuleFor(r => r.PhoneNumber).MaximumLength(15).NotEmpty();
         RuleFor(r => r.EmailAddress).MaximumLength(75).NotEmpty().EmailAddress();
         RuleFor(r => r.DocumentKeyCodes)
