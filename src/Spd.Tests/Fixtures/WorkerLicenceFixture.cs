49e919878dff6569b688f6d6dabe649c91d10d16
Ruben <ruben.araya@quartech.com>
2024-03-21 18:02:42 -0300

    Add Worker Licence objects fixture

---
+++ src/Spd.Tests/Fixtures/WorkerLicenceFixture.cs
@@ -0,0 +1,31 @@
+﻿using AutoFixture;
+using Spd.Manager.Licence;
+using Spd.Manager.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Tests.Fixtures;
+public class WorkerLicenceFixture
+{
+    private readonly IFixture fixture;
+
+    public WorkerLicenceFixture(CancellationToken cancellationToken)
+    {
+        fixture = new Fixture();
+        fixture.Customize<DateOnly>(composer => composer.FromFactory<DateTime>(DateOnly.FromDateTime));
+        fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    public WorkerLicenceAppAnonymousSubmitRequest GenerateValidWorkerLicenceAppAnonymousSubmitRequest(ApplicationTypeCode applicationTypeCode = ApplicationTypeCode.New)
+    {
+        WorkerLicenceAppAnonymousSubmitRequest workerLicenceAppAnonymousSubmitRequest = fixture.Build<WorkerLicenceAppAnonymousSubmitRequest>()
+                .With(w => w.ApplicationTypeCode, applicationTypeCode)
+                .Create();
+    
+        return workerLicenceAppAnonymousSubmitRequest;
+    }
+}
