76200ff6fb44fceb10acfb77b26a50d6b7b91318
Ruben <ruben.araya@quartech.com>
2024-04-02 22:33:18 -0700

    Format code

--- src/Spd.Manager.Common/Admin/AdminManager.cs
+++ src/Spd.Manager.Common/Admin/AdminManager.cs
@@ -5,64 +5,62 @@ using Spd.Resource.Repository.Org;
 using Spd.Utilities.Address;
 using Spd.Utilities.Shared;
 
-namespace Spd.Manager.Common.Admin
+namespace Spd.Manager.Common.Admin;
+internal class AdminManager :
+    IRequestHandler<FindAddressQuery, IEnumerable<AddressFindResponse>>,
+    IRequestHandler<RetrieveAddressByIdQuery, IEnumerable<AddressRetrieveResponse>>,
+    IRequestHandler<GetBannerMsgQuery, string>,
+    IRequestHandler<GetReplacementProcessingTimeQuery, string>,
+    IRequestHandler<GetMinistryQuery, IEnumerable<MinistryResponse>>,
+    IAdminManager
 {
-    internal class AdminManager :
-        IRequestHandler<FindAddressQuery, IEnumerable<AddressFindResponse>>,
-        IRequestHandler<RetrieveAddressByIdQuery, IEnumerable<AddressRetrieveResponse>>,
-        IRequestHandler<GetBannerMsgQuery, string>,
-        IRequestHandler<GetReplacementProcessingTimeQuery, string>,
-        IRequestHandler<GetMinistryQuery, IEnumerable<MinistryResponse>>,
-        IAdminManager
+    private readonly IAddressAutocompleteClient _addressClient;
+    private readonly IConfigRepository _configRepo;
+    private readonly IOrgRepository _orgRepo;
+    private readonly IMapper _mapper;
+    public AdminManager(IAddressAutocompleteClient addressClient, IMapper mapper, IConfigRepository configRepo, IOrgRepository orgRepo)
     {
-        private readonly IAddressAutocompleteClient _addressClient;
-        private readonly IConfigRepository _configRepo;
-        private readonly IOrgRepository _orgRepo;
-        private readonly IMapper _mapper;
-        public AdminManager(IAddressAutocompleteClient addressClient, IMapper mapper, IConfigRepository configRepo, IOrgRepository orgRepo)
-        {
-            _addressClient = addressClient;
-            _mapper = mapper;
-            _configRepo = configRepo;
-            _orgRepo = orgRepo;
-        }
+        _addressClient = addressClient;
+        _mapper = mapper;
+        _configRepo = configRepo;
+        _orgRepo = orgRepo;
+    }
 
-        public async Task<IEnumerable<AddressFindResponse>> Handle(FindAddressQuery query, CancellationToken cancellationToken)
-        {
-            IEnumerable<AddressAutocompleteFindResponse> result =
-                (IEnumerable<AddressAutocompleteFindResponse>)await this._addressClient.Query(
-                    new AddressSearchQuery { SearchTerm = query.SearchTerm, Country = query.Country, LastId = query.LastId },
-                    cancellationToken);
-            return _mapper.Map<IEnumerable<AddressFindResponse>>(result);
-        }
+    public async Task<IEnumerable<AddressFindResponse>> Handle(FindAddressQuery query, CancellationToken cancellationToken)
+    {
+        IEnumerable<AddressAutocompleteFindResponse> result =
+            (IEnumerable<AddressAutocompleteFindResponse>)await this._addressClient.Query(
+                new AddressSearchQuery { SearchTerm = query.SearchTerm, Country = query.Country, LastId = query.LastId },
+                cancellationToken);
+        return _mapper.Map<IEnumerable<AddressFindResponse>>(result);
+    }
 
-        public async Task<IEnumerable<AddressRetrieveResponse>> Handle(RetrieveAddressByIdQuery query, CancellationToken cancellationToken)
-        {
-            IEnumerable<AddressAutocompleteRetrieveResponse> result =
-                (IEnumerable<AddressAutocompleteRetrieveResponse>)await this._addressClient.Query(new AddressRetrieveQuery { Id = query.Id }, cancellationToken);
-            return _mapper.Map<IEnumerable<AddressRetrieveResponse>>(result);
-        }
+    public async Task<IEnumerable<AddressRetrieveResponse>> Handle(RetrieveAddressByIdQuery query, CancellationToken cancellationToken)
+    {
+        IEnumerable<AddressAutocompleteRetrieveResponse> result =
+            (IEnumerable<AddressAutocompleteRetrieveResponse>)await this._addressClient.Query(new AddressRetrieveQuery { Id = query.Id }, cancellationToken);
+        return _mapper.Map<IEnumerable<AddressRetrieveResponse>>(result);
+    }
 
-        public async Task<string> Handle(GetBannerMsgQuery query, CancellationToken cancellationToken)
-        {
-            return (await _configRepo.Query(new ConfigQuery(IConfigRepository.BANNER_MSG_CONFIG_KEY), cancellationToken))
-                .ConfigItems
-                .First()
-                .Value;
-        }
+    public async Task<string> Handle(GetBannerMsgQuery query, CancellationToken cancellationToken)
+    {
+        return (await _configRepo.Query(new ConfigQuery(IConfigRepository.BANNER_MSG_CONFIG_KEY), cancellationToken))
+            .ConfigItems
+            .First()
+            .Value;
+    }
 
-        public async Task<string> Handle(GetReplacementProcessingTimeQuery query, CancellationToken cancellationToken)
-        {
-            return (await _configRepo.Query(new ConfigQuery(IConfigRepository.LICENSING_REPLACEMENTPROCESSINGTIME_KEY), cancellationToken))
-                .ConfigItems
-                .First()
-                .Value;
-        }
+    public async Task<string> Handle(GetReplacementProcessingTimeQuery query, CancellationToken cancellationToken)
+    {
+        return (await _configRepo.Query(new ConfigQuery(IConfigRepository.LICENSING_REPLACEMENTPROCESSINGTIME_KEY), cancellationToken))
+            .ConfigItems
+            .First()
+            .Value;
+    }
 
-        public async Task<IEnumerable<MinistryResponse>> Handle(GetMinistryQuery request, CancellationToken cancellationToken)
-        {
-            var result = (OrgsQryResult)await _orgRepo.QueryOrgAsync(new OrgsQry(null, ParentOrgId: SpdConstants.BcGovOrgId, IncludeInactive: true), cancellationToken);
-            return _mapper.Map<IEnumerable<MinistryResponse>>(result.OrgResults);
-        }
+    public async Task<IEnumerable<MinistryResponse>> Handle(GetMinistryQuery request, CancellationToken cancellationToken)
+    {
+        var result = (OrgsQryResult)await _orgRepo.QueryOrgAsync(new OrgsQry(null, ParentOrgId: SpdConstants.BcGovOrgId, IncludeInactive: true), cancellationToken);
+        return _mapper.Map<IEnumerable<MinistryResponse>>(result.OrgResults);
     }
 }
