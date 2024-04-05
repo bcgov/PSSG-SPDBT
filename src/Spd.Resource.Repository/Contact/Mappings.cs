392934cb1a33fd52f72fd094916fb7f0221a2b9f
Ruben <ruben.araya@quartech.com>
2024-04-05 09:11:36 -0700

    Separate alias mapping to properly assign source type in case entity "spd_alias" is queried

--- src/Spd.Resource.Repository/Contact/Mappings.cs
+++ src/Spd.Resource.Repository/Contact/Mappings.cs
@@ -71,13 +71,21 @@ namespace Spd.Resource.Repository.Contact
             .IncludeBase<ContactCmd, contact>();
 
             _ = CreateMap<Alias, spd_alias>()
-              .ForMember(d => d.spd_aliasid, opt => opt.MapFrom(s => s.Id))
-              .ForMember(d => d.spd_firstname, opt => opt.MapFrom(s => s.GivenName))
-              .ForMember(d => d.spd_surname, opt => opt.MapFrom(s => s.Surname))
-              .ForMember(d => d.spd_middlename1, opt => opt.MapFrom(s => s.MiddleName1))
-              .ForMember(d => d.spd_middlename2, opt => opt.MapFrom(s => s.MiddleName2))
-              .ForMember(d => d.spd_source, opt => opt.MapFrom(s => AliasSourceTypeOptionSet.UserEntered))
-              .ReverseMap();
+             .ForMember(d => d.spd_aliasid, opt => opt.MapFrom(s => s.Id))
+             .ForMember(d => d.spd_firstname, opt => opt.MapFrom(s => s.GivenName))
+             .ForMember(d => d.spd_surname, opt => opt.MapFrom(s => s.Surname))
+             .ForMember(d => d.spd_middlename1, opt => opt.MapFrom(s => s.MiddleName1))
+             .ForMember(d => d.spd_middlename2, opt => opt.MapFrom(s => s.MiddleName2))
+             .ForMember(d => d.spd_source, opt => opt.MapFrom(s => AliasSourceTypeOptionSet.UserEntered))
+             .ReverseMap();
+
+            _ = CreateMap<spd_alias, Alias>()
+             .ForMember(d => d.Id, opt => opt.MapFrom(s => s.spd_aliasid))
+             .ForMember(d => d.GivenName, opt => opt.MapFrom(s => s.spd_firstname))
+             .ForMember(d => d.Surname, opt => opt.MapFrom(s => s.spd_surname))
+             .ForMember(d => d.MiddleName1, opt => opt.MapFrom(s => s.spd_middlename1))
+             .ForMember(d => d.MiddleName2, opt => opt.MapFrom(s => s.spd_middlename2))
+             .ForMember(d => d.SourceType, opt => opt.MapFrom(s => s.spd_source));
 
             _ = CreateMap<spd_licence, LicenceInfo>()
              .ForMember(d => d.Id, opt => opt.MapFrom(s => s.spd_licenceid))
