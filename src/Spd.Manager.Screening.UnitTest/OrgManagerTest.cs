1f086396d2af58202dceaa791926ed44e7f02cd4
Peggy <peggy.zhang@quartech.com>
2024-04-08 08:47:32 -0700

    Link existing organization to Business BCeID (#925)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spdbt-2426-Link existing organization to Business BCeID

---
+++ src/Spd.Manager.Screening.UnitTest/OrgManagerTest.cs
@@ -0,0 +1,70 @@
+using AutoMapper;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.Extensions.Caching.Distributed;
+using Moq;
+using Spd.Resource.Repository;
+using Spd.Resource.Repository.Org;
+
+namespace Spd.Manager.Screening.UnitTest;
+
+public class OrgManagerTest
+{
+    private Mock<IOrgRepository> mockOrgRepo = new();
+    private Mock<IDataProtectionProvider> mockDataProtection = new();
+    private Mock<IMapper> mockMapper = new();
+    private Mock<IDistributedCache> mockCache = new();
+    private string encodedOrgId;
+    private Guid orgId;
+
+    private OrgManager sut;
+
+    public OrgManagerTest()
+    {
+        mockDataProtection.Setup(d => d.CreateProtector(It.IsAny<string>()))
+           .Returns(DataProtectionProvider.Create(nameof(OrgInvitationLinkCreateCommand)).CreateProtector("test").ToTimeLimitedDataProtector());
+
+        sut = new OrgManager(mockOrgRepo.Object,
+            mockMapper.Object,
+            mockCache.Object,
+            mockDataProtection.Object);
+    }
+
+    [Fact]
+    public async void Handle_OrgInvitationLinkCreateCommand_Return_CorrectLinkResponse()
+    {
+        //Arrange
+        Guid orgId = Guid.NewGuid();
+        mockOrgRepo.Setup(m => m.QueryOrgAsync(It.Is<OrgByIdentifierQry>(q => q.OrgId == orgId), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new OrgQryResult(new OrgResult() { ServiceTypes = new List<ServiceTypeEnum> { ServiceTypeEnum.CRRP_EMPLOYEE } }));
+        OrgInvitationLinkCreateCommand cmd = new(orgId, "localhost");
+
+        //Act
+        var result = await sut.Handle(cmd, CancellationToken.None);
+
+        //Assert
+        Assert.IsType<OrgInvitationLinkResponse>(result);
+        Assert.StartsWith("localhost?encodedOrgId=", result.OrgInvitationLinkUrl);
+        this.encodedOrgId = result.OrgInvitationLinkUrl.Split("=")[1];
+        this.orgId = orgId;
+    }
+
+    [Fact]
+    public async void Handle_OrgInvitationLinkVerifyCommand_Return_CorrectVerifyResponse()
+    {
+        //Arrange
+        if (this.encodedOrgId == null)
+        {
+            Handle_OrgInvitationLinkCreateCommand_Return_CorrectLinkResponse();
+        }
+        mockOrgRepo.Setup(m => m.QueryOrgAsync(It.Is<OrgByIdentifierQry>(q => q.OrgId == this.orgId), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new OrgQryResult(new OrgResult() { ServiceTypes = new List<ServiceTypeEnum> { ServiceTypeEnum.CRRP_EMPLOYEE } }));
+        OrgInvitationLinkVerifyCommand cmd = new(this.encodedOrgId);
+
+        //Act
+        var result = await sut.Handle(cmd, CancellationToken.None);
+
+        //Assert
+        Assert.IsType<OrgInviteVerifyResponse>(result);
+        Assert.True(result.LinkIsValid);
+    }
+}
