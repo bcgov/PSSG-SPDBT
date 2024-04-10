30cbebf2ac18c1a90ba94341248144121fc26925
Peggy <peggy.zhang@quartech.com>
2024-04-10 09:38:27 -0700

    add validation if there is application in progress, the profile cannot change. (#938)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spdbt-2449

--- src/Spd.Manager.Licence.UnitTest/ApplicantProfileManagerTest.cs
+++ src/Spd.Manager.Licence.UnitTest/ApplicantProfileManagerTest.cs
@@ -1,13 +1,18 @@
-﻿using AutoMapper;
+﻿using AutoFixture;
+using AutoMapper;
 using Moq;
 using Spd.Resource.Repository.Contact;
 using Spd.Resource.Repository.Document;
 using Spd.Resource.Repository.Identity;
+using Spd.Resource.Repository.LicenceApplication;
+using Spd.Utilities.Shared.Exceptions;
 
 namespace Spd.Manager.Licence.UnitTest
 {
     public class ApplicantProfileManagerTest
     {
+        private readonly IFixture fixture;
+        private Mock<ILicenceApplicationRepository> mockLicAppRepo = new();
         private Mock<IIdentityRepository> mockIdRepo = new();
         private Mock<IDocumentRepository> mockDocRepo = new();
         private Mock<IContactRepository> mockContactRepo = new();
@@ -15,11 +20,17 @@ namespace Spd.Manager.Licence.UnitTest
         private ApplicantProfileManager sut;
         public ApplicantProfileManagerTest()
         {
+            fixture = new Fixture();
+            fixture.Customize<DateOnly>(composer => composer.FromFactory<DateTime>(DateOnly.FromDateTime));
+            fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+            fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+
             sut = new ApplicantProfileManager(mockIdRepo.Object,
                 mockContactRepo.Object,
                 mockMapper.Object,
                 null,
-                mockDocRepo.Object);
+                mockDocRepo.Object,
+                mockLicAppRepo.Object);
         }
 
         [Fact]
@@ -37,5 +48,19 @@ namespace Spd.Manager.Licence.UnitTest
             mockContactRepo.VerifyAll();
         }
 
+        [Fact]
+        public async void Handle_ApplicantUpdateCommand_WithAppInProgress_ShouldThrowException()
+        {
+            //Arrange
+            ApplicantUpdateCommand cmd = new(Guid.NewGuid(), fixture.Create<ApplicantUpdateRequest>(), fixture.Create<IEnumerable<LicAppFileInfo>>());
+            mockLicAppRepo.Setup(a => a.QueryAsync(It.Is<LicenceAppQuery>(q => q.ApplicantId == cmd.ApplicantId), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(fixture.Create<IEnumerable<LicenceAppListResp>>);
+
+            //Act
+            Func<Task> act = () => sut.Handle(cmd, CancellationToken.None);
+
+            //Assert
+            await Assert.ThrowsAsync<ApiException>(act);
+        }
     }
 }
