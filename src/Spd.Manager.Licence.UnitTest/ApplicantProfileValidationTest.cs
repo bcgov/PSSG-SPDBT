007c54ab5b73c64ece711819a75463e0a76094f1
QSL\RubenA <ruben.araya@qaurtech.com>
2024-04-10 15:59:43 -0700

    Add unit test for applicant profile validator

--- src/Spd.Manager.Licence.UnitTest/ApplicantProfileValidationTest.cs
+++ src/Spd.Manager.Licence.UnitTest/ApplicantProfileValidationTest.cs
@@ -1,10 +1,5 @@
 ﻿using AutoFixture;
 using FluentValidation.TestHelper;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Spd.Manager.Licence.UnitTest;
 public class ApplicantProfileValidationTest
