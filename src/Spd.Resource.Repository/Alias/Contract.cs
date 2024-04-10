085817329e65e622abfb0a837aa761f9f14dda49
QSL\RubenA <ruben.araya@qaurtech.com>
2024-04-10 08:20:34 -0700

    Move alias functions into its own repository

---
+++ src/Spd.Resource.Repository/Alias/Contract.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+
+namespace Spd.Resource.Repository.Alias;
+public interface IAliasRepository
+{
+    public Task<Unit> CreateAliasAsync(CreateAliasCommand cmd, CancellationToken cancellationToken);
+    public Task DeleteAliasAsync(Guid aliasId, CancellationToken cancellationToken);
+    public Task UpdateAliasAsync(UpdateAliasCommand cmd, CancellationToken cancellationToken);
+}
+
+public record CreateAliasCommand
+{
+    public Guid ContactId { get; set; }
+    public Alias Alias { get; set; }
+};
+public record UpdateAliasCommand
+{
+    public IEnumerable<Alias> Aliases { get; set; } = [];
+}
