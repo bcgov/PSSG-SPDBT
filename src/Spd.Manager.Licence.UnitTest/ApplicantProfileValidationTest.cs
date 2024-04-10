cc99fab07d48ca8c5c961c2dc1db6684da234df0
QSL\RubenA <ruben.araya@qaurtech.com>
2024-04-10 15:56:39 -0700

    Add unit test for applicant profile validator

---
+++ src/Spd.Manager.Licence.UnitTest/ApplicantProfileValidationTest.cs
@@ -0,0 +1,45 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Manager.Licence.UnitTest;
+public class ApplicantProfileValidationTest
+{
+    private readonly ApplicantUpdateRequestValidator validator;
+    private readonly IFixture fixture;
+
+    public ApplicantProfileValidationTest()
+    {
+        validator = new ApplicantUpdateRequestValidator();
+
+        fixture = new Fixture();
+        fixture.Customize<DateOnly>(composer => composer.FromFactory<DateTime>(DateOnly.FromDateTime));
+        fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    [Fact]
+    public void ApplicantUpdateRequestValidator_ShouldPass()
+    {
+        var address = fixture.Build<Address>()
+            .With(a => a.AddressLine1, new string('a', 100))
+            .With(a => a.City, new string('a', 100))
+            .With(a => a.City, new string('a', 100))
+            .With(a => a.PostalCode, new string('a', 20))
+            .Create();
+
+        var model = fixture.Build<ApplicantUpdateRequest>()
+            .With(r => r.EmailAddress, "test@test.com")
+            .With(r => r.PhoneNumber, new string('9', 15))
+            .With(r => r.MailingAddress, address)
+            .With(r => r.ResidentialAddress, address)
+            .Create();
+
+        var result = validator.TestValidate(model);
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+}
