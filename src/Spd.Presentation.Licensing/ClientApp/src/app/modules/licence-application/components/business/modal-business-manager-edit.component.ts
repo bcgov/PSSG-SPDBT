2417601d80b33ec45919cfdd14dccf6101a0aa87
Carol Carpenter <carol.carpenter@quartech.com>
2024-04-11 14:16:48 -0700

    wip

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/modal-business-manager-edit.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/modal-business-manager-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { FormErrorStateMatcher } from '@app/shared/directives/form-error-state-matcher.directive';
 import { SPD_CONSTANTS } from '@app/core/constants/constants';
 import { BusinessApplicationService } from '@app/modules/licence-application/services/business-application.service';
+import { FormErrorStateMatcher } from '@app/shared/directives/form-error-state-matcher.directive';
 
 export interface UserDialogData {
 	// user: OrgUserResponse;
@@ -14,52 +14,62 @@ export interface UserDialogData {
 	template: `
 		<div mat-dialog-title>{{ title }}</div>
 		<mat-dialog-content>
-			<app-common-address [form]="form" [isWizardStep]="false"></app-common-address>
-
 			<form [formGroup]="form" novalidate>
 				<div class="row">
-					<div class="col-md-6">
+					<div class="col-lg-6 col-md-12">
 						<mat-form-field>
-							<mat-label>Branch Manager</mat-label>
-							<input matInput formControlName="managerName" maxlength="100" [errorStateMatcher]="matcher" />
-							<mat-error *ngIf="form.get('managerName')?.hasError('required')">This is required</mat-error>
+							<mat-label>Given Name</mat-label>
+							<input matInput formControlName="givenName" [errorStateMatcher]="matcher" maxlength="40" />
+							<mat-error *ngIf="form.get('givenName')?.hasError('required')"> This is required </mat-error>
+						</mat-form-field>
+					</div>
+					<div class="col-lg-6 col-md-12">
+						<mat-form-field>
+							<mat-label>Middle Name 1 <span class="optional-label">(optional)</span></mat-label>
+							<input matInput formControlName="middleName1" maxlength="40" />
+						</mat-form-field>
+					</div>
+					<div class="col-lg-6 col-md-12">
+						<mat-form-field>
+							<mat-label>Middle Name 2 <span class="optional-label">(optional)</span></mat-label>
+							<input matInput formControlName="middleName2" maxlength="40" />
+						</mat-form-field>
+					</div>
+					<div class="col-lg-6 col-md-12">
+						<mat-form-field>
+							<mat-label>Surname</mat-label>
+							<input matInput formControlName="surname" [errorStateMatcher]="matcher" maxlength="40" />
+							<mat-error *ngIf="form.get('surname')?.hasError('required')"> This is required </mat-error>
 						</mat-form-field>
 					</div>
 
-					<div class="col-md-6">
+					<div class="col-lg-6 col-md-12">
 						<mat-form-field>
-							<mat-label>Manager's Security Worker Licence Number (optional)</mat-label>
-							<input matInput formControlName="managerSwlNumber" maxlength="10" [errorStateMatcher]="matcher" />
-						</mat-form-field>
-					</div>
-
-					<div class="col-md-6">
-						<mat-form-field>
-							<mat-label>Manager's Phone Number</mat-label>
+							<mat-label>Phone Number</mat-label>
 							<input
 								matInput
-								formControlName="managerPhoneNumber"
+								formControlName="phoneNumber"
 								[mask]="phoneMask"
 								[showMaskTyped]="false"
 								[errorStateMatcher]="matcher"
 							/>
-							<mat-error *ngIf="form.get('managerPhoneNumber')?.hasError('required')">This is required</mat-error>
-							<mat-error *ngIf="form.get('managerPhoneNumber')?.hasError('mask')">This must be 10 digits</mat-error>
+							<mat-error *ngIf="form.get('phoneNumber')?.hasError('required')">This is required</mat-error>
+							<mat-error *ngIf="form.get('phoneNumber')?.hasError('mask')">This must be 10 digits</mat-error>
 						</mat-form-field>
 					</div>
 
-					<div class="col-md-6">
+					<div class="col-lg-6 col-md-12">
 						<mat-form-field>
 							<mat-label>Email</mat-label>
 							<input
 								matInput
-								formControlName="managerEmail"
+								formControlName="emailAddress"
 								placeholder="name@domain.com"
 								maxlength="75"
 								[errorStateMatcher]="matcher"
 							/>
-							<mat-error *ngIf="form.get('managerEmail')?.hasError('email')"> Must be a valid email address </mat-error>
-							<mat-error *ngIf="form.get('managerEmail')?.hasError('required')">This is required</mat-error>
+							<mat-error *ngIf="form.get('emailAddress')?.hasError('email')"> Must be a valid email address </mat-error>
+							<mat-error *ngIf="form.get('emailAddress')?.hasError('required')">This is required</mat-error>
 						</mat-form-field>
 					</div>
 				</div>
@@ -82,7 +92,7 @@ export class ModalBusinessManagerEditComponent implements OnInit {
 	title = '';
 	isEdit = false;
 
-	form = this.businessApplicationService.branchInBcFormGroup;
+	form = this.businessApplicationService.managerFormGroup;
 
 	phoneMask = SPD_CONSTANTS.phone.displayMask;
 	matcher = new FormErrorStateMatcher();
