96207f6534840fc0d1025baf50ab257b9685b9ae
QSL\RubenA <ruben.araya@quartech.com>
2024-04-25 16:50:57 -0700

    Add imports

--- src/Spd.Manager.Licence/BizLicenceAppContract.cs
+++ src/Spd.Manager.Licence/BizLicenceAppContract.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Spd.Manager.Shared;
 
 namespace Spd.Manager.Licence;
 
