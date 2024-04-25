d2e4e81192c1f5a072e2cc8e1e60f02381728440
QSL\RubenA <ruben.araya@quartech.com>
2024-04-25 16:35:31 -0700

    Add business licensing controller (partial implementation)

--- src/Spd.Presentation.Licensing/Controllers/BizLicensingController.cs
+++ src/Spd.Presentation.Licensing/Controllers/BizLicensingController.cs
@@ -1,14 +1,32 @@
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Spd.Manager.Licence;
 using Spd.Utilities.Shared;
+using System.ComponentModel.DataAnnotations;
 
 namespace Spd.Presentation.Licensing.Controllers
 {
     [ApiController]
     public class BizLicensingController : SpdControllerBase
     {
-
-        public BizLicensingController()
+        private readonly IMediator _mediator;
+        public BizLicensingController(IMediator mediator)
         {
+            _mediator = mediator;
+        }
+
+        /// <summary>
+        /// Save Business Licence Application
+        /// </summary>
+        /// <param name="bizUpsertRequest"></param>
+        /// <returns></returns>
+        [Route("api/business-licence")]
+        [Authorize(Policy = "OnlyBcsc")]
+        [HttpPost]
+        public async Task<Unit> SaveBusinessLicenceApplication([FromBody][Required] BizLicenceAppUpsertRequest bizUpsertRequest, CancellationToken ct)
+        {
+            return default;
         }
     }
 }
