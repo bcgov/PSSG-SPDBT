d07c737035a5ae1e5b2e781ce33cc4a9251e4f62
QSL\RubenA <ruben.araya@quartech.com>
2024-04-26 07:34:53 -0700

    Add biz licence application mapping

---
+++ src/Spd.Resource.Repository/BizLicenceApplication/Mappings.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.Dynamics.CRM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Resource.Repository.BizLicenceApplication;
+internal class Mappings : Profile
+{
+    public Mappings()
+    {
+        CreateMap<CreateBizLicenceApplicationCmd, spd_application>();
+    }
+}
