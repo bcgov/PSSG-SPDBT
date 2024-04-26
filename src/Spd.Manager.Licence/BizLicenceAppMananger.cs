c189bf8c8a5ab9d020f10fd0baa584b65cf539b8
QSL\RubenA <ruben.araya@quartech.com>
2024-04-26 07:32:12 -0700

    Add biz licence manager

--- src/Spd.Manager.Licence/BizLicenceAppMananger.cs
+++ src/Spd.Manager.Licence/BizLicenceAppMananger.cs
@@ -1,4 +1,21 @@
-﻿namespace Spd.Manager.Licence;
-internal class BizLicenceAppMananger
+﻿using AutoMapper;
+using MediatR;
+using Spd.Resource.Repository.LicenceApplication;
+
+namespace Spd.Manager.Licence;
+internal class BizLicenceAppMananger :
+    IRequestHandler<BizLicenceAppNewCommand, Guid>,
+    IBizLicenceAppMananger
 {
+    public BizLicenceAppMananger(
+        ILicenceApplicationRepository licenceAppRepository,
+        IMapper mapper)
+    {
+
+    }
+
+    public async Task<Guid> Handle(BizLicenceAppNewCommand cmd, CancellationToken cancellationToken)
+    {
+        return Guid.NewGuid();
+    }
 }
