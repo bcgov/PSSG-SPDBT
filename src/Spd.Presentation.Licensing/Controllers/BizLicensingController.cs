95e14dab0bbfd02f07e699233f43f2fa89b7a754
QSL\RubenA <ruben.araya@quartech.com>
2024-04-26 09:41:12 -0700

    Add new biz document upload endpoint
    (cherry picked from commit 4554fa6c589a24de17d9b3ef6e6e442689e88a16)

--- src/Spd.Presentation.Licensing/Controllers/BizLicensingController.cs
+++ src/Spd.Presentation.Licensing/Controllers/BizLicensingController.cs
@@ -4,15 +4,18 @@ using Microsoft.AspNetCore.Mvc;
 using Spd.Manager.Licence;
 using Spd.Utilities.Shared;
 using System.ComponentModel.DataAnnotations;
+using System.Security.Principal;
 
 namespace Spd.Presentation.Licensing.Controllers
 {
     [ApiController]
     public class BizLicensingController : SpdControllerBase
     {
+        private readonly IPrincipal _currentUser;
         private readonly IMediator _mediator;
-        public BizLicensingController(IMediator mediator)
+        public BizLicensingController(IPrincipal currentUser, IMediator mediator)
         {
+            _currentUser = currentUser;
             _mediator = mediator;
         }
 
@@ -27,6 +30,24 @@ namespace Spd.Presentation.Licensing.Controllers
         public async Task<Unit> SaveBusinessLicenceApplication([FromBody][Required] BizLicenceAppUpsertRequest bizUpsertRequest, CancellationToken ct)
         {
             return default;
+        }
+
+        /// <summary>
+        /// Upload business licence application files to transient storage
+        /// </summary>
+        /// <param name="fileUploadRequest"></param>
+        /// <param name="licenceAppId"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        [Route("api/business-licence/{licenceAppId}/files")]
+        [HttpPost]
+        [RequestSizeLimit(26214400)] //25M
+        [Authorize(Policy = "OnlyBcsc")]
+        public async Task<IEnumerable<LicenceAppDocumentResponse>> UploadLicenceAppFiles([FromForm][Required] LicenceAppDocumentUploadRequest fileUploadRequest, [FromRoute] Guid licenceAppId, CancellationToken ct)
+        {
+            var applicantInfo = _currentUser.GetBcscUserIdentityInfo();
+
+            return await _mediator.Send(new CreateDocumentInTransientStoreCommand(fileUploadRequest, applicantInfo.Sub, licenceAppId), ct);
         }
     }
 }
