7982496943a6c3f29367215c91221e8d080054eb
QSL\RubenA <ruben.araya@quartech.com>
2024-04-26 07:32:54 -0700

    Add biz licence application repository

---
+++ src/Spd.Resource.Repository/BizLicenceApplication/BizLicenceApplicationRepository.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Spd.Utilities.Dynamics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Resource.Repository.BizLicenceApplication;
+internal class BizLicenceApplicationRepository : IBizLicenceApplicationRepository
+{
+    private readonly DynamicsContext _context;
+    private readonly IMapper _mapper;
+
+    public BizLicenceApplicationRepository(IDynamicsContextFactory ctx, IMapper mapper)
+    {
+        _context = ctx.CreateChangeOverwrite();
+        _mapper = mapper;
+    }
+
+    public async Task<Guid> CreateBizLicenceApplicationAsync(CreateBizLicenceApplicationCmd cmd, CancellationToken ct)
+    {
+        return Guid.NewGuid();
+    }
+}
