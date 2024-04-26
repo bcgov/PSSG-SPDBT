f6ab80ad6b91f0ab2d8d59dedc6eec8af7753b47
peggy-quartech <peggy.zhang@quartech.com>
2024-04-26 15:29:56 -0700

    remove

--- src/Spd.Engines.Transformation/Documents/IDocumentTransformStrategy.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Spd.Engines.Transformation.Documents;
-
-internal interface IDocumentTransformStrategy
-{
-    bool CanTransform(DocumentTransformRequest request);
-
-    Task<DocumentTransformResponse> Transform(DocumentTransformRequest request, CancellationToken cancellationToken);
-}
