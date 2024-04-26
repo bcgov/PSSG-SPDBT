4639e893943141c9adf5ac689f9b15ec894dc19a
QSL\RubenA <ruben.araya@quartech.com>
2024-04-26 07:33:49 -0700

    Add biz licence application contract

---
+++ src/Spd.Resource.Repository/BizLicenceApplication/Contract.cs
@@ -0,0 +1,18 @@
+﻿using Spd.Resource.Repository.LicenceApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Resource.Repository.BizLicenceApplication;
+
+public partial interface IBizLicenceApplicationRepository
+{
+    public Task<Guid> CreateBizLicenceApplicationAsync(CreateBizLicenceApplicationCmd cmd, CancellationToken ct);
+}
+
+public record CreateBizLicenceApplicationCmd()
+{
+
+};
