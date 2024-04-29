268986e6d739569d7da6f8f6e703bcebac1de1cf
Peggy <peggy.zhang@quartech.com>
2024-04-29 12:22:58 -0700

    finger print request letter printing  (#986)
    # Description
    
    This PR includes the following proposed change(s):
    
    -spdbt-2464-BCMP / OpenShift - Send Fingerprint Letter
    -spdbt-2465- BCMP / OpenShift - Fingerprint Letter Job Status
    
    ---------
    
    Co-authored-by: ytqsl <yossi.tamari@quartech.com>
    Co-authored-by: ytqsl <yossi.tamari@live.com>

---
+++ src/Spd.Manager.Printing/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spd.Manager.Printing.Documents;
+using Spd.Manager.Printing.Documents.TransformationStrategies;
+using Spd.Utilities.Hosting;
+
+namespace Spd.Manager.Printing
+{
+    public class ServiceExtension : IConfigureComponentServices
+    {
+        public void ConfigureServices(ConfigurationServices configurationServices)
+        {
+            configurationServices.Services.AddTransient<IPrintingManager, PrintingManager>();
+            configurationServices.Services.AddScoped<IDocumentTransformationEngine, DocumentTransformationEngine>();
+            configurationServices.Services.AddScoped<IDocumentTransformStrategy, FingerprintLetterTransformStrategy>();
+        }
+    }
+}
---
+++ src/Spd.Resource.Repository.IntegrationTest/OptionSetRepositoryTest.cs
@@ -0,0 +1,45 @@
+using Microsoft.Extensions.DependencyInjection;
+using Spd.Resource.Repository.OptionSet;
+using Spd.Utilities.Dynamics;
+
+namespace Spd.Resource.Repository.IntegrationTest;
+
+public class OptionSetRepositoryTest : IClassFixture<IntegrationTestSetup>
+{
+    private readonly IOptionSetRepository _optionsetRepo;
+    private DynamicsContext _context;
+
+    public OptionSetRepositoryTest(IntegrationTestSetup testSetup)
+    {
+        _optionsetRepo = testSetup.ServiceProvider.GetRequiredService<IOptionSetRepository>();
+        _context = testSetup.ServiceProvider.GetRequiredService<IDynamicsContextFactory>().CreateReadOnly();
+    }
+
+    [Theory]
+    [InlineData(GenderEnum.M, "Male")]
+    [InlineData(GenderEnum.F, "Female")]
+    [InlineData(GenderEnum.U, "Unspecified")]
+    public async Task GetLabelAsync_GenderEnum_Run_Correctly(GenderEnum t, string expected)
+    {
+        //Action
+        var response = await _optionsetRepo.GetLabelAsync(t, CancellationToken.None);
+
+        //Assert
+        Assert.NotNull(response);
+        Assert.Equal(expected, response);
+    }
+
+    [Theory]
+    [InlineData(EmployeeInteractionTypeCode.Adults, "Vulnerable Adults")]
+    [InlineData(EmployeeInteractionTypeCode.ChildrenAndAdults, "Children and Vulnerable Adults")]
+    [InlineData(EmployeeInteractionTypeCode.Children, "Children")]
+    public async Task GetLabelAsync_EmployeeInteractionTypeCode_RunCorrectly(EmployeeInteractionTypeCode t, string expected)
+    {
+        //Action
+        var response = await _optionsetRepo.GetLabelAsync(t, CancellationToken.None);
+
+        //Assert
+        Assert.NotNull(response);
+        Assert.Equal(expected, response);
+    }
+}
