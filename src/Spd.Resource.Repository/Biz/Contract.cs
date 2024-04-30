508916bfb5412080520f68ea8bb0fae4ed8d7af0
QSL\RubenA <ruben.araya@quartech.com>
2024-04-30 15:29:46 -0700

    Add biz branches as part of the response

--- src/Spd.Resource.Repository/Biz/Contract.cs
+++ src/Spd.Resource.Repository/Biz/Contract.cs
@@ -33,6 +33,7 @@
         public string? BizLegalName { get; set; }
         public Guid? BizGuid { get; set; }
         public BizTypeEnum BizType { get; set; }
+        public IEnumerable<Addr>? BranchAddress { get; set; }
     }
     public record BizResult : Biz
     {
