fae137553968866443415eef8eb2216da7af3175
QSL\RubenA <ruben.araya@quartech.com>
2024-05-01 13:42:00 -0700

    Adjust biz id mapping

--- src/Spd.Manager.Licence/Mappings.cs
+++ src/Spd.Manager.Licence/Mappings.cs
@@ -198,7 +198,7 @@ internal class Mappings : Profile
             .ForMember(d => d.BizId, opt => opt.MapFrom(s => s.Id));
 
         CreateMap<BizResult, BizProfileResponse>()
-            .ForMember(d => d.BizId, opt => opt.MapFrom(s => s.Id))
+            .ForMember(d => d.BizId, opt => opt.MapFrom(s => s.BizGuid))
             .ForMember(d => d.BizTradeName, opt => opt.MapFrom(s => s.BizName))
             .ForMember(d => d.BizTypeCode, opt => opt.MapFrom(s => s.BizType))
             .ForMember(d => d.ServiceTypeCodes, opt => opt.MapFrom(s => GetServiceTypeCodes(s.ServiceTypes)))
