f94769803855deb79f23737840057ee96cacc2bc
peggy-quartech <peggy.zhang@quartech.com>
2024-04-30 17:29:50 -0700

    read permit update info

--- src/Spd.Resource.Repository/Licence/Mappings.cs
+++ src/Spd.Resource.Repository/Licence/Mappings.cs
@@ -21,7 +21,23 @@ namespace Spd.Resource.Repository.Licence
              .ForMember(d => d.LicenceStatusCode, opt => opt.MapFrom(s => GetLicenceStatusEnum(s.statuscode)))
              .ForMember(d => d.LicenceHolderLastName, opt => opt.MapFrom(s => s.spd_LicenceHolder_contact.lastname))
              .ForMember(d => d.LicenceHolderMiddleName1, opt => opt.MapFrom(s => s.spd_LicenceHolder_contact.spd_middlename1))
-             .ForMember(d => d.NameOnCard, opt => opt.MapFrom(s => s.spd_nameonlicence));
+             .ForMember(d => d.NameOnCard, opt => opt.MapFrom(s => s.spd_nameonlicence))
+             .ForMember(d => d.PermitOtherRequiredReason, opt => opt.MapFrom(s => s.spd_permitpurposeother))
+             .ForMember(d => d.EmployerName, opt => opt.MapFrom(s => s.spd_employername))
+             .ForMember(d => d.SupervisorName, opt => opt.MapFrom(s => s.spd_employercontactname))
+             .ForMember(d => d.SupervisorEmailAddress, opt => opt.MapFrom(s => s.spd_employeremail))
+             .ForMember(d => d.SupervisorPhoneNumber, opt => opt.MapFrom(s => s.spd_employerphonenumber))
+             .ForMember(d => d.EmployerPrimaryAddress, opt => opt.MapFrom(s => s))
+             .ForMember(d => d.Rationale, opt => opt.MapFrom(s => s.spd_rationale))
+             .ForMember(d => d.PermitPurposeEnums, opt => opt.MapFrom(s => SharedMappingFuncs.GetPermitPurposeEnums(s.spd_permitpurpose)));
+
+            _ = CreateMap<spd_licence, Addr>()
+             .ForMember(d => d.AddressLine1, opt => opt.MapFrom(s => s.spd_employeraddress1))
+             .ForMember(d => d.AddressLine2, opt => opt.MapFrom(s => s.spd_employeraddress2))
+             .ForMember(d => d.City, opt => opt.MapFrom(s => s.spd_employercity))
+             .ForMember(d => d.Province, opt => opt.MapFrom(s => s.spd_employerprovince))
+             .ForMember(d => d.Country, opt => opt.MapFrom(s => s.spd_employercountry))
+             .ForMember(d => d.PostalCode, opt => opt.MapFrom(s => s.spd_employerpostalcode));
         }
 
         internal static LicenceStatusEnum? GetLicenceStatusEnum(int? optionset)
--- src/Spd.Resource.Repository/SharedMappingFuncs.cs
+++ src/Spd.Resource.Repository/SharedMappingFuncs.cs
@@ -135,4 +135,18 @@ internal static class SharedMappingFuncs
         if (optionset == null) return null;
         return Enum.Parse<PoliceOfficerRoleEnum>(Enum.GetName(typeof(PoliceOfficerRoleOptionSet), optionset));
     }
+
+    internal static string? GetPermitPurposeOptionSets(IEnumerable<PermitPurposeEnum>? permitPurposes)
+    {
+        if (permitPurposes == null) return null;
+        var result = String.Join(',', permitPurposes.Select(p => ((int)Enum.Parse<PermitPurposeOptionSet>(p.ToString())).ToString()).ToArray());
+        return string.IsNullOrWhiteSpace(result) ? null : result;
+    }
+
+    internal static IEnumerable<PermitPurposeEnum> GetPermitPurposeEnums(string? optionsetStr)
+    {
+        if (optionsetStr == null) return null;
+        string[] strs = optionsetStr.Split(',');
+        return strs.Select(s => Enum.Parse<PermitPurposeEnum>(Enum.GetName(typeof(PermitPurposeOptionSet), Int32.Parse(s)))).ToList();
+    }
 }
