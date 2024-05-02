b071771c96a07502807d43e70f46db38d133d23c
QSL\RubenA <ruben.araya@quartech.com>
2024-05-02 11:29:37 -0700

    Add biz update request validator

---
+++ src/Spd.Manager.Licence/BizProfileValidation.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+
+namespace Spd.Manager.Licence;
+public class BizProfileUpdateRequestValidator : AbstractValidator<BizProfileUpdateRequest>
+{
+    public BizProfileUpdateRequestValidator()
+    {
+        RuleFor(r => r.BizId).NotEmpty();
+        RuleFor(r => r.BizTypeCode).NotEmpty().IsInEnum();
+        RuleFor(r => r.BizBCAddress).NotEmpty();
+        RuleFor(r => r.BizMailingAddress)
+            .NotEmpty()
+            .When(r => r.MailingAddressIsSameBizAddress == false);
+    }
+}
