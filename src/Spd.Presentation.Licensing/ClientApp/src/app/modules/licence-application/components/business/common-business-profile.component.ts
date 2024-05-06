59095f9cb236030efcd10047414f19ea041c0d65
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-05-06 14:41:48 -0700

    Business profile integration (#1013)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Business profile GET integration

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/common-business-profile.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/common-business-profile.component.ts
@@ -15,7 +15,7 @@ import { CommonBusinessInformationComponent } from './common-business-informatio
 							<mat-panel-title>Business Information</mat-panel-title>
 						</mat-expansion-panel-header>
 
-						<div class="my-3">
+						<div class="mt-3">
 							<section>
 								<app-common-business-information
 									[form]="businessInformationFormGroup"
@@ -33,7 +33,7 @@ import { CommonBusinessInformationComponent } from './common-business-informatio
 							<mat-panel-title>Business Address</mat-panel-title>
 						</mat-expansion-panel-header>
 
-						<div class="my-3">
+						<div class="mt-3">
 							<app-alert type="info" icon="" [showBorder]="false">
 								This is the address of the business's primary location
 							</app-alert>
@@ -50,14 +50,14 @@ import { CommonBusinessInformationComponent } from './common-business-informatio
 				</mat-accordion>
 			</div>
 
-			<div class="col-lg-6 col-md-12" *ngIf="businessAddressIsNotInBc">
+			<div class="col-lg-6 col-md-12" *ngIf="!isBcBusinessAddress">
 				<mat-accordion>
 					<mat-expansion-panel class="mb-3" [expanded]="true" [disabled]="true">
 						<mat-expansion-panel-header>
 							<mat-panel-title>B.C. Business Address</mat-panel-title>
 						</mat-expansion-panel-header>
 
-						<div class="my-3">
+						<div class="mt-3">
 							<app-alert type="info" icon="" [showBorder]="false">
 								Provide an address in British Columbia for document service
 							</app-alert>
@@ -81,7 +81,7 @@ import { CommonBusinessInformationComponent } from './common-business-informatio
 							<mat-panel-title>Mailing Address</mat-panel-title>
 						</mat-expansion-panel-header>
 
-						<div class="my-3">
+						<div class="mt-3">
 							<app-alert type="info" icon="" [showBorder]="false">
 								Provide your mailing address, if different from your business address
 							</app-alert>
@@ -113,7 +113,7 @@ import { CommonBusinessInformationComponent } from './common-business-informatio
 							<mat-panel-title>Branches in B.C.</mat-panel-title>
 						</mat-expansion-panel-header>
 
-						<div class="my-3">
+						<div class="mt-3">
 							<app-alert type="info" icon="" [showBorder]="false"
 								>Branches in B.C. where licenced employees work</app-alert
 							>
@@ -126,92 +126,17 @@ import { CommonBusinessInformationComponent } from './common-business-informatio
 				</mat-accordion>
 			</div>
 		</div>
-
-		<!-- <div class="text-minor-heading pt-2 pb-3">Business Information</div>
-		<section>
-			<app-common-business-information [form]="businessInformationFormGroup"></app-common-business-information>
-		</section>
-
-		<div class="row mt-3">
-			<div class="col-lg-6 col-md-12">
-				<mat-divider class="mat-divider-main"></mat-divider>
-				<div class="text-minor-heading pt-2 pb-3">Business Address</div>
-				<app-alert type="info" icon="" [showBorder]="false">
-					This is the address of the business's primary location
-				</app-alert>
-
-				<section>
-					<app-common-business-address
-						[form]="businessAddressFormGroup"
-						[isWizardStep]="false"
-						[isReadonly]="isReadonly"
-					></app-common-business-address>
-				</section>
-			</div>
-
-			<div class="col-lg-6 col-md-12" *ngIf="businessAddressIsNotInBc">
-				<mat-divider class="mat-divider-main"></mat-divider>
-				<div class="text-minor-heading pt-2 pb-3">B.C. Business Address</div>
-				<app-alert type="info" icon="" [showBorder]="false">
-					Provide an address in British Columbia for document service
-				</app-alert>
-
-				<section>
-					<app-common-address
-						[form]="bcBusinessAddressFormGroup"
-						[isWizardStep]="false"
-						[isReadonly]="isReadonly"
-					></app-common-address>
-				</section>
-			</div>
-
-			<div class="col-lg-6 col-md-12">
-				<mat-divider class="mat-divider-main"></mat-divider>
-				<div class="text-minor-heading pt-2 pb-3">Mailing Address</div>
-				<app-alert type="info" icon="" [showBorder]="false">
-					Provide your mailing address, if different from your business address
-				</app-alert>
-
-				<ng-container *ngIf="isMailingTheSame; else mailingIsDifferentSection">
-					<div class="mb-3">
-						<mat-icon style="vertical-align: bottom;">label_important</mat-icon> The business address and mailing
-						address are the same
-					</div>
-				</ng-container>
-				<ng-template #mailingIsDifferentSection>
-					<section>
-						<app-common-address
-							[form]="mailingAddressFormGroup"
-							[isWizardStep]="false"
-							[isReadonly]="isReadonly"
-						></app-common-address>
-					</section>
-				</ng-template>
-			</div>
-		</div>
-
-		<div class="row mt-3">
-			<div class="col-12">
-				<mat-divider class="mat-divider-main"></mat-divider>
-				<div class="text-minor-heading pt-2 pb-3">Branches in B.C.</div>
-				<app-alert type="info" icon="" [showBorder]="false">Branches in B.C. where licenced employees work</app-alert>
-
-				<app-common-business-bc-branches [form]="branchesInBcFormGroup"></app-common-business-bc-branches>
-			</div>
-		</div> -->
 	`,
 	styles: [],
 })
 export class CommonBusinessProfileComponent implements LicenceChildStepperStepComponent {
-	// @Input() isReadonlyPersonalInfo!: boolean;
-	// @Input() isReadonlyMailingAddress!: boolean;
-	isReadonly = false; //TODO update to support read only mode
-
 	@Input() businessInformationFormGroup!: FormGroup;
 	@Input() businessAddressFormGroup!: FormGroup;
 	@Input() bcBusinessAddressFormGroup!: FormGroup;
 	@Input() mailingAddressFormGroup!: FormGroup;
 	@Input() branchesInBcFormGroup!: FormGroup;
+	@Input() isReadonly = true;
+	@Input() isBcBusinessAddress = true;
 
 	@ViewChild(CommonBusinessInformationComponent) businessInformationComponent!: CommonBusinessInformationComponent;
 	@ViewChild(CommonBusinessBcBranchesComponent) businessBcBranchesComponent!: CommonBusinessBcBranchesComponent;
@@ -219,7 +144,7 @@ export class CommonBusinessProfileComponent implements LicenceChildStepperStepCo
 	isFormValid(): boolean {
 		const isValid1 = this.businessInformationComponent.isFormValid();
 		const isValid2 = this.isFormGroupValid(this.businessAddressFormGroup);
-		const isValid3 = this.businessAddressIsNotInBc ? this.isFormGroupValid(this.bcBusinessAddressFormGroup) : true;
+		const isValid3 = this.isBcBusinessAddress ? this.isFormGroupValid(this.bcBusinessAddressFormGroup) : true;
 		const isValid4 = this.isMailingTheSame ? true : this.isFormGroupValid(this.mailingAddressFormGroup);
 		const isValid5 = this.businessBcBranchesComponent.isFormValid();
 
@@ -236,9 +161,5 @@ export class CommonBusinessProfileComponent implements LicenceChildStepperStepCo
 
 	get isMailingTheSame(): boolean {
 		return this.businessAddressFormGroup.get('isMailingTheSame')?.value ?? false;
-	}
-	get businessAddressIsNotInBc(): boolean {
-		const formValue = this.businessAddressFormGroup.value;
-		return formValue.addressSelected && formValue.province != 'British Columbia';
 	}
 }
