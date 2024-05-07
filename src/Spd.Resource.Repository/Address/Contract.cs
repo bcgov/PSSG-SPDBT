341c0129bee8b51511ffeae1a56d6e3392ca8d5b
QSL\RubenA <ruben.araya@quartech.com>
2024-05-07 14:02:37 -0700

    Move methods delete and update address to independent repository

---
+++ src/Spd.Resource.Repository/Address/Contract.cs
@@ -0,0 +1,13 @@
+﻿using Spd.Resource.Repository.Biz;
+
+namespace Spd.Resource.Repository.Address;
+public interface IAddressRepository
+{
+    Task DeleteAddressesAsync(List<Guid?> addressIds, CancellationToken cancellationToken);
+    Task UpdateAddressAsync(UpdateAddressCmd cmd, CancellationToken ct);
+}
+
+public record UpdateAddressCmd
+{
+    public IEnumerable<BranchAddr> Addresses { get; set; } = [];
+}
--- src/Spd.Resource.Repository/Biz/BizRepository.cs
+++ src/Spd.Resource.Repository/Biz/BizRepository.cs
@@ -77,50 +77,6 @@ namespace Spd.Resource.Repository.Biz
             };
         }
 
-        public async Task DeleteAddressesAsync(List<Guid?> addressIds, CancellationToken ct)
-        {
-            foreach (var addressId in addressIds)
-            {
-                spd_address? address = _context.spd_addresses.Where(a =>
-                    a.spd_addressid == addressId &&
-                    a.statecode == DynamicsConstants.StateCode_Active
-                ).FirstOrDefault();
-
-                if (address == null)
-                {
-                    _logger.LogError($"Address to be deleted was not found");
-                    throw new ArgumentException("cannot find address to be deleted");
-                }
-
-                address.statecode = DynamicsConstants.StateCode_Inactive;
-                address.statuscode = DynamicsConstants.StatusCode_Inactive;
-                _context.UpdateObject(address);
-            }
-
-            await _context.SaveChangesAsync(ct);
-        }
-
-        public async Task UpdateAddressAsync(UpdateAddressCmd cmd, CancellationToken ct)
-        {
-            foreach (BranchAddr address in cmd.Addresses)
-            {
-                spd_address? existingAddress = _context.spd_addresses.Where(a =>
-                    a.spd_addressid == address.BranchId &&
-                    a.statecode == DynamicsConstants.StateCode_Active
-                ).FirstOrDefault();
-            
-                if (existingAddress == null)
-                {
-                    _logger.LogError($"Address to be updated was not found");
-                    throw new ArgumentException("cannot find address to be updated");
-                }
-
-                _mapper.Map(address, existingAddress);
-                _context.UpdateObject(existingAddress);
-            }
-            await _context.SaveChangesAsync(ct);
-        }
-
         private async Task<BizResult?> BizUpdateAsync(BizUpdateCmd updateBizCmd, CancellationToken ct)
         {
             IQueryable<account> accounts = _context.accounts.Expand(a => a.spd_Organization_Addresses)
