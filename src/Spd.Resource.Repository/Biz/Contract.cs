3042944f72a3d7bf3f930302e2c851da4ac87bfc
QSL\RubenA <ruben.araya@quartech.com>
2024-05-08 12:34:45 -0700

    Adjust definition of branch addresses field

--- src/Spd.Resource.Repository/Biz/Contract.cs
+++ src/Spd.Resource.Repository/Biz/Contract.cs
@@ -29,7 +29,7 @@
         public string? BizLegalName { get; set; }
         public Guid? BizGuid { get; set; }
         public BizTypeEnum BizType { get; set; }
-        public IEnumerable<BranchAddr>? BranchAddresses { get; set; }
+        public IEnumerable<BranchAddr> BranchAddresses { get; set; } = Array.Empty<BranchAddr>();
     }
     public record BizResult : Biz
     {
