0ec634774f77650aca56b394cd26c02c39ade627
QSL\RubenA <ruben.araya@quartech.com>
2024-05-09 15:50:03 -0700

    Add biz profile update endpoint

--- src/Spd.Presentation.Licensing/Controllers/BizProfileController.cs
+++ src/Spd.Presentation.Licensing/Controllers/BizProfileController.cs
@@ -1,7 +1,11 @@
-﻿using MediatR;
+﻿using FluentValidation;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Spd.Manager.Licence;
+using Spd.Utilities.Shared.Exceptions;
+using System.Net;
+using System.Text.Json;
 
 namespace Spd.Presentation.Licensing.Controllers;
 
@@ -9,10 +13,13 @@ namespace Spd.Presentation.Licensing.Controllers;
 public class BizProfileController : ControllerBase
 {
     private readonly IMediator _mediator;
+    private readonly IValidator<BizProfileUpdateRequest> _bizProfileUpdateRequestValidator;
 
-    public BizProfileController(IMediator mediator)
+    public BizProfileController(IMediator mediator,
+    IValidator<BizProfileUpdateRequest> bizProfileUpdateRequestValidator)
     {
         _mediator = mediator;
+        _bizProfileUpdateRequestValidator = bizProfileUpdateRequestValidator;
     }
 
     /// <summary>
@@ -27,5 +34,30 @@ public class BizProfileController : ControllerBase
     public async Task<BizProfileResponse> GetProfile([FromRoute] Guid id, CancellationToken ct)
     {
         return await _mediator.Send(new GetBizProfileQuery(id), ct);
+    }
+
+    /// <summary>
+    /// Update Business profile
+    /// </summary>
+    /// <param name="bizId"></param>
+    /// <param name="request"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
+    [Route("api/biz/{bizId}")]
+    [Authorize(Policy = "OnlyBceid")]
+    [HttpPut]
+    public async Task<Guid> UpdateBizProfile([FromRoute] string bizId, BizProfileUpdateRequest request, CancellationToken ct)
+    {
+        if (!Guid.TryParse(bizId, out Guid bizGuidId))
+            throw new ApiException(HttpStatusCode.BadRequest, $"{nameof(bizId)} is not a valid guid.");
+
+        var validateResult = await _bizProfileUpdateRequestValidator.ValidateAsync(request, ct);
+        if (!validateResult.IsValid)
+            throw new ApiException(HttpStatusCode.BadRequest, JsonSerializer.Serialize(validateResult.Errors));
+
+        BizProfileUpdateCommand command = new(bizGuidId, request);
+        await _mediator.Send(command, ct);
+
+        return bizGuidId;
     }
 }
