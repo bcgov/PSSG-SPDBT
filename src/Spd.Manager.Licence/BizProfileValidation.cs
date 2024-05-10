067cc981eb6665bee8aefe444b4f4c224902b91f
QSL\RubenA <ruben.araya@quartech.com>
2024-05-10 09:48:10 -0700

    Add additional validations for biz profile update request

--- src/Spd.Manager.Licence/BizProfileValidation.cs
+++ src/Spd.Manager.Licence/BizProfileValidation.cs
@@ -5,10 +5,14 @@ public class BizProfileUpdateRequestValidator : AbstractValidator<BizProfileUpda
 {
     public BizProfileUpdateRequestValidator()
     {
+        RuleFor(r => r.BizTradeName).NotEmpty();
         RuleFor(r => r.BizTypeCode).NotEmpty().IsInEnum();
         RuleFor(r => r.BizBCAddress).NotEmpty();
         RuleFor(r => r.BizMailingAddress)
             .NotEmpty()
+            .When(r => r.MailingAddressIsSameBizAddress == false);
+        RuleFor(r => r)
+            .Must(r => r.BizMailingAddress.AddressLine1 != r.BizBCAddress.AddressLine1)
             .When(r => r.MailingAddressIsSameBizAddress == false);
     }
 }
