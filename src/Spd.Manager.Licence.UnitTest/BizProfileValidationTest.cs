bf6681b7d29fecdc9b4e564f53834ade2410b885
QSL\RubenA <ruben.araya@quartech.com>
2024-05-10 09:55:49 -0700

    Add unit test for biz profile update request validator

--- src/Spd.Manager.Licence.UnitTest/BizProfileValidationTest.cs
+++ src/Spd.Manager.Licence.UnitTest/BizProfileValidationTest.cs
@@ -41,4 +41,25 @@ public class BizProfileValidationTest
         var result = validator.TestValidate(model);
         result.ShouldNotHaveAnyValidationErrors();
     }
+
+    [Fact]
+    public void MailingAddressEmpty_WhenIsNotTheSameAsBizAddress_ShouldThrowException()
+    {
+        var address = fixture.Build<Address>()
+            .With(a => a.AddressLine1, new string('a', 100))
+            .With(a => a.City, new string('a', 100))
+            .With(a => a.Country, new string('a', 100))
+            .With(a => a.PostalCode, new string('a', 20))
+            .Create();
+
+        var model = fixture.Build<BizProfileUpdateRequest>()
+            .With(r => r.BizTypeCode, BizTypeCode.NonRegisteredPartnership)
+            .With(r => r.BizBCAddress, address)
+            .With(r => r.MailingAddressIsSameBizAddress, false)
+            .Without(r => r.BizMailingAddress)
+            .Create();
+
+        var result = validator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(r => r.BizMailingAddress);
+    }
 }
