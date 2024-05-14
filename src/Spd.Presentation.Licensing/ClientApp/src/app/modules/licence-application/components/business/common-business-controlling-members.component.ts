724e1685241823d20ee552fd720301c01ed27ebe
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-05-14 13:52:38 -0700

    biz licence lookup (#1035)
    # Description
    
    This PR includes the following proposed change(s):
    
    - update handling of licence lookup. Move to common service

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/common-business-controlling-members.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/common-business-controlling-members.component.ts
@@ -7,7 +7,7 @@ import { UtilService } from '@app/core/services/util.service';
 import { LicenceChildStepperStepComponent } from '@app/modules/licence-application/services/licence-application.helper';
 import { DialogComponent, DialogOptions } from '@app/shared/components/dialog.component';
 import { HotToastService } from '@ngneat/hot-toast';
-import { ModalMemberWithSwlAddComponent } from './modal-member-with-swl-add.component';
+import { LookupSwlDialogData, ModalLookupSwlComponent } from './modal-lookup-swl.component';
 import { ModalMemberWithoutSwlEditComponent } from './modal-member-without-swl-edit.component';
 
 @Component({
@@ -17,11 +17,11 @@ import { ModalMemberWithoutSwlEditComponent } from './modal-member-without-swl-e
 			<div class="row mt-4" *ngIf="dataSource.data.length > 0">
 				<div class="col-12">
 					<mat-table [dataSource]="dataSource">
-						<ng-container matColumnDef="fullName">
+						<ng-container matColumnDef="licenceHolderName">
 							<mat-header-cell *matHeaderCellDef>Full Name</mat-header-cell>
 							<mat-cell *matCellDef="let member">
 								<span class="mobile-label">Full Name:</span>
-								{{ member | fullname | default }}
+								{{ member.licenceHolderName | default }}
 							</mat-cell>
 						</ng-container>
 
@@ -33,11 +33,11 @@ import { ModalMemberWithoutSwlEditComponent } from './modal-member-without-swl-e
 							</mat-cell>
 						</ng-container>
 
-						<ng-container matColumnDef="status">
+						<ng-container matColumnDef="licenceStatusCode">
 							<mat-header-cell *matHeaderCellDef>Status</mat-header-cell>
 							<mat-cell *matCellDef="let member">
 								<span class="mobile-label">Status:</span>
-								{{ member.status | default }}
+								{{ member.licenceStatusCode | default }}
 							</mat-cell>
 						</ng-container>
 
@@ -135,7 +135,15 @@ export class CommonBusinessControllingMembersComponent implements OnInit, Licenc
 	memberList: Array<any> = [];
 
 	dataSource!: MatTableDataSource<any>;
-	columns: string[] = ['fullName', 'licenceNumber', 'status', 'expiryDate', 'clearanceStatus', 'action1', 'action2'];
+	columns: string[] = [
+		'licenceHolderName',
+		'licenceNumber',
+		'licenceStatusCode',
+		'expiryDate',
+		'clearanceStatus',
+		'action1',
+		'action2',
+	];
 
 	constructor(private dialog: MatDialog, private utilService: UtilService, private hotToastService: HotToastService) {}
 
@@ -171,10 +179,14 @@ export class CommonBusinessControllingMembersComponent implements OnInit, Licenc
 	}
 
 	onAddMemberWithSWL(): void {
+		const dialogOptions: LookupSwlDialogData = {
+			title: 'Add Member with Security Worker Licence',
+			notValidSwlMessage: `'Cancel' to exit this dialog and then add them as a member without a security worker licence to proceed.`,
+		};
 		this.dialog
-			.open(ModalMemberWithSwlAddComponent, {
+			.open(ModalLookupSwlComponent, {
 				width: '800px',
-				data: {}, //dialogOptions,
+				data: dialogOptions,
 			})
 			.afterClosed()
 			.subscribe((resp: any) => {
