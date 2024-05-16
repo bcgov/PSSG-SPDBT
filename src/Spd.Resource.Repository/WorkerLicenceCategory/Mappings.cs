098fd78c033527e2398c5c39f7cf4aef9bf31131
Peggy <peggy.zhang@quartech.com>
2024-05-15 17:11:19 -0700

    printing preview and resize (#1038)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spdbt-2450(BCMP/ OpenShift - Card Preview functionality)
    - spdbt-2451(BCMP / OpenShift - Photo image resize functionality)

---
+++ src/Spd.Resource.Repository/WorkerLicenceCategory/Mappings.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.Dynamics.CRM;
+using Spd.Resource.Repository.LicenceApplication;
+using Spd.Utilities.Dynamics;
+
+namespace Spd.Resource.Repository.WorkerLicenceCategory;
+
+internal class Mappings : Profile
+{
+    public Mappings()
+    {
+        _ = CreateMap<spd_licencecategory, WorkerLicenceCategoryResp>()
+         .ForMember(d => d.WorkerCategoryTypeName, opt => opt.MapFrom(s => s.spd_categoryname))
+         .ForMember(d => d.Id, opt => opt.MapFrom(s => s.spd_licencecategoryid))
+         .ForMember(d => d.WorkerCategoryTypeCode, opt => opt.MapFrom(s => GeWorkerCategoryTypeCode(s.spd_licencecategoryid)));
+    }
+
+    internal static WorkerCategoryTypeEnum GeWorkerCategoryTypeCode(Guid? workerCatgoryId)
+    {
+        string key = DynamicsContextLookupHelpers.LicenceCategoryDictionary.FirstOrDefault(s => s.Value == workerCatgoryId).Key;
+        return Enum.Parse<WorkerCategoryTypeEnum>(key);
+    }
+}
+
