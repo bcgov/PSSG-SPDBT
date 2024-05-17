f11d51e0ffea522b648c1442a2510bd485fdb368
QSL\RubenA <ruben.araya@quartech.com>
2024-05-17 16:03:25 -0700

    Add unit test for get licence by id query service

--- src/Spd.Manager.Licence.UnitTest/LicenceManagerTest.cs
+++ src/Spd.Manager.Licence.UnitTest/LicenceManagerTest.cs
@@ -4,6 +4,7 @@ using Moq;
 using Spd.Manager.Shared;
 using Spd.Resource.Repository.Document;
 using Spd.Resource.Repository.Licence;
+using Spd.Resource.Repository.LicenceApplication;
 using Spd.Utilities.FileStorage;
 using Spd.Utilities.Shared.Exceptions;
 
@@ -194,5 +195,24 @@ public class LicenceManagerTest
         var result = await sut.Handle(request, CancellationToken.None);
 
         Assert.Equal(applicantId, result.First().LicenceHolderId);
+    }
+
+    [Fact]
+    public async void Handle_LicenceByIdQuery_Return_LicenceResponse()
+    {
+        Guid licenceId = Guid.NewGuid();
+        LicenceResp licenceResp = fixture.Build<LicenceResp>()
+            .With(r => r.LicenceId, licenceId)
+            .With(r => r.PermitPurposeEnums, new List<PermitPurposeEnum>() { PermitPurposeEnum.ProtectionOfPersonalProperty })
+            .Create();
+
+        mockLicRepo.Setup(m => m.GetAsync(It.Is<Guid>(g => g.Equals(licenceId)), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(licenceResp);
+
+        LicenceByIdQuery request = new(licenceId);
+
+        var result = await sut.Handle(request, CancellationToken.None);
+
+        Assert.Equal(licenceId, result.LicenceId);
     }
 }
