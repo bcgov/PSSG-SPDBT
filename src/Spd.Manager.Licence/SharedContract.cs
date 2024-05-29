ef4f1af6f332c8034592c08f908ab21e226583c9
QSL\RubenA <ruben.araya@quartech.com>
2024-05-29 11:37:43 -0700

    Create new shared component licence application base

--- src/Spd.Manager.Licence/SharedContract.cs
+++ src/Spd.Manager.Licence/SharedContract.cs
@@ -1,3 +1,5 @@
+using Spd.Manager.Shared;
+
 namespace Spd.Manager.Licence;
 
 public class UploadFileRequest
@@ -196,4 +198,12 @@ public record ContactInfo
     public string? MiddleName1 { get; set; }
     public string? MiddleName2 { get; set; }
     public string? Surname { get; set; }
+}
+
+public record LicenceAppBase
+{
+    public WorkerLicenceTypeCode? WorkerLicenceTypeCode { get; set; }
+    public ApplicationTypeCode? ApplicationTypeCode { get; set; }
+    public BizTypeCode? BizTypeCode { get; set; }
+    public LicenceTermCode? LicenceTermCode { get; set; } //for biz licence term, only 1,2,3 year
 }
