6a97d68fa8407b801c39b83a359db9d69b73e317
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-05-30 10:48:14 -0700

    Screening SPDBT-2554 - update wording (#1084)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Screening SPDBT-2554 - update wording

--- src/Spd.Presentation.Screening/ClientApp/src/app/shared/components/screening-application-steps/step-components/sa-checklist.component.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/shared/components/screening-application-steps/step-components/sa-checklist.component.ts
@@ -9,39 +9,26 @@ import { PayerPreferenceTypeCode } from 'src/app/api/models';
 				<app-step-title title="To submit this criminal record check form, you will need:"></app-step-title>
 				<div class="row">
 					<div class="offset-lg-2 col-lg-8 col-md-12 col-sm-12 mx-auto">
-						<ul *ngIf="payeeType === payerPreferenceTypeCodes.Applicant">
+						<ul>
 							<li>
-								Your BC Services Card Login (recommended), or other
+								Your BC Services Card Login (recommended) or other
 								<a
 									href="https://www2.gov.bc.ca/gov/content/safety/crime-prevention/criminal-record-check/law-policy"
 									target="_blank"
-									>government-issued photo identification</a
+									>government issued photo identification</a
 								>.
 							</li>
 							<li>
 								If you do not have the BC Services Card Login, you can
 								<a href="https://id.gov.bc.ca/account/setup-instruction" target="_blank">set it up now</a> and return to
-								this application process when you have completed that process.
+								this application when you have completed that process.
 							</li>
-							<li *ngIf="isCrrpa">
-								A method of payment (Visa, Master Card, American Express, Visa Debit, Mastercard Debit). The criminal
-								record fee is non-refundable.
-							</li>
-						</ul>
-						<ul *ngIf="payeeType === payerPreferenceTypeCodes.Organization">
-							<li>
-								Your BC Services Card Login (recommended), or other
-								<a
-									href="https://www2.gov.bc.ca/gov/content/safety/crime-prevention/criminal-record-check/law-policy"
-									target="_blank"
-									>government issued identification</a
-								>.
-							</li>
-							<li>
-								If you do not have the BC Services Card Login, you can
-								<a href="https://id.gov.bc.ca/account/setup-instruction" target="_blank">set it up now</a> and return to
-								this application process when you have completed that process.
-							</li>
+							<ng-container *ngIf="isCrrpa && payeeType === payerPreferenceTypeCodes.Applicant">
+								<li>
+									A method of payment (Visa, Mastercard, American Express, Visa Debit, Mastercard Debit). The criminal
+									record fee is non-refundable.
+								</li>
+							</ng-container>
 						</ul>
 					</div>
 				</div>
