0dd3694b40bcbed0fbc75d2b7b5829ac9471d84b
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-06-03 12:17:14 -0700

    Business Licence - Partial save (#1097)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Use licence lookup component globally (for expired licence lookup)
    - Business Licence - partial save, resume 
    - Sole Proprietor and non-sole proprietor flows
    
    ---------
    
    Co-authored-by: peggy-quartech <peggy.zhang@quartech.com>

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/step-business-licence-liability.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/licence-application/components/business/step-business-licence-liability.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { LicenceDocumentTypeCode } from '@app/api/models';
+import { FileUploadComponent } from '@app/shared/components/file-upload.component';
+import { HotToastService } from '@ngneat/hot-toast';
 import { BusinessApplicationService } from '../../services/business-application.service';
 import { LicenceChildStepperStepComponent } from '../../services/licence-application.helper';
 
@@ -53,30 +56,32 @@ import { LicenceChildStepperStepComponent } from '../../services/licence-applica
 export class StepBusinessLicenceLiabilityComponent implements LicenceChildStepperStepComponent {
 	form = this.businessApplicationService.liabilityFormGroup;
 
-	constructor(private businessApplicationService: BusinessApplicationService) {}
+	@ViewChild(FileUploadComponent) fileUploadComponent!: FileUploadComponent;
 
-	onFileUploaded(_file: File): void {
-		// if (this.authenticationService.isLoggedIn()) {
-		// 	const proofTypeCode =
-		// 		this.isCanadianCitizen.value == BooleanTypeCode.Yes
-		// 			? this.canadianCitizenProofTypeCode.value
-		// 			: this.isCanadianResident.value;
-		// 	this.permitApplicationService.addUploadDocument(proofTypeCode, file).subscribe({
-		// 		next: (resp: any) => {
-		// 			const matchingFile = this.attachments.value.find((item: File) => item.name == file.name);
-		// 			matchingFile.documentUrlId = resp.body[0].documentUrlId;
-		// 		},
-		// 		error: (error: any) => {
-		// 			console.log('An error occurred during file upload', error);
-		// 			this.hotToastService.error('An error occurred during the file upload. Please try again.');
-		// 			this.fileUploadComponent.removeFailedFile(file);
-		// 		},
-		// 	});
-		// }
+	constructor(
+		private hotToastService: HotToastService,
+		private businessApplicationService: BusinessApplicationService
+	) {}
+
+	onFileUploaded(file: File): void {
+		this.businessApplicationService.hasValueChanged = true;
+		if (this.businessApplicationService.isAutoSave()) {
+			this.businessApplicationService.addUploadDocument(LicenceDocumentTypeCode.BizInsurance, file).subscribe({
+				next: (resp: any) => {
+					const matchingFile = this.attachments.value.find((item: File) => item.name == file.name);
+					matchingFile.documentUrlId = resp.body[0].documentUrlId;
+				},
+				error: (error: any) => {
+					console.log('An error occurred during file upload', error);
+					this.hotToastService.error('An error occurred during the file upload. Please try again.');
+					this.fileUploadComponent.removeFailedFile(file);
+				},
+			});
+		}
 	}
 
 	onFileRemoved(): void {
-		// this.permitApplicationService.hasValueChanged = true;
+		this.businessApplicationService.hasValueChanged = true;
 	}
 
 	isFormValid(): boolean {
