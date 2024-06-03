a230a506c5f9c430ff0e5446c671534ff08da102
QSL\RubenA <ruben.araya@quartech.com>
2024-06-03 10:35:58 -0700

    Add business licence application request validator

---
+++ src/Spd.Manager.Licence/BizLicAppValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+
+namespace Spd.Manager.Licence;
+public class BizLicAppSubmitRequestValidator : AbstractValidator<BizLicAppUpsertRequest>
+{
+    public BizLicAppSubmitRequestValidator()
+    {
+        RuleFor(r => r.BizId).NotEqual(Guid.Empty);
+        RuleFor(r => r.HasExpiredLicence).NotEmpty();
+        RuleFor(r => r.ExpiredLicenceId)
+            .NotEmpty()
+            .When(r => r.HasExpiredLicence == true);
+        RuleFor(r => r.NoBranding).NotEmpty();
+        RuleFor(r => r.UseDogs).NotEmpty();
+        RuleFor(r => r.ApplicantIsBizManager).NotEmpty();
+        RuleFor(r => r.BizManagerContactInfo).NotEmpty();
+        RuleFor(r => r.ApplicantContactInfo)
+            .NotEmpty()
+            .When(r => r.ApplicantIsBizManager == false);
+        RuleFor(r => r.WorkerLicenceTypeCode).NotEmpty();
+
+
+        RuleFor(r => r.ApplicationTypeCode).NotEmpty();
+        RuleFor(r => r.BizTypeCode).NotEmpty();
+        RuleFor(r => r.LicenceTermCode).NotEmpty();
+    }
+}
