8f6b60fdbb2331182d48c635e978c92d79364681
QSL\RubenA <ruben.araya@quartech.com>
2024-06-11 14:12:58 -0700

    Revert changes related to business type code, as it will be preserved as part of the upsert request

--- src/Spd.Manager.Licence/BizLicAppValidation.cs
+++ src/Spd.Manager.Licence/BizLicAppValidation.cs
@@ -16,6 +16,7 @@ public class BizLicAppSubmitRequestValidator : AbstractValidator<BizLicAppUpsert
             .NotEmpty()
             .When(r => r.HasExpiredLicence == true);
         RuleFor(r => r.NoBranding).NotEmpty();
+        RuleFor(r => r.BizTypeCode).NotEmpty();
         RuleFor(r => r.UseDogs)
             .NotEmpty()
             .When(r => r.CategoryCodes.Contains(WorkerCategoryTypeCode.SecurityGuard));
