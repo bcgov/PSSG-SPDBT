844d5114a97046025d84b787bc686710a3c330b9
Peggy <peggy.zhang@quartech.com>
2024-06-11 16:36:38 -0700

    fix 2687-Unauthorised User Licensing details not popping up for the first time user login. (#1139)
    # Description
    
    This PR includes the following proposed change(s):
    
    - fix 2687 -Unauthorised User Licensing details not popping up for the
    first time user login.

--- src/Spd.Resource.Repository.IntegrationTest/ContactRepositoryTest.cs
+++ src/Spd.Resource.Repository.IntegrationTest/ContactRepositoryTest.cs
@@ -13,7 +13,7 @@ public class ContactRepositoryTest : IClassFixture<IntegrationTestSetup>
     public ContactRepositoryTest(IntegrationTestSetup testSetup)
     {
         _contactRepository = testSetup.ServiceProvider.GetService<IContactRepository>();
-        _context = testSetup.ServiceProvider.GetRequiredService<IDynamicsContextFactory>().Create();
+        _context = testSetup.ServiceProvider.GetRequiredService<IDynamicsContextFactory>().CreateChangeOverwrite();
     }
 
     [Fact]
@@ -31,23 +31,91 @@ public class ContactRepositoryTest : IClassFixture<IntegrationTestSetup>
         _context.AddTospd_licences(oldContactLic);
         _context.SetLink(oldContactLic, nameof(spd_licence.spd_LicenceHolder_contact), oldContact);
         await _context.SaveChangesAsync();
-        _context.DetachAll();
+
+        try
+        {
+            //Act
+            await _contactRepository.MergeContactsAsync(new MergeContactsCmd() { OldContactId = oldContactId, NewContactId = newContactId }, CancellationToken.None);
+
+            //Assert
+            contact? old = await _context.contacts.Where(c => c.contactid == oldContactId).FirstOrDefaultAsync();
+            Assert.NotNull(old);
+            Assert.Equal(1, old.statecode);
+            spd_licence? oldLic = _context.spd_licences.Where(c => c.spd_licenceid == oldContactLicId).FirstOrDefault();
+            Assert.NotNull(oldLic);
+            Assert.Equal(newContactId, oldLic._spd_licenceholder_value);
+        }
+        finally
+        {
+            //Annihilate : When we have delete privilege, we need to do following
+            _context.DeleteObject(oldContactLic);
+            _context.DeleteObject(oldContact);
+            _context.DeleteObject(newContact);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    [Theory]
+    [InlineData("spd_integration_firstname", "spd_integration_lastname", null, null, "2024-01-01", null, 2)]
+    [InlineData("spd_integration_firstname", "spd_integration_lastname", "", null, "2024-01-01", null, 2)]
+    [InlineData("spd_integration_firstname", "spd_integration_lastname", "", "", "2024-01-01", null, 1)]
+    [InlineData("spd_integration_firstname", "spd_integration_lastname3", "", "", "2023-01-01", null, 0)]
+    [InlineData("spd_integration_firstname", "spd_integration_lastname", "", null, "2024-01-01", "EmptyGuid", 1)]
+    public async Task QueryAsync_Run_Correctly(string? firstName, string? lastName, string? midName1, string? midName2, string? birthDate, string? identityIdStr, int resultCount)
+    {
+        //Arrange
+        Guid contact1Id = Guid.NewGuid();
+        contact contact1 = new()
+        {
+            contactid = contact1Id,
+            firstname = IntegrationTestSetup.DataPrefix + "firstname",
+            lastname = IntegrationTestSetup.DataPrefix + "lastname",
+            spd_middlename1 = null,
+            spd_middlename2 = "test",
+            birthdate = new Microsoft.OData.Edm.Date(2024, 1, 1)
+        };
+        _context.AddTocontacts(contact1);
+        Guid contact2Id = Guid.NewGuid();
+        contact contact2 = new()
+        {
+            contactid = contact2Id,
+            firstname = IntegrationTestSetup.DataPrefix + "firstname",
+            lastname = IntegrationTestSetup.DataPrefix + "lastname",
+            spd_middlename1 = "",
+            spd_middlename2 = null,
+            birthdate = new Microsoft.OData.Edm.Date(2024, 1, 1)
+        };
+        _context.AddTocontacts(contact2);
+        Guid identityId = Guid.NewGuid();
+        spd_identity identity = new() { spd_identityid = identityId };
+        _context.AddTospd_identities(identity);
+        _context.AddLink(contact2, nameof(contact.spd_contact_spd_identity), identity);
+        await _context.SaveChangesAsync();
 
         //Act
-        await _contactRepository.MergeContactsAsync(new MergeContactsCmd() { OldContactId = oldContactId, NewContactId = newContactId }, CancellationToken.None);
+        ContactQry qry = new()
+        {
+            FirstName = firstName,
+            LastName = lastName,
+            MiddleName1 = midName1,
+            MiddleName2 = midName2,
+            BirthDate = new DateOnly(2024, 1, 1),
+            IdentityId = identityIdStr == "EmptyGuid" ? Guid.Empty : null
+        };
+        ContactListResp result = await _contactRepository.QueryAsync(qry, CancellationToken.None);
 
-        //Assert
-        contact? old = await _context.contacts.Where(c => c.contactid == oldContactId).FirstOrDefaultAsync();
-        Assert.NotNull(old);
-        Assert.Equal(1, old.statecode);
-        spd_licence? oldLic = _context.spd_licences.Where(c => c.spd_licenceid == oldContactLicId).FirstOrDefault();
-        Assert.NotNull(oldLic);
-        Assert.Equal(newContactId, oldLic._spd_licenceholder_value);
-
-        //Annihilate : When we have delete privilege, we need to do following
-        //_context.DeleteObject(oldContactLic);
-        //_context.DeleteObject(oldContact);
-        //_context.DeleteObject(newContact);
-        //await _context.SaveChangesAsync();
+        try
+        {
+            //Assert
+            Assert.Equal(resultCount, result.Items.Count());
+        }
+        finally
+        {
+            //Annihilate : When we have delete privilege, we need to do following
+            _context.DeleteObject(identity);
+            _context.DeleteObject(contact1);
+            _context.DeleteObject(contact2);
+            await _context.SaveChangesAsync();
+        }
     }
 }
