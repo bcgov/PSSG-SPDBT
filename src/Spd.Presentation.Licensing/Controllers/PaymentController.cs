3b1225ceb97351f60f898180b70b32f8ce8755e7
peggy-quartech <peggy.zhang@quartech.com>
2024-06-13 09:36:35 -0700

    temp

--- src/Spd.Presentation.Licensing/Controllers/PaymentController.cs
+++ src/Spd.Presentation.Licensing/Controllers/PaymentController.cs
@@ -260,6 +260,117 @@ namespace Spd.Presentation.Licensing.Controllers
         }
         #endregion
 
+        #region auth-biz-payment
+        /// <summary>
+        /// Return the direct pay payment link 
+        /// </summary>
+        /// <param name="paymentLinkCreateRequest">which include Payment link create request</param>
+        /// <returns></returns>
+        [Route("api/auth-biz-licence/{applicationId}/payment-link")]
+        [HttpPost]
+        [Authorize(Policy = "BCeID")]
+        public async Task<PaymentLinkResponse> GetBizLicencePaymentLink([FromBody][Required] PaymentLinkCreateRequest paymentLinkCreateRequest)
+        {
+            string? hostUrl = _configuration.GetValue<string>("HostUrl");
+            string redirectUrl = $"{hostUrl}api/auth-biz-licence/payment-result";
+            return await _mediator.Send(new PaymentLinkCreateCommand(paymentLinkCreateRequest, redirectUrl, _paymentsConfiguration.MaxOnlinePaymentFailedTimes));
+        }
+
+        /// <summary>
+        /// redirect url for paybc to redirect to
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/auth-biz-licence/payment-result")]
+        [HttpGet]
+        [Authorize(Policy = "BCeID")]
+        public async Task<ActionResult> ProcessBizLicencePaymentResult([FromQuery] PaybcPaymentResultViewModel paybcResult)
+        {
+            string? hostUrl = _configuration.GetValue<string>("HostUrl");
+            string? successPath = _paymentsConfiguration.AuthPersonalLicPaymentSuccessPath;
+            string? failPath = _paymentsConfiguration.AuthPersonalLicPaymentFailPath;
+            string? cancelPath = _paymentsConfiguration.AuthPersonalLicPaymentCancelPath;
+            string? errorPath = _paymentsConfiguration.AuthPersonalLicPaymentErrorPath;
+
+            try
+            {
+                PaybcPaymentResult paybcPaymentResult = _mapper.Map<PaybcPaymentResult>(paybcResult);
+
+                if (!paybcPaymentResult.Success && paybcPaymentResult.MessageText == "Payment Canceled")
+                {
+                    _logger.LogInformation("Payment is being cancelled.");
+                    return Redirect($"{hostUrl}{cancelPath}{paybcPaymentResult.ApplicationId}");
+                }
+
+                var paymentId = await _mediator.Send(new PaymenCreateCommand(Request.QueryString.ToString(), paybcPaymentResult));
+                if (paybcPaymentResult.Success)
+                    return Redirect($"{hostUrl}{successPath}{paymentId}");
+
+                return Redirect($"{hostUrl}{failPath}{paymentId}");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Payment result processing has errors +{ex}");
+                return Redirect($"{hostUrl}{errorPath}");
+            }
+        }
+
+        /// <summary>
+        /// Get the payment result for application and payment
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/auth-licence/payments/{paymentId}")]
+        [HttpGet]
+        [Authorize(Policy = "BcscBCeID")]
+        public async Task<PaymentResponse> GetApplicantLicencePaymentResult([FromRoute] Guid paymentId)
+        {
+            return await _mediator.Send(new PaymentQuery(paymentId));
+        }
+
+        /// <summary>
+        /// Get the failed payment times for an application
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/auth-licence/{applicationId}/payment-attempts")]
+        [HttpGet]
+        [Authorize(Policy = "BcscBCeID")]
+        public async Task<int> GetApplicantFailedPaymentAttempts([FromRoute] Guid applicationId)
+        {
+            return await _mediator.Send(new PaymentFailedAttemptCountQuery(applicationId));
+        }
+
+        /// <summary>
+        /// download the receipt for successful payment
+        /// </summary>
+        /// <param name="applicationId"></param>
+        /// <returns>FileStreamResult</returns>
+        [Route("api/auth-licence/{applicationId}/payment-receipt")]
+        [HttpGet]
+        [Authorize(Policy = "BcscBCeID")]
+        public async Task<FileStreamResult> ApplicantLicenceDownloadReceiptAsync([FromRoute] Guid applicationId)
+        {
+            FileResponse response = await _mediator.Send(new PaymentReceiptQuery(applicationId));
+            var content = new MemoryStream(response.Content);
+            var contentType = response.ContentType ?? "application/octet-stream";
+            return File(content, contentType, response.FileName);
+        }
+
+        /// <summary>
+        /// download the manual payment form
+        /// </summary>
+        /// <param name="applicationId"></param>
+        /// <returns>FileStreamResult</returns>
+        [Route("api/auth-licence/{applicationId}/manual-payment-form")]
+        [HttpGet]
+        [Authorize(Policy = "BcscBCeID")]
+        public async Task<FileStreamResult> ApplicantLicenceDownloadManualPaymentFormAsync([FromRoute] Guid applicationId)
+        {
+            FileResponse response = await _mediator.Send(new ManualPaymentFormQuery(applicationId));
+            var content = new MemoryStream(response.Content);
+            var contentType = response.ContentType ?? "application/octet-stream";
+            return File(content, contentType, response.FileName);
+        }
+        #endregion
+
         #region secure-payment-link for dynamics email.
         /// <summary>
         /// Redirect to PayBC the direct pay payment page 
