7142d72ce017524ef75f6f14fcb7e1fcab6b4e08
QSL\RubenA <ruben.araya@quartech.com>
2024-06-24 16:59:31 -0700

    Add business portal user create command

---
+++ src/Spd.Manager.Licence/BizPortalUserManager.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using Spd.Resource.Repository.Identity;
+using Spd.Resource.Repository.Org;
+using Spd.Resource.Repository.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Manager.Licence;
+internal class BizPortalUserManager
+    : IRequestHandler<BizPortalUserCreateCommand, BizPortalUserResponse>,
+    IBizPortalUserManager
+{
+    private readonly IMapper _mapper;
+
+    public BizPortalUserManager(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task<BizPortalUserResponse> Handle(BizPortalUserCreateCommand request, CancellationToken ct)
+    {
+        
+    }
+}
