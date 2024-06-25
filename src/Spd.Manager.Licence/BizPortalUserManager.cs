fc129323e9e307863e3584b2cd02e76b4f17ba11
QSL\RubenA <ruben.araya@quartech.com>
2024-06-25 12:30:22 -0700

    Add business portal user create command

--- src/Spd.Manager.Licence/BizPortalUserManager.cs
+++ src/Spd.Manager.Licence/BizPortalUserManager.cs
@@ -1,13 +1,8 @@
 ﻿using AutoMapper;
 using MediatR;
-using Spd.Resource.Repository.Identity;
-using Spd.Resource.Repository.Org;
-using Spd.Resource.Repository.User;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Spd.Resource.Repository.PortalUser;
+using Spd.Utilities.Shared.Exceptions;
+using System.Net;
 
 namespace Spd.Manager.Licence;
 internal class BizPortalUserManager
@@ -15,14 +10,26 @@ internal class BizPortalUserManager
     IBizPortalUserManager
 {
     private readonly IMapper _mapper;
+    private readonly IPortalUserRepository _portalUserRepository;
 
-    public BizPortalUserManager(IMapper mapper)
+    public BizPortalUserManager(IMapper mapper, IPortalUserRepository portalUserRepository)
     {
         _mapper = mapper;
+        _portalUserRepository = portalUserRepository;
     }
 
     public async Task<BizPortalUserResponse> Handle(BizPortalUserCreateCommand request, CancellationToken ct)
     {
-        
+        PortalUserListResp existingUsersResult = await _portalUserRepository.QueryAsync(
+            new PortalUserQry() { OrgId = request.BizPortalUserCreateRequest.OrganizationId },
+            ct);
+
+        //check if email already exists for the user
+        if (existingUsersResult.Items.Any(u => u.UserEmail != null && u.UserEmail.Equals(request.BizPortalUserCreateRequest.Email, StringComparison.InvariantCultureIgnoreCase)))
+        {
+            throw new DuplicateException(HttpStatusCode.BadRequest, $"User email {request.BizPortalUserCreateRequest.Email} has been used by another user");
+        }
+
+        return new BizPortalUserResponse();
     }
 }
