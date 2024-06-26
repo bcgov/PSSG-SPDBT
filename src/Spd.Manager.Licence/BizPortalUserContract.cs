a7918e67e25e9f0ccc71391d4384804ebaac111c
QSL\RubenA <ruben.araya@quartech.com>
2024-06-25 17:23:43 -0700

    Add contact authorization type codes

--- src/Spd.Manager.Licence/BizPortalUserContract.cs
+++ src/Spd.Manager.Licence/BizPortalUserContract.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using System.ComponentModel;
 
 namespace Spd.Manager.Licence;
 public interface IBizPortalUserManager
@@ -13,7 +14,7 @@ public record BizPortalUserCreateRequest : BizPortalUserUpsertRequest { }
 public abstract record BizPortalUserUpsertRequest
 {
     public Guid OrganizationId { get; set; }
-    //public ContactAuthorizationTypeCode ContactAuthorizationTypeCode { get; set; }
+    public ContactAuthorizationTypeCode ContactAuthorizationTypeCode { get; set; }
     public string FirstName { get; set; } = null!;
     public string LastName { get; set; } = null!;
     public string Email { get; set; } = null!;
@@ -25,11 +26,20 @@ public class BizPortalUserResponse
 {
     public Guid Id { get; set; }
     public Guid OrganizationId { get; set; }
-    //public ContactAuthorizationTypeCode ContactAuthorizationTypeCode { get; set; }
+    public ContactAuthorizationTypeCode ContactAuthorizationTypeCode { get; set; }
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string? JobTitle { get; set; }
     public string? PhoneNumber { get; set; }
     public bool IsActive { get; set; }
+}
+
+public enum ContactAuthorizationTypeCode
+{
+    [Description("Primary Authorized Business Manager Contact")]
+    PrimaryBusinessManager,
+
+    [Description("Authorized Business Manager Contact")]
+    BusinessManager
 }
