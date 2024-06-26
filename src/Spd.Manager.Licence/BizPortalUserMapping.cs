df1d08b6b7bd6b66afa95fdd7b311c0d1f154550
QSL\RubenA <ruben.araya@quartech.com>
2024-06-25 17:24:33 -0700

    Add business portal user mapper

---
+++ src/Spd.Manager.Licence/BizPortalUserMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Spd.Resource.Repository.User;
+
+namespace Spd.Manager.Licence;
+internal class BizPortalUserMapping : Profile
+{
+    public BizPortalUserMapping()
+    {
+        CreateMap<BizPortalUserCreateRequest, User>();
+        CreateMap<BizPortalUserCreateRequest, UserResult>()
+            .ForMember(d => d.Id, opt => opt.Ignore())
+            .IncludeBase<BizPortalUserCreateRequest, User>();
+    }
+}
