4610cf7881eeb6f94634033af1c0b7c705c82934
QSL\RubenA <ruben.araya@quartech.com>
2024-06-27 11:07:07 -0700

    Map portal user response and user result

--- src/Spd.Manager.Licence/BizPortalUserMapping.cs
+++ src/Spd.Manager.Licence/BizPortalUserMapping.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Spd.Resource.Repository.PortalUser;
 using Spd.Resource.Repository.User;
 
 namespace Spd.Manager.Licence;
@@ -6,6 +7,11 @@ internal class BizPortalUserMapping : Profile
 {
     public BizPortalUserMapping()
     {
+        CreateMap<PortalUserResp, UserResult>()
+            .ForMember(d => d.UserGuid, opt => opt.Ignore())
+            .ForMember(d => d.IsActive, opt => opt.Ignore())
+            .ForMember(d => d.ContactAuthorizationTypeCode, opt => opt.MapFrom(s => s.ContactRoleCode))
+            .ForMember(d => d.Email, opt => opt.MapFrom(s => s.UserEmail));
         CreateMap<BizPortalUserCreateRequest, User>();
         CreateMap<BizPortalUserCreateRequest, UserResult>()
             .ForMember(d => d.Id, opt => opt.Ignore())
