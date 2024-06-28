1e2a9ffea468bdd063827f32c1e7002003efd03c
QSL\RubenA <ruben.araya@quartech.com>
2024-06-28 08:22:50 -0700

    Remove unnecessary fields

--- src/Spd.Manager.Licence/BizPortalUserContract.cs
+++ src/Spd.Manager.Licence/BizPortalUserContract.cs
@@ -30,9 +30,6 @@ public class BizPortalUserResponse
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string? JobTitle { get; set; }
-    public string? PhoneNumber { get; set; }
-    public bool IsActive { get; set; }
 }
 
 public enum ContactAuthorizationTypeCode
