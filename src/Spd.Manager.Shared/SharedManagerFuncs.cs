ff16286ce1c308e58f193cbcc1fd32802b282ae2
QSL\RubenA <ruben.araya@quartech.com>
2024-06-28 15:14:12 -0700

    Adjust validation to check number of primary users based on service category

--- src/Spd.Manager.Shared/SharedManagerFuncs.cs
+++ src/Spd.Manager.Shared/SharedManagerFuncs.cs
@@ -1,5 +1,4 @@
 ﻿using Spd.Resource.Repository;
-using Spd.Resource.Repository.Org;
 using Spd.Resource.Repository.User;
 using Spd.Utilities.Shared.Exceptions;
 using System.Net;
@@ -7,16 +6,24 @@ using System.Net;
 namespace Spd.Manager.Shared;
 public static class SharedManagerFuncs
 {
-    public static void CheckMaxRoleNumberRuleAsync(OrgQryResult org, List<UserResult> userList)
+    public static void CheckMaxRoleNumberRuleAsync(
+        int maxContacts, 
+        int maxPrimaryContacts, 
+        List<UserResult> userList, 
+        PortalUserServiceCategoryEnum serviceCategoryEnum = PortalUserServiceCategoryEnum.Screening)
     {
-        int maxContacts = org != null ? org.OrgResult.MaxContacts : 0;
-        int maxPrimaryContacts = org != null ? org.OrgResult.MaxPrimaryContacts : 0;
+        int primaryUserNo = 0;
         int userNo = userList.Count;
         if (userNo > maxContacts)
         {
             throw new OutOfRangeException(HttpStatusCode.BadRequest, $"No more contacts can be created. The limit of {maxContacts} contacts has been reached.");
         }
-        int primaryUserNo = userList.Count(u => u.ContactAuthorizationTypeCode == ContactRoleCode.Primary);
+
+        if (serviceCategoryEnum == PortalUserServiceCategoryEnum.Screening)
+            primaryUserNo = userList.Count(u => u.ContactAuthorizationTypeCode == ContactRoleCode.Primary);
+        else
+            primaryUserNo = userList.Count(u => u.ContactAuthorizationTypeCode == ContactRoleCode.PrimaryBusinessManager);
+
         if (primaryUserNo > maxPrimaryContacts)
         {
             throw new OutOfRangeException(HttpStatusCode.BadRequest, $"No more primary contacts can be created. The limit of {maxPrimaryContacts} primary contacts has been reached.");
