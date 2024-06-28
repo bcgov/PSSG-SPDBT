fe75f299bd934547a3f4ff9635fbb64787603123
QSL\RubenA <ruben.araya@quartech.com>
2024-06-28 14:36:22 -0700

    Map job title and phone number

--- src/Spd.Resource.Repository/PortalUser/Mappings.cs
+++ src/Spd.Resource.Repository/PortalUser/Mappings.cs
@@ -13,6 +13,8 @@ namespace Spd.Resource.Repository.PortalUser
             .ForMember(d => d.FirstName, opt => opt.MapFrom(s => s.spd_firstname))
             .ForMember(d => d.LastName, opt => opt.MapFrom(s => s.spd_surname))
             .ForMember(d => d.UserEmail, opt => opt.MapFrom(s => s.spd_emailaddress1))
+            .ForMember(d => d.PhoneNumber, opt => opt.MapFrom(s => s.spd_phonenumber))
+            .ForMember(d => d.JobTitle, opt => opt.MapFrom(s => s.spd_jobtitle))
             .ForMember(d => d.IdentityId, opt => opt.MapFrom(s => s._spd_identityid_value))
             .ForMember(d => d.IsPSA, opt => opt.MapFrom(s => SharedMappingFuncs.GetBool(s.spd_psa)))
             .ForMember(d => d.OrganizationId, opt => opt.MapFrom(s => s._spd_organizationid_value))
@@ -24,6 +26,8 @@ namespace Spd.Resource.Repository.PortalUser
             .ForMember(d => d.spd_surname, opt => opt.MapFrom(s => s.LastName))
             .ForMember(d => d.spd_firstname, opt => opt.MapFrom(s => s.FirstName))
             .ForMember(d => d.spd_emailaddress1, opt => opt.MapFrom(s => s.EmailAddress))
+            .ForMember(d => d.spd_phonenumber, opt => opt.MapFrom(s => s.PhoneNumber))
+            .ForMember(d => d.spd_jobtitle, opt => opt.MapFrom(s => s.JobTitle))
             .ForMember(d => d.spd_servicecategory, opt => opt.MapFrom(s =>
                 s.PortalUserServiceCategory == PortalUserServiceCategoryEnum.Licensing ?
                 (int)PortalUserServiceCategoryOptionSet.Licensing : (int)PortalUserServiceCategoryOptionSet.Screening));
