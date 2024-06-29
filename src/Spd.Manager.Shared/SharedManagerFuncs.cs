849aeb3ef0125f2f25ab36db63e597b38ed5c540
QSL\RubenA <ruben.araya@quartech.com>
2024-06-29 09:25:59 -0700

    Refactor validation

--- src/Spd.Manager.Shared/SharedManagerFuncs.cs
+++ src/Spd.Manager.Shared/SharedManagerFuncs.cs
@@ -1,6 +1,4 @@
-﻿using Spd.Resource.Repository;
-using Spd.Resource.Repository.User;
-using Spd.Utilities.Shared.Exceptions;
+﻿using Spd.Utilities.Shared.Exceptions;
 using System.Net;
 
 namespace Spd.Manager.Shared;
@@ -9,25 +7,19 @@ public static class SharedManagerFuncs
     public static void CheckMaxRoleNumberRuleAsync(
         int maxContacts, 
         int maxPrimaryContacts, 
-        List<UserResult> userList, 
-        PortalUserServiceCategoryEnum serviceCategoryEnum = PortalUserServiceCategoryEnum.Screening)
+        int primaryUserNo, 
+        int userNo)
     {
-        int primaryUserNo = 0;
-        int userNo = userList.Count;
         if (userNo > maxContacts)
         {
             throw new OutOfRangeException(HttpStatusCode.BadRequest, $"No more contacts can be created. The limit of {maxContacts} contacts has been reached.");
         }
-
-        if (serviceCategoryEnum == PortalUserServiceCategoryEnum.Screening)
-            primaryUserNo = userList.Count(u => u.ContactAuthorizationTypeCode == ContactRoleCode.Primary);
-        else
-            primaryUserNo = userList.Count(u => u.ContactAuthorizationTypeCode == ContactRoleCode.PrimaryBusinessManager);
-
+        
         if (primaryUserNo > maxPrimaryContacts)
         {
             throw new OutOfRangeException(HttpStatusCode.BadRequest, $"No more primary contacts can be created. The limit of {maxPrimaryContacts} primary contacts has been reached.");
         }
+
         if (primaryUserNo < 1)
         {
             throw new OutOfRangeException(HttpStatusCode.BadRequest, "There must be at least one primary user");
