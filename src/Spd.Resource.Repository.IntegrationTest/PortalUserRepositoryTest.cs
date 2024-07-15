48bf4a600b15c841eff7807f6a7654e26db4c609
QSL\RubenA <ruben.araya@quartech.com>
2024-07-15 15:48:50 -0700

    Create test for update portal user service

--- src/Spd.Resource.Repository.IntegrationTest/PortalUserRepositoryTest.cs
+++ src/Spd.Resource.Repository.IntegrationTest/PortalUserRepositoryTest.cs
@@ -76,5 +76,54 @@ public class PortalUserRepositoryTest : IClassFixture<IntegrationTestSetup>
         await _context.SaveChangesAsync();
     }
 
-    
+    [Fact]
+    public async Task UpdatePortalUserAsync_Run_Correctly()
+    {
+        // Arrange
+        Guid identityId = Guid.NewGuid();
+        Guid accountId = Guid.NewGuid();
+        Guid bizContactId = Guid.NewGuid();
+
+        spd_identity identity = new spd_identity() { spd_identityid = identityId };
+        _context.AddTospd_identities(identity);
+        account account = new account() { accountid = accountId };
+        _context.AddToaccounts(account);
+        spd_businesscontact businessContact = new spd_businesscontact() { spd_businesscontactid = bizContactId };
+        _context.AddTospd_businesscontacts(businessContact);
+        _context.SaveChanges();
+
+        UpdatePortalUserCmd cmd = new()
+        {
+            FirstName = IntegrationTestSetup.DataPrefix + "firstName",
+            LastName = IntegrationTestSetup.DataPrefix + "lastName",
+            EmailAddress = "test@test.com",
+            IdentityId = identityId,
+            ContactRoleCode = ContactRoleCode.BusinessManager,
+            OrgId = accountId
+        };
+
+        // Act
+        var response = await _portalUserRepo.ManageAsync(cmd, CancellationToken.None);
+
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotEqual(Guid.Empty, response.Id);
+        Assert.Equal(cmd.OrgId, response.OrganizationId);
+        Assert.Equal(response.ContactRoleCode, ContactRoleCode.BusinessManager);
+
+        // Annihilate
+        spd_portaluser? portalUser = _context.spd_portalusers.Where(u => u.spd_portaluserid == response.Id).FirstOrDefault();
+
+        _context.SetLink(portalUser, nameof(portalUser.spd_OrganizationId), null);
+        _context.SetLink(portalUser, nameof(portalUser.spd_IdentityId), null);
+
+        _context.DeleteObject(identity);
+        _context.DeleteObject(account);
+        await _context.SaveChangesAsync();
+
+        spd_portaluser? portalUserToRemove = _context.spd_portalusers.Where(u => u.spd_portaluserid == response.Id).FirstOrDefault();
+
+        _context.DeleteObject(portalUserToRemove);
+        await _context.SaveChangesAsync();
+    }
 }
