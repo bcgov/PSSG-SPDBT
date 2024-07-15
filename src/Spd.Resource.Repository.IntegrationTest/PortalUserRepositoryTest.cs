fc35066fd79a95ee6e59946036debbb197e344bd
QSL\RubenA <ruben.araya@quartech.com>
2024-07-15 15:48:05 -0700

    Complete test for create portal user service

--- src/Spd.Resource.Repository.IntegrationTest/PortalUserRepositoryTest.cs
+++ src/Spd.Resource.Repository.IntegrationTest/PortalUserRepositoryTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.Dynamics.CRM;
 using Microsoft.Extensions.DependencyInjection;
 using Spd.Resource.Repository.PortalUser;
 using Spd.Utilities.Dynamics;
@@ -28,32 +29,52 @@ public class PortalUserRepositoryTest : IClassFixture<IntegrationTestSetup>
         Assert.NotNull(response);
     }
 
-    // * TODO finish test
-    //[Fact]
-    //public async Task CreatePortalUserAsync_Run_Correctly()
-    //{
-    //    // Arrange
-    //    CreatePortalUserCmd cmd = new()
-    //    {
-    //        PortalUserServiceCategory = PortalUserServiceCategoryEnum.Licensing,
-    //        EmailAddress = "test@test.com",
-    //        IdentityId = Guid.NewGuid(),
-    //        ContactRoleCode = ContactRoleCode.PrimaryBusinessManager,
-    //        OrgId = Guid.NewGuid()
-    //    };
+    [Fact]
+    public async Task CreatePortalUserAsync_Run_Correctly()
+    {
+        // Arrange
+        Guid identityId = Guid.NewGuid();
+        Guid accountId = Guid.NewGuid();
 
-    //    // Act
-    //    var response = await _portalUserRepo.ManageAsync(cmd, CancellationToken.None);
+        spd_identity identity = new spd_identity() { spd_identityid = identityId };
+        _context.AddTospd_identities(identity);
+        account account = new account() { accountid = accountId };
+        _context.AddToaccounts(account);
+        _context.SaveChanges();
 
-    //    // Assert
-    //    Assert.NotNull(response);
-    //    Assert.NotEqual(Guid.Empty, response.Id);
-    //    Assert.Equal(cmd.OrgId, response.OrganizationId);
+        CreatePortalUserCmd cmd = new()
+        {
+            PortalUserServiceCategory = PortalUserServiceCategoryEnum.Licensing,
+            EmailAddress = "test@test.com",
+            IdentityId = identityId,
+            ContactRoleCode = ContactRoleCode.PrimaryBusinessManager,
+            OrgId = accountId
+        };
 
-    //    // Annihilate
-    //    spd_portaluser? portaluser = _context.spd_portalusers.Where(u => u.spd_portaluserid == response.Id).FirstOrDefault();
+        // Act
+        var response = await _portalUserRepo.ManageAsync(cmd, CancellationToken.None);
 
-    //    _context.DeleteObject(portaluser);
-    //    await _context.SaveChangesAsync();
-    //}
+        // Assert
+        Assert.NotNull(response);
+        Assert.NotEqual(Guid.Empty, response.Id);
+        Assert.Equal(cmd.OrgId, response.OrganizationId);
+        Assert.Equal(ContactRoleCode.PrimaryBusinessManager, response.ContactRoleCode);
+        
+        // Annihilate
+        spd_portaluser? portalUser = _context.spd_portalusers.Where(u => u.spd_portaluserid == response.Id).FirstOrDefault();
+
+        _context.SetLink(portalUser, nameof(portalUser.spd_OrganizationId), null);
+        _context.SetLink(portalUser, nameof(portalUser.spd_IdentityId), null);
+
+        _context.DeleteObject(identity);
+        _context.DeleteObject(account);
+        await _context.SaveChangesAsync();
+
+        spd_portaluser? portalUserToRemove = _context.spd_portalusers.Where(u => u.spd_portaluserid == response.Id).FirstOrDefault();
+
+        _context.DeleteObject(portalUserToRemove);
+        await _context.SaveChangesAsync();
+    }
+
+    
 }
