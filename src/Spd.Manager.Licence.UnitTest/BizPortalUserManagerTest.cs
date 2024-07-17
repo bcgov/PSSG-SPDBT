f904db29d77af7289dd31b1038f9daad67812c22
QSL\RubenA <ruben.araya@quartech.com>
2024-07-17 09:53:08 -0700

    Add test for business portal user list service

--- src/Spd.Manager.Licence.UnitTest/BizPortalUserManagerTest.cs
+++ src/Spd.Manager.Licence.UnitTest/BizPortalUserManagerTest.cs
@@ -1,7 +1,7 @@
 ﻿using AutoFixture;
 using Moq;
 using Spd.Resource.Repository.PortalUser;
-using Spd.Resource.Repository.Org;
+using Spd.Resource.Repository.Biz;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,7 +14,7 @@ public class BizPortalUserManagerTest
 {
     private readonly IFixture fixture;
     private Mock<IPortalUserRepository> mockPortalUserRepo = new();
-    private Mock<IOrgRepository> mockOrgRepo = new();
+    private Mock<IBizRepository> mockBizRepo = new();
 
     private BizPortalUserManager sut;
 
@@ -30,11 +30,38 @@ public class BizPortalUserManagerTest
             x.AddProfile<BizPortalUserMapping>();
         });
         var mapper = mapperConfig.CreateMapper();
+
+        sut = new BizPortalUserManager(mapper, mockPortalUserRepo.Object, mockBizRepo.Object);
     }
 
     [Fact]
     public async void Handle_BizPortalUserCreateCommand_Return_BizPortalUserResponse()
     {
 
+    }
+
+    [Fact]
+    public async void Handle_BizPortalUserListQuery_Return_BizPortalUserResponse()
+    {
+        // Arrange
+        Guid bizId = Guid.NewGuid();
+        BizPortalUserListQuery qry = new(bizId);
+        PortalUserResp portalUserResp = new() { OrganizationId = bizId};
+        PortalUserListResp portalUserListResp = new()
+        {
+            Items = new List<PortalUserResp>() { portalUserResp }
+        };
+
+        mockPortalUserRepo.Setup(m => m.QueryAsync(It.Is<PortalUserQry>(q => q.OrgId == bizId), CancellationToken.None))
+            .ReturnsAsync(portalUserListResp);
+        mockBizRepo.Setup(m => m.GetBizAsync(It.Is<Guid>(q => q == bizId), CancellationToken.None))
+            .ReturnsAsync(new BizResult());
+
+        // Act
+        var result = await sut.Handle(qry, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<BizPortalUserListResponse>(result);
+        Assert.True(result.Users.Any(u => u.BizId == bizId));
     }
 }
