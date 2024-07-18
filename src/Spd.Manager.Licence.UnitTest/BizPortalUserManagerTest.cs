c8f3026b2335685e245cb7dfa2142dfa6837e7ef
QSL\RubenA <ruben.araya@quartech.com>
2024-07-18 09:12:14 -0700

    Add test for business portal user update service

--- src/Spd.Manager.Licence.UnitTest/BizPortalUserManagerTest.cs
+++ src/Spd.Manager.Licence.UnitTest/BizPortalUserManagerTest.cs
@@ -8,13 +8,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Spd.Resource.Repository.Biz;
 
 namespace Spd.Manager.Licence.UnitTest;
 public class BizPortalUserManagerTest
 {
     private readonly IFixture fixture;
     private Mock<IPortalUserRepository> mockPortalUserRepo = new();
-    private Mock<IOrgRepository> mockOrgRepo = new();
+    private Mock<IBizRepository> mockBizRepo = new();
 
     private BizPortalUserManager sut;
 
@@ -30,11 +31,36 @@ public class BizPortalUserManagerTest
             x.AddProfile<BizPortalUserMapping>();
         });
         var mapper = mapperConfig.CreateMapper();
+
+        sut = new BizPortalUserManager(mapper, mockPortalUserRepo.Object, mockBizRepo.Object);
     }
 
     [Fact]
     public async void Handle_BizPortalUserCreateCommand_Return_BizPortalUserResponse()
-    {
+    {   
+    }
 
+    [Fact]
+    public async void Handle_BizPortalUserUpdateCommand_Return_BizPortalUserResponse()
+    {
+        // Arrange
+        Guid bizId = Guid.NewGuid();
+        Guid userId = Guid.NewGuid();
+        BizPortalUserUpdateRequest bizPortalUserUpdateRequest = new()
+        {
+            BizId = bizId,
+            ContactAuthorizationTypeCode = Shared.ContactAuthorizationTypeCode.PrimaryBusinessManager,
+            FirstName = "test",
+            LastName = "test",
+            Email = "test@test.com",
+            JobTitle = "dev",
+            PhoneNumber = "9001234567"
+        };
+        BizPortalUserUpdateCommand cmd = new(userId, bizPortalUserUpdateRequest);
+        PortalUserResp portalUserResp = new() { OrganizationId = bizId, ContactRoleCode = Resource.Repository.ContactRoleCode.PrimaryBusinessManager };
+        PortalUserListResp portalUserListResp = new()
+        {
+            Items = new List<PortalUserResp>() { portalUserResp }
+        };
     }
 }
