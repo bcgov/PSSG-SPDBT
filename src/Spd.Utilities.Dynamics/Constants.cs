9103c18a6aecb84b03796bd74c90dccb1566e7ab
Peggy <peggy.zhang@quartech.com>
2024-07-19 11:07:17 -0700

    Fix/not log s3 not found and assign to correct team for clearance access (#1237)
    # Description
    
    This PR includes the following proposed change(s):
    
    - spd-2850: screening, assign to correct team for new clearance access
    - remove log error when the file cannot be found.

--- src/Spd.Utilities.Dynamics/Constants.cs
+++ src/Spd.Utilities.Dynamics/Constants.cs
@@ -9,5 +9,6 @@
         public static readonly string Client_Service_Team_Guid = "65e0b015-ee9d-ed11-b83d-00505683fbf4";
         public static readonly string Licensing_Client_Service_Team_Guid = "9872da9f-397f-ee11-b846-00505683fbf4";
         public static readonly string Licensing_Risk_Assessment_Coordinator_Team_Guid = "b322c8f6-a8eb-ed11-b843-00505683fbf4";
+        public static readonly string Screening_Risk_Assessment_Coordinator_Team_Guid = "4c48c814-32c1-ed11-b840-00505683fbf4";
     }
 }
