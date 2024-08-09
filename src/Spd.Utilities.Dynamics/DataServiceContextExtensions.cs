f8a0388ed4086c146afd263a03f39ccaa3909b1e
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-08-09 14:38:02 -0700

    SPDBT-2891: convert deploymentconfig to deployment (#1285)

--- src/Spd.Utilities.Dynamics/DataServiceContextExtensions.cs
+++ src/Spd.Utilities.Dynamics/DataServiceContextExtensions.cs
@@ -46,12 +46,16 @@ public static class DataServiceContextExtensions
     }
 
     public static async Task<T?> SingleOrDefaultAsync<T>(this IQueryable<T> query, CancellationToken ct = default)
-        where T : crmbaseentity => (await ((DataServiceQuery<T>)query).ExecuteAsync(ct)).SingleOrDefault();
+        where T : crmbaseentity => (await ((DataServiceQuery<T>)query).Take(2).ExecuteForEditAsync(ct)).SingleOrDefault();
 
     public static async Task<T?> FirstOrDefaultAsync<T>(this IQueryable<T> query, CancellationToken ct = default)
-        where T : crmbaseentity => (await ((DataServiceQuery<T>)query).ExecuteAsync(ct)).FirstOrDefault();
+        where T : crmbaseentity => (await ((DataServiceQuery<T>)query).Take(1).ExecuteForEditAsync(ct)).FirstOrDefault();
 
     public static async Task<IEnumerable<T>> GetAllPagesAsync<T>(this IQueryable<T> query, CancellationToken ct = default)
         where T : crmbaseentity =>
         await ((DataServiceQuery<T>)query).GetAllPagesAsync(ct);
+
+    public static async Task<DataServiceCollection<T>> ExecuteForEditAsync<T>(this IQueryable<T> query, CancellationToken ct = default)
+        where T : crmbaseentity =>
+        new DataServiceCollection<T>(await ((DataServiceQuery<T>)query).ExecuteAsync(ct));
 }
