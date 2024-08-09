8daecaa1fca8a313d81a91bab21d61ee47902003
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-08-09 10:47:16 -0700

    Http header (#1281)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Add bizId, bizUserId to http header
    
    Co-authored-by: Peggy <peggy.zhang@quartech.com>

--- src/Spd.Presentation.Licensing/ClientApp/src/app/core/interceptors/auth-token.interceptor.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/core/interceptors/auth-token.interceptor.ts
@@ -3,11 +3,13 @@ import { Injectable } from '@angular/core';
 import { OAuthResourceServerErrorHandler } from 'angular-oauth2-oidc';
 import { Observable } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AuthUserBceidService } from '../services/auth-user-bceid.service';
 import { AuthenticationService } from '../services/authentication.service';
 
 @Injectable()
 export class AuthTokenInterceptor implements HttpInterceptor {
 	constructor(
+		private authUserBceidService: AuthUserBceidService,
 		private authenticationService: AuthenticationService,
 		private errorHandler: OAuthResourceServerErrorHandler
 	) {}
@@ -24,7 +26,13 @@ export class AuthTokenInterceptor implements HttpInterceptor {
 		if (!token) return next.handle(req);
 
 		const header = 'Bearer ' + token;
-		const headers = req.headers.set('Authorization', header);
+		let headers = req.headers.set('Authorization', header);
+		if (this.authUserBceidService.bceidUserProfile?.bizId) {
+			headers = req.headers.set('bizId', this.authUserBceidService.bceidUserProfile.bizId);
+		}
+		if (this.authUserBceidService.bceidUserProfile?.bizUserId) {
+			headers = req.headers.set('bizUserId', this.authUserBceidService.bceidUserProfile.bizUserId);
+		}
 		req = req.clone({ headers });
 
 		return next.handle(req).pipe(catchError((err) => this.errorHandler.handleError(err)));
