450be9dc76f8cb331b5ad5f15c682ded29f5e6f6
Emad Salamat <Emad.Salamat@quartech.com>
2024-08-16 14:31:16 -0700

    [SPDBT-2915] add contract and cotnroller for crc controlling member (unauth post)

---
+++ src/Spd.Manager.Licence/CrcControllingMemberContract.cs
@@ -0,0 +1,62 @@
+﻿using MediatR;
+using Spd.Manager.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Manager.Licence;
+public abstract record CrcControllingMemberApp
+{
+    public int AccessCode { get; set; } //is it int or string?
+    public string GivenName { get; set; }
+    public string? MiddleName1 { get; set; }
+    public string? MiddleName2 { get; set; }
+    public string Surname { get; set; }
+    public bool IsTermsOfUseAccepted { get; set; }
+    public DateTime DateSigned { get; set; }
+    public DateTime DateOfBirth { get; set; }
+    public GenderCode Gender { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Email { get; set; }
+    public bool HasPreviousName { get; set; }
+    public bool HasBcDriverLicence { get; set; }
+    public string? LicenceNumber { get; set; }
+
+    public bool IsPoliceOrPeaceOfficer { get; set; }
+    public PoliceOfficerRoleCode? PoliceOfficerRoleCode { get; set; }
+    // public LicenceAppDocumentUpload? LetterOfConflictDoucuments { get; set; }
+
+    public bool IsCanadianCitizen { get; set; }
+    //public LicenceAppDocumentUpload? CitizenshipProofDoucuments { get; set; }
+    public DateOnly? CitizenshipProofExpiryDate { get; set; }
+    //public LicenceAppDocumentUpload? FingerPrintDoucuments { get; set; }
+    //public LicenceAppDocumentUpload? GovernmentIssuedIdDoucuments1 { get; set; }
+    //public LicenceAppDocumentUpload? GovernmentIssuedIdDoucuments2 { get; set; }
+
+    public bool HasCriminalHistory { get; set; }
+    public string? CriminalHistoryDetail { get; set; }
+
+    public bool HasBankruptcyHistory { get; set; }
+    public string? BankruptcyHistoryDetail { get; set; }
+
+    public bool HasMentalCondition { get; set; }
+    //public LicenceAppDocumentUpload? MentalHealthConditionDoucuments { get; set; }
+
+    public Address Address { get; set; }
+}
+
+public record CrcControllingMemberCommandResponse : CrcControllingMemberUpsertResponse
+{
+};
+
+public record CrcControllingMemberUpsertResponse
+{
+}
+public record CrcControllingMemberUpsertRequest : CrcControllingMemberApp
+{
+    public Guid? CrcControllingMemberId { get; set; }
+};
+
+public record CrcControllingMemberUpsertCommand(CrcControllingMemberUpsertRequest CrcControllingMemberUpsertRequest) : IRequest<CrcControllingMemberCommandResponse>;
---
+++ src/Spd.Presentation.Licensing/Controllers/CrcControllingMemberController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Spd.Manager.Licence;
+using Spd.Utilities.Shared;
+using Spd.Utilities.Shared.Exceptions;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Spd.Presentation.Licensing.Controllers;
+
+[ApiController]
+public class CrcControllingMemberController : SpdControllerBase
+{
+    private readonly IMediator _mediator;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mediator"></param>
+    public CrcControllingMemberController (IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Save New Licence Crc Controlling Member
+    /// </summary>
+    /// <param name="CrcControllingMemberUpsertRequest"></param>
+    /// <returns></returns>
+    [Route("api/business-licence/New-Crc-Controlling-Member")]
+    [HttpPost]
+    public async Task<CrcControllingMemberCommandResponse> SaveCrcControllingMemberApplication([FromBody][Required] CrcControllingMemberUpsertRequest CrcControllingMemberUpsertRequest, CancellationToken ct)
+    {
+       return await _mediator.Send(new CrcControllingMemberUpsertCommand(CrcControllingMemberUpsertRequest), ct);
+    }
+}
