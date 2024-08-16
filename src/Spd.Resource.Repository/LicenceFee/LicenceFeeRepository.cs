a6e815adedf5ccb5b64069464b9ca498674ccf23
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-08-16 15:30:39 -0700

    readded licensing fees cache

--- src/Spd.Resource.Repository/LicenceFee/LicenceFeeRepository.cs
+++ src/Spd.Resource.Repository/LicenceFee/LicenceFeeRepository.cs
@@ -1,60 +1,57 @@
 ﻿using AutoMapper;
-using Microsoft.Dynamics.CRM;
+using Microsoft.Extensions.Caching.Distributed;
 using Spd.Utilities.Dynamics;
 
 namespace Spd.Resource.Repository.LicenceFee;
 
-internal class LicenceFeeRepository : ILicenceFeeRepository
+internal class LicenceFeeRepository(IDynamicsContextFactory contextFactory, IMapper mapper, IDistributedCache cache) : ILicenceFeeRepository
 {
-    private readonly DynamicsContext _context;
-    private readonly IMapper _mapper;
-
-    public LicenceFeeRepository(IDynamicsContextFactory ctx, IMapper mapper)
-    {
-        _context = ctx.Create();
-        _mapper = mapper;
-    }
+    private readonly DynamicsContext context = contextFactory.Create();
 
     public async Task<LicenceFeeListResp> QueryAsync(LicenceFeeQry qry, CancellationToken cancellationToken)
     {
-        IQueryable<spd_licencefee> feeResult = _context.spd_licencefees.Expand(a => a.spd_ServiceTypeId);
+        var feeResult = await cache.GetAsync(
+            "license-fees",
+            async ct => await context.spd_licencefees.Expand(a => a.spd_ServiceTypeId).GetAllPagesAsync(ct),
+            TimeSpan.FromMinutes(60),
+            cancellationToken) ?? [];
 
         if (!qry.IncludeInactive)
             feeResult = feeResult.Where(d => d.statecode != DynamicsConstants.StateCode_Inactive);
 
         if (qry.WorkerLicenceTypeEnum != null)
         {
-            DynamicsContextLookupHelpers.ServiceTypeGuidDictionary.TryGetValue(qry.WorkerLicenceTypeEnum.ToString(), out Guid stGuid);
+            DynamicsContextLookupHelpers.ServiceTypeGuidDictionary.TryGetValue(qry.WorkerLicenceTypeEnum.ToString()!, out Guid stGuid);
             feeResult = feeResult.Where(f => f._spd_servicetypeid_value == stGuid);
         }
 
         if (qry.LicenceTermEnum != null)
         {
-            int term = (int)Enum.Parse<LicenceTermOptionSet>(qry.LicenceTermEnum.ToString());
+            int term = (int)Enum.Parse<LicenceTermOptionSet>(qry.LicenceTermEnum.ToString()!);
             feeResult = feeResult.Where(f => f.spd_term == term);
         }
 
         if (qry.ApplicationTypeEnum != null)
         {
-            int type = (int)Enum.Parse<LicenceApplicationTypeOptionSet>(qry.ApplicationTypeEnum.ToString());
+            int type = (int)Enum.Parse<LicenceApplicationTypeOptionSet>(qry.ApplicationTypeEnum.ToString()!);
             feeResult = feeResult.Where(f => f.spd_type == type);
         }
 
         if (qry.BizTypeEnum != null)
         {
-            int bizType = (int)Enum.Parse<BizTypeOptionSet>(qry.BizTypeEnum.ToString());
+            int bizType = (int)Enum.Parse<BizTypeOptionSet>(qry.BizTypeEnum.ToString()!);
             feeResult = feeResult.Where(f => f.spd_businesstype == bizType);
         }
 
         if (qry.HasValidSwl90DayLicence != null)
         {
-            int hasValidSwl90Day = (int)SharedMappingFuncs.GetYesNo(qry.HasValidSwl90DayLicence);
+            int hasValidSwl90Day = (int)SharedMappingFuncs.GetYesNo(qry.HasValidSwl90DayLicence!)!;
             feeResult = feeResult.Where(f => f.spd_hasvalidswl90daylicence == hasValidSwl90Day);
         }
 
         return new LicenceFeeListResp
         {
-            LicenceFees = _mapper.Map<IEnumerable<LicenceFeeResp>>(feeResult)
+            LicenceFees = mapper.Map<IEnumerable<LicenceFeeResp>>(feeResult)
         };
     }
 }
