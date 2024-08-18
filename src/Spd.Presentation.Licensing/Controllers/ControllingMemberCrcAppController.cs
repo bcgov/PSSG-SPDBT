a90d064a776b966ba9134c062bcc2f189c020f16
Emad <121242386+esdd1995@users.noreply.github.com>
2024-08-17 21:30:02 -0700

    [SPDBT-2915]  crc controlling memeber (#1308)
    # Description
    
    This PR includes the following proposed change(s):
    
    - add contract (manager) and cotnroller for crc controlling
    member(unauth post)

---
+++ src/Spd.Presentation.Licensing/Controllers/ControllingMemberCrcAppController.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Spd.Manager.Licence;
+using Spd.Utilities.Shared;
+using Spd.Utilities.Shared.Exceptions;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace Spd.Presentation.Licensing.Controllers;
+
+[ApiController]
+public class ControllingMemberCrcAppController : SpdControllerBase
+{
+    private readonly IMediator _mediator;
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="mediator"></param>
+    public ControllingMemberCrcAppController (IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    /// <summary>
+    /// Save New Licence Crc Controlling Member
+    /// </summary>
+    /// <param name="ControllingMemberCrcSubmitRequest"></param>
+    /// <returns></returns>
+    [Route("api/controlling-member-crc-applications/anonymous/submit")]
+    [HttpPost]
+    public async Task<ControllingMemberCrcAppCommandResponse> SubmitControllingMemberCrcApplication([FromBody][Required] ControllingMemberCrcAppSubmitRequest ControllingMemberCrcSubmitRequest, CancellationToken ct)
+    {
+       return await _mediator.Send(new ControllingMemberCrcAppSubmitRequestCommand(ControllingMemberCrcSubmitRequest), ct);
+    }
+}
