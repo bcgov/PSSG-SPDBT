6f847c6547db1c722506acea4ad2dda87cae37a6
Emad <Emad.Salamat@quartech.com>
2024-08-19 12:52:12 -0700

    rename some fields

--- src/Spd.Manager.Licence/ControllingMemberCrcAppContract.cs
+++ src/Spd.Manager.Licence/ControllingMemberCrcAppContract.cs
@@ -15,23 +15,25 @@ public abstract record ControllingMemberCrcApp
     public string? MiddleName2 { get; set; }
     public string? Surname { get; set; }
     public bool? IsTermsOfUseAccepted { get; set; }
-    public DateTime DateSigned { get; set; }
+    public DateTime AgreeToCompleteAndAccurate { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public GenderCode? GenderCode { get; set; }
     public string? PhoneNumber { get; set; }
     public string? EmailAddress { get; set; }
     public bool? HasPreviousNames { get; set; }
     public IEnumerable<Alias> Aliases { get; set; } = Array.Empty<Alias>();
-    public bool? HasBcDriverLicence { get; set; }
-    public string? BcDriverLicenceNumber { get; set; }
+    public bool? hasBcDriversLicence { get; set; }
+    public string? BcDriversLicenceNumber { get; set; }
 
     public bool? IsPoliceOrPeaceOfficer { get; set; }
     public PoliceOfficerRoleCode? PoliceOfficerRoleCode { get; set; }
+    public string? OtherOfficerRole { get; set; }
+
     // public LicenceAppDocumentUpload? LetterOfConflictDoucuments { get; set; }
 
     public bool? IsCanadianCitizen { get; set; }
     //public LicenceAppDocumentUpload? CitizenshipProofDoucuments { get; set; }
-    public DateOnly? CitizenshipProofExpiryDate { get; set; }
+    //public DateOnly? CitizenshipProofExpiryDate { get; set; }
     //public LicenceAppDocumentUpload? FingerPrintDoucuments { get; set; }
     //public LicenceAppDocumentUpload? GovernmentIssuedIdDoucuments1 { get; set; }
     //public LicenceAppDocumentUpload? GovernmentIssuedIdDoucuments2 { get; set; }
@@ -42,7 +44,7 @@ public abstract record ControllingMemberCrcApp
     public bool? HasBankruptcyHistory { get; set; }
     public string? BankruptcyHistoryDetail { get; set; }
 
-    public bool? HasMentalCondition { get; set; }
+    public bool? isTreatedForMHC { get; set; }
     //public LicenceAppDocumentUpload? MentalHealthConditionDoucuments { get; set; }
 
     public Address? ResidentialAddress { get; set; }
