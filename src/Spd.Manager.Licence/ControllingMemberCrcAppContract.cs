ba1b6c74bc34a48ac6178d0d45b5e775a5b22cb9
Emad <Emad.Salamat@quartech.com>
2024-08-19 13:43:49 -0700

    eddit uppercase

--- src/Spd.Manager.Licence/ControllingMemberCrcAppContract.cs
+++ src/Spd.Manager.Licence/ControllingMemberCrcAppContract.cs
@@ -14,15 +14,14 @@ public abstract record ControllingMemberCrcApp
     public string? MiddleName1 { get; set; }
     public string? MiddleName2 { get; set; }
     public string? Surname { get; set; }
-    public bool? agreeToCompleteAndAccurate { get; set; }
-    public DateTime AgreeToCompleteAndAccurateDate { get; set; }
+    public bool? AgreeToCompleteAndAccurate { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public GenderCode? GenderCode { get; set; }
     public string? PhoneNumber { get; set; }
     public string? EmailAddress { get; set; }
     public bool? HasPreviousNames { get; set; }
     public IEnumerable<Alias> Aliases { get; set; } = Array.Empty<Alias>();
-    public bool? hasBcDriversLicence { get; set; }
+    public bool? HasBcDriversLicence { get; set; }
     public string? BcDriversLicenceNumber { get; set; }
 
     public bool? IsPoliceOrPeaceOfficer { get; set; }
@@ -33,7 +32,7 @@ public abstract record ControllingMemberCrcApp
 
     public bool? IsCanadianCitizen { get; set; }
     //public LicenceAppDocumentUpload? CitizenshipProofDoucuments { get; set; }
-    public DateOnly? CitizenshipProofExpiryDate { get; set; }
+    //public DateOnly? CitizenshipProofExpiryDate { get; set; }
     //public LicenceAppDocumentUpload? FingerPrintDoucuments { get; set; }
     //public LicenceAppDocumentUpload? GovernmentIssuedIdDoucuments1 { get; set; }
     //public LicenceAppDocumentUpload? GovernmentIssuedIdDoucuments2 { get; set; }
