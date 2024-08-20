72c6301323063a806cd14da71e67570bad34646c
Emad <121242386+esdd1995@users.noreply.github.com>
2024-08-20 11:47:46 -0700

    Emad/controlling member crc manager, contract and mappings (#1318)
    # Description
    
    This PR includes the following proposed change(s):
    - Added `ControllingMemberCrcAppManager` and Handle for
    `ControllingMemberCrcAppSubmitRequestCommand`
    - Added Contract.cs and `CreateControllingMemberCrcAppCmd`
    - Added mapping for `ControllingMemberCrcAppSubmitRequest `=>
    `CreateControllingMemberCrcAppCmd` (Manager.Licence)
    - Added Mappings.cs for repo layer (CreateCmd to `spd_contact` and
    `spd_application`)

---
+++ src/Spd.Manager.Licence/ControllingMemberCrcAppManager.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using MediatR;
+using Spd.Resource.Repository.ControllingMemberCrcApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Manager.Licence;
+internal class ControllingMemberCrcAppManager :
+        IRequestHandler<ControllingMemberCrcAppSubmitRequestCommand, ControllingMemberCrcAppCommandResponse>,
+        IControllingMemberCrcAppManager
+{
+    protected readonly IMapper _mapper;
+
+    public ControllingMemberCrcAppManager(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    public async Task<ControllingMemberCrcAppCommandResponse> Handle(ControllingMemberCrcAppSubmitRequestCommand cmd, CancellationToken ct)
+    {
+        ControllingMemberCrcAppSubmitRequest request = cmd.ControllingMemberCrcAppSubmitRequest;
+        //save the application
+        CreateControllingMemberCrcAppCmd createApp = _mapper.Map<CreateControllingMemberCrcAppCmd>(request);
+        //var response = await _controllingMemberCrcAppRepository.CreateControllingMemberCrcApplicationAsync(createApp, ct);
+        
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Spd.Resource.Repository/ControllingMemberCrcApplication/Contract.cs
@@ -0,0 +1,53 @@
+﻿using Spd.Resource.Repository.Alias;
+using Spd.Resource.Repository.Application;
+using Spd.Resource.Repository.PersonLicApplication;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spd.Resource.Repository.ControllingMemberCrcApplication;
+
+public record ControllingMemberCrcApplication
+{
+    public string? GivenName { get; set; }
+    public string? MiddleName1 { get; set; }
+    public string? MiddleName2 { get; set; }
+    public string? Surname { get; set; }
+    public bool? AgreeToCompleteAndAccurate { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public GenderEnum? GenderCode { get; set; }
+    public string? PhoneNumber { get; set; }
+    public string? EmailAddress { get; set; }
+    public bool? HasPreviousNames { get; set; }
+    public IEnumerable<AliasResp> Aliases { get; set; }
+    public bool? HasBcDriversLicence { get; set; }
+    public string? BcDriversLicenceNumber { get; set; }
+    public bool? IsPoliceOrPeaceOfficer { get; set; }
+    public PoliceOfficerRoleEnum? PoliceOfficerRoleCode { get; set; }
+    public string? OtherOfficerRole { get; set; }
+    public bool? IsCanadianCitizen { get; set; }
+    public bool? HasCriminalHistory { get; set; }
+    public string? CriminalHistoryDetail { get; set; }
+    public bool? HasBankruptcyHistory { get; set; }
+    public string? BankruptcyHistoryDetail { get; set; }
+    public bool? IsTreatedForMHC { get; set; }
+    public ResidentialAddr? ResidentialAddressData { get; set; }
+}
+
+public record CreateControllingMemberCrcAppCmd() : ControllingMemberCrcApplication
+{
+
+};
+public record ControllingMemberCrcApplicationResp() : ControllingMemberCrcApplication
+{
+    //TODO: what are response props?
+    public Guid? ControllingMemberCrcAppId { get; set; }
+    public Guid? ContactId { get; set; }
+    //public DateOnly? ExpiryDate { get; set; }
+    //public ApplicationPortalStatusEnum? ApplicationPortalStatus { get; set; }
+    //public string? CaseNumber { get; set; }
+    //public LicenceTermEnum? OriginalLicenceTermCode { get; set; }
+    //public string? ExpiredLicenceNumber { get; set; }
+}
