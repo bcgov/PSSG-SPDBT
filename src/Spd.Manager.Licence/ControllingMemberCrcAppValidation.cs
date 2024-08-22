cf10d19defba28bc3e0666becef9886590024665
Emad <Emad.Salamat@quartech.com>
2024-08-22 15:19:02 -0700

    change validation condition

--- src/Spd.Manager.Licence/ControllingMemberCrcAppValidation.cs
+++ src/Spd.Manager.Licence/ControllingMemberCrcAppValidation.cs
@@ -12,40 +12,60 @@ public class ControllingMemberCrcAppValidator : AbstractValidator<ControllingMem
     public ControllingMemberCrcAppValidator()
     {
         RuleFor(r => r.ParentBizLicApplicationId).NotEmpty();
-        RuleFor(r => r.Surname).NotEmpty();
+        RuleFor(r => r.Surname)
+            .MaximumLength(40)
+            .NotEmpty();
+        RuleFor(r => r.GivenName)
+            .MaximumLength(40);
+        RuleFor(r => r.MiddleName1)
+            .MaximumLength(40);
+        RuleFor(r => r.MiddleName2)
+            .MaximumLength(40);
         RuleFor(r => r.GenderCode).NotEmpty();
         RuleFor(r => r.DateOfBirth).NotEmpty();
-        RuleFor(r => r.PhoneNumber).NotEmpty();
-        RuleFor(r => r.EmailAddress).NotEmpty().EmailAddress();
+        RuleFor(r => r.PhoneNumber)
+            .MaximumLength(15);
+        RuleFor(r => r.EmailAddress)
+            .MaximumLength(75)
+            .EmailAddress();
         RuleFor(r => r.ResidentialAddress).NotEmpty();
         RuleFor(r => r.ResidentialAddress.AddressLine1)
             .NotEmpty()
             .MaximumLength(100)
             .When(r => r.ResidentialAddress != null);
-        RuleFor(r => r.ResidentialAddress.City).NotEmpty()
+        RuleFor(r => r.ResidentialAddress.AddressLine2)
             .MaximumLength(100)
             .When(r => r.ResidentialAddress != null);
-        RuleFor(r => r.ResidentialAddress.Province).NotEmpty()
+        RuleFor(r => r.ResidentialAddress.City)
+            .NotEmpty()
             .MaximumLength(100)
             .When(r => r.ResidentialAddress != null);
-        RuleFor(r => r.ResidentialAddress.Country).NotEmpty()
+        RuleFor(r => r.ResidentialAddress.Province)
+            .NotEmpty()
             .MaximumLength(100)
             .When(r => r.ResidentialAddress != null);
-        RuleFor(r => r.ResidentialAddress.PostalCode).NotEmpty()
+        RuleFor(r => r.ResidentialAddress.Country)
+            .NotEmpty()
+            .MaximumLength(100)
+            .When(r => r.ResidentialAddress != null);
+        RuleFor(r => r.ResidentialAddress.PostalCode)
+            .NotEmpty()
             .MaximumLength(20)
             .When(r => r.ResidentialAddress != null);
         RuleFor(r => r.HasCriminalHistory).NotEmpty();
         RuleFor(r => r.CriminalHistoryDetail)
             .NotEmpty()
-            .MaximumLength(1000)
+            .MaximumLength(250)
             .When(r => r.HasCriminalHistory == true);
-        RuleFor(r => r.HasBankruptcyHistory).NotEmpty();
+        RuleFor(r => r.HasBankruptcyHistory)
+            .NotEmpty();
         RuleFor(r => r.BankruptcyHistoryDetail)
             .NotEmpty()
-            .MaximumLength(1000)
+            .MaximumLength(250)
             .When(r => r.HasBankruptcyHistory == true);
 
-        RuleFor(r => r.IsPoliceOrPeaceOfficer).NotEmpty();
+        RuleFor(r => r.IsPoliceOrPeaceOfficer)
+            .NotEmpty();
         RuleFor(r => r.PoliceOfficerRoleCode)
             .NotEmpty()
             .When(r => r.IsPoliceOrPeaceOfficer == true);
@@ -56,23 +76,30 @@ public class ControllingMemberCrcAppValidator : AbstractValidator<ControllingMem
 
         RuleFor(r => r.Aliases)
             .NotEmpty()
-            .Must(r => r.Count() <= Constants.MaximumNumberOfUserEnteredAliases)
+            .Must(r => r.Count() <= 3)
             .When(r => r.HasPreviousNames == true)
-            .WithMessage("No more than 10 user entered aliases are allowed");
+            .WithMessage("No more than 3 user entered aliases are allowed");
 
         RuleForEach(r => r.Aliases)
             .ChildRules(aliases =>
             {
-                aliases.RuleFor(alias => alias.Surname)
+                aliases.RuleFor(r => r.Surname)
+                    .MaximumLength(40)
                     .NotEmpty()
                     .WithMessage("Surname name is required for all aliases.");
+                aliases.RuleFor(r => r.GivenName)
+                    .MaximumLength(40);
+                aliases.RuleFor(r => r.MiddleName1)
+                    .MaximumLength(40);
+                aliases.RuleFor(r => r.MiddleName2)
+                    .MaximumLength(40);
             })
             .When(r => r.Aliases != null && r.HasPreviousNames == true);
 
         RuleFor(r => r.IsTreatedForMHC).NotEmpty();
         RuleFor(r => r.IsCanadianCitizen).NotEmpty();
         RuleFor(r => r.BcDriversLicenceNumber)
-            .MaximumLength(7)
+            .MaximumLength(8)
             .When(r => r.HasBcDriversLicence == true);
         RuleFor(r => r.AgreeToCompleteAndAccurate).Equal(true);
     }
