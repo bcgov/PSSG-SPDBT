54905abe508f063297a9dcf418743627c9cc6457
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-08-27 10:57:10 -0700

    Spd.Utilities.Recaptcha warnings cleanup

--- src/Spd.Utilities.Recaptcha/GoogleReCaptchaConfiguration.cs
+++ src/Spd.Utilities.Recaptcha/GoogleReCaptchaConfiguration.cs
@@ -1,7 +1,10 @@
 ﻿namespace Spd.Utilities.Recaptcha;
+
 public class GoogleReCaptchaConfiguration
 {
+#pragma warning disable S1075 // URIs should not be hardcoded
     private const string UriString = "https://www.google.com/recaptcha/api/siteverify";
+#pragma warning restore S1075 // URIs should not be hardcoded
 
     public string SecretKey { get; set; } = null!;
     public Uri Url { get; set; } = new Uri(UriString);
