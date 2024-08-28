adf68775e1451ecb973f9c8fabe53c41b4949430
Peggy <peggy.zhang@quartech.com>
2024-08-28 16:59:42 -0700

    support unauth sp abort biz app (#1350)
    # Description
    
    This PR includes the following proposed change(s):
    
    - This is to support unauth sp app, biz go back to swl flow, we need to
    save the licenceApp in the cookie to ensure it is one session.

--- src/Spd.Presentation.Licensing/Controllers/SessionConstants.cs
+++ src/Spd.Presentation.Licensing/Controllers/SessionConstants.cs
@@ -3,6 +3,7 @@
 public static class SessionConstants
 {
     public static readonly string AnonymousRequestDataProtectorName = "AppAnonymousSubmitRequest";
-    public static readonly string AnonymousApplicationContext = "LicenceApplicationContext";
+    public static readonly string AnonymousApplicationContext = "LicenceApplicationContext"; //used for anonymous update/renew/replace, get application content
     public static readonly string AnonymousApplicationSubmitKeyCode = "LicenceApplicationSubmitKeyCode";
+    public static readonly string AnonymousSoleProprietorApplicationContext = "SoleProprietorApplicationContext"; //used for unauth sole proprietor, biz abort the app
 }
