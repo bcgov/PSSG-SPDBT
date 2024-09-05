ba255c2fb2c085124e7734bc9cd1071317e42be3
Yossi Tamari <Yossi.Tamari@quartech.com>
2024-09-05 08:25:07 -0700

    added auto discovery of health checks

--- src/Spd.Utilities.Hosting/ServiceExtensionsHealthChecks.cs
+++ src/Spd.Utilities.Hosting/ServiceExtensionsHealthChecks.cs
@@ -2,21 +2,30 @@
 using Amazon.S3;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Spd.Utilities.Dynamics;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Reflection;
 
 namespace Spd.Utilities.Hosting;
 
 public static class ServiceExtensionsHealthChecks
 {
-    public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration, Assembly[] assemblies)
     {
+        var healthChecks = assemblies.SelectMany(a => a.Discover<IHealthCheck>());
+
         var healthCheckBuilder = services.AddHealthChecks();
-        healthCheckBuilder.AddCheck<DynamicsHealthCheck>("dynamics");
+
+        foreach (var healthCheck in healthChecks)
+        {
+            healthCheckBuilder.Add(new HealthCheckRegistration(healthCheck.Name, sp => (IHealthCheck)sp.GetRequiredService(healthCheck), null, null));
+        }
+
         var redisConnection = configuration.GetValue<string?>("RedisConnection");
         if (redisConnection != null)
         {
             healthCheckBuilder.AddRedis(redisConnection);
         }
+
         var s3url = configuration.GetValue<string?>("storage:MainBucketSettings:url");
         if (s3url != null)
         {
