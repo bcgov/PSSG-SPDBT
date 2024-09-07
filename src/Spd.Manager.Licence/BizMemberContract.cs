c2b887228fd4ef4c21c4e6eef6f0c80344950ebb
Peggy <peggy.zhang@quartech.com>
2024-09-06 17:22:41 -0700

    refactor cm api (#1368)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [refactor cm api](spdbt-3011: refactor controlling member api)

--- src/Spd.Manager.Licence/BizMemberContract.cs
+++ src/Spd.Manager.Licence/BizMemberContract.cs
@@ -5,6 +5,11 @@ namespace Spd.Manager.Licence;
 public interface IBizMemberManager
 {
     public Task<Members> Handle(GetBizMembersQuery query, CancellationToken ct);
+    public Task<BizMemberResponse> Handle(CreateBizEmployeeCommand cmd, CancellationToken ct);
+    public Task<BizMemberResponse> Handle(CreateBizSwlControllingMemberCommand cmd, CancellationToken ct);
+    public Task<BizMemberResponse> Handle(CreateBizNonSwlControllingMemberCommand cmd, CancellationToken ct);
+    public Task<BizMemberResponse> Handle(UpdateBizNonSwlControllingMemberCommand cmd, CancellationToken ct);
+    public Task<Unit> Handle(DeleteBizMemberCommand cmd, CancellationToken ct);
     public Task<Unit> Handle(UpsertBizMembersCommand cmd, CancellationToken ct);
     public Task<ControllingMemberInvitesCreateResponse> Handle(BizControllingMemberNewInviteCommand command, CancellationToken ct);
     public Task<ControllingMemberAppInviteVerifyResponse> Handle(VerifyBizControllingMemberInviteCommand command, CancellationToken ct);
@@ -12,9 +17,18 @@ public interface IBizMemberManager
 
 public record BizControllingMemberNewInviteCommand(Guid BizContactId, Guid UserId, string HostUrl) : IRequest<ControllingMemberInvitesCreateResponse>;
 public record VerifyBizControllingMemberInviteCommand(string InviteEncryptedCode) : IRequest<ControllingMemberAppInviteVerifyResponse>;
-
 public record GetBizMembersQuery(Guid BizId, Guid? AppId = null) : IRequest<Members>;
-
+public record UpsertBizMembersCommand(
+    Guid BizId,
+    Guid? ApplicationId,
+    Members Members,
+    IEnumerable<LicAppFileInfo> LicAppFileInfos) : IRequest<Unit>; //deprecated
+public record CreateBizEmployeeCommand(Guid BizId, SwlContactInfo Employee) : IRequest<BizMemberResponse>;
+public record CreateBizSwlControllingMemberCommand(Guid BizId, SwlContactInfo SwlControllingMember) : IRequest<BizMemberResponse>;
+public record CreateBizNonSwlControllingMemberCommand(Guid BizId, NonSwlContactInfo NonSwlControllingMember) : IRequest<BizMemberResponse>;
+public record UpdateBizNonSwlControllingMemberCommand(Guid BizId, Guid BizContactId, NonSwlContactInfo NonSwlControllingMember) : IRequest<BizMemberResponse>;
+public record DeleteBizMemberCommand(Guid BizId, Guid BizContactId) : IRequest<Unit>;
+public record BizMemberResponse(Guid? bizContactId);
 public record Members
 {
     public IEnumerable<SwlContactInfo> SwlControllingMembers { get; set; }
@@ -26,12 +40,6 @@ public record MembersRequest : Members
 {
     public IEnumerable<Guid> ControllingMemberDocumentKeyCodes { get; set; } = Array.Empty<Guid>();//the document is saved in cache.
 }
-
-public record UpsertBizMembersCommand(
-    Guid BizId,
-    Guid? ApplicationId,
-    Members Members,
-    IEnumerable<LicAppFileInfo> LicAppFileInfos) : IRequest<Unit>;
 
 public record ControllingMemberInvitesCreateResponse(Guid BizContactId)
 {
