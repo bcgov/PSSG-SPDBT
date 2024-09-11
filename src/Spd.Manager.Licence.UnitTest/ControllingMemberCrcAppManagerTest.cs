2bbdc43195007fb10f2a09315e310cb844aef0ed
Emad <121242386+esdd1995@users.noreply.github.com>
2024-09-11 13:33:53 -0700

    alias update, clean get response (#1380)
    # Description
    
    This PR includes the following proposed change(s):
    - refactored `get` response object and removed unnecessary data
    - generated method for handling updating personal information (contact,
    address, aliases), prevent history record in partial save mode
    - refactored manager test
    - endpoint for update application
    - cleanup, mappings and validation

--- src/Spd.Manager.Licence.UnitTest/ControllingMemberCrcAppManagerTest.cs
+++ src/Spd.Manager.Licence.UnitTest/ControllingMemberCrcAppManagerTest.cs
@@ -1,14 +1,17 @@
 ﻿using AutoMapper;
+using Microsoft.Dynamics.CRM;
 using Moq;
 using Spd.Manager.Licence;
 using Spd.Resource.Repository;
 using Spd.Resource.Repository.ApplicationInvite;
+using Spd.Resource.Repository.Contact;
 using Spd.Resource.Repository.ControllingMemberCrcApplication;
 using Spd.Resource.Repository.ControllingMemberInvite;
 using Spd.Resource.Repository.Document;
 using Spd.Resource.Repository.LicApp;
 using Spd.Resource.Repository.Licence;
 using Spd.Resource.Repository.LicenceFee;
+using Spd.Resource.Repository.Tasks;
 using Spd.Utilities.FileStorage;
 using Spd.Utilities.Shared.Exceptions;
 using Mappings = Spd.Manager.Licence.Mappings;
@@ -16,7 +19,6 @@ using Mappings = Spd.Manager.Licence.Mappings;
 namespace Spd.Manager.Licence.UnitTest;
 public class ControllingMemberCrcAppManagerTests
 {
-    private readonly Mock<ControllingMemberCrcAppManager> _managerMock;
     private readonly Mock<IMapper> _mapperMock;
     private readonly Mock<IDocumentRepository> _documentRepositoryMock;
     private readonly Mock<ILicenceFeeRepository> _feeRepositoryMock;
@@ -26,6 +28,8 @@ public class ControllingMemberCrcAppManagerTests
     private readonly Mock<IControllingMemberCrcRepository> _controllingMemberCrcRepositoryMock;
     private readonly Mock<ILicAppRepository> _licAppRepositoryMock;
     private readonly Mock<IControllingMemberInviteRepository> _cmInviteRepositoryMock;
+    private readonly Mock<IContactRepository> _contactRepositroyMock;
+    private readonly Mock<ITaskRepository> _taskRepositoryMock;
 
     private ControllingMemberCrcAppManager sut;
 
@@ -40,6 +44,8 @@ public class ControllingMemberCrcAppManagerTests
         _controllingMemberCrcRepositoryMock = new Mock<IControllingMemberCrcRepository>();
         _licAppRepositoryMock = new Mock<ILicAppRepository>();
         _cmInviteRepositoryMock = new Mock<IControllingMemberInviteRepository>();
+        _contactRepositroyMock = new Mock<IContactRepository>();
+        _taskRepositoryMock = new Mock<ITaskRepository>();
         var mapperConfig = new MapperConfiguration(x =>
         {
             x.AddProfile<Mappings>();
@@ -50,7 +56,8 @@ public class ControllingMemberCrcAppManagerTests
         _feeRepositoryMock.Object,
         _licenceRepositoryMock.Object,
         _mainFileServiceMock.Object,
-        _transientFileServiceMock.Object, _controllingMemberCrcRepositoryMock.Object, _cmInviteRepositoryMock.Object, _licAppRepositoryMock.Object);
+        _transientFileServiceMock.Object, _controllingMemberCrcRepositoryMock.Object, 
+        _cmInviteRepositoryMock.Object,_contactRepositroyMock.Object, _taskRepositoryMock.Object, _licAppRepositoryMock.Object);
     }
 
     [Fact]
@@ -59,6 +66,12 @@ public class ControllingMemberCrcAppManagerTests
         Guid applicantId = Guid.NewGuid();
         Guid controllingMemberAppId = Guid.NewGuid();
         Guid inviteId = Guid.NewGuid();
+        ContactResp contact = new()
+        {
+           Id = applicantId
+        };
+        SetupMockRepositories(applicantId, controllingMemberAppId, contact, inviteId);
+
         // Arrange
         var request = new ControllingMemberCrcAppSubmitRequest
         {
@@ -76,6 +89,55 @@ public class ControllingMemberCrcAppManagerTests
                     new LicAppFileInfo { LicenceDocumentTypeCode = LicenceDocumentTypeCode.CanadianPassport }
             });
 
+
+        // Act
+        var result = await sut.Handle(command, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<ControllingMemberCrcAppCommandResponse>(result);
+
+        Assert.NotNull(result);
+        Assert.Equal(result.ControllingMemberAppId, controllingMemberAppId);
+        _mainFileServiceMock.Verify();
+        _transientFileServiceMock.Verify();
+    }
+
+
+    [Fact]
+    public async Task Handle_anonymous_submit_WhithoutFingerPrint_ShouldReturnError()
+    {
+        Guid applicantId = Guid.NewGuid();
+        Guid controllingMemberAppId = Guid.NewGuid();
+        Guid inviteId = Guid.NewGuid();
+        ContactResp contact = new()
+        {
+            Id = applicantId,
+        };
+        SetupMockRepositories(applicantId, controllingMemberAppId, contact, inviteId);
+
+        // Arrange
+        var request = new ControllingMemberCrcAppSubmitRequest
+        {
+            IsCanadianCitizen = true,
+            IsPoliceOrPeaceOfficer = true,
+            IsTreatedForMHC = false,
+            InviteId = inviteId,
+        };
+
+        var command = new ControllingMemberCrcAppNewCommand(request,
+            new List<LicAppFileInfo>
+                {
+                    new LicAppFileInfo { LicenceDocumentTypeCode = LicenceDocumentTypeCode.PoliceBackgroundLetterOfNoConflict },
+                    new LicAppFileInfo { LicenceDocumentTypeCode = LicenceDocumentTypeCode.CanadianPassport }
+            });
+
+
+        // Act and Assert
+        Func<Task> act = () => sut.Handle(command, CancellationToken.None);
+        await Assert.ThrowsAsync<ApiException>(act);
+    }
+    private void SetupMockRepositories(Guid applicantId, Guid controllingMemberAppId, ContactResp contact, Guid inviteId)
+    {
         _licAppRepositoryMock.Setup(a => a.QueryAsync(It.IsAny<LicenceAppQuery>(), CancellationToken.None))
                .ReturnsAsync(new List<LicenceAppListResp>()); //no dup lic app
         _licenceRepositoryMock.Setup(a => a.QueryAsync(It.IsAny<LicenceQry>(), CancellationToken.None)) //no dup lic
@@ -104,62 +166,18 @@ public class ControllingMemberCrcAppManagerTests
                     Status = ApplicationInviteStatusEnum.Sent
                 }
             });
-
-        // Act
-        var result = await sut.Handle(command, CancellationToken.None);
-
-        // Assert
-        Assert.IsType<ControllingMemberCrcAppCommandResponse>(result);
-
-        Assert.NotNull(result);
-        Assert.Equal(result.ControllingMemberAppId, controllingMemberAppId);
-        _mainFileServiceMock.Verify();
-        _transientFileServiceMock.Verify();
-    }
-
-    [Fact]
-    public async Task Handle_anonymous_submit_WhithoutFingerPrint_ShouldReturnError()
-    {
-        Guid applicantId = Guid.NewGuid();
-        Guid controllingMemberAppId = Guid.NewGuid();
-        // Arrange
-        var request = new ControllingMemberCrcAppSubmitRequest
-        {
-            IsCanadianCitizen = true,
-            IsPoliceOrPeaceOfficer = true,
-            IsTreatedForMHC = false,
-        };
-
-        var command = new ControllingMemberCrcAppNewCommand(request,
-            new List<LicAppFileInfo>
+        _contactRepositroyMock.Setup(c => c.GetAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(contact);
+        
+        _contactRepositroyMock.Setup(c => c.ManageAsync(It.IsAny<CreateContactCmd>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(contact);
+        _cmInviteRepositoryMock.Setup(i => i.QueryAsync(It.IsAny<ControllingMemberInviteQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new List<ControllingMemberInviteResp>()
+            {
+                new ControllingMemberInviteResp()
                 {
-                    new LicAppFileInfo { LicenceDocumentTypeCode = LicenceDocumentTypeCode.PoliceBackgroundLetterOfNoConflict },
-                    new LicAppFileInfo { LicenceDocumentTypeCode = LicenceDocumentTypeCode.CanadianPassport }
+                    Id = inviteId
+                }
             });
-
-        _licAppRepositoryMock.Setup(a => a.QueryAsync(It.IsAny<LicenceAppQuery>(), CancellationToken.None))
-               .ReturnsAsync(new List<LicenceAppListResp>()); //no dup lic app
-        _licenceRepositoryMock.Setup(a => a.QueryAsync(It.IsAny<LicenceQry>(), CancellationToken.None)) //no dup lic
-            .ReturnsAsync(new LicenceListResp()
-            {
-                Items = new List<LicenceResp> { }
-            });
-        _controllingMemberCrcRepositoryMock.Setup(a => a.SaveControllingMemberCrcApplicationAsync(It.IsAny<SaveControllingMemberCrcAppCmd>(), CancellationToken.None))
-            .ReturnsAsync(new ControllingMemberCrcApplicationCmdResp(controllingMemberAppId, applicantId));
-        _documentRepositoryMock.Setup(m => m.QueryAsync(It.Is<DocumentQry>(q => q.ApplicationId == controllingMemberAppId), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new DocumentListResp()
-            {
-                Items = new List<DocumentResp> { new() }
-            });
-        _transientFileServiceMock.Setup(m => m.HandleQuery(It.IsAny<FileMetadataQuery>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync(new FileMetadataQueryResult("key", "folder", null));
-        _mainFileServiceMock.Setup(m => m.HandleCopyStorageFromTransientToMainCommand(It.IsAny<CopyStorageFromTransientToMainCommand>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync("string");
-        _transientFileServiceMock.Setup(m => m.HandleDeleteCommand(It.IsAny<StorageDeleteCommand>(), It.IsAny<CancellationToken>()))
-            .ReturnsAsync("string");
-
-        // Act and Assert
-        Func<Task> act = () => sut.Handle(command, CancellationToken.None);
-        await Assert.ThrowsAsync<ApiException>(act);
     }
 }
