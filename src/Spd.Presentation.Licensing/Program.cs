541ff492df652cde3d000789f9dc3bbc2fab806e
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-09-13 13:43:24 -0700

    SPDBT-3021: logging and observability for Licensing Portal (#1392)

--- src/Spd.Presentation.Licensing/Program.cs
+++ src/Spd.Presentation.Licensing/Program.cs
@@ -1,6 +1,5 @@
 using FluentValidation;
-using HealthChecks.UI.Client;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Serilog;
 using Spd.Presentation.Licensing;
 using Spd.Presentation.Licensing.Services;
 using Spd.Presentation.Licensing.Swagger;
@@ -18,97 +17,104 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-string assembliesPrefix = "Spd";
+var secretsFilePath = builder.Configuration.GetValue<string>("SECRETS_FILE");
+if (!string.IsNullOrEmpty(secretsFilePath)) builder.Configuration.AddJsonFile(secretsFilePath, true, true);
 
-Assembly[] assemblies = Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? string.Empty, "*.dll", SearchOption.TopDirectoryOnly)
-     .Where(assembly =>
-     {
-         var assemblyName = Path.GetFileName(assembly);
-         return !assemblyName.StartsWith("System.") && !assemblyName.StartsWith("Microsoft.") && (string.IsNullOrEmpty(assembliesPrefix) || assemblyName.StartsWith(assembliesPrefix));
-     })
-     .Select(assembly => Assembly.LoadFrom(assembly))
-     .ToArray();
+var logger = builder.ConfigureWebApplicationObservability();
 
-var secretsFile = Environment.GetEnvironmentVariable($"SECRETS_FILE");
-if (!string.IsNullOrEmpty(secretsFile)) builder.Configuration.AddJsonFile(secretsFile, true, true);
+logger.Information("Starting up");
 
-builder.Services.ConfigureCors(builder.Configuration);
-var assemblyName = $"{typeof(Program).GetTypeInfo().Assembly.GetName().Name}";
-builder.Services.ConfigureSwagger(assemblyName);
-builder.Services.ConfigureDataProtection(builder.Configuration, "ProtectionShareKeyApp");
-builder.Services
-    .AddEndpointsApiExplorer()
-    .AddControllers()
-    .AddJsonOptions(x =>
+try
+{
+    var assemblies = ReflectionExtensions.DiscoverLocalAessemblies(prefix: "Spd.");
+
+    builder.Services.ConfigureCors(builder.Configuration);
+    var assemblyName = $"{typeof(Program).GetTypeInfo().Assembly.GetName().Name}";
+
+    builder.Services.ConfigureDataProtection(builder.Configuration, "ProtectionShareKeyApp");
+    builder.Services.ConfigureSwagger(assemblyName);
+    builder.Services.ConfigureDataProtection(builder.Configuration, "ProtectionShareKeyApp");
+    builder.Services
+        .AddEndpointsApiExplorer()
+        .AddControllers()
+        .AddJsonOptions(x =>
+        {
+            x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        });
+
+    builder.Services.ConfigureAuthentication(builder.Configuration);
+    builder.Services.ConfigureAuthorization();
+    builder.Services.AddHttpContextAccessor();
+    builder.Services.AddRequestDecompression().AddResponseCompression();
+    builder.Services.AddValidatorsFromAssemblies(assemblies);
+    builder.Services.AddTransient<IPrincipal>(provider => provider.GetService<IHttpContextAccessor>()?.HttpContext?.User);
+    builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+
+    var redisConnection = builder.Configuration.GetValue<string>("RedisConnection");
+    if (!string.IsNullOrWhiteSpace(redisConnection))
     {
-        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-    });
+        builder.Services.AddStackExchangeRedisCache(options => options.Configuration = redisConnection);
+    }
+    else
+    {
+        logger.Warning("Redis distributed cache is not configure correctly");
+        builder.Services.AddDistributedMemoryCache();
+    }
 
-builder.Services.ConfigureAuthentication(builder.Configuration);
-builder.Services.ConfigureAuthorization();
-builder.Services.AddHttpContextAccessor();
-builder.Services.AddValidatorsFromAssemblies(assemblies);
-builder.Services.AddTransient<IPrincipal>(provider => provider.GetService<IHttpContextAccessor>()?.HttpContext?.User);
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assemblies));
+    builder.Services.AddAutoMapper(assemblies);
+    builder.Services.AddFileStorageProxy(builder.Configuration);
+    builder.Services.AddTransient<IMultipartRequestService, MultipartRequestService>();
+    builder.Services.AddFileStorageProxy(builder.Configuration);
+    builder.Services
+      .AddBCeIDService(builder.Configuration)
+      .AddGoogleRecaptcha(builder.Configuration)
+      .AddPaymentService(builder.Configuration)
+      .AddDynamicsProxy(builder.Configuration)
+      .AddAddressAutoComplete(builder.Configuration);
+    builder.Services.ConfigureComponentServices(builder.Configuration, builder.Environment, assemblies);
+    builder.Services.AddHealthChecks();
 
-//add cache
-string? redisConnection = builder.Configuration.GetValue<string>("RedisConnection");
+    var app = builder.Build();
 
-if (redisConnection != null && !string.IsNullOrWhiteSpace(redisConnection))
-{
-    builder.Services.AddStackExchangeRedisCache(options => options.Configuration = redisConnection);
+    // Configure the HTTP request pipeline.
+    app.UsePathBase(builder.Configuration.GetValue("BASE_PATH", string.Empty));
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseForwardedHeaders();
+    app.UseCors();
+    app.UseResponseCaching();
+    app.UseRequestDecompression();
+    app.UseResponseCompression();
+    app.UseStaticFiles();
+    app.MapFallbackToFile("index.html");
+    app.UseRouting();
+    app.UseObservabilityMiddleware();
+    app.UseHealthChecks();
+    app.UseAuthentication();
+    app.UseMiddleware<UsersMiddleware>();
+    app.UseAuthorization();
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+
+    // Initialize slow dependencies
+    app.Services.GetRequiredService<AutoMapper.IConfigurationProvider>().CompileMappings();
+
+    await app.RunAsync();
+
+    logger.Information("Stopped");
+    return 0;
 }
-else
+catch (Exception e)
 {
-    builder.Services.AddDistributedMemoryCache();
+    logger.Fatal(e, "An unhandled exception occurred during bootstrapping");
+    return -1;
 }
-
-builder.Services.AddAutoMapper(assemblies);
-builder.Services.AddFileStorageProxy(builder.Configuration);
-builder.Services.AddTransient<IMultipartRequestService, MultipartRequestService>();
-builder.Services.AddFileStorageProxy(builder.Configuration);
-builder.Services
-  .AddBCeIDService(builder.Configuration)
-  .AddGoogleRecaptcha(builder.Configuration)
-  .AddPaymentService(builder.Configuration)
-  .AddDynamicsProxy(builder.Configuration)
-  .AddAddressAutoComplete(builder.Configuration);
-builder.Services.ConfigureComponentServices(builder.Configuration, builder.Environment, assemblies);
-
-builder.Services.AddHealthChecks();
-
-var app = builder.Build();
-
-// Initialize slow dependencies
-app.Services.GetRequiredService<AutoMapper.IConfigurationProvider>().CompileMappings();
-
-// Configure the HTTP request pipeline.
-app.UsePathBase(builder.Configuration.GetValue("BASE_PATH", string.Empty));
-if (app.Environment.IsDevelopment())
+finally
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    await Log.CloseAndFlushAsync();
 }
-
-app.UseStaticFiles();
-app.UseRouting();
-app.UseAuthentication();
-app.UseMiddleware<UsersMiddleware>();
-app.UseAuthorization();
-
-app.MapHealthChecks("/health/startup", new HealthCheckOptions
-{
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-app.MapHealthChecks("/health/liveness", new HealthCheckOptions { Predicate = _ => false })
-   .ShortCircuit();
-app.MapHealthChecks("/health/ready", new HealthCheckOptions { Predicate = _ => false })
-   .ShortCircuit();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-app.MapFallbackToFile("index.html");
-
-await app.RunAsync();
--- src/Spd.Utilities.Hosting/ServiceExtensionsHealthChecks.cs
+++ src/Spd.Utilities.Hosting/ServiceExtensionsHealthChecks.cs
@@ -1,22 +1,34 @@
 ﻿using Amazon.Runtime;
 using Amazon.S3;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Spd.Utilities.Dynamics;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Reflection;
 
 namespace Spd.Utilities.Hosting;
 
 public static class ServiceExtensionsHealthChecks
 {
-    public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration)
+    public static IServiceCollection AddHealthChecks(this IServiceCollection services, IConfiguration configuration, Assembly[] assemblies)
     {
+        var healthChecks = assemblies.SelectMany(a => a.Discover<IHealthCheck>());
+
         var healthCheckBuilder = services.AddHealthChecks();
-        healthCheckBuilder.AddCheck<DynamicsHealthCheck>("dynamics");
+
+        foreach (var healthCheck in healthChecks)
+        {
+            healthCheckBuilder.Add(new HealthCheckRegistration(healthCheck.Name, sp => (IHealthCheck)sp.GetRequiredService(healthCheck), null, null));
+        }
+
         var redisConnection = configuration.GetValue<string?>("RedisConnection");
         if (redisConnection != null)
         {
             healthCheckBuilder.AddRedis(redisConnection);
         }
+
         var s3url = configuration.GetValue<string?>("storage:MainBucketSettings:url");
         if (s3url != null)
         {
@@ -38,5 +50,17 @@ public static class ServiceExtensionsHealthChecks
         }
 
         return services;
+    }
+
+    public static void UseHealthChecks(this WebApplication webApplication)
+    {
+        webApplication.MapHealthChecks("/health/startup", new HealthCheckOptions
+        {
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+        });
+        webApplication.MapHealthChecks("/health/liveness", new HealthCheckOptions { Predicate = _ => false })
+           .ShortCircuit();
+        webApplication.MapHealthChecks("/health/ready", new HealthCheckOptions { Predicate = _ => false })
+           .ShortCircuit();
     }
 }
