70966ae8e6f17a0f8a0e303604d39c21b1c7515b
Peggy <peggy.zhang@quartech.com>
2024-09-17 14:10:57 -0700

    spdbt-3067: get anonymous applicant endpoint (#1411)
    # Description
    
    This PR includes the following proposed change(s):
    
    -spdbt-3067: Add anonymous endpoint for controlling member to get the
    applicant profile.

--- src/Spd.Presentation.Licensing/Controllers/SessionConstants.cs
+++ src/Spd.Presentation.Licensing/Controllers/SessionConstants.cs
@@ -6,4 +6,5 @@ public static class SessionConstants
     public static readonly string AnonymousApplicationContext = "LicenceApplicationContext"; //used for anonymous update/renew/replace, get application content
     public static readonly string AnonymousApplicationSubmitKeyCode = "LicenceApplicationSubmitKeyCode";
     public static readonly string AnonymousSoleProprietorApplicationContext = "SoleProprietorApplicationContext"; //used for unauth sole proprietor, biz abort the app
+    public static readonly string AnonymousApplicantContext = "ApplicantContext"; //used for anonymous update for applicant
 }
