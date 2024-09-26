5058531c77837b52155a690f645c78d12fc43a3d
Peggy <peggy.zhang@quartech.com>
2024-09-26 15:46:46 -0700

    Create contractor for swl and bl update. Get document info for LicenceResponse (#1441)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Create contractor for swl and bl update. 
    - Get document info for LicenceResponse

---
+++ src/Spd.Manager.Licence/SecurityWorkerLicenceContract.cs
@@ -0,0 +1,55 @@
+using MediatR;
+using Spd.Manager.Shared;
+
+namespace Spd.Manager.Licence;
+public interface ISecurityWorkerLicenceManager
+{
+    public Task<WorkerLicenceCommandResponse> Handle(WorkerLicenceReplaceCommand command, CancellationToken ct);
+    public Task<WorkerLicenceCommandResponse> Handle(WorkerLicenceRenewCommand command, CancellationToken ct);
+    public Task<WorkerLicenceCommandResponse> Handle(WorkerLicenceUpdateCommand command, CancellationToken ct);
+}
+
+public record WorkerLicenceReplaceCommand(
+    WorkerLicenceReplaceRequest WorkerLicenceReplaceRequest,
+    IEnumerable<LicAppFileInfo> LicAppFileInfos)
+    : IRequest<WorkerLicenceCommandResponse>;
+
+public record WorkerLicenceRenewCommand(
+    WorkerLicenceRenewRequest WorkerLicenceRenewRequest,
+    IEnumerable<LicAppFileInfo> LicAppFileInfos,
+    bool IsAuthenticated = false)
+    : IRequest<WorkerLicenceCommandResponse>;
+
+public record WorkerLicenceUpdateCommand(
+    WorkerLicenceUpdateRequest WorkerLicenceUpdateRequest,
+    IEnumerable<LicAppFileInfo> LicAppFileInfos)
+    : IRequest<WorkerLicenceCommandResponse>;
+
+public record WorkerLicenceResponse
+{
+    public decimal? Cost { get; set; }
+};
+
+public record WorkerLicenceRequest
+{
+    public IEnumerable<Guid>? NewDocumentCacheKeyCodes { get; set; } //for new documents uploaded in update/renew/unauth, saved in cache
+    public IEnumerable<Guid>? ValidPreviousDocumentIds { get; set; } //documentUrlId, used for update/renew/
+    public Guid OriginalLicenceId { get; set; } //for renew, replace, update licence it should be original licence id. 
+    public ApplicationOriginTypeCode ApplicationOriginTypeCode { get; set; } = ApplicationOriginTypeCode.Portal;
+}
+
+public record WorkerLicenceUpdateRequest : WorkerLicenceRequest
+{
+    public LicenceTermCode? LicenceTermCode { get; set; } //for biz licence term, only 1,2,3 year
+    public bool? CarryAndUseRestraints { get; set; }
+    public bool? UseDogs { get; set; }
+    public bool? IsDogsPurposeProtection { get; set; }
+    public bool? IsDogsPurposeDetectionDrugs { get; set; }
+    public bool? IsDogsPurposeDetectionExplosives { get; set; }
+    public IEnumerable<WorkerCategoryTypeCode> CategoryCodes { get; set; } = Array.Empty<WorkerCategoryTypeCode>();
+    public string? CriminalChargeDescription { get; set; } // should be in ApplicantProfile?
+}
+
+public record WorkerLicenceRenewRequest : WorkerLicenceRequest;
+public record WorkerLicenceReplaceRequest : WorkerLicenceRequest;
+
