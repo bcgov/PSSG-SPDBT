566adae1c22563a713282cd25feefb3c8f6fa340
peggy-quartech <peggy.zhang@quartech.com>
2024-10-03 11:35:54 -0700

    fix middleName issue

---
+++ src/Spd.Utilities.LogonUser.UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+namespace Spd.Utilities.LogonUser.UnitTest;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData("givenname1", "givenname1", null)]
+    [InlineData("given", "givenname1", "given")]
+    [InlineData("givenname1 givenname2", "givenname1", "givenname2")]
+    [InlineData(" givenname1 givenname2 ", "givenname1 ", "givenname2")]
+    [InlineData("givenname1 givenname1", "givenname1 ", "givenname1")]
+    [InlineData("", "", null)]
+    [InlineData(null, "", null)]
+    [InlineData(null, null, null)]
+    public void GetMiddleNames_ShouldWork(string givenNames, string firstName, string expectedMiddleName)
+    {
+        var middleNames = IPrincipalExtensions.GetMiddleNames(givenNames, firstName);
+        Assert.Equal(middleNames.Item1, expectedMiddleName);
+    }
+}
