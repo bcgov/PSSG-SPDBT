94982a925525b10426e5c9b85a8f5c4203773876
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-10-09 17:18:06 -0700

    SPDBT-1282: file scanning (#1477)
    Co-authored-by: Peggy <peggy.zhang@quartech.com>

---
+++ src/Spd.Utilities.FileScanning/NoOpScanner.cs
@@ -0,0 +1,9 @@
+﻿namespace Spd.Utilities.FileScanning;
+
+internal class NoOpScanner : IFileScanProvider
+{
+    public async Task<FileScanResult> ScanAsync(Stream stream, CancellationToken ct)
+    {
+        return await Task.FromResult(new FileScanResult(ScanResult.Clean));
+    }
+}
