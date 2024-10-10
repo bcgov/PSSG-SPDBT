f85ad451b827a1678dbb350ebf645c720b45d0f2
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-10-09 17:18:06 -0700

    SPDBT-1282: file scanning (#1477)
    Co-authored-by: Peggy <peggy.zhang@quartech.com>

---
+++ src/Spd.Utilities.FileScanning/ClamAvScanner.cs
@@ -0,0 +1,18 @@
+﻿using nClam;
+
+namespace Spd.Utilities.FileScanning;
+
+internal class ClamAvScanner(IClamClient clamClient) : IFileScanProvider
+{
+    public async Task<FileScanResult> ScanAsync(Stream stream, CancellationToken ct)
+    {
+        var results = await clamClient.SendAndScanFileAsync(stream, ct);
+
+        return results.Result switch
+        {
+            ClamScanResults.Clean => new FileScanResult(ScanResult.Clean),
+            ClamScanResults.VirusDetected => new FileScanResult(ScanResult.NotClean),
+            _ => new FileScanResult(ScanResult.Error, results.RawResult),
+        };
+    }
+}
