ce4cb861095961cca2105d04c58f58be8e5367e4
Yossi T <48697347+ytqsl@users.noreply.github.com>
2024-10-09 17:18:06 -0700

    SPDBT-1282: file scanning (#1477)
    Co-authored-by: Peggy <peggy.zhang@quartech.com>

---
+++ src/Spd.Utilities.FileScanning/Configurer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using nClam;
+using Spd.Utilities.Hosting;
+
+namespace Spd.Utilities.FileScanning;
+
+public class Configurer : IConfigureComponents
+{
+    public void Configure(ConfigurationContext configurationServices)
+    {
+        var settings = configurationServices.Configuration.GetSection("FileScanning").Get<FileScanningSettings>();
+
+        if (settings?.ClamAvUrl != null)
+        {
+            configurationServices.logger.LogInformation("File scanning is configured to use ClamAv at {Url}", settings.ClamAvUrl);
+            configurationServices.Services.AddSingleton<IClamClient>(_ =>
+            {
+                return new ClamClient(settings.ClamAvUrl.Host, settings.ClamAvUrl.Port)
+                {
+                    MaxStreamSize = settings.MaxFileSize
+                };
+            });
+            configurationServices.Services.AddTransient<IFileScanProvider, ClamAvScanner>();
+        }
+        else
+        {
+            configurationServices.logger.LogWarning("File scanning is not configured");
+            configurationServices.Services.AddTransient<IFileScanProvider, NoOpScanner>();
+        }
+    }
+}
+
+public record FileScanningSettings
+{
+    public Uri? ClamAvUrl { get; set; }
+    public long MaxFileSize { get; set; } = 100_000_000;
+}
