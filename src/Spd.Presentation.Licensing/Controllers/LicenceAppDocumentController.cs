072211b06da8774383c69e8ffa4c466dbd3ce891
peggy-quartech <peggy.zhang@quartech.com>
2024-10-11 16:33:17 -0700

    add file scan

--- src/Spd.Presentation.Licensing/Controllers/LicenceAppDocumentController.cs
+++ src/Spd.Presentation.Licensing/Controllers/LicenceAppDocumentController.cs
@@ -4,9 +4,12 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Spd.Manager.Licence;
+using Spd.Utilities.FileScanning;
 using Spd.Utilities.LogonUser;
 using Spd.Utilities.Recaptcha;
+using Spd.Utilities.Shared.Exceptions;
 using System.ComponentModel.DataAnnotations;
+using System.Net;
 using System.Security.Principal;
 
 namespace Spd.Presentation.Licensing.Controllers;
@@ -14,17 +17,18 @@ namespace Spd.Presentation.Licensing.Controllers;
 public class LicenceAppDocumentController : SpdLicenceControllerBase
 {
     private readonly IMediator _mediator;
-    private readonly IConfiguration _configuration;
+    private readonly IFileScanProvider _fileScanProvider;
     private readonly IPrincipal _currentUser;
 
     public LicenceAppDocumentController(IMediator mediator, IDistributedCache cache,
         IDataProtectionProvider dpProvider,
         IPrincipal currentUser,
         IRecaptchaVerificationService recaptchaVerificationService,
-        IConfiguration configuration) : base(cache, dpProvider, recaptchaVerificationService, configuration)
+        IConfiguration configuration,
+        IFileScanProvider fileScanProvider) : base(cache, dpProvider, recaptchaVerificationService, configuration)
     {
         _mediator = mediator;
-        _configuration = configuration;
+        _fileScanProvider = fileScanProvider;
         _currentUser = currentUser;
     }
 
@@ -42,8 +46,9 @@ public class LicenceAppDocumentController : SpdLicenceControllerBase
     public async Task<IEnumerable<LicenceAppDocumentResponse>> UploadLicenceAppFiles([FromForm][Required] LicenceAppDocumentUploadRequest fileUploadRequest, [FromRoute] Guid licenceAppId, CancellationToken ct)
     {
         VerifyFiles(fileUploadRequest.Documents);
+        await FileVirusScanAsync(fileUploadRequest.Documents, ct);
 
-        if (User.HasClaim("Policy", "OnlyBcsc"))
+        if (_currentUser.GetIdentityProvider() == null) //bcsc identity provider is null
         {
             var applicantInfo = _currentUser.GetBcscUserIdentityInfo();
             return await _mediator.Send(new CreateDocumentInTransientStoreCommand(fileUploadRequest, applicantInfo.Sub, licenceAppId), ct);
@@ -64,6 +69,7 @@ public class LicenceAppDocumentController : SpdLicenceControllerBase
     public async Task<Guid> UploadFilesToCache([FromForm][Required] LicenceAppDocumentUploadRequest fileUploadRequest, CancellationToken ct)
     {
         VerifyFiles(fileUploadRequest.Documents);
+        await FileVirusScanAsync(fileUploadRequest.Documents, ct);
 
         CreateDocumentInCacheCommand command = new(fileUploadRequest);
         var newFileInfos = await _mediator.Send(command, ct);
@@ -104,11 +110,22 @@ public class LicenceAppDocumentController : SpdLicenceControllerBase
     {
         await VerifyKeyCode();
         VerifyFiles(fileUploadRequest.Documents);
+        await FileVirusScanAsync(fileUploadRequest.Documents, ct);
 
         CreateDocumentInCacheCommand command = new(fileUploadRequest);
         var newFileInfos = await _mediator.Send(command, ct);
         Guid fileKeyCode = Guid.NewGuid();
         await Cache.SetAsync(fileKeyCode.ToString(), newFileInfos, TimeSpan.FromMinutes(30), ct);
         return fileKeyCode;
+    }
+
+    protected async Task FileVirusScanAsync(IList<IFormFile> documents, CancellationToken ct)
+    {
+        foreach (IFormFile file in documents)
+        {
+            var result = await _fileScanProvider.ScanAsync(file.OpenReadStream(), ct);
+            if (result.Result != ScanResult.Clean)
+                throw new ApiException(HttpStatusCode.BadRequest, "The uploaded file is not clean.");
+        }
     }
 }
