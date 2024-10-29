978ef5522ac4ecab3e7aa0fe83b93ce9a93f697a
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2024-10-29 15:09:31 -0700

    [SPDBT-3200] FE - min date validation (#1667)
    # Description
    
    This PR includes the following proposed change(s):
    
    - SPDBT-3200 - add min date validation to all date fields - birth dates,
    expiry dates, etc

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/personal-licence-application/components/anonymous/permit-wizard-step-components/step-permit-citizenship.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/personal-licence-application/components/anonymous/permit-wizard-step-components/step-permit-citizenship.component.ts
@@ -80,9 +80,9 @@ import { PermitApplicationService } from '@core/services/permit-application.serv
 									<mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle>
 									<mat-datepicker #picker startView="multi-year"></mat-datepicker>
 									<mat-error *ngIf="form.get('expiryDate')?.hasError('required')"> This is required </mat-error>
-									<mat-error *ngIf="form.get('expiryDate')?.hasError('matDatepickerMin')"
-										>Invalid expiry date</mat-error
-									>
+									<mat-error *ngIf="form.get('expiryDate')?.hasError('matDatepickerMin')">
+										Invalid expiry date
+									</mat-error>
 								</mat-form-field>
 							</div>
 						</div>
@@ -158,9 +158,9 @@ import { PermitApplicationService } from '@core/services/permit-application.serv
 										<mat-datepicker-toggle matIconSuffix [for]="picker"></mat-datepicker-toggle>
 										<mat-datepicker #picker startView="multi-year"></mat-datepicker>
 										<mat-error *ngIf="form.get('expiryDate')?.hasError('required')"> This is required </mat-error>
-										<mat-error *ngIf="form.get('expiryDate')?.hasError('matDatepickerMin')"
-											>Invalid expiry date</mat-error
-										>
+										<mat-error *ngIf="form.get('expiryDate')?.hasError('matDatepickerMin')">
+											Invalid expiry date
+										</mat-error>
 									</mat-form-field>
 								</div>
 							</div>
@@ -236,9 +236,9 @@ import { PermitApplicationService } from '@core/services/permit-application.serv
 											<mat-error *ngIf="form.get('governmentIssuedExpiryDate')?.hasError('required')">
 												This is required
 											</mat-error>
-											<mat-error *ngIf="form.get('governmentIssuedExpiryDate')?.hasError('matDatepickerMin')"
-												>Invalid expiry date</mat-error
-											>
+											<mat-error *ngIf="form.get('governmentIssuedExpiryDate')?.hasError('matDatepickerMin')">
+												Invalid expiry date
+											</mat-error>
 										</mat-form-field>
 									</div>
 								</div>
