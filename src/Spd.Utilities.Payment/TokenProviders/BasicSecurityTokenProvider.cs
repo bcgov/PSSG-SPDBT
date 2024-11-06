70517b2ea0097fa9a0c9be2b1d74bef3ef3e8d64
Peggy <peggy.zhang@quartech.com>
2024-11-06 15:56:55 -0800

    refund config for licensing and screening (#1716)
    # Description
    
    This PR includes the following proposed change(s):
    
    - refund config for licensing and screening

--- src/Spd.Utilities.Payment/TokenProviders/BasicSecurityTokenProvider.cs
+++ src/Spd.Utilities.Payment/TokenProviders/BasicSecurityTokenProvider.cs
@@ -1,33 +1,28 @@
 ﻿using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Spd.Utilities.Payment.TokenProviders;
 
 internal class BasicSecurityTokenProvider(
     IHttpClientFactory httpClientFactory,
     IDistributedCache cache,
-    IOptions<PayBCSettings> options,
-    ILogger<BasicSecurityTokenProvider> logger) : SecurityTokenProvider(httpClientFactory, cache, options, logger)
+    ILogger<BasicSecurityTokenProvider> logger) : SecurityTokenProvider(httpClientFactory, cache, logger)
 {
-    private const string cacheKey = "paybc_refund_oauth_token";
+    public override async Task<string> AcquireToken(OAuthSettings oAuthSettings, CancellationToken ct = default) =>
+        await AcquireRefundServiceToken(oAuthSettings, ct) ?? string.Empty;
 
-    public override async Task<string> AcquireToken(CancellationToken ct = default) =>
-        await cache.GetAsync(cacheKey, AcquireRefundServiceToken,
-            TimeSpan.FromMinutes(options.DirectRefund.AuthenticationSettings.OAuthTokenCachedInMins), ct) ?? string.Empty;
-
-    protected async ValueTask<string?> AcquireRefundServiceToken(CancellationToken ct)
+    protected async ValueTask<string?> AcquireRefundServiceToken(OAuthSettings oAuthSettings, CancellationToken ct)
     {
-        return await AcquireTokenInternal(options.DirectRefund.AuthenticationSettings, "GetTokenForRefund", typeof(BasicAccessToken), ct);
+        return await AcquireTokenInternal(oAuthSettings, "GetTokenForRefund", typeof(BasicAccessToken), ct);
     }
 
-    protected override async Task<HttpResponseMessage> GetToken(CancellationToken ct)
+    protected override async Task<HttpResponseMessage> GetToken(OAuthSettings oAuthSettings, CancellationToken ct)
     {
         using var httpClient = httpClientFactory.CreateClient("oauth");
-        string secret = $"{options.DirectRefund.AuthenticationSettings.ClientId}:{options.DirectRefund.AuthenticationSettings.ClientSecret}";
+        string secret = $"{oAuthSettings.ClientId}:{oAuthSettings.ClientSecret}";
         string basicToken = System.Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(secret));
         httpClient.DefaultRequestHeaders.Add("Basic-Token", "Basic " + basicToken);
-        return await httpClient.GetAsync(options.DirectRefund.AuthenticationSettings.OAuth2TokenEndpointUrl, ct);
+        return await httpClient.GetAsync(oAuthSettings.OAuth2TokenEndpointUrl, ct);
     }
 }
 
