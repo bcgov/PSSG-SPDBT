552e0cf0dfc51b638e9c64d52741becffcc56a55
peggy-quartech <peggy.zhang@quartech.com>
2024-12-03 18:04:08 -0800

    code

--- src/Spd.Presentation.Licensing/Program.cs
+++ src/Spd.Presentation.Licensing/Program.cs
@@ -75,6 +75,17 @@ try
     app.UseResponseCaching();
     app.UseRequestDecompression();
     app.UseResponseCompression();
+    app.Use(async (context, next) =>
+    {
+        var isMaintenance = builder.Configuration.GetValue<bool>("MaintenanceMode"); // Get the value from appsettings.json
+        if (isMaintenance && !context.Request.Path.Value.Contains("/maintenance.html"))
+        {
+            context.Response.Redirect("/maintenance.html");
+            //context.Response.Redirect("/index.html");
+            return;
+        }
+        await next();
+    });
     app.UseStaticFiles();
     app.MapFallbackToFile("index.html");
     app.UseRouting();
