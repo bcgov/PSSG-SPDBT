1920f84d09940bb3d6aa7355d8c9df07bd87f49e
Peggy <peggy.zhang@quartech.com>
2024-12-04 18:16:03 -0800

    add C# for maintenance page (#1968)
    # Description
    
    This PR includes the following proposed change(s):
    
    - maintenance mode show static maintenance page

--- src/Spd.Presentation.Licensing/Program.cs
+++ src/Spd.Presentation.Licensing/Program.cs
@@ -76,7 +76,14 @@ try
     app.UseRequestDecompression();
     app.UseResponseCompression();
     app.UseStaticFiles();
-    app.MapFallbackToFile("index.html");
+    if (app.Configuration.GetValue("MaintenanceMode", false))
+    {
+        app.MapFallbackToFile("maintenance/spd-unavailable.html");
+    }
+    else
+    {
+        app.MapFallbackToFile("index.html");
+    }
     app.UseRouting();
     app.UseObservabilityMiddleware();
     app.UseHealthChecks();
