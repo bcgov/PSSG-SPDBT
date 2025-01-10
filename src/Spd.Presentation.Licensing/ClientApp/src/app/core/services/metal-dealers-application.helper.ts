b9fc4d5bfb8cc9c25d3d479288004e9b6e06f154
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-01-10 11:00:18 -0800

    Metal dealers (#2069)
    # Description
    
    This PR includes the following proposed change(s):
    
    - start work on metal dealers

---
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/core/services/metal-dealers-application.helper.ts
@@ -0,0 +1,165 @@
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CommonApplicationHelper } from '@app/core/services/common-application.helper';
+import { ConfigService } from '@app/core/services/config.service';
+import { FileUtilService } from '@app/core/services/file-util.service';
+import { UtilService } from '@app/core/services/util.service';
+import { FormControlValidators } from '@app/core/validators/form-control.validators';
+import { FormGroupValidators } from '@app/core/validators/form-group.validators';
+import { FormatDatePipe } from '@app/shared/pipes/format-date.pipe';
+import { SPD_CONSTANTS } from '../constants/constants';
+
+export abstract class MetalDealersApplicationHelper extends CommonApplicationHelper {
+	businessOwnerFormGroup: FormGroup = this.formBuilder.group({
+		legalBusinessName: new FormControl('', [FormControlValidators.required]),
+		tradeName: new FormControl('', [FormControlValidators.required]),
+		givenName: new FormControl(''),
+		middleName: new FormControl(''),
+		surname: new FormControl('', [FormControlValidators.required]),
+	});
+
+	businessManagerFormGroup: FormGroup = this.formBuilder.group({
+		givenName: new FormControl(''),
+		middleName: new FormControl(''),
+		surname: new FormControl('', [FormControlValidators.required]),
+		phoneNumber: new FormControl('', [FormControlValidators.required]),
+		emailAddress: new FormControl('', [FormControlValidators.email]),
+	});
+
+	businessAddressFormGroup: FormGroup = this.formBuilder.group({
+		addressSelected: new FormControl(false),
+		addressLine1: new FormControl(''),
+		addressLine2: new FormControl(''),
+		city: new FormControl(''),
+		postalCode: new FormControl(''),
+		province: new FormControl(''),
+		country: new FormControl(''),
+	});
+
+	businessMailingAddressFormGroup: FormGroup = this.formBuilder.group(
+		{
+			addressSelected: new FormControl(false),
+			addressLine1: new FormControl(''),
+			addressLine2: new FormControl(''),
+			city: new FormControl(''),
+			postalCode: new FormControl(''),
+			province: new FormControl(''),
+			country: new FormControl(''),
+			isAddressTheSame: new FormControl(false),
+		},
+		{
+			validators: [
+				FormGroupValidators.conditionalDefaultRequiredTrueValidator(
+					'addressSelected',
+					(_form) => _form.get('isAddressTheSame')?.value != true
+				),
+				FormGroupValidators.conditionalRequiredValidator(
+					'addressLine1',
+					(_form) => _form.get('isAddressTheSame')?.value != true
+				),
+				FormGroupValidators.conditionalRequiredValidator(
+					'city',
+					(_form) => _form.get('isAddressTheSame')?.value != true
+				),
+				FormGroupValidators.conditionalRequiredValidator(
+					'postalCode',
+					(_form) => _form.get('isAddressTheSame')?.value != true
+				),
+				FormGroupValidators.conditionalRequiredValidator(
+					'province',
+					(_form) => _form.get('isAddressTheSame')?.value != true
+				),
+				FormGroupValidators.conditionalRequiredValidator(
+					'country',
+					(_form) => _form.get('isAddressTheSame')?.value != true
+				),
+			],
+		}
+	);
+
+	branchesFormGroup: FormGroup = this.formBuilder.group({
+		branches: this.formBuilder.array([]),
+	});
+
+	branchFormGroup: FormGroup = this.formBuilder.group({
+		addressSelected: new FormControl(false, [Validators.requiredTrue]),
+		addressLine1: new FormControl('', [FormControlValidators.required]),
+		addressLine2: new FormControl(''),
+		city: new FormControl('', [FormControlValidators.required]),
+		postalCode: new FormControl('', [FormControlValidators.required]),
+		province: new FormControl('', [FormControlValidators.required]),
+		country: new FormControl('', [
+			FormControlValidators.required,
+			FormControlValidators.requiredValue(SPD_CONSTANTS.address.countryCA, SPD_CONSTANTS.address.countryCanada),
+		]),
+		givenName: new FormControl(''),
+		middleName: new FormControl(''),
+		surname: new FormControl('', [FormControlValidators.required]),
+		phoneNumber: new FormControl('', [FormControlValidators.required]),
+		emailAddress: new FormControl('', [FormControlValidators.email]),
+	});
+
+	consentAndDeclarationFormGroup: FormGroup = this.formBuilder.group({
+		check1: new FormControl(null, [Validators.requiredTrue]),
+		agreeToCompleteAndAccurate: new FormControl(null, [Validators.requiredTrue]),
+		dateSigned: new FormControl({ value: null, disabled: true }),
+		captchaFormGroup: new FormGroup({
+			token: new FormControl('', FormControlValidators.required),
+		}),
+	});
+
+	constructor(
+		formBuilder: FormBuilder,
+		protected configService: ConfigService,
+		protected formatDatePipe: FormatDatePipe,
+		protected utilService: UtilService,
+		protected fileUtilService: FileUtilService
+	) {
+		super(formBuilder);
+	}
+
+	getFullNameWithMiddle(
+		givenName: string | null | undefined,
+		middleName: string | null | undefined,
+		surname: string | null | undefined
+	): string {
+		const userNameArray: string[] = [];
+		if (givenName) {
+			userNameArray.push(givenName);
+		}
+		if (middleName) {
+			userNameArray.push(middleName);
+		}
+		if (surname) {
+			userNameArray.push(surname);
+		}
+		return userNameArray.join(' ');
+	}
+
+	getSummarybusinessOwnerDataname(modelData: any): string {
+		return this.getFullNameWithMiddle(
+			modelData.businessOwnerData.givenName,
+			modelData.businessOwnerData.middleName,
+			modelData.businessOwnerData.surname
+		);
+	}
+	getSummarybusinessOwnerDatalegalBusinessName(modelData: any): string {
+		return modelData.businessOwnerData.legalBusinessName ?? '';
+	}
+	getSummarybusinessOwnerDatatradeName(modelData: any): string {
+		return modelData.businessOwnerData.tradeName ?? '';
+	}
+
+	getSummarybusinessManagerDataname(modelData: any): string {
+		return this.getFullNameWithMiddle(
+			modelData.businessManagerData.givenName,
+			modelData.businessManagerData.middleName,
+			modelData.businessManagerData.surname
+		);
+	}
+	getSummarybusinessManagerDataphoneNumber(modelData: any): string {
+		return modelData.businessManagerData.phoneNumber ?? '';
+	}
+	getSummarybusinessManagerDataemailAddress(modelData: any): string {
+		return modelData.businessManagerData.emailAddress ?? '';
+	}
+}
---
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/metal-dealers-and-recyclers/components/step-metal-dealers-checklist.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-step-metal-dealers-checklist',
+	template: `
+		<app-step-section title="Checklist">
+			<div class="row">
+				<div class="col-xl-8 col-lg-12 col-md-12 col-sm-12 mx-auto">
+					<!-- <p><strong>Business Licence:</strong> Is a Municipal or Regional District business licence</p> -->
+
+					<div class="fw-semibold fs-6 mb-2">Terms and Conditions of Registration:</div>
+					<ul>
+						<li class="metal-dealers-checklist-label">No registration fee</li>
+						<li class="metal-dealers-checklist-label">Term of registration is 3 years</li>
+						<li class="metal-dealers-checklist-label">
+							Must provide business name, address, telephone number, and email address (if any)
+						</li>
+						<li class="metal-dealers-checklist-label">Must provide address of additional business locations</li>
+						<li class="metal-dealers-checklist-label">
+							Must provide the identity of person(s) responsible for the daily management of the business
+						</li>
+						<li class="metal-dealers-checklist-label">
+							Must provide copies of business licence registration documents
+						</li>
+						<li class="metal-dealers-checklist-label">
+							Must display registration certificate in a conspicuous place at each of the business locations
+						</li>
+						<li class="metal-dealers-checklist-label">
+							Registration must not be transferred unless the Registrar consents in writing to the transfer
+						</li>
+						<li class="metal-dealers-checklist-label">
+							On the expiry, cancellation, suspension or refusal of a renewal of a registration, the registrant must
+							immediately surrender the registration and all duplicates to the registrar
+						</li>
+						<li class="metal-dealers-checklist-label">
+							The registrant must not carry on a business using a name other than the name specified in the registration
+						</li>
+					</ul>
+				</div>
+			</div>
+		</app-step-section>
+	`,
+	styles: [
+		`
+			.metal-dealers-checklist-label {
+				color: var(--color-primary);
+				line-height: 2em;
+			}
+		`,
+	],
+	standalone: false,
+})
+export class StepMetalDealersChecklistComponent {}
