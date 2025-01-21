b65f9654a76594a19d90f088541d1fd11bb8bf11
Carol Carpenter <carol.carpenter@quartech.com>
2025-01-21 07:29:05 -0800

    fix

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/metal-dealers-and-recyclers/metal-dealers-and-recyclers-routing.module.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/metal-dealers-and-recyclers/metal-dealers-and-recyclers-routing.module.ts
@@ -27,7 +27,7 @@ const routes: Routes = [
 	},
 	{
 		path: '',
-		redirectTo: MetalDealersAndRecyclersRoutes.pathMetalDealersAndRecyclers(),
+		redirectTo: MetalDealersAndRecyclersRoutes.path(),
 		pathMatch: 'full',
 	},
 ];
