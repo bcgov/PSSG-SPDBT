253e7c867372354e3a36fdb4f1786d5a1a322d45
Peggy <peggy.zhang@quartech.com>
2025-01-28 09:45:40 -0800

    [SPDBT-3555]GDSD view model update (#2133)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-3555]GDSD view model update

--- src/Spd.Manager.Licence/GDSDAppValidation.cs
+++ src/Spd.Manager.Licence/GDSDAppValidation.cs
@@ -25,11 +25,11 @@ public class GDSDTeamLicenceAppBaseValidator<T> : AbstractValidator<T> where T :
         RuleFor(r => r.ServiceTypeCode).Must(t => t == ServiceTypeCode.ArmouredVehiclePermit || t == ServiceTypeCode.BodyArmourPermit); //must be team, dog trainer or retired dog
         RuleFor(r => r.ApplicationTypeCode).NotEmpty();
         RuleFor(r => r.Surname).MaximumLength(40).NotEmpty();
-        RuleFor(r => r.LegalGivenName).MaximumLength(40);
+        RuleFor(r => r.GivenName).MaximumLength(40);
         RuleFor(r => r.MiddleName).MaximumLength(40);
         RuleFor(r => r.DateOfBirth).NotNull().NotEmpty().Must(d => d > new DateOnly(1800, 1, 1));
         RuleFor(r => r.ContactPhoneNumber).MaximumLength(15).NotEmpty();
-        RuleFor(r => r.ContactEmail).MaximumLength(75).When(r => r.ContactEmail != null);
+        RuleFor(r => r.ContactEmailAddress).MaximumLength(75).When(r => r.ContactEmailAddress != null);
         //mailing address
         RuleFor(r => r.MailingAddress.Province).NotEmpty().When(r => r.MailingAddress != null);
         RuleFor(r => r.MailingAddress.City).NotEmpty().MaximumLength(100).When(r => r.MailingAddress != null);
@@ -37,8 +37,8 @@ public class GDSDTeamLicenceAppBaseValidator<T> : AbstractValidator<T> where T :
         RuleFor(r => r.MailingAddress.Country).NotEmpty().MaximumLength(100).When(r => r.MailingAddress != null);
         RuleFor(r => r.MailingAddress.PostalCode).NotEmpty().MaximumLength(20).When(r => r.MailingAddress != null);
 
-        RuleFor(r => r.DogInfoNewCreditSchool).NotEmpty().When(r => r.DogTrainedByAccreditSchool);
-        RuleFor(r => r.DogInfoNewCreditSchool.DogName).NotEmpty().When(r => r.DogTrainedByAccreditSchool);
+        RuleFor(r => r.DogInfoNewAccreditedSchool).NotEmpty().When(r => r.DogTrainedByAccreditedSchool);
+        RuleFor(r => r.DogInfoNewAccreditedSchool.DogName).NotEmpty().When(r => r.DogTrainedByAccreditedSchool);
 
         //RuleFor(r => r.ExpiredLicenceId).NotEmpty().When(r => r.HasExpiredLicence == true);
         //RuleFor(r => r.HasCriminalHistory).NotEmpty();
