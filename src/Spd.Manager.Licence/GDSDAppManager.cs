1930c94b8ef24b81e68e0194ec26d0f05ad7a7eb
Peggy <peggy.zhang@quartech.com>
2025-01-28 06:53:08 -0800

    [SPDBT-3555]be: GDSD Team Licence Contract (#2132)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-3555]be: GDSD Team Licence Contract

---
+++ src/Spd.Manager.Licence/GDSDAppManager.cs
@@ -0,0 +1,61 @@
+using AutoMapper;
+using MediatR;
+using Spd.Resource.Repository.Contact;
+using Spd.Resource.Repository.Document;
+using Spd.Resource.Repository.LicApp;
+using Spd.Resource.Repository.Licence;
+using Spd.Resource.Repository.LicenceFee;
+using Spd.Utilities.FileStorage;
+
+namespace Spd.Manager.Licence;
+internal class GDSDAppManager :
+        LicenceAppManagerBase,
+        IRequestHandler<GDSDTeamLicenceApplicationQuery, GDSDTeamLicenceAppResponse>,
+        IRequestHandler<GDSDTeamLicenceAppUpsertCommand, GDSDAppCommandResponse>,
+        IRequestHandler<GDSDTeamLicenceAppSubmitCommand, GDSDAppCommandResponse>,
+        IRequestHandler<GDSDTeamLicenceAppAnonymousSubmitCommand, GDSDAppCommandResponse>,
+        IGDSDAppManager
+{
+    private readonly IContactRepository _contactRepository;
+
+    public GDSDAppManager(
+        ILicenceRepository licenceRepository,
+        IMapper mapper,
+        IDocumentRepository documentUrlRepository,
+        ILicenceFeeRepository feeRepository,
+        IContactRepository contactRepository,
+        IMainFileStorageService mainFileStorageService,
+        ITransientFileStorageService transientFileStorageService,
+        ILicAppRepository licAppRepository)
+        : base(mapper,
+            documentUrlRepository,
+            feeRepository,
+            licenceRepository,
+            mainFileStorageService,
+            transientFileStorageService,
+            licAppRepository)
+    {
+        _contactRepository = contactRepository;
+    }
+
+    //anonymous
+    public Task<GDSDAppCommandResponse> Handle(GDSDTeamLicenceAppAnonymousSubmitCommand command, CancellationToken ct)
+    {
+        return Task.FromResult<GDSDAppCommandResponse>(null);
+    }
+
+    //auth
+    public Task<GDSDTeamLicenceAppResponse> Handle(GDSDTeamLicenceApplicationQuery query, CancellationToken ct)
+    {
+        return Task.FromResult<GDSDTeamLicenceAppResponse>(null);
+    }
+    public Task<GDSDAppCommandResponse> Handle(GDSDTeamLicenceAppUpsertCommand command, CancellationToken ct)
+    {
+        return Task.FromResult<GDSDAppCommandResponse>(null);
+    }
+    public Task<GDSDAppCommandResponse> Handle(GDSDTeamLicenceAppSubmitCommand command, CancellationToken ct)
+    {
+        return Task.FromResult<GDSDAppCommandResponse>(null);
+    }
+
+}
