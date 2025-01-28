eef811f959e4521259d6412c306caad2f28d2015
peggy-quartech <peggy.zhang@quartech.com>
2025-01-28 12:39:09 -0800

    add validation and unit tests

---
+++ src/Spd.Manager.Licence.UnitTest/GDSDAppValidationTest.cs
@@ -0,0 +1,526 @@
+﻿using AutoFixture;
+using FluentValidation.TestHelper;
+
+namespace Spd.Manager.Licence.UnitTest;
+public class GDSDAppValidationTest
+{
+    private readonly GDSDTeamLicenceAppUpsertRequestValidator _validator;
+    private readonly GraduationInfoValidator _graduationValidator;
+    private readonly TrainingInfoValidator _trainingValidator;
+    private readonly IFixture fixture;
+
+    public GDSDAppValidationTest()
+    {
+        _validator = new GDSDTeamLicenceAppUpsertRequestValidator();
+        _graduationValidator = new GraduationInfoValidator();
+        _trainingValidator = new TrainingInfoValidator();
+
+        fixture = new Fixture();
+        fixture.Behaviors.Remove(new ThrowingRecursionBehavior());
+        fixture.Behaviors.Add(new OmitOnRecursionBehavior());
+    }
+
+    [Fact]
+    public void Should_HaveError_When_ApplicantId_IsEmpty()
+    {
+
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { ApplicantId = Guid.Empty };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ApplicantId)
+              .WithErrorMessage("'Applicant Id' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_ApplicantId_IsProvided()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { ApplicantId = Guid.NewGuid() };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.ApplicantId);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_ServiceTypeCode_IsInvalid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { ServiceTypeCode = Shared.ServiceTypeCode.MCFD };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ServiceTypeCode)
+              .WithErrorMessage("The specified condition was not met for 'Service Type Code'.");
+    }
+
+    [Fact]
+    public void Should_HaveError_When_Surname_IsEmpty()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { Surname = string.Empty };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Surname)
+              .WithErrorMessage("'Surname' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_Surname_IsProvided()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { Surname = "Smith" };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.Surname);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_DateOfBirth_IsInvalid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { DateOfBirth = new DateOnly(1700, 1, 1) };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.DateOfBirth)
+              .WithErrorMessage("The specified condition was not met for 'Date Of Birth'.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_DateOfBirth_IsValid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { DateOfBirth = new DateOnly(2000, 1, 1) };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.DateOfBirth);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_ContactPhoneNumber_IsEmpty()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { ContactPhoneNumber = string.Empty };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ContactPhoneNumber)
+              .WithErrorMessage("'Contact Phone Number' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_ContactPhoneNumber_IsValid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { Surname = "test", ContactPhoneNumber = "1234567890" };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.ContactPhoneNumber);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_ContactEmailAddress_IsInvalid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { ContactEmailAddress = "invalid-email" };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.ContactEmailAddress)
+              .WithErrorMessage("'Contact Email Address' is not a valid email address.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_ContactEmailAddress_IsValid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest { ContactEmailAddress = "test@example.com" };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.ContactEmailAddress);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_MailingAddress_IsInvalid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest
+        {
+            MailingAddress = new MailingAddress { City = string.Empty, Province = string.Empty }
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor("MailingAddress.City")
+              .WithErrorMessage("'City' must not be empty.");
+        result.ShouldHaveValidationErrorFor("MailingAddress.Province")
+              .WithErrorMessage("'Province' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_MailingAddress_IsValid()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest
+        {
+            MailingAddress = new MailingAddress
+            {
+                City = "Sample City",
+                Province = "Sample Province",
+                AddressLine1 = "123 Sample St",
+                PostalCode = "12345",
+                Country = "Sample Country"
+            }
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor("MailingAddress.City");
+        result.ShouldNotHaveValidationErrorFor("MailingAddress.Province");
+    }
+
+    [Fact]
+    public void Should_HaveError_When_DogInfoNewAccreditedSchool_IsEmpty_And_TrainedByAccreditedSchool()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest
+        {
+            DogTrainedByAccreditedSchool = true,
+            DogInfoNewAccreditedSchool = null
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.DogInfoNewAccreditedSchool)
+              .WithErrorMessage("'Dog Info New Accredited School' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_NotHaveError_When_DogInfoNewAccreditedSchool_IsValid_And_TrainedByAccreditedSchool()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest
+        {
+            DogTrainedByAccreditedSchool = true,
+            DogInfoNewAccreditedSchool = new DogInfoNewAccreditedSchool { DogName = "Buddy" }
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldNotHaveValidationErrorFor(x => x.DogInfoNewAccreditedSchool);
+    }
+
+    [Fact]
+    public void Should_HaveError_When_DogInfoNewAccreditedSchool_ServiceDogTasks_ExceedsMaxLength()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest
+        {
+            DogTrainedByAccreditedSchool = true,
+            DogInfoNewAccreditedSchool = new DogInfoNewAccreditedSchool { ServiceDogTasks = new string('A', 1001) }
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor("DogInfoNewAccreditedSchool.ServiceDogTasks")
+              .WithErrorMessage("The length of 'Service Dog Tasks' must be 1000 characters or fewer. You entered 1001 characters.");
+    }
+
+    [Fact]
+    public void Should_HaveError_When_GraduationInfo_IsEmpty_And_TrainedByAccreditedSchool()
+    {
+        // Arrange
+        var model = new GDSDTeamLicenceAppUpsertRequest
+        {
+            DogTrainedByAccreditedSchool = true,
+            GraduationInfo = null
+        };
+
+        // Act
+        var result = _validator.TestValidate(model);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.GraduationInfo)
+              .WithErrorMessage("'Graduation Info' must not be empty.");
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_AccreditedSchoolName_Exceeds_MaxLength()
+    {
+        var model = new GraduationInfo { AccreditedSchoolName = new string('A', 251) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.AccreditedSchoolName);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_AccreditedSchoolName_Is_Within_Limit()
+    {
+        var model = new GraduationInfo { AccreditedSchoolName = new string('A', 250) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.AccreditedSchoolName);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_SchoolContactSurname_Exceeds_MaxLength()
+    {
+        var model = new GraduationInfo { SchoolContactSurname = new string('A', 41) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.SchoolContactSurname);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_SchoolContactSurname_Is_Within_Limit()
+    {
+        var model = new GraduationInfo { SchoolContactSurname = new string('A', 40) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.SchoolContactSurname);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_SchoolContactGivenName_Exceeds_MaxLength()
+    {
+        var model = new GraduationInfo { SchoolContactGivenName = new string('A', 41) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.SchoolContactGivenName);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_SchoolContactGivenName_Is_Within_Limit()
+    {
+        var model = new GraduationInfo { SchoolContactGivenName = new string('A', 40) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.SchoolContactGivenName);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_SchoolContactEmailAddress_Is_Invalid()
+    {
+        var model = new GraduationInfo { SchoolContactEmailAddress = "invalid-email" };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.SchoolContactEmailAddress);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_SchoolContactEmailAddress_Is_Valid()
+    {
+        var model = new GraduationInfo { SchoolContactEmailAddress = "valid.email@example.com" };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.SchoolContactEmailAddress);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_SchoolContactPhoneNumber_Exceeds_MaxLength()
+    {
+        var model = new GraduationInfo { SchoolContactPhoneNumber = new string('1', 31) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.SchoolContactPhoneNumber);
+    }
+
+    [Fact]
+    public void Should_Not_Have_Error_When_SchoolContactPhoneNumber_Is_Within_Limit()
+    {
+        var model = new GraduationInfo { SchoolContactPhoneNumber = new string('1', 30) };
+        var result = _graduationValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.SchoolContactPhoneNumber);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainingBizName()
+    {
+        var model = new TrainingInfo { TrainingBizName = new string('a', 500) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingBizName);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_TrainingBizName_Too_Long()
+    {
+        var model = new TrainingInfo { TrainingBizName = new string('a', 501) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainingBizName);
+    }
+
+    [Fact]
+    public void Should_Validate_TrainingBizMailingAddress_When_Not_Null()
+    {
+        var model = new TrainingInfo { TrainingBizMailingAddress = new MailingAddress() }; // Assuming MailingAddress is a valid object.
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingBizMailingAddress);
+    }
+
+    [Fact]
+    public void Should_Not_Validate_TrainingBizMailingAddress_When_Null()
+    {
+        var model = new TrainingInfo { TrainingBizMailingAddress = null };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingBizMailingAddress);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainingBizContactGivenName()
+    {
+        var model = new TrainingInfo { TrainingBizContactGivenName = new string('a', 40) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingBizContactGivenName);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_TrainingBizContactGivenName_Too_Long()
+    {
+        var model = new TrainingInfo { TrainingBizContactGivenName = new string('a', 41) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainingBizContactGivenName);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainingBizContactEmailAddress()
+    {
+        var model = new TrainingInfo { TrainingBizContactEmailAddress = "email@example.com" };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingBizContactEmailAddress);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_Invalid_TrainingBizContactEmailAddress()
+    {
+        var model = new TrainingInfo { TrainingBizContactEmailAddress = "invalid-email" };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainingBizContactEmailAddress);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainingDateFrom()
+    {
+        var model = new TrainingInfo { TrainingDateFrom = new DateOnly(2023, 1, 1) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingDateFrom);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_TrainingDateFrom_Before_1800()
+    {
+        var model = new TrainingInfo { TrainingDateFrom = new DateOnly(1799, 12, 31) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainingDateFrom);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainingDateTo()
+    {
+        var model = new TrainingInfo { TrainingDateTo = new DateOnly(2023, 12, 31) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingDateTo);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_TrainingDateTo_Before_1800()
+    {
+        var model = new TrainingInfo { TrainingDateTo = new DateOnly(1799, 12, 31) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainingDateTo);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainingName()
+    {
+        var model = new TrainingInfo { TrainingName = new string('a', 100) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainingName);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_TrainingName_Too_Long()
+    {
+        var model = new TrainingInfo { TrainingName = new string('a', 101) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainingName);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_WhatLearned()
+    {
+        var model = new TrainingInfo { WhatLearned = new string('a', 1000) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.WhatLearned);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_WhatLearned_Too_Long()
+    {
+        var model = new TrainingInfo { WhatLearned = new string('a', 1001) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.WhatLearned);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_TrainerEmailAddress_When_Not_Null()
+    {
+        var model = new TrainingInfo { TrainerEmailAddress = "trainer@example.com" };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.TrainerEmailAddress);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_Invalid_TrainerEmailAddress()
+    {
+        var model = new TrainingInfo { TrainerEmailAddress = "invalid-email" };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.TrainerEmailAddress);
+    }
+
+    [Fact]
+    public void Should_Have_Valid_HoursPracticingSkill()
+    {
+        var model = new TrainingInfo { HoursPracticingSkill = new string('a', 100) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldNotHaveValidationErrorFor(x => x.HoursPracticingSkill);
+    }
+
+    [Fact]
+    public void Should_Have_Validation_Error_For_HoursPracticingSkill_Too_Long()
+    {
+        var model = new TrainingInfo { HoursPracticingSkill = new string('a', 101) };
+        var result = _trainingValidator.TestValidate(model);
+        result.ShouldHaveValidationErrorFor(x => x.HoursPracticingSkill);
+    }
+}
--- src/Spd.Manager.Licence/GDSDAppValidation.cs
+++ src/Spd.Manager.Licence/GDSDAppValidation.cs
@@ -7,7 +7,7 @@ public class GDSDTeamLicenceAppUpsertRequestValidator : GDSDTeamLicenceAppBaseVa
 {
     public GDSDTeamLicenceAppUpsertRequestValidator()
     {
-
+        RuleFor(r => r.ApplicantId).NotEmpty();
     }
 }
 
@@ -15,6 +15,7 @@ public class GDSDTeamLicenceAppAnonymousSubmitRequestValidator : GDSDTeamLicence
 {
     public GDSDTeamLicenceAppAnonymousSubmitRequestValidator()
     {
+
     }
 }
 
@@ -22,39 +23,139 @@ public class GDSDTeamLicenceAppBaseValidator<T> : AbstractValidator<T> where T :
 {
     public GDSDTeamLicenceAppBaseValidator()
     {
-        RuleFor(r => r.ServiceTypeCode).Must(t => t == ServiceTypeCode.ArmouredVehiclePermit || t == ServiceTypeCode.BodyArmourPermit); //must be team, dog trainer or retired dog
+        RuleFor(r => r.ServiceTypeCode).Must(t => t == ServiceTypeCode.GDSDTeamCertification); //must be team, dog trainer or retired dog
         RuleFor(r => r.ApplicationTypeCode).NotEmpty();
         RuleFor(r => r.Surname).MaximumLength(40).NotEmpty();
         RuleFor(r => r.GivenName).MaximumLength(40);
         RuleFor(r => r.MiddleName).MaximumLength(40);
         RuleFor(r => r.DateOfBirth).NotNull().NotEmpty().Must(d => d > new DateOnly(1800, 1, 1));
-        RuleFor(r => r.ContactPhoneNumber).MaximumLength(15).NotEmpty();
-        RuleFor(r => r.ContactEmailAddress).MaximumLength(75).When(r => r.ContactEmailAddress != null);
-        //mailing address
-        RuleFor(r => r.MailingAddress.Province).NotEmpty().When(r => r.MailingAddress != null);
-        RuleFor(r => r.MailingAddress.City).NotEmpty().MaximumLength(100).When(r => r.MailingAddress != null);
-        RuleFor(r => r.MailingAddress.AddressLine1).NotEmpty().MaximumLength(100).When(r => r.MailingAddress != null);
-        RuleFor(r => r.MailingAddress.Country).NotEmpty().MaximumLength(100).When(r => r.MailingAddress != null);
-        RuleFor(r => r.MailingAddress.PostalCode).NotEmpty().MaximumLength(20).When(r => r.MailingAddress != null);
+        RuleFor(r => r.ContactPhoneNumber).MaximumLength(30).NotEmpty();
+        RuleFor(r => r.ContactEmailAddress).EmailAddress().MaximumLength(75).When(r => r.ContactEmailAddress != null);
 
-        RuleFor(r => r.DogInfoNewAccreditedSchool).NotEmpty().When(r => r.DogTrainedByAccreditedSchool);
-        RuleFor(r => r.DogInfoNewAccreditedSchool.DogName).NotEmpty().When(r => r.DogTrainedByAccreditedSchool);
+        RuleFor(r => r.MailingAddress).SetValidator(new MailingAddressValidator())
+            .When(r => r.MailingAddress != null);
+        RuleFor(r => r.DogInfoNewAccreditedSchool).NotEmpty()
+            .When(r => r.DogTrainedByAccreditedSchool);
+        RuleFor(r => r.DogInfoNewAccreditedSchool)
+            .SetValidator(new DogInfoNewAccreditedSchoolValidator())
+            .When(r => r.DogInfoNewAccreditedSchool != null);
+        RuleFor(r => r.GraduationInfo).NotEmpty()
+            .When(r => r.DogTrainedByAccreditedSchool);
+        RuleFor(r => r.GraduationInfo)
+            .SetValidator(new GraduationInfoValidator())
+            .When(r => r.GraduationInfo != null);
 
-        //RuleFor(r => r.ExpiredLicenceId).NotEmpty().When(r => r.HasExpiredLicence == true);
-        //RuleFor(r => r.HasCriminalHistory).NotEmpty();
-        //RuleFor(r => r.HasBcDriversLicence).NotEmpty();
-        //RuleFor(r => r.HairColourCode).NotEmpty();
-        //RuleFor(r => r.EyeColourCode).NotEmpty();
-        //RuleFor(r => r.Height).NotEmpty();
-        //RuleFor(r => r.HeightUnitCode).NotEmpty();
-        //RuleFor(r => r.Weight).NotEmpty();
-        //RuleFor(r => r.WeightUnitCode).NotEmpty();
-        //RuleFor(r => r.IsMailingTheSameAsResidential).NotEmpty();
+        RuleFor(r => r.DogInfoNewWithoutAccreditedSchool).NotEmpty()
+            .When(r => !r.DogTrainedByAccreditedSchool);
+        RuleFor(r => r.DogInfoNewWithoutAccreditedSchool)
+            .SetValidator(new DogInfoNewWithoutAccreditedSchoolValidator())
+            .When(r => r.DogInfoNewWithoutAccreditedSchool != null);
+        RuleFor(r => r.TrainingInfo).NotEmpty()
+            .When(r => !r.DogTrainedByAccreditedSchool);
+        RuleFor(r => r.TrainingInfo)
+            .SetValidator(new TrainingInfoValidator())
+            .When(r => r.TrainingInfo != null);
 
-        //RuleFor(r => r.IsCanadianCitizen).NotEmpty();
+        RuleFor(r => r.DogInfoRenew).NotEmpty()
+            .When(r => r.ApplicationTypeCode == ApplicationTypeCode.Renewal);
+        RuleFor(r => r.DogInfoRenew)
+            .SetValidator(new DogInfoRenewValidator())
+            .When(r => r.DogInfoRenew != null);
 
-        //RuleFor(r => r.HasNewCriminalRecordCharge).NotNull()
-        //    .When(r => r.ApplicationTypeCode == ApplicationTypeCode.Renewal || r.ApplicationTypeCode == ApplicationTypeCode.Update);
+    }
+}
+
+public class DogInfoNewValidator : AbstractValidator<DogInfoNew>
+{
+    public DogInfoNewValidator()
+    {
+        RuleFor(x => x.DogName).NotEmpty().MaximumLength(50);
+        RuleFor(r => r.DogDateOfBirth).NotNull().NotEmpty().Must(d => d > new DateOnly(1800, 1, 1));
+        RuleFor(r => r.DogBreed).NotEmpty().MaximumLength(50);
+        RuleFor(r => r.DogGender).Must(t => t == GenderCode.M || t == GenderCode.F);
+    }
+}
+
+public class DogInfoNewAccreditedSchoolValidator : AbstractValidator<DogInfoNewAccreditedSchool>
+{
+    public DogInfoNewAccreditedSchoolValidator()
+    {
+        Include(new DogInfoNewValidator());
+        RuleFor(r => r.ServiceDogTasks).MaximumLength(1000);
+    }
+}
+
+public class DogInfoNewWithoutAccreditedSchoolValidator : AbstractValidator<DogInfoNewWithoutAccreditedSchool>
+{
+    public DogInfoNewWithoutAccreditedSchoolValidator()
+    {
+        Include(new DogInfoNewValidator());
+    }
+}
+
+public class DogInfoRenewValidator : AbstractValidator<DogInfoRenew>
+{
+    public DogInfoRenewValidator()
+    {
+        RuleFor(r => r.CurrentDogCertificate).NotEmpty().MaximumLength(15);
+    }
+}
+
+public class MailingAddressValidator : AbstractValidator<MailingAddress>
+{
+    public MailingAddressValidator()
+    {
+        RuleFor(r => r.Province).NotEmpty().MaximumLength(100);
+        RuleFor(r => r.City).NotEmpty().MaximumLength(100);
+        RuleFor(r => r.AddressLine1).NotEmpty().MaximumLength(100);
+        RuleFor(r => r.Country).NotEmpty().MaximumLength(100);
+        RuleFor(r => r.PostalCode).NotEmpty().MaximumLength(20);
+    }
+}
+
+public class GraduationInfoValidator : AbstractValidator<GraduationInfo>
+{
+    public GraduationInfoValidator()
+    {
+        RuleFor(x => x.AccreditedSchoolName).MaximumLength(250);
+        RuleFor(r => r.SchoolContactSurname).MaximumLength(40);
+        RuleFor(r => r.SchoolContactGivenName).MaximumLength(40);
+        RuleFor(r => r.SchoolContactEmailAddress).MaximumLength(75)
+            .EmailAddress()
+            .When(r => r.SchoolContactEmailAddress != null);
+        RuleFor(r => r.SchoolContactPhoneNumber).MaximumLength(30);
+    }
+}
+
+public class TrainingInfoValidator : AbstractValidator<TrainingInfo>
+{
+    public TrainingInfoValidator()
+    {
+        RuleFor(x => x.TrainingBizName).MaximumLength(500);
+        RuleFor(r => r.TrainingBizMailingAddress)
+            .SetValidator(new MailingAddressValidator())
+            .When(r => r.TrainingBizMailingAddress != null);
+        RuleFor(r => r.TrainingBizContactGivenName).MaximumLength(40);
+        RuleFor(r => r.TrainingBizContactSurname).MaximumLength(40);
+        RuleFor(r => r.TrainingBizContactEmailAddress).MaximumLength(75).EmailAddress().When(r => r.TrainingBizContactEmailAddress != null);
+        RuleFor(r => r.TrainingBizContactPhoneNumber).MaximumLength(30);
+        RuleFor(r => r.TrainingDateFrom).Must(d => d > new DateOnly(1800, 1, 1)).When(r => r.TrainingDateFrom != null);
+        RuleFor(r => r.TrainingDateTo).Must(d => d > new DateOnly(1800, 1, 1)).When(r => r.TrainingDateTo != null);
+        RuleFor(r => r.TrainingDateTo).Must(d => d > new DateOnly(1800, 1, 1)).When(r => r.TrainingDateTo != null);
+        RuleFor(r => r.TrainingName).MaximumLength(100);
+        RuleFor(r => r.WhatLearned).MaximumLength(1000);
+
+        RuleFor(r => r.TrainingDetail).MaximumLength(1000);
+        RuleFor(r => r.DogTrainerCredential).MaximumLength(100);
+        RuleFor(r => r.TrainingTime).MaximumLength(15);
+        RuleFor(r => r.TrainerGivenName).MaximumLength(40);
+        RuleFor(r => r.TrainerSurname).MaximumLength(40);
+        RuleFor(r => r.TrainerEmailAddress).MaximumLength(75).EmailAddress().When(r => r.TrainerEmailAddress != null);
+        RuleFor(r => r.TrainerPhoneNumber).MaximumLength(30);
+        RuleFor(r => r.HoursPracticingSkill).MaximumLength(100);
+
+        RuleFor(r => r.SpecializedTasks).MaximumLength(100);
+        RuleFor(r => r.WhenPerformed).MaximumLength(100);
     }
 }
 
