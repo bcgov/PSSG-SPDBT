a23dd9fa84e0a340676231f4470daf97a4366f10
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-02-12 10:47:31 -0800

    [SPDBT-3630] SWL - Licence Category Selection (#2223)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Licensing Portal Language Updates - SWL - Licence Category Selection

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/personal-licence-application/components/shared/worker-licence-wizard-step-components/step-worker-licence-expired.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/personal-licence-application/components/shared/worker-licence-wizard-step-components/step-worker-licence-expired.component.ts
@@ -11,7 +11,7 @@ import { WorkerApplicationService } from '@app/core/services/worker-application.
 			<div class="row">
 				<div class="offset-md-2 col-md-8 col-sm-12">
 					<app-alert type="info" icon="info">
-						Provide information from your expired licence to reduce processing time.
+						Providing information from your expired licence may speed up the processing time.
 					</app-alert>
 				</div>
 			</div>
