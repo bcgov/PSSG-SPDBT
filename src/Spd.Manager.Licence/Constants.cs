24c45168d6eb106f4d71d81debf51b9fa8c7b564
Peggy <peggy.zhang@quartech.com>
2025-02-13 15:45:12 -0800

    [SPDBT-3683]Allow Replacements at any time (#2233)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-3683]Allow Replacements at any time

--- src/Spd.Manager.Licence/Constants.cs
+++ src/Spd.Manager.Licence/Constants.cs
@@ -1,7 +1,6 @@
 ﻿namespace Spd.Manager.Licence;
 internal static class Constants
 {
-    internal const int LicenceReplaceValidBeforeExpirationInDays = 14;
     internal const int LicenceWith123YearsRenewValidBeforeExpirationInDays = 90;
     internal const int LicenceWith90DaysRenewValidBeforeExpirationInDays = 60;
     internal const int LicenceUpdateValidBeforeExpirationInDays = 14;//Licence holder can't request an update within 14 days of expiry date
