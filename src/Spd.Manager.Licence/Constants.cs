3feaef6c1399e50df718c67f0687100315ccd955
Peggy <peggy.zhang@quartech.com>
2025-02-18 09:20:35 -0800

    [SPDBT-3673]Make a feature switch for biz bceid login in licensing. Do not allow user to select any one org from the org list. (#2242)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-3673]Make a feature switch for biz bceid login in licensing. Do
    not allow user to select any one org from the org list.

--- src/Spd.Manager.Licence/Constants.cs
+++ src/Spd.Manager.Licence/Constants.cs
@@ -1,6 +1,7 @@
 ﻿namespace Spd.Manager.Licence;
 internal static class Constants
 {
+    internal const int LicenceReplaceValidBeforeExpirationInDays = 14;
     internal const int LicenceWith123YearsRenewValidBeforeExpirationInDays = 90;
     internal const int LicenceWith90DaysRenewValidBeforeExpirationInDays = 60;
     internal const int LicenceUpdateValidBeforeExpirationInDays = 14;//Licence holder can't request an update within 14 days of expiry date
