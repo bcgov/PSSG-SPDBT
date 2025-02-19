dc383fe7917234d39dc1a6ca276194eb33cf7524
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-02-19 14:15:07 -0800

    Module path bug fix (#2247)
    # Description
    
    This PR includes the following proposed change(s):
    
    - Module path bug fix

--- src/Spd.Presentation.Licensing/ClientApp/src/app/app-routes.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/app-routes.ts
@@ -1,17 +1,10 @@
-import { BusinessLicenceApplicationRoutes } from './modules/business-licence-application/business-license-application-routes';
-import { ControllingMemberCrcRoutes } from './modules/controlling-member-crc/controlling-member-crc-routes';
-import { GuideDogServiceDogRoutes } from './modules/guide-dog-service-dog/guide-dog-service-dog-routes';
-import { MetalDealersAndRecyclersRoutes } from './modules/metal-dealers-and-recyclers/metal-dealers-and-recyclers-routes';
-import { PersonalLicenceApplicationRoutes } from './modules/personal-licence-application/personal-licence-application-routes';
-import { SecurityLicenceStatusVerificationRoutes } from './modules/security-licence-status-verification/security-licence-status-verification-routes';
-
 export class AppRoutes {
-	public static readonly PERSONAL_LICENCE_APPLICATION = PersonalLicenceApplicationRoutes.MODULE_PATH;
-	public static readonly BUSINESS_LICENCE_APPLICATION = BusinessLicenceApplicationRoutes.MODULE_PATH;
-	public static readonly CONTROLLING_MEMBERS_CRC = ControllingMemberCrcRoutes.MODULE_PATH;
-	public static readonly SECURITY_LICENCE_STATUS_VERIFICATION = SecurityLicenceStatusVerificationRoutes.MODULE_PATH;
-	public static readonly METAL_DEALERS_AND_RECYCLERS = MetalDealersAndRecyclersRoutes.MODULE_PATH;
-	public static readonly GUIDE_DOG_SERVICE_DOG = GuideDogServiceDogRoutes.MODULE_PATH;
+	public static readonly PERSONAL_LICENCE_APPLICATION = 'personal-licence';
+	public static readonly BUSINESS_LICENCE_APPLICATION = 'business-licence';
+	public static readonly CONTROLLING_MEMBERS_CRC = 'controlling-member-crc';
+	public static readonly SECURITY_LICENCE_STATUS_VERIFICATION = 'security-licence-status-verification';
+	public static readonly METAL_DEALERS_AND_RECYCLERS = 'metal-dealers-and-recyclers';
+	public static readonly GUIDE_DOG_SERVICE_DOG = 'guide-dog-service-dog';
 	public static readonly LANDING = '';
 	public static readonly ACCESS_DENIED = 'access-denied';
 	public static readonly INVITATION_DENIED = 'invitation-denied';
