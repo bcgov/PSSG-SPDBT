e2203afd06c77cb8b4654b2e447dc36ed20c3ce9
Peggy <peggy.zhang@quartech.com>
2025-02-24 12:59:06 -0800

    fix lic lookup endpoint for gdsd (#2268)
    # Description
    
    This PR includes the following proposed change(s):
    
    -    fix lic lookup endpoint for gdsd

--- src/Spd.Presentation.Licensing/Controllers/LicenceController.cs
+++ src/Spd.Presentation.Licensing/Controllers/LicenceController.cs
@@ -110,9 +110,11 @@ namespace Spd.Presentation.Licensing.Controllers
                 latestAppId = await _mediator.Send(new GetLatestWorkerLicenceApplicationIdQuery((Guid)response.LicenceHolderId));
             else if (response?.ServiceTypeCode == ServiceTypeCode.SecurityBusinessLicence)
                 return response;
-            //throw new ApiException(HttpStatusCode.BadRequest, "Biz licensing does not support anonymous.");
-            else if (response != null)
+            else if (response?.ServiceTypeCode == ServiceTypeCode.BodyArmourPermit || response?.ServiceTypeCode == ServiceTypeCode.ArmouredVehiclePermit)
                 latestAppId = await _mediator.Send(new GetLatestPermitApplicationIdQuery((Guid)response.LicenceHolderId, (ServiceTypeCode)response.ServiceTypeCode));
+            else
+                //gdsd, dog
+                latestAppId = response?.LicenceAppId ?? Guid.Empty;
 
             if (response != null)
             {
