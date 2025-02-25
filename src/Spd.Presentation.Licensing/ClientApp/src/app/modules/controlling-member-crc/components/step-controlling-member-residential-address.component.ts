539d6cf38802a37850a47d3ab344cc41db7634f4
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-02-25 12:29:17 -0800

    [FE] language updates (#2271)
    # Description
    
    This PR includes the following proposed change(s):
    
    - SPDBT-3708 update age
    - SPDBT-3636 expired permit text
    - SPDBT-3646 crc address text
    - SPDBT-3641 checklist text
    - SPDBT-3645 proof of insurance text
    - SPDBT-3643 branding renewal text

--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/controlling-member-crc/components/step-controlling-member-residential-address.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/controlling-member-crc/components/step-controlling-member-residential-address.component.ts
@@ -12,11 +12,11 @@ import { LicenceChildStepperStepComponent } from '@app/core/services/util.servic
 			<div class="row" *ngIf="isLoggedIn">
 				<div class="col-md-8 col-sm-12 mx-auto">
 					<app-alert type="info" icon="">
-						Has your residential address changed?
+						Have you moved?
 						<a aria-label="Navigate to address change online site" [href]="addressChangeUrl" target="_blank"
-							>Change your address online</a
+							>Update your address online.</a
 						>
-						to update this information on your BC Services Card. Any changes you make will then be updated here.
+						Any changes you make will automatically be updated here.
 					</app-alert>
 				</div>
 			</div>
