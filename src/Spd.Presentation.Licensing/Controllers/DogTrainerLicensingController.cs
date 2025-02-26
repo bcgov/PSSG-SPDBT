bd3c79c0100faf797a0352341d1a74a36aae1980
peggy-quartech <peggy.zhang@quartech.com>
2025-02-25 16:00:40 -0800

    cleanup

--- src/Spd.Presentation.Licensing/Controllers/DogTrainerLicensingController.cs
+++ src/Spd.Presentation.Licensing/Controllers/DogTrainerLicensingController.cs
@@ -3,10 +3,7 @@ using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Spd.Manager.Licence;
-using Spd.Manager.Shared;
 using Spd.Utilities.Recaptcha;
-using Spd.Utilities.Shared.Exceptions;
-using System.Net;
 using System.Security.Principal;
 
 namespace Spd.Presentation.Licensing.Controllers
@@ -42,23 +39,7 @@ namespace Spd.Presentation.Licensing.Controllers
         [HttpPost]
         public async Task<GDSDAppCommandResponse> SubmitDogTrainerAppAnonymous(GDSDTeamLicenceAppAnonymousSubmitRequest anonymousSubmitRequest, CancellationToken ct)
         {
-            await VerifyKeyCode();
-
-            IEnumerable<LicAppFileInfo> newDocInfos = await GetAllNewDocsInfoAsync(anonymousSubmitRequest.DocumentKeyCodes, ct);
-            //var validateResult = await _teamAppAnonymousSubmitRequestValidator.ValidateAsync(anonymousSubmitRequest, ct);
-            //if (!validateResult.IsValid)
-            //    throw new ApiException(HttpStatusCode.BadRequest, JsonSerializer.Serialize(validateResult.Errors));
-            anonymousSubmitRequest.ApplicationOriginTypeCode = ApplicationOriginTypeCode.WebForm;
-
-            GDSDAppCommandResponse? response = null;
-            if (anonymousSubmitRequest.ApplicationTypeCode == ApplicationTypeCode.New)
-            {
-                GDSDTeamLicenceAppAnonymousSubmitCommand command = new(anonymousSubmitRequest, newDocInfos);
-                response = await _mediator.Send(command, ct);
-            }
-            SetValueToResponseCookie(SessionConstants.AnonymousApplicationSubmitKeyCode, String.Empty);
-            SetValueToResponseCookie(SessionConstants.AnonymousApplicationContext, String.Empty);
-            return new GDSDAppCommandResponse { LicenceAppId = response?.LicenceAppId };
+            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -73,34 +54,8 @@ namespace Spd.Presentation.Licensing.Controllers
         [HttpPost]
         public async Task<GDSDAppCommandResponse> RenewReplaceDogTrainerAppAnonymous(GDSDTeamLicenceAppChangeRequest anonymousChangeRequest, CancellationToken ct)
         {
-            await VerifyKeyCode();
+            throw new NotImplementedException();
 
-            IEnumerable<LicAppFileInfo> newDocInfos = await GetAllNewDocsInfoAsync(anonymousChangeRequest.DocumentKeyCodes, ct);
-            //var validateResult = await _teamAppChangeValidator.ValidateAsync(anonymousChangeRequest, ct);
-            //if (!validateResult.IsValid)
-            //    throw new ApiException(HttpStatusCode.BadRequest, JsonSerializer.Serialize(validateResult.Errors));
-            //anonymousChangeRequest.ApplicationOriginTypeCode = ApplicationOriginTypeCode.WebForm;
-
-            GDSDAppCommandResponse? response = null;
-            if (anonymousChangeRequest.ApplicationTypeCode == ApplicationTypeCode.New)
-            {
-                throw new ApiException(HttpStatusCode.BadRequest, "New GDSD is not supported in this endpoint");
-            }
-
-            if (anonymousChangeRequest.ApplicationTypeCode == ApplicationTypeCode.Renewal)
-            {
-                GDSDTeamLicenceAppRenewCommand command = new(anonymousChangeRequest, newDocInfos);
-                response = await _mediator.Send(command, ct);
-            }
-
-            if (anonymousChangeRequest.ApplicationTypeCode == ApplicationTypeCode.Replacement)
-            {
-                GDSDTeamLicenceAppReplaceCommand command = new(anonymousChangeRequest, newDocInfos);
-                response = await _mediator.Send(command, ct);
-            }
-            SetValueToResponseCookie(SessionConstants.AnonymousApplicationSubmitKeyCode, String.Empty);
-            SetValueToResponseCookie(SessionConstants.AnonymousApplicationContext, String.Empty);
-            return new GDSDAppCommandResponse { LicenceAppId = response?.LicenceAppId };
         }
         #endregion anonymous
     }
