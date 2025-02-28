fe2c83393d607bee63ab90635dd7279a36c7f5fe
Peggy <peggy.zhang@quartech.com>
2025-02-27 17:56:46 -0800

    [SPDBT-3733] OS-be-dog-trainer create endpoint for new dog trainer certification. (#2278)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-3733] OS-be-dog-trainer create endpoint for new dog trainer
    certification.

--- src/Spd.Presentation.Licensing/Controllers/DogTrainerLicensingController.cs
+++ src/Spd.Presentation.Licensing/Controllers/DogTrainerLicensingController.cs
@@ -1,10 +1,15 @@
+using FluentValidation;
 using MediatR;
 using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Distributed;
 using Spd.Manager.Licence;
+using Spd.Manager.Shared;
 using Spd.Utilities.Recaptcha;
+using Spd.Utilities.Shared.Exceptions;
+using System.Net;
 using System.Security.Principal;
+using System.Text.Json;
 
 namespace Spd.Presentation.Licensing.Controllers
 {
@@ -13,16 +18,19 @@ namespace Spd.Presentation.Licensing.Controllers
     {
         private readonly IPrincipal _currentUser;
         private readonly IMediator _mediator;
+        private readonly IValidator<DogTrainerRequest> _dogTrainerNewValidator;
 
         public DogTrainerLicensingController(IPrincipal currentUser,
             IMediator mediator,
             IConfiguration configuration,
             IRecaptchaVerificationService recaptchaVerificationService,
             IDistributedCache cache,
-            IDataProtectionProvider dpProvider) : base(cache, dpProvider, recaptchaVerificationService, configuration)
+            IDataProtectionProvider dpProvider,
+            IValidator<DogTrainerRequest> dogTrainerNewValidator) : base(cache, dpProvider, recaptchaVerificationService, configuration)
         {
             _currentUser = currentUser;
             _mediator = mediator;
+            _dogTrainerNewValidator = dogTrainerNewValidator;
         }
 
         #region anonymous
@@ -32,27 +40,38 @@ namespace Spd.Presentation.Licensing.Controllers
         /// After fe done with the uploading files, then fe do post with json payload, inside payload, it needs to contain an array of keycode for the files.
         /// The session keycode is stored in the cookies.
         /// </summary>
-        /// <param name="anonymousSubmitRequest">PermitAppAnonymousSubmitRequest data</param>
+        /// <param name="newRequest">DogTrainerRequest data</param>
         /// <param name="ct"></param>
         /// <returns></returns>
         [Route("api/dog-trainer-app/anonymous/submit")]
         [HttpPost]
-        public async Task<GDSDAppCommandResponse> SubmitDogTrainerAppAnonymous(GDSDTeamLicenceAppAnonymousSubmitRequest anonymousSubmitRequest, CancellationToken ct)
+        public async Task<DogTrainerAppCommandResponse> SubmitDogTrainerAppAnonymous(DogTrainerRequest newRequest, CancellationToken ct)
         {
-            throw new NotImplementedException();
+            //await VerifyKeyCode();
+            IEnumerable<LicAppFileInfo> newDocInfos = await GetAllNewDocsInfoAsync(newRequest.DocumentKeyCodes, ct);
+
+            var validateResult = await _dogTrainerNewValidator.ValidateAsync(newRequest, ct);
+            if (!validateResult.IsValid)
+                throw new ApiException(HttpStatusCode.BadRequest, JsonSerializer.Serialize(validateResult.Errors));
+            newRequest.ApplicationOriginTypeCode = ApplicationOriginTypeCode.WebForm;
+
+            DogTrainerAppCommandResponse response = await _mediator.Send(new DogTrainerLicenceAppAnonymousSubmitCommand(newRequest, newDocInfos), ct);
+            SetValueToResponseCookie(SessionConstants.AnonymousApplicationSubmitKeyCode, String.Empty);
+            SetValueToResponseCookie(SessionConstants.AnonymousApplicationContext, String.Empty);
+            return new DogTrainerAppCommandResponse { LicenceAppId = response?.LicenceAppId };
         }
 
         /// <summary>
-        /// Submit/new GDSD Team Certification application Anonymously
+        /// Renew, Replace Dog Trainer application Anonymously
         /// After fe done with the uploading files, then fe do post with json payload, inside payload, it needs to contain an array of keycode for the files.
         /// The session keycode is stored in the cookies.
         /// </summary>
-        /// <param name="anonymousChangeRequest">PermitAppAnonymousSubmitRequest data</param>
+        /// <param name="anonymousChangeRequest">DogTrainerChangeRequest data</param>
         /// <param name="ct"></param>
         /// <returns></returns>
         [Route("api/dog-trainer-app/anonymous/change")]
         [HttpPost]
-        public async Task<GDSDAppCommandResponse> RenewReplaceDogTrainerAppAnonymous(GDSDTeamLicenceAppChangeRequest anonymousChangeRequest, CancellationToken ct)
+        public async Task<DogTrainerAppCommandResponse> RenewReplaceDogTrainerAppAnonymous(DogTrainerChangeRequest anonymousChangeRequest, CancellationToken ct)
         {
             throw new NotImplementedException();
 
