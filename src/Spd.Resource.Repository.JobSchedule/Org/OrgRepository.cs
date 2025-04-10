a8a5bbbdf13fd384187dd76c7d948d2941ccaa7e
peggy-quartech <peggy.zhang@quartech.com>
2025-04-09 17:52:34 -0700

    add job scheduler

---
+++ src/Spd.Resource.Repository.JobSchedule/Org/OrgRepository.cs
@@ -0,0 +1,55 @@
+using AutoMapper;
+using Spd.Utilities.Dynamics;
+
+namespace Spd.Resource.Repository.JobSchedule.Org;
+internal class OrgRepository : IOrgRepository
+{
+    private readonly DynamicsContext _context;
+    private readonly IMapper _mapper;
+    public OrgRepository(IDynamicsContextFactory ctx,
+        IMapper mapper)
+    {
+        _context = ctx.Create();
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<ResultResp>> RunMonthlyInvoiceAsync(CancellationToken ct)
+    {
+        var accounts = _context.accounts.Where(a => a.statecode == DynamicsConstants.StateCode_Active)
+            .Where(a => a.spd_eligibleforcreditpayment == (int)YesNoOptionSet.Yes)
+            .ToList();
+
+        using var semaphore = new SemaphoreSlim(10); // Limit to 10 concurrent requests
+
+        var tasks = accounts.Select(async a =>
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                var response = await a.spd_MonthlyInvoice().GetValueAsync(ct);
+                ResultResp rr = _mapper.Map<ResultResp>(response);
+                rr.OrgId = a.accountid.Value;
+                return rr;
+            }
+            catch (Exception ex)
+            {
+                return new ResultResp { IsSuccess = false, ResultStr = ex.Message, OrgId = a.accountid.Value };
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        });
+
+        var results = await Task.WhenAll(tasks);
+
+        foreach (var result in results)
+        {
+            Console.WriteLine(result);
+        }
+        return results;
+    }
+
+
+}
+
