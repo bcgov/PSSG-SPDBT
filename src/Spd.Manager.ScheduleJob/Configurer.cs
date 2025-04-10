0a1630334355a2f29439ae1955061224b0032d47
Peggy <peggy.zhang@quartech.com>
2025-04-10 13:27:05 -0700

    [SPDBT-3836] Dynamics Helper: New endpoint for "Create Monthly Invoices" job (#2449)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-3836] Dynamics Helper: New endpoint for "Create Monthly
    Invoices" job

---
+++ src/Spd.Manager.ScheduleJob/Configurer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spd.Utilities.Hosting;
+
+namespace Spd.Manager.ScheduleJob;
+
+public class Configurer : IConfigureComponents
+{
+    public void Configure(ConfigurationContext configurationServices)
+    {
+        configurationServices.Services.AddTransient<IScheduleJobManager, ScheduleJobManager>();
+    }
+}
+
