619c020646c61e5288c7a8c4975c84b6ffb1b84e
peggy-quartech <peggy.zhang@quartech.com>
2025-04-11 11:47:10 -0700

    temp

--- src/Spd.Presentation.Dynamics/Controllers/ScheduleJobController.cs
+++ src/Spd.Presentation.Dynamics/Controllers/ScheduleJobController.cs
@@ -1,5 +1,4 @@
 using MediatR;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Spd.Manager.ScheduleJob;
 using Spd.Utilities.Shared;
@@ -9,7 +8,7 @@ namespace Spd.Presentation.Dynamics.Controllers;
 /// <summary>
 /// ScheduleJob support for dynamics
 /// </summary>
-[Authorize]
+//[Authorize]
 public class ScheduleJobController : SpdControllerBase
 {
     private readonly IMediator _mediator;
--- src/Spd.Resource.Repository.JobSchedule/Org/Contract.cs
+++ src/Spd.Resource.Repository.JobSchedule/Org/Contract.cs
@@ -3,6 +3,7 @@ namespace Spd.Resource.Repository.JobSchedule.Org
     public interface IOrgRepository
     {
         public Task<IEnumerable<ResultResp>> RunMonthlyInvoiceAsync(CancellationToken cancellationToken);
+        public Task<IEnumerable<ResultResp>> RunGeneralFunctionAsync(CancellationToken cancellationToken);
     }
 
     public record ResultResp
