4460cc610f8271ced9e2f171a95b443d5b2deb65
peggy-quartech <peggy.zhang@quartech.com>
2025-04-14 14:28:11 -0700

    test

---
+++ src/Spd.Resource.Repository.JobSchedule/GeneralizeScheduleJob/Contract.cs
@@ -0,0 +1,22 @@
+namespace Spd.Resource.Repository.JobSchedule.GeneralizeScheduleJob
+{
+    public interface IGeneralizeScheduleJobRepository
+    {
+        public Task<IEnumerable<ResultResp>> RunJobsAsync(RunJobRequest request, CancellationToken cancellationToken);
+    }
+
+    public record RunJobRequest
+    {
+        public String PrimaryEntityName { get; set; }
+        public string PrimaryEntityIdName { get; set; }
+        public string? PrimaryEntityFilterStr { get; set; }
+        public string PrimaryEntityActionStr { get; set; }
+    }
+
+    public record ResultResp
+    {
+        public bool IsSuccess { get; set; }
+        public string? ResultStr { get; set; }
+        public Guid PrimaryEntityId { get; set; }
+    }
+}
