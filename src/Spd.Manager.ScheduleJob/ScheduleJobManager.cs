99b81625975afd8f948146a356adb549d447ef97
Peggy <peggy.zhang@quartech.com>
2025-04-15 10:20:05 -0700

    schedule job session adjustment (#2466)
    # Description
    
    This PR includes the following proposed change(s):
    
    - make less concurrent request (limitation from open shift)
    - make fire and leave for manager to create invoice report
    - generalized code (not used yet)
    - add logging and progress notification

--- src/Spd.Manager.ScheduleJob/ScheduleJobManager.cs
+++ src/Spd.Manager.ScheduleJob/ScheduleJobManager.cs
@@ -1,5 +1,6 @@
-﻿using MediatR;
+using MediatR;
 using Microsoft.Extensions.Logging;
+using Spd.Resource.Repository.JobSchedule.GeneralizeScheduleJob;
 using Spd.Resource.Repository.JobSchedule.Org;
 using Spd.Resource.Repository.JobSchedule.ScheduleJobSession;
 using Spd.Utilities.Shared.Exceptions;
@@ -16,16 +17,20 @@ public class ScheduleJobManager :
 {
     private readonly IScheduleJobSessionRepository _scheduleJobSessionRepository;
     private readonly IOrgRepository _orgRepository;
+    private readonly IGeneralizeScheduleJobRepository _generalizeScheduleJobRepository;
     private readonly ILogger<IScheduleJobManager> _logger;
 
     public ScheduleJobManager(IScheduleJobSessionRepository scheduleJobSessionRepository,
         IOrgRepository orgRepository,
+        IGeneralizeScheduleJobRepository generalizeScheduleJobRepository,
         ILogger<IScheduleJobManager> logger)
     {
         _scheduleJobSessionRepository = scheduleJobSessionRepository;
         _orgRepository = orgRepository;
+        _generalizeScheduleJobRepository = generalizeScheduleJobRepository;
         _logger = logger;
     }
+
     public async Task<Unit> Handle(RunScheduleJobSessionCommand cmd, CancellationToken ct)
     {
         ScheduleJobSessionResp? resp = await _scheduleJobSessionRepository.GetAsync(cmd.JobSessionId, ct);
@@ -34,6 +39,7 @@ public class ScheduleJobManager :
             throw new ApiException(HttpStatusCode.BadRequest, "The schedule job session does not exist.");
         }
 
+        //used for Org MonthlyInvoice
         if (resp.PrimaryEntity == "account" && resp.EndPoint.Equals("spd_MonthlyInvoiceJob"))
         {
             Stopwatch stopwatch = Stopwatch.StartNew();
@@ -41,13 +47,42 @@ public class ScheduleJobManager :
             stopwatch.Stop();
 
             //update result in JobSession
-            UpdateScheduleJobSessionCmd updateResultCmd = GetUpdateScheduleJobSessionCmd(cmd.JobSessionId, result, Decimal.Round((decimal)(stopwatch.ElapsedMilliseconds / 1000), 2));
+            UpdateScheduleJobSessionCmd updateResultCmd = CreateUpdateScheduleJobSessionCmd(cmd.JobSessionId, result, Decimal.Round((decimal)(stopwatch.ElapsedMilliseconds / 1000), 2));
             await _scheduleJobSessionRepository.ManageAsync(updateResultCmd, ct);
         }
+
         return default;
     }
 
-    private UpdateScheduleJobSessionCmd GetUpdateScheduleJobSessionCmd(Guid jobSessionId, IEnumerable<ResultResp> results, decimal durationInSec)
+    //generalized,waiting for mano testing
+    //public async Task<Unit> Handle(RunScheduleJobSessionCommand cmd, CancellationToken ct)
+    //{
+    //    ScheduleJobSessionResp? resp = await _scheduleJobSessionRepository.GetAsync(cmd.JobSessionId, ct);
+    //    if (resp == null)
+    //    {
+    //        throw new ApiException(HttpStatusCode.BadRequest, "The schedule job session does not exist.");
+    //    }
+
+    //    Stopwatch stopwatch = Stopwatch.StartNew();
+    //    //request will from bcgov_schedulejob
+    //    RunJobRequest request = new RunJobRequest
+    //    {
+    //        PrimaryEntityActionStr = "spd_MonthlyInvoice",
+    //        PrimaryEntityName = "accounts",
+    //        PrimaryEntityFilterStr = "statecode eq 0 && spd_eligibleforcreditpayment eq 100000001",
+    //        PrimaryEntityIdName = "accountid"
+    //    };
+    //    var result = await _generalizeScheduleJobRepository.RunJobsAsync(request, ct);
+    //    stopwatch.Stop();
+
+    //    //update result in JobSession
+    //    UpdateScheduleJobSessionCmd updateResultCmd = CreateUpdateScheduleJobSessionCmd(cmd.JobSessionId, result, Decimal.Round((decimal)(stopwatch.ElapsedMilliseconds / 1000), 2));
+    //    await _scheduleJobSessionRepository.ManageAsync(updateResultCmd, ct);
+
+    //    return default;
+    //}
+
+    private UpdateScheduleJobSessionCmd CreateUpdateScheduleJobSessionCmd(Guid jobSessionId, IEnumerable<ResultResp> results, decimal durationInSec)
     {
         UpdateScheduleJobSessionCmd cmd = new UpdateScheduleJobSessionCmd();
         cmd.ScheduleJobSessionId = jobSessionId;
@@ -59,7 +94,11 @@ public class ScheduleJobManager :
         if (cmd.JobSessionStatusCode == JobSessionStatusCode.Failed)
         {
             string error = JsonSerializer.Serialize(results.Where(r => !r.IsSuccess).ToList());
-            _logger.LogError(error);
+            _logger.LogError($"job failed with {error}");
+        }
+        else
+        {
+            _logger.LogInformation($"job runs successfully");
         }
         return cmd;
     }
