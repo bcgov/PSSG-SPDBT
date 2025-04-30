742fc99684b6d1aa340895e7f28b467c52cc5b8c
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-04-30 09:38:58 -0700

    [FE] swl/permit bug fixes (#2498)
    # Description
    
    This PR includes the following proposed change(s):
    
    - simplify component/step matching
    - bug: permit was not validating criminal history in the wizard
    - bug: fix some summary 'edit' button labels and navigation
    - bug: make display of 'next: review' button display consistent in all
    wizards
    - bug: do not set dog/restraints in model without a SecurityGuard
    category

--- src/Spd.Presentation.Licensing/ClientApp/src/app/core/services/permit-application.service.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/core/services/permit-application.service.ts
@@ -252,6 +252,7 @@ export class PermitApplicationService extends PermitApplicationHelper {
 			// );
 
 			return (
+				this.criminalHistoryFormGroup.valid &&
 				this.citizenshipFormGroup.valid &&
 				this.bcDriversLicenceFormGroup.valid &&
 				this.characteristicsFormGroup.valid &&
@@ -1202,6 +1203,11 @@ export class PermitApplicationService extends PermitApplicationHelper {
 			bcDriversLicenceNumber: permitLicenceAppl.bcDriversLicenceNumber,
 		};
 
+		const criminalHistoryData = {
+			hasCriminalHistory: this.utilService.booleanToBooleanType(permitLicenceAppl.hasCriminalHistory),
+			criminalChargeDescription: '',
+		};
+
 		// if this is a permit update, use the data supplied in 'updateLicenceData' (where applicable),
 		// other use the data in the 'resp'
 		const permitLicenceData = associatedLicence ?? permitLicenceAppl;
@@ -1396,6 +1402,7 @@ export class PermitApplicationService extends PermitApplicationHelper {
 				bcDriversLicenceData,
 				citizenshipData,
 				photographOfYourselfData,
+				criminalHistoryData,
 			},
 			{
 				emitEvent: false,
