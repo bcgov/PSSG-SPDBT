fd4a056e60bb2fa54f2a5eceb0dad06df57bc274
peggy-quartech <peggy.zhang@quartech.com>
2025-05-02 15:54:10 -0700

    review

--- src/Spd.Presentation.Dynamics/Controllers/ScheduleJobController.cs
+++ src/Spd.Presentation.Dynamics/Controllers/ScheduleJobController.cs
@@ -36,8 +36,10 @@ public class ScheduleJobController : SpdControllerBase
         [FromRoute] Guid sessionId,
         CancellationToken ct)
     {
-        int? concurrentRequests = _configuration.GetValue<int>("ScheduleJobConcurrentRequests");
-        _mediator.Send(new RunScheduleJobSessionCommand(sessionId, concurrentRequests ?? 5), ct);
+        int concurrentRequests = _configuration.GetValue<int>("ScheduleJobConcurrentRequests");
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        _mediator.Send(new RunScheduleJobSessionCommand(sessionId, concurrentRequests), ct);
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         return Ok();
     }
 }
