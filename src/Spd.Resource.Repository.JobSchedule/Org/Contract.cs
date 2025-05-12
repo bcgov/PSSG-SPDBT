7521a682b52c4d7c7a9974f19b24ccfe099e38d6
Peggy <peggy.zhang@quartech.com>
2025-05-12 12:23:52 -0700

    [SPDBT-4087] Schedule job: when run on staging env, we got "out of memory" exception. Need to investigate. (#2565)
    # Description
    
    This PR includes the following proposed change(s):
    
    -[SPDBT-4087]Schedule job: when run on staging env, we got "out of
    memory" exception. Need to investigate.

--- src/Spd.Resource.Repository.JobSchedule/Org/Contract.cs
+++ src/Spd.Resource.Repository.JobSchedule/Org/Contract.cs
@@ -5,5 +5,6 @@ namespace Spd.Resource.Repository.JobSchedule.Org
     public interface IOrgRepository
     {
         public Task<IEnumerable<ResultResp>> RunMonthlyInvoiceAsync(int concurrentRequests, CancellationToken cancellationToken);
+        public Task<IEnumerable<ResultResp>> RunMonthlyInvoiceInChuncksAsync(RunJobRequest request, int concurrentRequests, CancellationToken cancellationToken);
     }
 }
