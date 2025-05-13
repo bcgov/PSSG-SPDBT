e31c0f8acd1ddca04d8ca1dc826be376ef17723d
Peggy <peggy.zhang@quartech.com>
2025-05-13 13:54:02 -0700

    [SPDBT-4087] Memory leak fix (#2570)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-4087] Memory leak fix

---
+++ src/Spd.Presentation.Dynamics/Services/IScheduleJobQueue.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Channels;
+
+namespace Spd.Presentation.Dynamics.Services;
+public interface IScheduleJobQueue
+{
+    void Enqueue(Guid sessionId, int concurrentRequests);
+    bool TryDequeue(out (Guid sessionId, int concurrentRequests) job);
+    ChannelReader<(Guid sessionId, int concurrentRequests)> Reader { get; }
+}
---
+++ src/Spd.Presentation.Dynamics/Services/ScheduleJobQueue.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Channels;
+
+namespace Spd.Presentation.Dynamics.Services;
+public class ScheduleJobQueue : IScheduleJobQueue
+{
+    private readonly Channel<(Guid, int)> _channel = Channel.CreateUnbounded<(Guid, int)>();
+
+    public void Enqueue(Guid sessionId, int concurrentRequests)
+    {
+        _channel.Writer.TryWrite((sessionId, concurrentRequests));
+    }
+
+    public bool TryDequeue(out (Guid sessionId, int concurrentRequests) job)
+    {
+        var reader = _channel.Reader;
+        if (reader.TryRead(out var result))
+        {
+            job = result;
+            return true;
+        }
+
+        job = default;
+        return false;
+    }
+
+    public ChannelReader<(Guid, int)> Reader => _channel.Reader;
+}
