b5148b46b80b43d58dc8f5464161291ad032628f
Carol Carpenter <carol.carpenter@quartech.com>
2025-05-21 14:18:04 -0700

    SPDBT-4135 hide error details in Prod

--- src/Spd.Presentation.Licensing/ClientApp/src/app/shared/components/dialog-oops.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/shared/components/dialog-oops.component.ts
@@ -1,25 +1,37 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConfigService } from '@app/core/services/config.service';
 
 export interface DialogOopsOptions {
 	message?: string;
+	is400Error: boolean;
 }
 
 @Component({
 	selector: 'app-spd-dialog-oops',
 	template: `
 		<mat-dialog-content class="mat-dialog-content">
-			<div class="d-flex justify-content-center">
-				<img
-					class="error-image"
-					src="./assets/something-went-wrong.png"
-					(error)="onHandleMissingImage($event)"
-					alt="Something went wrong"
-				/>
-			</div>
-			<h2 class="mt-2">Oops! Something went wrong</h2>
-			<p>Looks like something went wrong on our end. Please try again later.</p>
-			<p class="px-4 py-2 error-message" [ngStyle]="{ 'word-break': 'break-word' }" [innerHTML]="data.message"></p>
+			<ng-container *ngIf="!is400Error">
+				<div class="d-flex justify-content-center">
+					<img
+						class="error-image"
+						src="./assets/something-went-wrong.png"
+						(error)="onHandleMissingImage($event)"
+						alt="Something went wrong"
+					/>
+				</div>
+				<h2 class="mt-2">Oops! Something went wrong</h2>
+				<p>Looks like something went wrong on our end. Please try again or contact SPD at 1-855-587-0185 (option 2).</p>
+			</ng-container>
+
+			<p
+				*ngIf="errorMessage"
+				class="px-4 py-2 "
+				[ngClass]="is400Error ? '' : 'error-message'"
+				[ngStyle]="{ 'word-break': 'break-word' }"
+				[innerHTML]="errorMessage"
+			></p>
+
 			<ng-template appDialogContent></ng-template>
 		</mat-dialog-content>
 
@@ -62,8 +74,19 @@ export interface DialogOopsOptions {
 	],
 	standalone: false,
 })
-export class DialogOopsComponent {
-	constructor(@Inject(MAT_DIALOG_DATA) public data: DialogOopsOptions) {}
+export class DialogOopsComponent implements OnInit {
+	errorMessage: string | null | undefined = null;
+	is400Error!: boolean;
+
+	constructor(
+		private configService: ConfigService,
+		@Inject(MAT_DIALOG_DATA) public data: DialogOopsOptions
+	) {}
+
+	ngOnInit(): void {
+		this.is400Error = this.data.is400Error ?? false;
+		this.errorMessage = this.configService.isProduction() && !this.is400Error ? null : this.data.message;
+	}
 
 	public onHandleMissingImage(event: Event) {
 		(event.target as HTMLImageElement).style.display = 'none';
