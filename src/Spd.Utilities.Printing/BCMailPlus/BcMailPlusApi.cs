0110cba779a5dda7c9caac1f09f58f7b9599b59b
Peggy <peggy.zhang@quartech.com>
2025-05-22 12:32:48 -0700

    [SPDBT-4113] BMCP-printing cards in batch. (#2591)
    # Description
    
    This PR includes the following proposed change(s):
    
    -[SPDBT-4113] BMCP-printing cards in batch.

--- src/Spd.Utilities.Printing/BCMailPlus/BcMailPlusApi.cs
+++ src/Spd.Utilities.Printing/BCMailPlus/BcMailPlusApi.cs
@@ -1,7 +1,7 @@
-﻿using System.Net.Mime;
+using Microsoft.Extensions.Options;
+using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using Microsoft.Extensions.Options;
 
 namespace Spd.Utilities.Printing.BCMailPlus;
 
