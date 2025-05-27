7f57d4e2df421db7563f9730297dc05233e5f3dd
peggy-quartech <peggy.zhang@quartech.com>
2025-05-27 12:17:15 -0700

    remove screening

--- src/Spd.Presentation.GuideDogServiceDog/Controllers/Mapping.cs
+++ src/Spd.Presentation.GuideDogServiceDog/Controllers/Mapping.cs
@@ -1,7 +1,6 @@
 ﻿using AutoMapper;
 using Spd.Manager.Payment;
-using Spd.Manager.Screening;
-using Spd.Utilities.LogonUser;
+//using Spd.Manager.Screening;
 using System.Globalization;
 
 namespace Spd.Presentation.Screening.Controllers;
@@ -10,28 +9,6 @@ internal class Mappings : Profile
 {
     public Mappings()
     {
-        CreateMap<BceidIdentityInfo, PortalUserIdentity>();
-        CreateMap<IdirUserIdentityInfo, IdirUserIdentity>();
-        CreateMap<OrgResponse, AppOrgResponse>()
-           .ForMember(d => d.OrgId, opt => opt.MapFrom(s => s.Id))
-           .ForMember(d => d.OrgName, opt => opt.MapFrom(s => s.OrganizationName))
-           .ForMember(d => d.OrgPhoneNumber, opt => opt.MapFrom(s => s.PhoneNumber))
-           .ForMember(d => d.OrgEmail, opt => opt.MapFrom(s => s.Email))
-           .ForMember(d => d.OrgAddressLine1, opt => opt.MapFrom(s => s.AddressLine1))
-           .ForMember(d => d.OrgAddressLine2, opt => opt.MapFrom(s => s.AddressLine2))
-           .ForMember(d => d.OrgCity, opt => opt.MapFrom(s => s.AddressCity))
-           .ForMember(d => d.OrgProvince, opt => opt.MapFrom(s => s.AddressProvince))
-           .ForMember(d => d.OrgCountry, opt => opt.MapFrom(s => s.AddressCountry))
-           .ForMember(d => d.OrgPostalCode, opt => opt.MapFrom(s => s.AddressPostalCode))
-           .ForMember(d => d.WorksWith, opt => opt.MapFrom(s => s.EmployeeInteractionType))
-           .ForMember(d => d.GivenName, opt => opt.Ignore())
-           .ForMember(d => d.Surname, opt => opt.Ignore())
-           .ForMember(d => d.EmailAddress, opt => opt.Ignore())
-           .ForMember(d => d.JobTitle, opt => opt.Ignore())
-           .ForMember(d => d.PayeeType, opt => opt.MapFrom(s => s.PayerPreference))
-           .ForMember(d => d.ScreeningType, opt => opt.Ignore())
-           .ForMember(d => d.ServiceType, opt => opt.MapFrom(s => s.ServiceTypes.FirstOrDefault()));
-
         CreateMap<PaybcPaymentResultViewModel, PaybcPaymentResult>()
            .ForMember(d => d.Success, opt => opt.MapFrom(s => s.trnApproved == 1))
            .ForMember(d => d.MessageText, opt => opt.MapFrom(s => s.messageText))
