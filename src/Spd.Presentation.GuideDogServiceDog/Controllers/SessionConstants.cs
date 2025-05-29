8c2d77cf75bf91744618e9f871680046bd8eb91f
Peggy <peggy.zhang@quartech.com>
2025-05-29 10:52:50 -0700

    [SPDBT-4129] Make GDSD seperate portal - gdsd and licensing cleanup (#2609)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-4129] Make GDSD seperate portal - gdsd and licensing cleanup

--- src/Spd.Presentation.GuideDogServiceDog/Controllers/SessionConstants.cs
+++ src/Spd.Presentation.GuideDogServiceDog/Controllers/SessionConstants.cs
@@ -2,9 +2,8 @@
 
 public static class SessionConstants
 {
-    public static readonly string AnonymousRequestDataProtectorName = "AppAnonymousSubmitRequest";
-    public static readonly string AnonymousApplicationContext = "LicenceApplicationContext"; //used for anonymous update/renew/replace, get application content
-    public static readonly string AnonymousApplicationSubmitKeyCode = "LicenceApplicationSubmitKeyCode";
-    public static readonly string AnonymousSoleProprietorApplicationContext = "SoleProprietorApplicationContext"; //used for unauth sole proprietor, biz abort the app
-    public static readonly string AnonymousApplicantContext = "ApplicantContext"; //used for anonymous update for applicant
+    public static readonly string AnonymousRequestDataProtectorName = "GDSDAnonymousSubmitRequest";
+    public static readonly string AnonymousApplicationContext = "GDSDApplicationContext"; //used for anonymous update/renew/replace, get application content
+    public static readonly string AnonymousApplicationSubmitKeyCode = "GDSDApplicationSubmitKeyCode";
+    public static readonly string AnonymousApplicantContext = "GDSDApplicantContext"; //used for anonymous update for applicant
 }
