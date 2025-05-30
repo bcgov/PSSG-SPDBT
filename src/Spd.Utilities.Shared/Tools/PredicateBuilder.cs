cf915d0f8dc5280a7c8ab96acb1363af0c6613ec
Peggy <peggy.zhang@quartech.com>
2025-05-30 11:43:39 -0700

    [SPDBT-4195] PROD: Batch Printing Schedule Job not working (#2612)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-4195] PROD: Batch Printing Schedule Job not working

---
+++ src/Spd.Utilities.Shared/Tools/PredicateBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq.Expressions;
+
+namespace Spd.Utilities.Shared.Tools;
+public static class PredicateBuilder
+{
+    public static Expression<Func<T, bool>> BuildOrPredicate<T, TValue>(string propertyName, IEnumerable<TValue> values)
+    {
+        var parameter = Expression.Parameter(typeof(T), "e");
+        Expression? predicate = null;
+
+        foreach (var value in values)
+        {
+            var left = Expression.PropertyOrField(parameter, propertyName);
+            var right = Expression.Constant(value, typeof(TValue));
+            var equals = Expression.Equal(left, right);
+
+            predicate = predicate == null ? equals : Expression.OrElse(predicate, equals);
+        }
+
+        return predicate == null
+            ? e => false // no values provided
+            : Expression.Lambda<Func<T, bool>>(predicate, parameter);
+    }
+}
+
