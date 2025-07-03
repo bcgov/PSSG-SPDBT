c9c7e0f3cdce857fd373622197e6d4d8a50c7d00
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-07-03 14:27:25 -0700

    [SPDBT-4291] Security Licence Status Verification - add recaptcha and bulk swl search (#2808)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [SPDBT-4291] Security Licence Status Verification - add recaptcha to
    all searches and add bulk swl search

---
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/security-licence-status-verification/components/security-licence-status-swl-search-results.component.ts
@@ -0,0 +1,132 @@
+import { Component, Input } from '@angular/core';
+import { LicenceBasicResponse, LicenceStatusCode, WorkerCategoryTypeCode } from '@app/api/models';
+import { SPD_CONSTANTS } from '@app/core/constants/constants';
+import { UtilService } from '@app/core/services/util.service';
+import { OptionsPipe } from '@app/shared/pipes/options.pipe';
+
+@Component({
+	selector: 'app-security-licence-status-swl-search-results',
+	template: `
+		@if (showSearchResults) {
+			@if (searchResults && searchResults.length > 0) {
+				<div class="row mb-3">
+					<div class="col-xxl-11 col-xl-12 col-lg-12 col-md-12 col-sm-12 mx-auto">
+						<mat-divider class="no-print my-3"></mat-divider>
+						<div class="text-minor-heading no-print my-3">Search results</div>
+						@for (licence of searchResults; track licence; let i = $index) {
+							<div class="summary-card-section summary-card-section__green mb-3 px-4 py-3">
+								@if (licence.licenceId) {
+									<div class="row">
+										<div class="col-xl-2 col-lg-2">
+											<div class="d-block text-muted mt-2 mt-lg-0">Licence Number</div>
+											<div class="text-minor-heading">
+												{{ licence.licenceNumber }}
+											</div>
+										</div>
+										<div class="col-xl-8 col-lg-8">
+											<div class="row">
+												<div class="col-xl-6 col-lg-6">
+													<div class="d-block text-muted mt-2 mt-lg-0">Licence Holder Name</div>
+													<div class="text-data fw-bold">{{ licence.licenceHolderName }}</div>
+												</div>
+												<div class="col-xl-6 col-lg-6">
+													<div class="d-block text-muted mt-2 mt-lg-0">Licence Type(s)</div>
+													<div class="text-data fw-bold">
+														<ul class="m-0">
+															@for (category of getSwlLicenceCategories(licence); track category; let i = $index) {
+																<li>{{ category }}</li>
+															}
+														</ul>
+													</div>
+												</div>
+												@if (!isLicenceActive(licence.licenceStatusCode)) {
+													<div class="mt-3">
+														<app-alert type="warning" icon="warn">
+															<strong>{{ licence.licenceNumber }} - {{ licence.licenceHolderName }}</strong> does not
+															hold a valid licence. If you believe they are working in security in B.C., please consider
+															submitting a
+															<a aria-label="Navigate to SPD complaint site" [href]="spdComplaintUrl" target="_blank"
+																>complaint</a
+															>.
+														</app-alert>
+													</div>
+												}
+											</div>
+										</div>
+										<div class="col-xl-2 col-lg-2 text-end">
+											<mat-chip-option
+												[selectable]="false"
+												class="appl-chip-option"
+												[ngClass]="getLicenceStatusClass(licence.licenceStatusCode)"
+											>
+												<span class="appl-chip-option-item mx-2 fs-5">
+													{{ getLicenceStatus(licence.licenceStatusCode) | default }}
+												</span>
+											</mat-chip-option>
+										</div>
+									</div>
+								} @else {
+									<div class="row">
+										<div class="col-xl-8 col-lg-8 my-auto">
+											<div class="text-minor-heading my-2">{{ licence.licenceNumber }} does not result in a match</div>
+										</div>
+									</div>
+								}
+							</div>
+						}
+					</div>
+				</div>
+			} @else {
+				<div class="row mt-3">
+					<div class="col-xxl-11 col-xl-12 col-lg-12 col-md-12 col-sm-12 mx-auto">
+						@if (searchResultsErrorName) {
+							<app-alert type="danger" icon="dangerous">
+								<strong>{{ searchResultsErrorName }}</strong> does not hold a valid licence. If you believe they are
+								working in security in B.C., please consider submitting a
+								<a aria-label="Navigate to SPD complaint site" [href]="spdComplaintUrl" target="_blank">complaint</a>.
+							</app-alert>
+						} @else {
+							<app-alert type="danger" icon="dangerous">No results match your search.</app-alert>
+						}
+					</div>
+				</div>
+			}
+		}
+	`,
+	styles: [],
+	standalone: false,
+})
+export class SecurityLicenceStatusSwlSearchResultsComponent {
+	spdComplaintUrl = SPD_CONSTANTS.urls.spdComplaintUrl;
+
+	@Input() showSearchResults!: boolean;
+	@Input() searchResultsErrorName!: string | null | undefined;
+	@Input() searchResults!: Array<LicenceBasicResponse> | null | undefined;
+
+	constructor(
+		private utilService: UtilService,
+		private optionsPipe: OptionsPipe
+	) {}
+
+	getLicenceStatusClass(licenceStatusCode: LicenceStatusCode | null | undefined): string {
+		return this.utilService.isLicenceActive(licenceStatusCode) ? 'mat-chip-green' : 'mat-chip-red';
+	}
+
+	getLicenceStatus(licenceStatusCode: LicenceStatusCode | null | undefined): string | null | undefined {
+		return this.utilService.isLicenceActive(licenceStatusCode) ? 'Active' : licenceStatusCode;
+	}
+
+	isLicenceActive(licenceStatusCode: LicenceStatusCode | null | undefined): boolean {
+		return this.utilService.isLicenceActive(licenceStatusCode);
+	}
+
+	getSwlLicenceCategories(licence: LicenceBasicResponse): Array<string> {
+		const catList = licence.categoryCodes?.map((item: WorkerCategoryTypeCode) =>
+			this.optionsPipe.transform(item, 'WorkerCategoryTypes')
+		);
+		if (licence.showSecurityGuardAST) {
+			catList?.push('Security Guard - AST');
+		}
+		return catList?.sort() ?? [];
+	}
+}
--- src/Spd.Presentation.Licensing/ClientApp/src/app/modules/security-licence-status-verification/components/security-licence-status-verification-sbl.component.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/modules/security-licence-status-verification/components/security-licence-status-verification-sbl.component.ts
@@ -1,153 +1,176 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LicenceBasicResponse, LicenceStatusCode } from '@app/api/models';
+import { LicenceBasicResponse, LicenceStatusCode, ServiceTypeCode } from '@app/api/models';
 import { LicenceService } from '@app/api/services';
 import { UtilService } from '@app/core/services/util.service';
+import { FormControlValidators } from '@app/core/validators/form-control.validators';
 import { FormErrorStateMatcher } from '@app/shared/directives/form-error-state-matcher.directive';
+import { Subject } from 'rxjs';
 import { SecurityLicenceStatusVerificationRoutes } from '../security-licence-status-verification-routes';
 
 @Component({
 	selector: 'app-security-licence-status-verification-sbl',
 	template: `
 		<section class="step-section">
-		  <div class="row">
-		    <div class="col-xxl-11 col-xl-12 col-lg-12 col-md-12 col-sm-12 mx-auto">
-		      <div class="row">
-		        <div class="col-xl-8 col-lg-8 col-md-8 col-sm-6 my-auto">
-		          <h2 class="fs-3">Verify a Security Business Licence</h2>
-		        </div>
-		
-		        <div class="col-xl-4 col-lg-4 col-md-12 no-print">
-		          <div class="d-flex justify-content-end">
-		            <button
-		              mat-stroked-button
-		              color="primary"
-		              class="large w-auto mb-3"
-		              aria-label="Back to main page"
-		              (click)="onBack()"
-		              >
-		              <mat-icon>arrow_back</mat-icon>Back
-		            </button>
-		          </div>
-		        </div>
-		
-		        <mat-divider class="mat-divider-main mb-4"></mat-divider>
-		
-		        <div class="col-12 mb-3">
-		          <app-alert type="info" icon="">
-		            Enter a security business <strong>licence number</strong> or at least the
-		            <strong>first three (3)</strong> letters of the name of a security business below. The results will
-		            display the Legal Business Name, the Trade Name, the Licence Number, the Licence Status (valid, not
-		            valid), and the Licence Type of any businesses matching the search criteria.
-		          </app-alert>
-		        </div>
-		      </div>
-		
-		      <form [formGroup]="form" novalidate>
-		        <div class="row mb-2">
-		          <div class="col-xl-5 col-lg-5 col-md-12">
-		            <mat-form-field>
-		              <mat-label>Business Licence Number</mat-label>
-		              <input
-		                matInput
-		                formControlName="businessLicenceNumber"
-		                oninput="this.value = this.value.toUpperCase()"
-		                placeholder="B123456"
-		                [errorStateMatcher]="matcher"
-		                maxlength="20"
-		                />
-		              </mat-form-field>
-		            </div>
-		
-		            <div class="col-xl-1 col-lg-1 col-md-12 text-center">
-		              <div class="text-minor-heading text-red my-3">OR</div>
-		            </div>
-		
-		            <div class="col-xl-6 col-lg-6 col-md-12">
-		              <mat-form-field>
-		                <mat-label>Business Name</mat-label>
-		                <input matInput formControlName="businessName" [errorStateMatcher]="matcher" maxlength="40" />
-		              </mat-form-field>
-		            </div>
-		          </div>
-		
-		          @if (showSearchDataError) {
-		            <app-alert type="danger" icon="dangerous">
-		              {{ searchDataError }}
-		            </app-alert>
-		          }
-		
-		          <div class="row no-print my-2">
-		            <div class="col-12 text-end">
-		              <button mat-flat-button color="primary" class="large w-auto" (click)="onSubmit()">Submit</button>
-		            </div>
-		          </div>
-		        </form>
-		
-		        @if (showSearchResults) {
-		          @if (searchResults.length > 0) {
-		            <div class="mb-3">
-		              <mat-divider class="my-3"></mat-divider>
-		              <div class="text-minor-heading my-3">Search Results</div>
-		              @for (licence of searchResults; track licence; let i = $index) {
-		                <div
-		                  class="summary-card-section summary-card-section__green mb-3 px-4 py-3"
-		                  >
-		                  <div class="row">
-		                    <div class="col-xl-2 col-lg-2">
-		                      <div class="d-block text-muted mt-2 mt-lg-0">Licence Number</div>
-		                      <div class="fs-5" style="color: var(--color-primary);">
-		                        {{ licence.licenceNumber }}
-		                      </div>
-		                    </div>
-		                    <div class="col-xl-8 col-lg-8">
-		                      <div class="row">
-		                        <div class="col-xl-6 col-lg-6">
-		                          <div class="d-block text-muted mt-2 mt-lg-0">Legal Business Name</div>
-		                          <div class="text-data fw-bold">{{ licence.bizLegalName }}</div>
-		                        </div>
-		                        <div class="col-xl-6 col-lg-6">
-		                          <div class="d-block text-muted mt-2 mt-lg-0">Trade Name</div>
-		                          <div class="text-data fw-bold">{{ licence.licenceHolderName }}</div>
-		                        </div>
-		                        <div class="col-xl-12 col-lg-6">
-		                          <div class="d-block text-muted mt-2">Licence Type(s)</div>
-		                          <div class="text-data fw-bold">
-		                            <ul class="m-0">
-		                              @for (category of licence.categoryCodes?.sort(); track category; let i = $index) {
-		                                <li>{{ category | options: 'WorkerCategoryTypes' }}</li>
-		                              }
-		                            </ul>
-		                          </div>
-		                        </div>
-		                      </div>
-		                    </div>
-		                    <div class="col-xl-2 col-lg-2 text-end">
-		                      <mat-chip-option
-		                        [selectable]="false"
-		                        class="appl-chip-option"
-		                        [ngClass]="getLicenceStatusClass(licence.licenceStatusCode)"
-		                        >
-		                        <span class="appl-chip-option-item mx-2 fs-5">{{
-		                          getLicenceStatus(licence.licenceStatusCode)
-		                        }}</span>
-		                      </mat-chip-option>
-		                    </div>
-		                  </div>
-		                </div>
-		              }
-		            </div>
-		          } @else {
-		            <div class="mt-3">
-		              <app-alert type="danger" icon="dangerous"> No results match your search. </app-alert>
-		            </div>
-		          }
-		        }
-		      </div>
-		    </div>
-		  </section>
-		`,
+			<div class="row">
+				<div class="col-xxl-11 col-xl-12 col-lg-12 col-md-12 col-sm-12 mx-auto">
+					<div class="row no-print">
+						<div class="col-xl-8 col-lg-8 col-md-8 col-sm-6 my-auto">
+							<h2 class="fs-3">Verify a security business licence</h2>
+						</div>
+
+						<div class="col-xl-4 col-lg-4 col-md-12">
+							<div class="d-flex justify-content-end">
+								<button
+									mat-stroked-button
+									color="primary"
+									class="large w-auto mb-3"
+									aria-label="Back to main page"
+									(click)="onBack()"
+								>
+									<mat-icon>arrow_back</mat-icon>Back
+								</button>
+							</div>
+						</div>
+
+						<mat-divider class="mat-divider-main mb-4"></mat-divider>
+
+						<div class="col-12 mb-3">
+							<app-alert type="info" icon="">
+								Enter a security business <strong>licence number</strong> or at least the
+								<strong>first three (3)</strong> letters of the name of a security business below. The results will
+								display the Legal Business Name, the Trade Name, the Licence Number, the Licence Status (valid, not
+								valid), and the Licence Type of any businesses matching the search criteria.
+							</app-alert>
+						</div>
+					</div>
+
+					<form [formGroup]="form" novalidate>
+						<div class="row no-print mb-2">
+							<div class="col-xl-5 col-lg-5 col-md-12">
+								<mat-form-field>
+									<mat-label>Business Licence Number</mat-label>
+									<input
+										matInput
+										formControlName="businessLicenceNumber"
+										oninput="this.value = this.value.toUpperCase()"
+										placeholder="B123456"
+										[errorStateMatcher]="matcher"
+										maxlength="20"
+									/>
+									@if (businessLicenceNumber.hasError('invalidCharsFormat')) {
+										<mat-error>Security business licence numbers can only include letters and numbers</mat-error>
+									}
+									@if (businessLicenceNumber.hasError('invalidStartWith')) {
+										<mat-error>Security business licence numbers must start with a "B"</mat-error>
+									}
+								</mat-form-field>
+							</div>
+
+							<div class="col-xl-1 col-lg-1 col-md-12 text-center">
+								<div class="text-minor-heading text-red my-3">OR</div>
+							</div>
+
+							<div class="col-xl-6 col-lg-6 col-md-12">
+								<mat-form-field>
+									<mat-label>Business Name</mat-label>
+									<input matInput formControlName="businessName" [errorStateMatcher]="matcher" maxlength="40" />
+									@if (businessName.hasError('minLength')) {
+										<mat-error>The business name must be at least 3 characters</mat-error>
+									}
+								</mat-form-field>
+							</div>
+
+							<div formGroupName="captchaFormGroup" class="col-12 mb-3 mt-4">
+								<app-captcha-v2
+									[captchaFormGroup]="captchaFormGroup"
+									[resetControl]="resetRecaptchaControl"
+								></app-captcha-v2>
+								@if (
+									(captchaFormGroup.get('token')?.dirty || captchaFormGroup.get('token')?.touched) &&
+									captchaFormGroup.get('token')?.invalid &&
+									captchaFormGroup.get('token')?.hasError('required')
+								) {
+									<mat-error class="mat-option-error">This is required </mat-error>
+								}
+							</div>
+
+							@if (showSearchDataError) {
+								<app-alert type="danger" icon="dangerous">
+									Enter either a business licence number, OR the business name is required.
+								</app-alert>
+							}
+
+							<div class="row my-2">
+								<div class="col-12 text-end">
+									<button mat-flat-button color="primary" class="large w-auto" (click)="onSubmit()">Submit</button>
+								</div>
+							</div>
+						</div>
+					</form>
+
+					@if (showSearchResults) {
+						@if (searchResults.length > 0) {
+							<div class="mb-3">
+								<mat-divider class="no-print my-3"></mat-divider>
+								<div class="text-minor-heading no-print my-3">Search results</div>
+								@for (licence of searchResults; track licence; let i = $index) {
+									<div class="summary-card-section summary-card-section__green mb-3 px-4 py-3">
+										<div class="row">
+											<div class="col-xl-2 col-lg-2">
+												<div class="d-block text-muted mt-2 mt-lg-0">Licence Number</div>
+												<div class="fs-5" style="color: var(--color-primary);">
+													{{ licence.licenceNumber }}
+												</div>
+											</div>
+											<div class="col-xl-8 col-lg-8">
+												<div class="row">
+													<div class="col-xl-6 col-lg-6">
+														<div class="d-block text-muted mt-2 mt-lg-0">Legal Business Name</div>
+														<div class="text-data fw-bold">{{ licence.bizLegalName }}</div>
+													</div>
+													<div class="col-xl-6 col-lg-6">
+														<div class="d-block text-muted mt-2 mt-lg-0">Trade Name</div>
+														<div class="text-data fw-bold">{{ licence.licenceHolderName }}</div>
+													</div>
+													<div class="col-xl-12 col-lg-6">
+														<div class="d-block text-muted mt-2">Licence Type(s)</div>
+														<div class="text-data fw-bold">
+															<ul class="m-0">
+																@for (category of licence.categoryCodes?.sort(); track category; let i = $index) {
+																	<li>{{ category | options: 'WorkerCategoryTypes' }}</li>
+																}
+															</ul>
+														</div>
+													</div>
+												</div>
+											</div>
+											<div class="col-xl-2 col-lg-2 text-end">
+												<mat-chip-option
+													[selectable]="false"
+													class="appl-chip-option"
+													[ngClass]="getLicenceStatusClass(licence.licenceStatusCode)"
+												>
+													<span class="appl-chip-option-item mx-2 fs-5">{{
+														getLicenceStatus(licence.licenceStatusCode)
+													}}</span>
+												</mat-chip-option>
+											</div>
+										</div>
+									</div>
+								}
+							</div>
+						} @else {
+							<div class="mt-3">
+								<app-alert type="danger" icon="dangerous"> No results match your search. </app-alert>
+							</div>
+						}
+					}
+				</div>
+			</div>
+		</section>
+	`,
 	styles: [
 		`
 			.text-red {
@@ -159,15 +182,19 @@ import { SecurityLicenceStatusVerificationRoutes } from '../security-licence-sta
 })
 export class SecurityLicenceStatusVerificationSblComponent {
 	showSearchDataError = false;
-	searchDataError = '';
 
 	showSearchResults = false;
-
-	searchResults: Array<any> = [{ licenceNumber: 'B1073' }, { licenceNumber: 'B1043' }, { licenceNumber: 'B9833' }];
+	searchResults: Array<any> = [];
+	resetRecaptchaControl: Subject<void> = new Subject<void>();
 
 	form = this.formBuilder.group({
-		businessLicenceNumber: new FormControl(''),
-		businessName: new FormControl(''),
+		businessLicenceNumber: new FormControl('', [
+			FormControlValidators.licenceNumberValidator(ServiceTypeCode.SecurityBusinessLicence),
+		]),
+		businessName: new FormControl('', [FormControlValidators.requiredMinLengthValidator()]),
+		captchaFormGroup: new FormGroup({
+			token: new FormControl('', [FormControlValidators.required]),
+		}),
 	});
 
 	matcher = new FormErrorStateMatcher();
@@ -193,21 +220,12 @@ export class SecurityLicenceStatusVerificationSblComponent {
 		const businessLicenceNumber = formValue.businessLicenceNumber?.trim();
 		const businessName = formValue.businessName?.trim();
 
-		let performSearch = true;
 		if ((businessLicenceNumber && businessName) || (!businessLicenceNumber && !businessName)) {
-			performSearch = false;
-		} else if (businessLicenceNumber && !businessLicenceNumber.startsWith('B')) {
-			this.searchDataError = 'The business licence number must start with a "B".';
-			performSearch = false;
-		} else if (businessName && businessName.length < 3) {
-			this.searchDataError = 'The business name must be at least 3 characters.';
-			performSearch = false;
-		}
-
-		if (!performSearch) {
 			this.showSearchDataError = true;
 			return;
 		}
+
+		if (!this.form.valid) return;
 
 		this.performSearch(businessLicenceNumber, businessName);
 	}
@@ -224,19 +242,23 @@ export class SecurityLicenceStatusVerificationSblComponent {
 		this.searchResults = [];
 
 		this.showSearchDataError = false;
-		this.searchDataError = 'Enter either a business licence number, OR the business name is required.';
-
 		this.showSearchResults = false;
 	}
 
+	private resetRecaptcha(): void {
+		this.resetRecaptchaControl.next(); // reset the recaptcha
+		this.captchaFormGroup.reset();
+	}
+
 	private performSearch(licenceNumber: string | undefined, businessName: string | undefined): void {
-		console.debug('licenceNumber', licenceNumber);
-		console.debug('businessName', businessName);
+		const formValue = this.form.getRawValue();
+		const recaptcha = { recaptchaCode: formValue.captchaFormGroup.token };
 
 		this.licenceService
-			.apiLicencesBusinessLicenceGet({
+			.apiLicencesBusinessLicencePost({
 				licenceNumber,
 				businessName,
+				body: recaptcha,
 			})
 			.subscribe((resps: Array<LicenceBasicResponse>) => {
 				const sortedResps = resps.sort((a, b) => {
@@ -246,6 +268,17 @@ export class SecurityLicenceStatusVerificationSblComponent {
 				this.searchResults = sortedResps;
 
 				this.showSearchResults = true;
+				this.resetRecaptcha();
 			});
+	}
+
+	get captchaFormGroup(): FormGroup {
+		return this.form.get('captchaFormGroup') as FormGroup;
+	}
+	get businessLicenceNumber(): FormControl {
+		return this.form.get('businessLicenceNumber') as FormControl;
+	}
+	get businessName(): FormControl {
+		return this.form.get('businessName') as FormControl;
 	}
 }
