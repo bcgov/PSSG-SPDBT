1f1cb3a2482b6cd2abc5afa33a34079b5956b1ee
carolcarpenter <47428671+carolcarpenter@users.noreply.github.com>
2025-07-09 16:29:19 -0700

    [SPDBT-4306] business licence cancel payment (#2823)
    # Description
    
    This PR includes the following proposed change(s):
    
    - {List all the changes, if possible add the jira ticket #}

--- src/Spd.Presentation.Licensing/Configurations/PaymentsConfiguration.cs
+++ src/Spd.Presentation.Licensing/Configurations/PaymentsConfiguration.cs
@@ -4,15 +4,15 @@ public class PaymentsConfiguration
 {
     public string UnauthPersonalLicPaymentSuccessPath { get; set; } = "security-licensing/payment-success/";
     public string UnauthPersonalLicPaymentFailPath { get; set; } = "security-licensing/payment-fail/";
-    public string UnauthPersonalLicPaymentCancelPath { get; set; } = "security-licensing/crc-list";
+    public string UnauthPersonalLicPaymentCancelPath { get; set; } = "security-licensing/payment-cancel";
     public string UnauthPersonalLicPaymentErrorPath { get; set; } = "security-licensing/payment-error/";
     public string AuthPersonalLicPaymentSuccessPath { get; set; } = "security-licensing/payment-success/";
     public string AuthPersonalLicPaymentFailPath { get; set; } = "security-licensing/payment-fail/";
-    public string AuthPersonalLicPaymentCancelPath { get; set; } = "security-licensing/crc-list";
+    public string AuthPersonalLicPaymentCancelPath { get; set; } = "security-licensing/payment-cancel";
     public string AuthPersonalLicPaymentErrorPath { get; set; } = "security-licensing/payment-error/";
     public string AuthBizLicPaymentSuccessPath { get; set; } = "security-biz-licensing/payment-success/";
     public string AuthBizLicPaymentFailPath { get; set; } = "security-biz-licensing/payment-fail/";
-    public string AuthBizLicPaymentCancelPath { get; set; } = "security-biz-licensing/crc-list";
+    public string AuthBizLicPaymentCancelPath { get; set; } = "security-biz-licensing/payment-cancel";
     public string AuthBizLicPaymentErrorPath { get; set; } = "security-biz-licensing/payment-error/";
     public int MaxOnlinePaymentFailedTimes { get; set; } = 3;
 }
