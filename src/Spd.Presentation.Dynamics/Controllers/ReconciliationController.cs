e4d878f0ce2bc82f600ba6f193e56b2bc331f6f2
Peggy <peggy.zhang@quartech.com>
2025-07-17 16:15:03 -0700

    [Tools]Generate Missing address for printing card (#2836)
    # Description
    
    This PR includes the following proposed change(s):
    
    - [Tools]Generate Missing address for printing card

--- src/Spd.Presentation.Dynamics/Controllers/ReconciliationController.cs
+++ src/Spd.Presentation.Dynamics/Controllers/ReconciliationController.cs
@@ -7,11 +7,11 @@ namespace Spd.Presentation.Dynamics.Controllers;
 
 [ApiController]
 [Authorize]
-public class FinancialReconciliationController : ControllerBase
+public class ReconciliationController : ControllerBase
 {
     private readonly IMediator _mediator;
 
-    public FinancialReconciliationController(IMediator mediator) : base()
+    public ReconciliationController(IMediator mediator) : base()
     {
         _mediator = mediator;
     }
@@ -21,5 +21,12 @@ public class FinancialReconciliationController : ControllerBase
     public async Task<SuccessPaymentResultProcessResponse> SubmitSuccessPaymentResults([FromBody] List<DuplicatedPaymentApplicationInfo> paymentResults, CancellationToken ct)
     {
         return await _mediator.Send(new GetDuplicatedApplicationNumberCommand(paymentResults), ct);
+    }
+
+    [HttpGet]
+    [Route("api/printing-reconciliation/addresses")]
+    public async Task<IEnumerable<CardPrintAddressReconcilationResponse>> CardPrintAddressReconcilation(CancellationToken ct)
+    {
+        return await _mediator.Send(new CardPrintAddressReconcilationCommand(), ct);
     }
 }
