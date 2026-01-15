11084be6f19865b3c7f30b8ea5562cd974d10b7b
shruthi-rapala <shruthi.rapala@quartech.com>
2026-01-15 11:05:05 -0800

    SPTBD# 4626 Licensing [PROD]: Update Wording on GDSD Portal - Fixed the wording on the GDSD steps

--- src/Spd.Presentation.GuideDogServiceDog/ClientApp/src/app/components/dog-trainer/step-dt-checklist-renewal.component.ts
+++ src/Spd.Presentation.GuideDogServiceDog/ClientApp/src/app/components/dog-trainer/step-dt-checklist-renewal.component.ts
@@ -11,7 +11,7 @@ import { SPD_CONSTANTS } from '@app/core/constants/constants';
 						Please note: The registrar will <b>not</b> issue or renew a dog trainer certificate unless the individual
 						identified in the application trains dogs on behalf of an accredited training school for the purpose of the
 						dogs becoming guide dogs or service dogs. <br>
-						<b>Dog trainer applications are submitted by accredited training schools.</b>
+						<b>Dog trainer applications must be submitted by accredited training schools.</b>
 					</p>
 
 					<div class="fw-semibold fs-6 mb-2">For all applicants:</div>
