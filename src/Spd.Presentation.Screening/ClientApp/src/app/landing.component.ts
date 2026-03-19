2932f78ce6985c9718d4903b77ac6005c1ed6dd0
shruthi-rapala <shruthi.rapala@quartech.com>
2026-03-19 10:52:19 -0700

    Updated the text to be aligned center Update the styles for the links

--- src/Spd.Presentation.Screening/ClientApp/src/app/landing.component.ts
+++ src/Spd.Presentation.Screening/ClientApp/src/app/landing.component.ts
@@ -10,12 +10,12 @@ import { PssoRoutes } from './modules/psso-portal/psso-routes';
 		<div class="landing-sections">
 			<section class="step-section p-4">
 				<h3>Criminal Records Review Program Organization Online Access</h3>
-				<p class="mt-4 lead">Submit and manage criminal record checks for your employees or volunteers under Criminal Records Act <br>
-				<a href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/00_96086_01" target="_blank">(CRRA).</a></p>
+				<p class="mt-4 lead">Submit and manage criminal record checks for your employees or volunteers under Criminal Records Review Act <br>
+				(<a href="https://www.bclaws.gov.bc.ca/civix/document/id/complete/statreg/00_96086_01" target="_blank">CRRA</a>).</p>
 
-				<div class="option-label"><b>Option 1: Log in with your Business BCeID</b></div>
-				<button mat-flat-button color="primary" class="large my-2" (click)="goToCrrp()">Business BCeID Account</button>
-				<div class="option-label mt-4"><b>Option 2: Don't have a Business BCeID ?</b></div>
+				<div><b>Option 1: Log in with your Business BCeID</b></div>
+				<button mat-flat-button color="primary" class="large my-2" (click)="goToCrrp()">Business BCeID</button>
+				<div class="mt-4"><b>Option 2: Don't have a Business BCeID ?</b></div>
 				<br>
 				<p>Give your applicants your organization's access code to submit their criminal record check online and provide them this link :
 				 <a href="https://justice.gov.bc.ca/screening/crrpa/org-access" target="_blank">https://justice.gov.bc.ca/screening/crrpa/org-access</a>
@@ -24,7 +24,7 @@ import { PssoRoutes } from './modules/psso-portal/psso-routes';
 			</section>
 			<section class="step-section p-4">
 				<h3>BC Public Service Security Screening Hiring Manager Online Access</h3>
-				<p class="mt-4 lead">Submit and manage criminal record checks</p>
+				<p class="mt-4 lead">Submit and manage criminal record checks.</p>
 				<div class="login-section">
 				<b>Login:</b>
 				<button mat-flat-button color="primary" class="large my-2" (click)="goToPsso()">IDIR Account</button>
@@ -50,23 +50,19 @@ import { PssoRoutes } from './modules/psso-portal/psso-routes';
 				flex: 1 1 420px;
 				max-width: 560px;
 				margin-top: 0 !important;
-			}
-
-			.option-label {
 				text-align: center;
 			}
 
 			.login-section {
-				text-align: center;
-				padding-top: 60px;
+              padding-top: 60px;
 			}
-
 			.CRRA-padding{
 				padding-top: 90px;
 			}
 
 			a {
 				color: var(--bs-link-color) !important;
+				text-decoration: underline !important;
 			}
 
 			@media (max-width: 991.98px) {
