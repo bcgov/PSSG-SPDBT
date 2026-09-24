6bb71a15fb81b0b560faf4ddd3e70348810b9704
JSyro <Jasyrotuck@gmail.com>
2026-09-24 10:05:51 -0700

    parse yyyyMMdd

--- src/Spd.Presentation.GuideDogServiceDog/ClientApp/src/app/shared/pipes/format-date.pipe.ts
+++ src/Spd.Presentation.GuideDogServiceDog/ClientApp/src/app/shared/pipes/format-date.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { SPD_CONSTANTS } from '@app/core/constants/constants';
-import { format } from 'date-fns';
+import { format,parse } from 'date-fns';
 
 @Pipe({
     name: 'formatDate',
@@ -8,6 +8,12 @@ import { format } from 'date-fns';
 })
 export class FormatDatePipe implements PipeTransform {
 	public transform(date: string | Date | undefined | null, dateFormat: string = SPD_CONSTANTS.date.dateFormat): string {
+
+		if (typeof date == "string" && (/^\d{8}$/.test(date))){
+			// date-fns does not accept yyyyMMdd like moment did
+			date = parse(date, 'yyyyMMdd', new Date());
+		}
+
 		return date ? format(new Date(date), dateFormat) : '';
 	}
 }
