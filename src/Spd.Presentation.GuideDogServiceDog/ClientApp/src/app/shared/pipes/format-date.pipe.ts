04ab470cba4f1b5ee219f69db3b2d98506c40993
JSyro <Jasyrotuck@gmail.com>
2026-09-24 11:37:53 -0700

    consistent

--- src/Spd.Presentation.GuideDogServiceDog/ClientApp/src/app/shared/pipes/format-date.pipe.ts
+++ src/Spd.Presentation.GuideDogServiceDog/ClientApp/src/app/shared/pipes/format-date.pipe.ts
@@ -1,6 +1,6 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { SPD_CONSTANTS } from '@app/core/constants/constants';
-import { format, parse, parseISO } from 'date-fns';
+import { format, parseISO } from 'date-fns';
+import { SPD_CONSTANTS } from 'src/app/core/constants/constants';
 
 @Pipe({
     name: 'formatDate',
@@ -10,10 +10,6 @@ export class FormatDatePipe implements PipeTransform {
 	public transform(date: string | Date | undefined | null, dateFormat: string = SPD_CONSTANTS.date.dateFormat): string {
 		if (!date) return '';
 
-		if (date instanceof Date) {
-			return format(date, dateFormat);
-		} else {
-			return format(parseISO(date), dateFormat);
-		}
+		return format(typeof date === 'string' ? parseISO(date) : date, dateFormat);
 	}
 }
--- src/Spd.Presentation.Licensing/ClientApp/src/app/shared/pipes/format-date.pipe.ts
+++ src/Spd.Presentation.Licensing/ClientApp/src/app/shared/pipes/format-date.pipe.ts
@@ -1,20 +1,15 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { SPD_CONSTANTS } from '@app/core/constants/constants';
 import { format, parseISO } from 'date-fns';
+import { SPD_CONSTANTS } from 'src/app/core/constants/constants';
 
 @Pipe({
-	name: 'formatDate',
-	standalone: false,
+    name: 'formatDate',
+    standalone: false
 })
 export class FormatDatePipe implements PipeTransform {
 	public transform(date: string | Date | undefined | null, dateFormat: string = SPD_CONSTANTS.date.dateFormat): string {
 		if (!date) return '';
 
-		if (date instanceof Date) {
-			return format(date, dateFormat);
-		} else {
-			return format(parseISO(date), dateFormat);
-		}
+		return format(typeof date === 'string' ? parseISO(date) : date, dateFormat);
 	}
 }
-
